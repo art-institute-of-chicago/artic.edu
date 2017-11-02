68ec5c3bccc07d430b34c8970a78a09cd595efdf
Mike Byrne <mike@thirteentwelve.com>
2017-11-02 12:14:29 +0000

    Added temp index with icons/type

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US" class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="format-detection" content="telephone=no">
+  <title>AIC</title>
+
+  <!-- Main Favicon -->
+  <link rel="shortcut icon" href="/dist/images/favicon.ico">
+  <!-- Apple Touch Icons (ipad/iphone standard+retina) -->
+  <link rel="apple-touch-icon" href="/dist/images/favicon-152.png"> <!-- General use iOS/Android icon, auto-downscaled by devices. -->
+  <link rel="apple-touch-icon" type="image/png" href="/dist/images/favicon-120.png" sizes="120x120"> <!-- iPhone retina touch icon -->
+  <link rel="apple-touch-icon" type="image/png" href="/dist/images/favicon-76.png" sizes="76x76"> <!-- iPad home screen icons -->
+  <!-- Favicon Fallbacks for old browsers that don't read .ico -->
+  <link rel="icon" type="image/png" href="/dist/images/favicon-32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/dist/images/favicon-16.png" sizes="16x16">
+
+  <!--[if lt IE 9]>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <![endif]-->
+  <!-- insert head.js - ideally minified and inline and not linked like this -->
+  <script src="/dist/scripts/head.js"></script>
+  <link href="/dist/styles/app.css" rel="stylesheet" />
+
+  <style>
+    #a17 {
+      padding-top: 0 !important;
+      color: #1a1a1a !important;
+    }
+    .baseline-resetter {
+      display: block;
+      width: 100%;
+      height: 4px;
+      margin-top: 20px;
+      background-color: #ccc;
+    }
+    .baseline-resetter:first-child {
+      margin-top: 0;
+    }
+    .sg-type-spec {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -ms-flex-flow: row wrap;
+      flex-flow: row wrap;
+      -webkit-box-pack: start;
+      -ms-flex-pack: start;
+      justify-content: flex-start;
+      -ms-flex-line-pack: start;
+      align-content: flex-start;
+      padding: 20px 0;
+      margin: 0;
+    }
+    .sg-type-spec > li {
+      -webkit-box-flex: 0;
+      -ms-flex: 1 0 320px;
+      flex: 1 0 320px;
+      padding: 0 20px 20px;
+      margin: 0;
+    }
+     .sg-type-spec p {
+        margin: 0;
+        padding: 0;
+     }
+  </style>
+</head>
+
+<body>
+<div id="a17">
+  <main id="content">
+    <ul class="sg-type-spec">
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-display-2">f-display-2</p>
+        <p class="f-display-2">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-display-2">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-display-1">f-display-1</p>
+        <p class="f-display-1">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-display-1">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-headline">f-headline</p>
+        <p class="f-headline">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-headline">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-headline-editorial">f-headline-editorial</p>
+        <p class="f-headline-editorial">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-headline-editorial">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-deck-2">f-deck-2</p>
+        <p class="f-deck-2">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-deck-2">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-deck-1">f-deck-1</p>
+        <p class="f-deck-1">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-deck-1">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+
+      <li>
+        <span class="baseline-resetter"></span>
+        <p class="f-body">f-body</p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+        <p class="f-body">The quick brown fox, <br>jumps over the lazy dog H<sub>2</sub>O<sup>2</sup></p>
+      </li>
+    </ul>
+
+    <div class="m-icons-demo">
+      <svg class="icon--alert"><use xlink:href="#icon--alert" /></svg>
+      <svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
+      <svg class="icon--calendar"><use xlink:href="#icon--calendar" /></svg>
+      <svg class="icon--check"><use xlink:href="#icon--check" /></svg>
+      <svg class="icon--clock"><use xlink:href="#icon--clock" /></svg>
+      <svg class="icon--close-circle"><use xlink:href="#icon--close-circle" /></svg>
+      <svg class="icon--close"><use xlink:href="#icon--close" /></svg>
+      <svg class="icon--info--24"><use xlink:href="#icon--info--24" /></svg>
+      <svg class="icon--location--24"><use xlink:href="#icon--location--24" /></svg>
+      <svg class="icon--location"><use xlink:href="#icon--location" /></svg>
+      <svg class="icon--logo--80"><use xlink:href="#icon--logo--80" /></svg>
+      <svg class="icon--logo--88"><use xlink:href="#icon--logo--88" /></svg>
+      <svg class="icon--logo--outline--80"><use xlink:href="#icon--logo--outline--80" /></svg>
+      <svg class="icon--logo--outline--88"><use xlink:href="#icon--logo--outline--88" /></svg>
+      <svg class="icon--minus--24"><use xlink:href="#icon--minus--24" /></svg>
+      <svg class="icon--new-window"><use xlink:href="#icon--new-window" /></svg>
+      <svg class="icon--play--48"><use xlink:href="#icon--play--48" /></svg>
+      <svg class="icon--play--64"><use xlink:href="#icon--play--64" /></svg>
+      <svg class="icon--play--96"><use xlink:href="#icon--play--96" /></svg>
+      <svg class="icon--plus--24"><use xlink:href="#icon--plus--24" /></svg>
+      <svg class="icon--print--24"><use xlink:href="#icon--print--24" /></svg>
+      <svg class="icon--quote--76"><use xlink:href="#icon--quote--76" /></svg>
+      <svg class="icon--return"><use xlink:href="#icon--return" /></svg>
+      <svg class="icon--search--24"><use xlink:href="#icon--search--24" /></svg>
+      <svg class="icon--share--24"><use xlink:href="#icon--share--24" /></svg>
+      <svg class="icon--user"><use xlink:href="#icon--user" /></svg>
+      <svg class="icon--zoom--24"><use xlink:href="#icon--zoom--24" /></svg>
+    </div>
+  </main>
+
+  <span class="design-grid-toggle design-grid-toggle--baseline" onClick="this.nextElementSibling.classList.toggle('js-hide');">Toggle baseline grid</span>
+  <span class="design-grid design-grid--baseline js-hide"></span>
+  <span class="design-grid-toggle design-grid-toggle--columns" data-env="Development" onClick="this.nextElementSibling.classList.toggle('js-hide');">Toggle columns grid</span>
+  <span class="design-grid design-grid--columns js-hide"></span>
+</div>
+
+<script src="/dist/scripts/app.js"></script>
+</body>
+</html>
+
