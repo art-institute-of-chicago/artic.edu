73337b1fe38ad9c55eb43827160f02fb50804228
Mike Byrne <mike@thirteentwelve.com>
2017-11-08 09:40:28 +0000

    Writing test listing markup for Marius grid test

--- index.php
+++ index.php
@@ -213,6 +213,116 @@
     </div>
 
 
+    <ul class="o-listing o-listing--keyline-top o-listing--2-col@small+ o-listing--4-col@large+">
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <em class="m-listing__type t-tag">Special Exhibition</em> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
+            <span class="m-listing__bottom m-listing__date f-secondary">Oct 29, 2017</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <em class="m-listing__type t-tag">Ongoing</em> <br>
+            <strong class="m-listing__title f-list-3">Along the Lines: Selected drawings by Saul Steinberg</strong> <br>
+            <span class="m-listing__bottom m-listing__date f-secondary">Oct 29, 2017</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <em class="m-listing__type t-tag">Ongoing</em> <br>
+            <strong class="m-listing__title f-list-3">Charles White Murals</strong> <br>
+            <span class="m-listing__bottom m-listing__date f-secondary">Through Nov 29, 2017</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <em class="m-listing__type t-tag">Ongoing</em> <br>
+            <strong class="m-listing__title f-list-3">Moholy-Nagy&mdash;Future Present</strong> <br>
+            <span class="m-listing__bottom m-listing__date f-secondary">Through Nov 29, 2017</span>
+          </span>
+        </a>
+      </li>
+    </ul>
+
+    <ul class="o-listing o-listing--keyline-top o-listing--2-col@small o-listing--3-col@medium o-listing--4-col@large o-listing--5-col@xlarge+">
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/300x300">
+          </span>
+          <span class="m-listing__meta">
+            <strong class="m-listing__title f-list-2">Resin Elephant</strong> <br>
+            <span class="m-listing__bottom m-listing__price f-secondary">$19.99</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/300x300">
+          </span>
+          <span class="m-listing__meta">
+            <strong class="m-listing__title f-list-2">Autumn Glass Leaves</strong> <br>
+            <span class="m-listing__bottom m-listing__price f-secondary">$68</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/300x300">
+          </span>
+          <span class="m-listing__meta">
+            <strong class="m-listing__title f-list-2">Modern Cuff Bracelet</strong> <br>
+            <span class="m-listing__bottom m-listing__price f-secondary"><strike>$80</strike> <span class="m-listing__sale-price">$59.99</span></span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/300x300">
+          </span>
+          <span class="m-listing__meta">
+            <strong class="m-listing__title f-list-2">Tapestry Duffle</strong> <br>
+            <span class="m-listing__bottom m-listing__price f-secondary">$150</span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/300x300">
+          </span>
+          <span class="m-listing__meta">
+            <strong class="m-listing__title f-list-2">Glass Pumpkin</strong> <br>
+            <span class="m-listing__bottom m-listing__price f-secondary">$79</span>
+          </span>
+        </a>
+      </li>
+    </ul>
+
+
     <ul class="sg-type-spec">
       <li>
         <p class="f-display-2">f-display-2</p>
