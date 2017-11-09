2d770f1a238067ce1ca53c9a48074b5a8b7989e4
Mike Byrne <mike@thirteentwelve.com>
2017-11-09 09:08:10 +0000

    Colset updates

--- _listitems.php
+++ _listitems.php
@@ -1,9 +1,8 @@
 <?php for ($i = 0; $i < 6; $i++): ?>
   <li class="m-listing">
     <a href="#" class="m-listing__link">
-      <span class="m-listing__meta">
-        <strong class="m-listing__title f-list-2">Title</strong> <br>
-        <span class="m-listing__bottom m-listing__price f-secondary">Bottom</span>
+      <span class="m-listing__img">
+        <img src="http://placehold.dev.area17.com/image/400x100">
       </span>
     </a>
   </li>
