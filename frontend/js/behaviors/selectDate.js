52f6692cf67c03671ac38eea9d117f53187a06eb
Mike Byrne <mike@thirteentwelve.com>
2017-11-09 18:07:37 +0000

    Can close calendar with escape or clickoutside

--- frontend/js/behaviors/selectDate.js
+++ frontend/js/behaviors/selectDate.js
@@ -21,6 +21,7 @@ const selectDate = function(container) {
 
   function _openCalendar(event) {
     event.preventDefault();
+    event.stopPropagation();
     opener.blur();
     if (!calendarOpen) {
       document.documentElement.classList.add('s-calendar-active');
@@ -55,17 +56,36 @@ const selectDate = function(container) {
     _closeCalendar();
   }
 
+  function _clicksOutside(event) {
+    if (calendarOpen) {
+      event.preventDefault();
+      event.stopPropagation();
+      _closeCalendar();
+    }
+  }
+
+  function _escape(event) {
+    var isInput = (event.target.tagName === 'INPUT');
+    if (calendarOpen && event.keyCode === 27 && !isInput) {
+      _closeCalendar();
+    }
+  }
+
   function _init() {
     opener.addEventListener('click', _openCalendar, false);
     clear.addEventListener('click', _clearSelected, false);
     container.addEventListener('calendar:dateSelected', _dateSelected, false);
+    document.addEventListener('click', _clicksOutside, false);
+    window.addEventListener('keyup', _escape, false);
   }
 
   this.destroy = function() {
     // remove specific event handlers
     opener.removeEventListener('click', _openCalendar);
-    clear.removeEventListener('click', _clearSelected, false);
+    clear.removeEventListener('click', _clearSelected);
     container.removeEventListener('calendar:dateSelected', _dateSelected);
+    document.removeEventListener('click', _clicksOutside);
+    window.removeEventListener('keyup', _escape);
 
     // remove properties of this behavior
     purgeProperties(this);
