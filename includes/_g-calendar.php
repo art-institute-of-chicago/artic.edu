cfec3d6a4fef887a01c69967acc4d1d13349d42e
Mike Byrne <mike@thirteentwelve.com>
2017-11-13 08:40:52 +0000

    Splitting static pages up into partials

---
+++ includes/_g-calendar.php
@@ -0,0 +1,20 @@
+<div class="m-calendar" id="calendar" data-behavior="calendar">
+  <b class="m-calendar__title f-caption" data-calendar-title></b>
+  <table>
+    <thead class="f-caption">
+      <tr>
+        <th title="Sunday">S</th>
+        <th title="Monday">M</th>
+        <th title="Tuesday">T</th>
+        <th title="Wednesday">W</th>
+        <th title="Thursday">T</th>
+        <th title="Friday">F</th>
+        <th title="Saturday">S</th>
+      </tr>
+    </thead>
+    <tbody class="f-secondary">
+    </tbody>
+  </table>
+  <button class="m-calendar__next" data-calendar-next><svg aria-title="Next month" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></button>
+  <button class="m-calendar__prev" data-calendar-prev><svg aria-title="Previous month" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></button>
+</div>
