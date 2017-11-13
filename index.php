3e9643daf04d87bd90161a56c143f28bc69b3d06
Mike Byrne <mike@thirteentwelve.com>
2017-11-13 11:11:23 +0000

    Fleshing out the homepage a little including the header

--- index.php
+++ index.php
@@ -24,19 +24,136 @@
   <script src="/dist/scripts/head.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/612324/7579192/css/fonts.css" />
   <link href="/dist/styles/app.css" rel="stylesheet" />
-
-  <style>
-    #a17 {
-      padding-top: 0 !important;
-      color: #1a1a1a !important;
-    }
-  </style>
 </head>
 
 <body>
 <div id="a17">
 
+  <header class="g-header">
+    <a class="g-header__logo" href="/"><svg class="icon--logo--88" aria-label="Art Institute of Chicago"><use xlink:href="#icon--logo--88" /></svg></a>
+    <nav class="g-header__nav-primary">
+      <ul>
+        <li><a href="#">Visit</a></li>
+        <li><a href="#">Exhibitions and events</a></li>
+        <li><a href="#">Art and ideas</a></li>
+        <li><a href="#"><svg class="icon--search--24" aria-label="Search site"><use xlink:href="#icon--search--24" /></svg></a></li>
+      </ul>
+    </nav>
+    <nav class="g-header__nav-secondary">
+      <ul>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Buy Tickets</a></li>
+        <li><a href="#">Become a member</a></li>
+      </ul>
+    </nav>
+    <p class="g-header__opening-hours f-secondary"><a href="#">Open daily 10:30&ndash;5:00, Thursdays until 8:00</a></p>
+  </header>
+
   <main id="content">
+    <ul class="o-features o-features--full">
+      <li class="m-listing m-listing--feature o-features__hero">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/350x200">
+          </span>
+          <span class="m-listing__meta">
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Now open</em>
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing m-listing--feature">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/350x200">
+          </span>
+          <span class="m-listing__meta">
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Special Exhibition</em>
+            </span> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing m-listing--feature">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/350x200">
+          </span>
+          <span class="m-listing__meta">
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Special Exhibition</em>
+            </span> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
+          </span>
+        </a>
+      </li>
+    </ul>
+
+    <div class="m-info-block">
+      <p class="f-deck">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget laoreet tortor. Quisque tristique laoreet lectus sit amet tempus. Aliquam vel eleifend nisi.</p>
+      <div class="m-info-block__footer">
+        <ul class="m-info-block__footer-actions-primary">
+          <li><a href="#" class="btn">Plan your visit</a></li>
+        </ul>
+        <ul class="m-info-block__footer-actions-secondary">
+          <li><a href="#">Hours and admission<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></li>
+          <li><a href="#">Directions and parking<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="m-title-bar">
+      <h2 class="m-title-bar__title f-module-title-1">What’s on Today</h2>
+      <ul class="m-title-bar__links">
+        <li><a href="#">Explore What’s on<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></li>
+      </ul>
+    </div>
+
+    <ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-rows o-grid-listing--2-col@medium o-grid-listing--2-col@large o-grid-listing--2-col@xlarge o-grid-listing--2-col@xxlarge">
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Exhibition</em>
+            </span> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
+          </span>
+        </a>
+      </li>
+      <li class="m-listing">
+        <a href="#" class="m-listing__link">
+          <span class="m-listing__img">
+            <img src="http://placehold.dev.area17.com/image/400x225">
+          </span>
+          <span class="m-listing__meta">
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Special Exhibition</em>
+              <span class="m-listing__closing-soon f-tag">Closing Soon</span>
+            </span> <br>
+            <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
+          </span>
+        </a>
+      </li>
+    </ul>
 
     <ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-rows o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
       <li class="m-listing">
@@ -45,9 +162,13 @@
             <img src="http://placehold.dev.area17.com/image/400x225">
           </span>
           <span class="m-listing__meta">
-            <em class="m-listing__type f-tag">Special Exhibition</em> <br>
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Special Exhibition</em>
+            </span> <br>
             <strong class="m-listing__title f-list-3">Cauleen Smith: Human_3.0 Reading Listz</strong> <br>
-            <span class="m-listing__bottom m-listing__date f-secondary">Oct 29, 2017</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
           </span>
         </a>
       </li>
@@ -57,9 +178,13 @@
             <img src="http://placehold.dev.area17.com/image/400x225">
           </span>
           <span class="m-listing__meta">
-            <em class="m-listing__type f-tag">Ongoing</em> <br>
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Ongoing</em>
+            </span> <br>
             <strong class="m-listing__title f-list-3">Along the Lines: Selected drawings by Saul Steinberg</strong> <br>
-            <span class="m-listing__bottom m-listing__date f-secondary">Oct 29, 2017</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Oct 29, 2017</span>
+            </span>
           </span>
         </a>
       </li>
@@ -69,9 +194,13 @@
             <img src="http://placehold.dev.area17.com/image/400x225">
           </span>
           <span class="m-listing__meta">
-            <em class="m-listing__type f-tag">Ongoing</em> <br>
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Ongoing</em>
+            </span> <br>
             <strong class="m-listing__title f-list-3">Charles White Murals</strong> <br>
-            <span class="m-listing__bottom m-listing__date f-secondary">Through Nov 29, 2017</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Through Nov 29, 2017</span>
+            </span>
           </span>
         </a>
       </li>
@@ -81,13 +210,24 @@
             <img src="http://placehold.dev.area17.com/image/400x225">
           </span>
           <span class="m-listing__meta">
-            <em class="m-listing__type f-tag">Ongoing</em> <br>
+            <span class="m-listing__meta-top">
+              <em class="m-listing__type f-tag">Ongoing</em>
+            </span> <br>
             <strong class="m-listing__title f-list-3">Moholy-Nagy&mdash;Future Present</strong> <br>
-            <span class="m-listing__bottom m-listing__date f-secondary">Through Nov 29, 2017</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__date f-secondary">Through Nov 29, 2017</span>
+            </span>
           </span>
         </a>
       </li>
     </ul>
+
+    <div class="m-title-bar">
+      <h2 class="m-title-bar__title f-module-title-1">AIC Shop</h2>
+      <ul class="m-title-bar__links">
+        <li><a href="#">Explore the Shop<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></li>
+      </ul>
+    </div>
 
     <ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-rows o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--5-col@xlarge o-grid-listing--5-col@xxlarge">
       <li class="m-listing">
@@ -97,7 +237,9 @@
           </span>
           <span class="m-listing__meta">
             <strong class="m-listing__title f-list-2">Resin Elephant</strong> <br>
-            <span class="m-listing__bottom m-listing__price f-secondary">$19.99</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__price f-secondary">$19.99</span>
+            </span>
           </span>
         </a>
       </li>
@@ -108,7 +250,9 @@
           </span>
           <span class="m-listing__meta">
             <strong class="m-listing__title f-list-2">Autumn Glass Leaves</strong> <br>
-            <span class="m-listing__bottom m-listing__price f-secondary">$68</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__price f-secondary">$68</span>
+            </span>
           </span>
         </a>
       </li>
@@ -119,7 +263,9 @@
           </span>
           <span class="m-listing__meta">
             <strong class="m-listing__title f-list-2">Modern Cuff Bracelet</strong> <br>
-            <span class="m-listing__bottom m-listing__price f-secondary"><strike>$80</strike> <span class="m-listing__sale-price">$59.99</span></span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__price f-secondary"><strike>$80</strike> <span class="m-listing__sale-price">$59.99</span></span>
+            </span>
           </span>
         </a>
       </li>
@@ -130,7 +276,9 @@
           </span>
           <span class="m-listing__meta">
             <strong class="m-listing__title f-list-2">Tapestry Duffle</strong> <br>
-            <span class="m-listing__bottom m-listing__price f-secondary">$150</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__price f-secondary">$150</span>
+            </span>
           </span>
         </a>
       </li>
@@ -141,7 +289,9 @@
           </span>
           <span class="m-listing__meta">
             <strong class="m-listing__title f-list-2">Glass Pumpkin</strong> <br>
-            <span class="m-listing__bottom m-listing__price f-secondary">$79</span>
+            <span class="m-listing__meta-bottom">
+              <span class="m-listing__price f-secondary">$79</span>
+            </span>
           </span>
         </a>
       </li>
