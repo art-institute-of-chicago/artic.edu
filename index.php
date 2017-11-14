26c6ccb71947670ef6cb3771d7c9255c08304af7
Mike Byrne <mike@thirteentwelve.com>
2017-11-14 11:49:27 +0000

    CSS tweaks from Marius for features

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
   <header class="g-header">
     <a class="g-header__logo" href="/"><svg class="icon--logo--88" aria-label="Art Institute of Chicago"><use xlink:href="#icon--logo--88" /></svg></a>
-    <nav class="g-header__nav-primary">
+    <nav class="g-header__nav-primary f-body">
       <ul>
         <li><a href="#">Visit</a></li>
         <li><a href="#">Exhibitions and events</a></li>
@@ -48,14 +48,14 @@
         <li><a href="#"><svg class="icon--search--24" aria-label="Search site"><use xlink:href="#icon--search--24" /></svg></a></li>
       </ul>
     </nav>
-    <nav class="g-header__nav-secondary">
+    <nav class="g-header__nav-secondary f-body">
       <ul>
         <li><a href="#">Shop</a></li>
         <li><a href="#">Buy Tickets</a></li>
         <li><a href="#">Become a member</a></li>
       </ul>
     </nav>
-    <p class="g-header__opening-hours f-secondary"><a href="#">Open daily 10:30&ndash;5:00, <br>Thursdays until 8:00</a></p>
+    <p class="g-header__opening-hours f-body"><a href="#">Open daily 10:30&ndash;5:00, <br>Thursdays until 8:00</a></p>
   </header>
 
   <main id="content">
