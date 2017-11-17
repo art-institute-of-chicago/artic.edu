b478e32941d5e802b12dfb2db2ae30ed2519186d
Mike Byrne <mike@thirteentwelve.com>
2017-11-17 13:10:43 +0000

    Re-instated footer markup

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,72 @@
+<footer id="footer" class="g-footer">
+  <a href="#a17" class="g-footer__top-link arrow-link arrow-link--up f-secondary">Back to top<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a>
+  <ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-split-rows o-grid-listing--2-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
+    <li>
+      <h3 class="f-body">The Art Institute of Chicago</h3>
+      <figure>
+        <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">
+          <img src="http://placehold.dev.area17.com/image/400x225">
+        </a>
+      </figure>
+    </li>
+    <li>
+      <p class="f-secondary"><a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">111 South Michigan Avenue, Chicago, IL 60603</a></p>
+      <p class="f-secondary">Open today 10:30-5:00 <br><a href="#">Hours and admission<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
+    </li>
+    <li>
+      <h3 class="f-list-2"><a href="#">About Us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+      <figure class="u-show@medium+">
+        <img src="http://placehold.dev.area17.com/image/400x225">
+      </figure>
+      <p class="f-secondary u-show@medium+">Explore our mission, board, leadership, our story. <br><a href="#">Explore about us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
+    </li>
+    <li>
+      <h3 class="f-list-2"><a href="#">Inside the museum<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+      <figure class="u-show@medium+">
+        <img src="http://placehold.dev.area17.com/image/400x225">
+      </figure>
+      <p class="f-secondary u-show@medium+">Discover our curation and conservation efforts <br><a href="#">Our initiatives<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
+    </li>
+    <li>
+      <h3 class="f-list-2">Visit</h3>
+      <ul class="f-secondary">
+        <li><a href="#">Plan your visit</a></li>
+        <li><a href="#">Exhibitions and events</a></li>
+        <li><a href="#">Art and ideas</a></li>
+      </ul>
+    </li>
+    <li>
+      <h3 class="f-list-2">Follow us</h3>
+      <ul class="f-secondary">
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">LinkedIn</a></li>
+        <li><a href="#">YouTube</a></li>
+      </ul>
+    </li>
+    <li>
+      <h3 class="f-list-2"><a href="#">Participate<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+      <figure class="u-show@medium+">
+        <img src="http://placehold.dev.area17.com/image/400x225">
+      </figure>
+      <p class="f-secondary u-show@medium+">Volunteer, travel programs, learning classes <br><a href="#">Get involved<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
+    </li>
+    <li>
+      <h3 class="f-list-2"><a href="#">Support us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+      <figure class="u-show@medium+">
+        <img src="http://placehold.dev.area17.com/image/400x225">
+      </figure>
+      <p class="f-secondary u-show@medium+">Membership, donations <br><a href="#">Support the institute<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
+    </li>
+  </ul>
+  <ul class="g-footer__legals f-secondary">
+    <li><a href="#">Articles</a></li>
+    <li><a href="#">Employment</a></li>
+    <li><a href="#">Venue rental</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">SAIC</a></li>
+    <li><a href="#">Terms</a></li>
+    <li><a href="#">Image licensing</a></li>
+  </ul>
+  <a href="#" class="g-footer__credit f-secondary">Site by AREA 17</a>
+</footer>
