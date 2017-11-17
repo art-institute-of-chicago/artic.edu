73535956108b6a110a2cfe85a81d81b591bb27c7
Mike Byrne <mike@thirteentwelve.com>
2017-11-17 13:10:30 +0000

    Intro block looses its footer, replaced by links bar

--- resources/views/shared/_intro-block.blade.php
+++ resources/views/shared/_intro-block.blade.php
@@ -1,21 +1,3 @@
 <div class="m-intro-block">
   <p class="f-deck">{{ $intro }}</p>
-  @if ((isset($primaryActions) and $primaryActions) or (isset($secondaryActions) and $secondaryActions))
-  <div class="m-intro-block__footer">
-    @if (isset($primaryActions) and $primaryActions)
-    <ul class="m-intro-block__footer-actions-primary">
-      @foreach ($primaryActions as $link)
-      <li><a href="{{ $link['href'] }}" class="btn">{{ $link['text'] }}</a></li>
-      @endforeach
-    </ul>
-    @endif
-    @if (isset($secondaryActions) and $secondaryActions)
-    <ul class="m-intro-block__footer-actions-secondary">
-      @foreach ($secondaryActions as $link)
-      <li><a href="{{ $link['href'] }}">{{ $link['text'] }}<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></li>
-      @endforeach
-    </ul>
-    @endif
-  </div>
-  @endif
 </div>
