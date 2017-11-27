e5970cfe997f63e3de60d95236ef3bb5385660fb
Fernando Petrelli <fernando@area17.com>
2017-11-27 18:34:18 -0300

    Create pages table. Using Pentagram approach of a pages table with types

---
+++ app/Models/Page.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use A17\CmsToolkit\Models\Behaviors\HasRevisions;
+use A17\CmsToolkit\Models\Behaviors\HasSlug;
+use A17\CmsToolkit\Models\Model;
+// use Illuminate\Database\Eloquent\Builder;
+
+class Page extends Model
+{
+    use HasSlug, HasRevisions;
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::addGlobalScope('type', function (Builder $builder) {
+    //         $builder->orderBy('type');
+    //     });
+    // }
+
+    public static $types = [
+        0 => 'Home',
+        1 => 'Exhibitions',
+        2 => 'Events'
+    ];
+
+    protected $fillable = [
+        'published',
+        'position',
+        'type',
+        'title'
+    ];
+
+    public $slugAttributes = [
+        'title',
+    ];
+
+    // public function featuredWorks()
+    // {
+    //     return $this->belongsToMany('App\Models\Work', 'page_work_work')->withPivot('position')->orderBy('position');
+    // }
+
+    // public function homeWorks()
+    // {
+    //     return $this->belongsToMany('App\Models\Work', 'page_home_work')->withPivot('position')->orderBy('position');
+    // }
+}
