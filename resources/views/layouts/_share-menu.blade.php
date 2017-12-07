64cb78e4972b4502b0cb23532029b92ef756824c
Mike Byrne <mike@thirteentwelve.com>
2017-12-07 14:48:58 +0000

    Added email to share component

--- resources/views/layouts/_share-menu.blade.php
+++ resources/views/layouts/_share-menu.blade.php
@@ -3,6 +3,7 @@
     <li><a href="#" class="f-secondary" data-shareMenu="facebook">Facebook</a></li>
     <li><a href="#" class="f-secondary" data-shareMenu="twitter">Twitter</a></li>
     <li><a href="#" class="f-secondary" data-shareMenu="pinterest">Pinterest</a></li>
+    <li><a href="#" class="f-secondary" data-shareMenu="email">Email</a></li>
     <li><button class="f-secondary" data-shareMenu="copy">Copy link</button></li>
   </ul>
 </div>
