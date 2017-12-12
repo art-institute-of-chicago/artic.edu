cf529f190e4691e888345a3f8902b69262c4df7e
Mike Byrne <mike@thirteentwelve.com>
2017-12-12 12:34:10 +0000

    Updating grid listing to specs, added scrolly

--- resources/views/statics/listinggrids.blade.php
+++ resources/views/statics/listinggrids.blade.php
@@ -21,12 +21,6 @@
 </ul>
 
 <div style="margin-top: 60px;"></div>
-<p class="f-secondary">o-grid-listing, no cols, o-grid-listing--gridlines-split-rows</p>
-<ul class="o-grid-listing o-grid-listing--gridlines-split-rows">
-  @include('shared._listitems')
-</ul>
-
-<div style="margin-top: 60px;"></div>
 <p class="f-secondary">o-grid-listing, 2 cols at small, 3 cols at medium, 4 cols large+ (most examples follow this)</p>
 <ul class="o-grid-listing o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
   @include('shared._listitems')
@@ -111,21 +105,17 @@
 </ul>
 
 <div style="margin-top: 60px;"></div>
-<p class="f-secondary">o-grid-listing--gridlines-split-rows</p>
-<ul class="o-grid-listing o-grid-listing--gridlines-split-rows o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
-  @include('shared._listitems')
-</ul>
-
-<div style="margin-top: 60px;"></div>
-<p class="f-secondary">o-grid-listing--gridlines-right o-grid-listing--gridlines-split-rows</p>
-<ul class="o-grid-listing o-grid-listing--gridlines-right o-grid-listing--gridlines-split-rows o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
-  @include('shared._listitems')
-</ul>
-
-<div style="margin-top: 60px;"></div>
-<p class="f-secondary">o-grid-listing--gridlines-cols o-grid-listing--gridlines-split-rows</p>
-<ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-split-rows o-grid-listing--2-col@small o-grid-listing--3-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
-  @include('shared._listitems')
+<p class="f-secondary">5 Column Shop</p>
+<ul class="o-grid-listing o-grid-listing--single-row o-grid-listing--scroll@xsmall o-grid-listing--scroll@small o-grid-listing--hide-extra@medium o-grid-listing--keyline-top o-grid-listing--gridlines-cols o-grid-listing--4-col@medium o-grid-listing--5-col@large o-grid-listing--5-col@xlarge o-grid-listing--5-col@xxlarge">
+  <?php for ($i = 0; $i < 5; $i++): ?>
+    <li class="m-listing">
+      <a href="#" class="m-listing__link">
+        <span class="m-listing__img" style="height: 200px !important;">
+          <img src="http://placehold.dev.area17.com/image/400x100" style="height: 200px !important;">
+        </span>
+      </a>
+    </li>
+  <?php endfor; ?>
 </ul>
 
 @endsection
