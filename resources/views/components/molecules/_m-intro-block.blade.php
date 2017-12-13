a4cf9edf2d4e4c592d21f8d933be2fce7665ab5e
Mike Byrne <mike@thirteentwelve.com>
2017-12-13 19:09:38 +0000

    intro block can now accept some links

--- resources/views/components/molecules/_m-intro-block.blade.php
+++ resources/views/components/molecules/_m-intro-block.blade.php
@@ -1,3 +1,15 @@
 <div class="m-intro-block">
-  <p class="f-deck">{{ $slot }}</p>
+  <p class="m-intro-block__intro {{ $font ?? 'f-deck' }}">{{ $slot }}</p>
+  @if (isset($links))
+  <ul class="m-intro-block__links">
+    @foreach ($links as $link)
+    <li class="m-intro-block__link{{ (isset($link['active']) and $link['active']) ? ' s-active' : '' }}">
+        <a class="m-intro-block__link-trigger {{ $link['font'] ?? 'f-buttons' }}{{ (isset($link['variation']) and $link['variation']) ? ' '.$link['variation'] : '' }}" href="{{ $link['href'] }}">
+          {{ $link['text'] }}
+          @if (isset($link['icon']) and $link['icon'])<svg aria-hidden="true" class="{{ $link['icon'] }}"><use xlink:href="#{{ $link['icon'] }}" /></svg>@endif
+        </a>
+    </li>
+    @endforeach
+  </ul>
+  @endif
 </div>
