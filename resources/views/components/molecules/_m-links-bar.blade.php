b2e083fae5e6f926f45a7ca60fa9e97d042fdc14
Mike Byrne <mike@thirteentwelve.com>
2017-12-13 19:10:07 +0000

    Links bar refactored, added title bar companion variation

--- resources/views/components/molecules/_m-links-bar.blade.php
+++ resources/views/components/molecules/_m-links-bar.blade.php
@@ -1,21 +1,21 @@
 @php
     $linksPrimaryFontClass = ' f-buttons';
-    $linksPrimaryFontClass = (isset($variation) and $variation === 'buttons') ? ' btn f-buttons' : $linksPrimaryFontClass;
-    $linksPrimaryFontClass = (isset($variation) and $variation === 'tabs') ? ' f-module-title-2' : $linksPrimaryFontClass;
+    $linksPrimaryFontClass = (isset($variation) and $variation === 'm-links-bar--buttons') ? ' btn f-buttons' : $linksPrimaryFontClass;
+    $linksPrimaryFontClass = (isset($variation) and $variation === 'm-links-bar--tabs') ? ' f-module-title-2' : $linksPrimaryFontClass;
 @endphp
-<nav class="m-links-bar{{ (isset($variation) and $variation) ? " m-links-bar--".$variation : "" }}">
+<nav class="m-links-bar{{ (isset($variation) and $variation) ? " ".$variation : "" }}">
 @if (isset($linksPrimary) and $linksPrimary)
   <ul class="m-links-bar__items-primary">
     @foreach ($linksPrimary as $link)
     <li class="m-links-bar__item{{ (isset($link['active']) and $link['active']) ? ' s-active' : '' }}">
         <a class="m-links-bar__item-trigger{{ $linksPrimaryFontClass }}{{ (isset($link['variation']) and $link['variation']) ? ' '.$link['variation'] : '' }}" href="{{ $link['href'] }}">
-          @if (isset($link['icon']) and $link['icon'] and isset($variation) and $variation === 'buttons')<svg aria-hidden="true" class="{{ $link['icon'] }}"><use xlink:href="#{{ $link['icon'] }}" /></svg>@endif
+          @if (isset($link['icon']) and $link['icon'] and isset($variation) and $variation === 'm-links-bar--buttons')<svg aria-hidden="true" class="{{ $link['icon'] }}"><use xlink:href="#{{ $link['icon'] }}" /></svg>@endif
           {{ $link['text'] }}
-          @if (isset($link['icon']) and $link['icon'] and (!isset($variation) or $variation !== 'buttons'))<svg aria-hidden="true" class="{{ $link['icon'] }}"><use xlink:href="#{{ $link['icon'] }}" /></svg>@endif
+          @if (isset($link['icon']) and $link['icon'] and (!isset($variation) or $variation !== 'm-links-bar--buttons'))<svg aria-hidden="true" class="{{ $link['icon'] }}"><use xlink:href="#{{ $link['icon'] }}" /></svg>@endif
         </a>
     </li>
     @endforeach
-    @if ($slot)
+    @if ($slot and strlen($slot) > 0)
         <li class="m-links-bar__item">{{ $slot }}</li>
     @endif
   </ul>
