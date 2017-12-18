63416b510ac1ab78599cbf2ca32527ec60e712be
Quentin Renard <quentin@area17.com>
2017-12-19 00:30:11 +0100

    Apply update database migrations

---
+++ database/migrations/2017_12_19_000106_update_mediables_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UpdateMediablesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('mediables', function (Blueprint $table) {
+            $table->dropColumn('crop_x2');
+            $table->dropColumn('crop_y2');
+            $table->dropColumn('background_position');
+            $table->json('metadatas');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('mediables', function (Blueprint $table) {
+            $table->integer('crop_x2')->nullable();
+            $table->integer('crop_y2')->nullable();
+            $table->string('background_position', 20)->default('top');
+        });
+
+    }
+}
