a0d02ad66fb1bbd0e35a1fcd778b484807c9bc05
Mike Byrne <mike@thirteentwelve.com>
2018-01-02 16:18:05 +0000

    text blocks output html and links have a style

--- resources/views/components/blocks/_text.blade.php
+++ resources/views/components/blocks/_text.blade.php
@@ -1,1 +1,1 @@
-<{{ $tag ?? 'p' }} class="{{ $font ?? 'f-body' }}{{ (isset($variation)) ? ' '.$variation : '' }}">{{ $slot }}</{{ $tag ?? 'p' }}>
+<{{ $tag ?? 'p' }} class="{{ $font ?? 'f-body' }}{{ (isset($variation)) ? ' '.$variation : '' }}">{!! $slot !!}</{{ $tag ?? 'p' }}>
