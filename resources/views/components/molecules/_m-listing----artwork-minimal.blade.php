7852a8389240cd1457679d4d07f6897877129337
Mike Byrne <mike@thirteentwelve.com>
2018-01-15 18:37:07 +0000

    Updated minimal artwork listing object positioning

--- resources/views/components/molecules/_m-listing----artwork-minimal.blade.php
+++ resources/views/components/molecules/_m-listing----artwork-minimal.blade.php
@@ -1,6 +1,6 @@
 <{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}">
     <a href="{{ $artwork->slug }}" class="m-listing__link">
-        <span class="m-listing__img m-listing__img--tall">
+        <span class="m-listing__img m-listing__img--tall m-listing__img--contain">
             @component('components.atoms._img')
                 @slot('src', $artwork->image['src'])
                 @slot('width', $artwork->image['width'])
