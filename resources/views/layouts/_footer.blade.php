696a9e7b1c04b49b3ee3f2fb94ee4548aad0d819
Tim Brook <tim@area17.com>
2018-01-18 14:08:48 -0600

    Add updated responsive footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,64 +1,98 @@
 <footer id="footer" class="g-footer">
-  <a href="#a17" class="g-footer__top-link arrow-link arrow-link--up f-secondary">Back to top<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a>
-  <ul class="o-grid-listing o-grid-listing--gridlines-cols o-grid-listing--gridlines-split-rows o-grid-listing--2-col@medium o-grid-listing--4-col@large o-grid-listing--4-col@xlarge o-grid-listing--4-col@xxlarge">
-    <li>
-      <h3 class="f-body">The Art Institute of Chicago</h3>
-      <figure>
-        <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">
-          <img src="http://placehold.dev.area17.com/image/400x225">
+  <a href="#a17" class="g-footer__top-link arrow-link arrow-link--up f-small-caps">
+    Back to top
+    <span>
+      <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
+    </span>
+  </a>
+
+  <div class="g-footer__inner">
+    <ul class="g-footer__grid">
+      <li>
+        <a class="g-footer__logo" href="/">
+          <svg aria-title="Art Institute of Chicago">
+            <use xlink:href="#icon--logo--outline--80" />
+            <use xlink:href="#icon--logo--outline--88" />
+            <use xlink:href="#icon--logo--outline--92" />
+          </svg>
         </a>
-      </figure>
-    </li>
-    <li>
-      <p class="f-secondary"><a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">111 South Michigan Avenue, Chicago, IL 60603</a></p>
-      <p class="f-secondary">Open today 10:30-5:00 <br><a href="#">Hours and admission<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
-    </li>
-    <li>
-      <h3 class="f-list-2"><a href="#">About Us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
-      <figure class="u-show@medium+">
-        <img src="http://placehold.dev.area17.com/image/400x225">
-      </figure>
-      <p class="f-secondary u-show@medium+">Explore our mission, board, leadership, our story. <br><a href="#">Explore about us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
-    </li>
-    <li>
-      <h3 class="f-list-2"><a href="#">Inside the museum<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
-      <figure class="u-show@medium+">
-        <img src="http://placehold.dev.area17.com/image/400x225">
-      </figure>
-      <p class="f-secondary u-show@medium+">Discover our curation and conservation efforts <br><a href="#">Our initiatives<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
-    </li>
-    <li>
-      <h3 class="f-list-2">Visit</h3>
-      <ul class="f-secondary">
-        <li><a href="#">Plan your visit</a></li>
-        <li><a href="#">Exhibitions and events</a></li>
-        <li><a href="#">Art and ideas</a></li>
-      </ul>
-    </li>
-    <li>
-      <h3 class="f-list-2">Follow us</h3>
-      <ul class="f-secondary">
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">LinkedIn</a></li>
-        <li><a href="#">YouTube</a></li>
-      </ul>
-    </li>
-    <li>
-      <h3 class="f-list-2"><a href="#">Participate<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
-      <figure class="u-show@medium+">
-        <img src="http://placehold.dev.area17.com/image/400x225">
-      </figure>
-      <p class="f-secondary u-show@medium+">Volunteer, travel programs, learning classes <br><a href="#">Get involved<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
-    </li>
-    <li>
-      <h3 class="f-list-2"><a href="#">Support us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
-      <figure class="u-show@medium+">
-        <img src="http://placehold.dev.area17.com/image/400x225">
-      </figure>
-      <p class="f-secondary u-show@medium+">Membership, donations <br><a href="#">Support the institute<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></p>
-    </li>
-  </ul>
+
+        <ul class="g-footer__link-list">
+          <li>Open today 10:30-5:00</li>
+          <li><a href="#">Visit</a></li>
+          <li><a href="#">Exhibitions and Events</a></li>
+          <li><a href="#">The Collection</a></li>
+        </ul>
+      </li>
+
+      <li class="u-hide@small-">
+        <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">
+          <svg aria-hidden="true" class="icon--footer_map_120x92"><use xlink:href="#icon--footer_map_120x92" /></svg>
+        </a>
+
+        <ul class="g-footer__link-list g-footer__link-list--spaced">
+          <li>
+            Historic Building <br>
+            <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">111 South Michigan Avenue, <br>Chicago, IL 60603</a>
+          </li>
+
+          <li>
+            Modern Wing <br>
+            <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">159 East Monroe Street, <br>Chicago, IL 60603</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="u-hide@small-">
+        <div class="g-footer__grid__group">
+          <h3 class="f-list-6"><a href="#">About Us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+
+          <ul class="g-footer__link-list">
+            <li><a href="#">Inside the museum</a></li>
+            <li><a href="#">Mission and History</a></li>
+            <li><a href="#">Leadership</a></li>
+            <li><a href="#">Financial Records</a></li>
+          </ul>
+        </div>
+
+        <div class="g-footer__grid__group">
+          <h3 class="f-list-6">Support us</h3>
+
+          <ul class="g-footer__link-list">
+            <li><a href="#">Membership</a></li>
+            <li><a href="#">Ways to Give</a></li>
+            <li><a href="#">Affiliate Groups</a></li>
+            <li><a href="#">Corporate Sponsorship</a></li>
+          </ul>
+        </div>
+      </li>
+
+      <li>
+        <div class="g-footer__grid__group u-hide@small-">
+          <h3 class="f-list-6"><a href="#">Learn with Us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+
+          <ul class="g-footer__link-list">
+            <li><a href="#">Families</a></li>
+            <li><a href="#">Teens</a></li>
+            <li><a href="#">Adults</a></li>
+            <li><a href="#">Educators</a></li>
+          </ul>
+        </div>
+
+        <div class="g-footer__grid__group">
+          <h3 class="f-list-6">Follow us</h3>
+
+          <ul class="g-footer__link-list">
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Pinterest</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+
   <ul class="g-footer__legals f-secondary">
     <li><a href="#">Articles</a></li>
     <li><a href="#">Employment</a></li>
@@ -68,5 +102,4 @@
     <li><a href="#">Terms</a></li>
     <li><a href="#">Image licensing</a></li>
   </ul>
-  <a href="#" class="g-footer__credit f-secondary">Site by AREA 17</a>
 </footer>
