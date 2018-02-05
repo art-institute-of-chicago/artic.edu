95bbc6c6af70d7e468e3d45ce9105b62dc7cc428
Mike Byrne <mike@thirteentwelve.com>
2018-02-05 14:25:26 +0000

    Icons task now generates icons template, updates

--- resources/views/statics/icons.blade.php
+++ resources/views/statics/icons.blade.php
@@ -3,49 +3,147 @@
 @section('content')
 
 <div class="sg-icons">
-  <svg class="icon--alert"><use xlink:href="#icon--alert" /></svg>
-  <svg class="icon--arrow--24"><use xlink:href="#icon--arrow--24" /></svg>
-  <svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-  <svg class="icon--calendar"><use xlink:href="#icon--calendar" /></svg>
-  <svg class="icon--caret"><use xlink:href="#icon--caret" /></svg>
-  <svg class="icon--check"><use xlink:href="#icon--check" /></svg>
-  <svg class="icon--clock"><use xlink:href="#icon--clock" /></svg>
-  <svg class="icon--close-circle"><use xlink:href="#icon--close-circle" /></svg>
-  <svg class="icon--close-dark"><use xlink:href="#icon--close-dark" /></svg>
-  <svg class="icon--close"><use xlink:href="#icon--close" /></svg>
-  <svg class="icon--download--24"><use xlink:href="#icon--download--24" /></svg>
-  <svg class="icon--filter--24"><use xlink:href="#icon--filter--24" /></svg>
-  <svg class="icon--info"><use xlink:href="#icon--info" /></svg>
-  <svg class="icon--info--24"><use xlink:href="#icon--info--24" /></svg>
-  <svg class="icon--info--32"><use xlink:href="#icon--info--32" /></svg>
-  <svg class="icon--info-i"><use xlink:href="#icon--info-i" /></svg>
-  <svg class="icon--location--24"><use xlink:href="#icon--location--24" /></svg>
-  <svg class="icon--location"><use xlink:href="#icon--location" /></svg>
-  <svg class="icon--logo--80"><use xlink:href="#icon--logo--80" /></svg>
-  <svg class="icon--logo--88"><use xlink:href="#icon--logo--88" /></svg>
-  <svg class="icon--logo--92"><use xlink:href="#icon--logo--92" /></svg>
-  <svg class="icon--logo--outline--80"><use xlink:href="#icon--logo--outline--80" /></svg>
-  <svg class="icon--logo--outline--88"><use xlink:href="#icon--logo--outline--88" /></svg>
-  <svg class="icon--logo--outline--92"><use xlink:href="#icon--logo--outline--92" /></svg>
-  <svg class="icon--minus--24"><use xlink:href="#icon--minus--24" /></svg>
-  <svg class="icon--new-window"><use xlink:href="#icon--new-window" /></svg>
-  <svg class="icon--no-image"><use xlink:href="#icon--no-image" /></svg>
-  <svg class="icon--play--48"><use xlink:href="#icon--play--48" /></svg>
-  <svg class="icon--play--64"><use xlink:href="#icon--play--64" /></svg>
-  <svg class="icon--play--96"><use xlink:href="#icon--play--96" /></svg>
-  <svg class="icon--plus--24"><use xlink:href="#icon--plus--24" /></svg>
-  <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
-  <svg class="icon--print--24"><use xlink:href="#icon--print--24" /></svg>
-  <svg class="icon--quote--48"><use xlink:href="#icon--quote--48" /></svg>
-  <svg class="icon--quote--68"><use xlink:href="#icon--quote--68" /></svg>
-  <svg class="icon--quote--76"><use xlink:href="#icon--quote--76" /></svg>
-  <svg class="icon--return"><use xlink:href="#icon--return" /></svg>
-  <svg class="icon--search--24"><use xlink:href="#icon--search--24" /></svg>
-  <svg class="icon--share--24"><use xlink:href="#icon--share--24" /></svg>
-  <svg class="icon--user"><use xlink:href="#icon--user" /></svg>
-  <svg class="icon--zoom--24"><use xlink:href="#icon--zoom--24" /></svg>
-  <svg class="icon--zoom-in--24"><use xlink:href="#icon--zoom-in--24" /></svg>
-  <svg class="icon--zoom-out--24"><use xlink:href="#icon--zoom-out--24" /></svg>
+  <svg class="icon--alert">
+    <use xlink:href="#icon--alert"></use>
+  </svg>
+  <svg class="icon--arrow">
+    <use xlink:href="#icon--arrow"></use>
+  </svg>
+  <svg class="icon--arrow--24">
+    <use xlink:href="#icon--arrow--24"></use>
+  </svg>
+  <svg class="icon--calendar">
+    <use xlink:href="#icon--calendar"></use>
+  </svg>
+  <svg class="icon--caret">
+    <use xlink:href="#icon--caret"></use>
+  </svg>
+  <svg class="icon--check">
+    <use xlink:href="#icon--check"></use>
+  </svg>
+  <svg class="icon--clock">
+    <use xlink:href="#icon--clock"></use>
+  </svg>
+  <svg class="icon--close">
+    <use xlink:href="#icon--close"></use>
+  </svg>
+  <svg class="icon--close--24">
+    <use xlink:href="#icon--close--24"></use>
+  </svg>
+  <svg class="icon--close-circle">
+    <use xlink:href="#icon--close-circle"></use>
+  </svg>
+  <svg class="icon--close-dark">
+    <use xlink:href="#icon--close-dark"></use>
+  </svg>
+  <svg class="icon--download--24">
+    <use xlink:href="#icon--download--24"></use>
+  </svg>
+  <svg class="icon--filter--24">
+    <use xlink:href="#icon--filter--24"></use>
+  </svg>
+  <svg class="icon--footer_map_120x92">
+    <use xlink:href="#icon--footer_map_120x92"></use>
+  </svg>
+  <svg class="icon--info">
+    <use xlink:href="#icon--info"></use>
+  </svg>
+  <svg class="icon--info--24">
+    <use xlink:href="#icon--info--24"></use>
+  </svg>
+  <svg class="icon--info--32">
+    <use xlink:href="#icon--info--32"></use>
+  </svg>
+  <svg class="icon--info-i">
+    <use xlink:href="#icon--info-i"></use>
+  </svg>
+  <svg class="icon--location">
+    <use xlink:href="#icon--location"></use>
+  </svg>
+  <svg class="icon--location--24">
+    <use xlink:href="#icon--location--24"></use>
+  </svg>
+  <svg class="icon--logo--80">
+    <use xlink:href="#icon--logo--80"></use>
+  </svg>
+  <svg class="icon--logo--88">
+    <use xlink:href="#icon--logo--88"></use>
+  </svg>
+  <svg class="icon--logo--92">
+    <use xlink:href="#icon--logo--92"></use>
+  </svg>
+  <svg class="icon--logo--outline--80">
+    <use xlink:href="#icon--logo--outline--80"></use>
+  </svg>
+  <svg class="icon--logo--outline--88">
+    <use xlink:href="#icon--logo--outline--88"></use>
+  </svg>
+  <svg class="icon--logo--outline--92">
+    <use xlink:href="#icon--logo--outline--92"></use>
+  </svg>
+  <svg class="icon--menu--24">
+    <use xlink:href="#icon--menu--24"></use>
+  </svg>
+  <svg class="icon--minus">
+    <use xlink:href="#icon--minus"></use>
+  </svg>
+  <svg class="icon--minus--24">
+    <use xlink:href="#icon--minus--24"></use>
+  </svg>
+  <svg class="icon--new-window">
+    <use xlink:href="#icon--new-window"></use>
+  </svg>
+  <svg class="icon--no-image">
+    <use xlink:href="#icon--no-image"></use>
+  </svg>
+  <svg class="icon--play--48">
+    <use xlink:href="#icon--play--48"></use>
+  </svg>
+  <svg class="icon--play--64">
+    <use xlink:href="#icon--play--64"></use>
+  </svg>
+  <svg class="icon--play--96">
+    <use xlink:href="#icon--play--96"></use>
+  </svg>
+  <svg class="icon--plus">
+    <use xlink:href="#icon--plus"></use>
+  </svg>
+  <svg class="icon--plus--24">
+    <use xlink:href="#icon--plus--24"></use>
+  </svg>
+  <svg class="icon--print--24">
+    <use xlink:href="#icon--print--24"></use>
+  </svg>
+  <svg class="icon--quote--48">
+    <use xlink:href="#icon--quote--48"></use>
+  </svg>
+  <svg class="icon--quote--68">
+    <use xlink:href="#icon--quote--68"></use>
+  </svg>
+  <svg class="icon--quote--76">
+    <use xlink:href="#icon--quote--76"></use>
+  </svg>
+  <svg class="icon--return">
+    <use xlink:href="#icon--return"></use>
+  </svg>
+  <svg class="icon--search--24">
+    <use xlink:href="#icon--search--24"></use>
+  </svg>
+  <svg class="icon--share--24">
+    <use xlink:href="#icon--share--24"></use>
+  </svg>
+  <svg class="icon--user">
+    <use xlink:href="#icon--user"></use>
+  </svg>
+  <svg class="icon--zoom--24">
+    <use xlink:href="#icon--zoom--24"></use>
+  </svg>
+  <svg class="icon--zoom-in--24">
+    <use xlink:href="#icon--zoom-in--24"></use>
+  </svg>
+  <svg class="icon--zoom-out--24">
+    <use xlink:href="#icon--zoom-out--24"></use>
+  </svg>
 </div>
 
 @endsection
