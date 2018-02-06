293a14bbfa175abeb20764a225bd1ef6e51bda5d
Pablo Barrios <ingpablobarrios@gmail.com>
2018-02-05 21:42:25 -0300

    Added Event Listener for the callback method of the SSO

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -27,6 +27,18 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (Aacotroneo\Saml2\Events\Saml2LoginEvent $event) {
+            $messageId = $event->getSaml2Auth()->getLastMessageId();
+            // your own code preventing reuse of a $messageId to stop replay attacks
+            $user = $event->getSaml2User();
+            $userData = [
+                'id' => $user->getUserId(),
+                'attributes' => $user->getAttributes(),
+                'assertion' => $user->getRawSamlAssertion()
+            ];
+             $laravelUser = {};//find user by ID or attribute
+             //if it does not exist create it and go on  or show an error message
+             Auth::login($laravelUser);
+        });
     }
 }
