20c89ea5065ba8e30f8c9c95a798f3a560c72804
Mike Byrne <mike@thirteentwelve.com>
2018-02-12 16:17:13 +0000

    Inline aside uses hr component not html for hr

--- resources/views/components/molecules/_m-inline-aside.blade.php
+++ resources/views/components/molecules/_m-inline-aside.blade.php
@@ -1,6 +1,7 @@
 <aside class="m-inline-aside{{ (isset($variation)) ? ' '.$variation : '' }}">
-    <span class="hr"></span>
     @if (isset($title))
+        @component('components.atoms._hr')
+        @endcomponent
         @component('components.blocks._text')
             @slot('font', ($titleFont ?? 'f-module-title-1'))
             @slot('tag', ($titleTag ?? 'h4'))
