1bb1c94bca846242080f5d3b69c28ec8f5d946f8
Mike Byrne <mike@thirteentwelve.com>
2018-02-19 10:42:37 +0000

    404 page added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="o-application-error">
+    <h2 class="f-headline">Looking for something?</h2>
+    <p class="f-body">Sorry, whatever you are looking for is no longer here or you typed in the wrong URL. Please double-check and try again.</p>
+    <p class="f-body"><a href="/">&lsaquo; Back to Homepage</a></p>
+</div>
+
+@endsection
