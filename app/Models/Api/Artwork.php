ce19b9a603f0ecc1857ec0996c8200e29423d4eb
Mubashar Iqbal <me@mubs.me>
2018-02-20 12:45:16 -0500

    refactor where details are generated

--- app/Models/Api/Artwork.php
+++ app/Models/Api/Artwork.php
@@ -21,4 +21,36 @@ class Artwork extends BaseApiModel
     {
         return route('artworks.show', $this->id);
     }
+
+    public function getArtworkDetailsBlock()
+    {
+
+        $details = [];
+        $details[] = array('key' => 'Artist', 'value' => 'The Artist Name');
+        if (!empty($this->date_display)) {
+            $details[] = array('key' => 'Date', 'value' => $this->date_display);
+        }
+        if (!empty($this->medium)) {
+            $details[] = array('key' => 'Medium', 'value' => $this->medium);
+        }
+        if (!empty($this->dimensions)) {
+            $details[] = array('key' => 'Dimensions', 'value' => $this->dimensions);
+        }
+        if (!empty($this->credit_line)) {
+            $details[] = array('key' => 'Credit line', 'value' => $this->credit_line);
+        }
+        if (!empty($this->main_reference_number)) {
+            $details[] = array('key' => 'Reference Number', 'value' => $this->main_reference_number);
+        }
+        if (!empty($this->copyright_notice)) {
+            $details[] = array('key' => 'Copyright', 'value' => $this->copyright_notice);
+        }
+
+        $block = array(
+          "type" => 'deflist',
+          "items" =>$details
+        );
+
+        return $block;
+    }
 }
