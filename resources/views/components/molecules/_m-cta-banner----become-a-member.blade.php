9eede346a41074296a3ca0d6af34f51bdc22ca23
Tim Brook <tim@area17.com>
2018-02-21 16:35:23 -0600

    #440 - add overlay to membership cta

--- resources/views/components/molecules/_m-cta-banner----become-a-member.blade.php
+++ resources/views/components/molecules/_m-cta-banner----become-a-member.blade.php
@@ -1,10 +1,12 @@
 <aside class="m-cta-banner m-cta-banner--parallax{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="bannerParallax">
-    <div class="m-cta-banner__img" data-parallax-img>
-        <img src="http://placeimg.com/2000/240/nature/grayscale">
-    </div>
-    <div class="m-cta-banner__txt">
-        <p class="m-cta-banner__title f-module-title-2">Become a Member</p>
-        <p class="m-cta-banner__msg f-list-2">Join today and get <em>exclusive access</em> to the Art Institute of Chicago.</p>
-        <p class="m-cta-banner__action"><a href="#" class="btn f-buttons btn--contrast">Learn More</a></p>
-    </div>
+    <a href="#">
+        <div class="m-cta-banner__img" data-parallax-img>
+            <img src="http://placeimg.com/2000/240/nature/grayscale">
+        </div>
+        <div class="m-cta-banner__txt">
+            <p class="m-cta-banner__title f-module-title-2">Become a Member</p>
+            <p class="m-cta-banner__msg f-list-2">Join today and get <em>exclusive access</em> to the Art Institute of Chicago.</p>
+            <p class="m-cta-banner__action"><span class="btn f-buttons btn--contrast">Learn More</span></p>
+        </div>
+    </a>
 </aside>
