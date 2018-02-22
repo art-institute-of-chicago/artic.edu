be22d8e2bb2feb84bab180239178c1313bc4cf39
Mike Byrne <mike@thirteentwelve.com>
2018-02-22 15:16:36 +0000

    home page responsive image settings added

--- resources/views/components/molecules/_m-cta-banner----become-a-member.blade.php
+++ resources/views/components/molecules/_m-cta-banner----become-a-member.blade.php
@@ -1,7 +1,26 @@
 <aside class="m-cta-banner m-cta-banner--parallax{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="bannerParallax">
     <a href="#">
         <div class="m-cta-banner__img" data-parallax-img>
-            <img src="http://placeimg.com/2000/240/nature/grayscale">
+            @component('components.atoms._img')
+                @slot('image', array(
+                    "sourceType" => 'imgix',
+                    "src" => 'https://wyss-prod.imgix.net/app/uploads/2017/11/29103410/Falkor-IMG_7110.jpg?',
+                    "width" => 4000,
+                    "height" => 3000,
+                ))
+                @slot('settings', array(
+                    'fit' => 'crop',
+                    'ratio' => '25:3',
+                    'srcset' => array(300,600,1000,1500,2000),
+                    'sizes' => aic_imageSizes(array(
+                          'xsmall' => '58',
+                          'small' => '58',
+                          'medium' => '58',
+                          'large' => '58',
+                          'xlarge' => '58',
+                    )),
+                ))
+            @endcomponent
         </div>
         <div class="m-cta-banner__txt">
             <p class="m-cta-banner__title f-module-title-2">Become a Member</p>
