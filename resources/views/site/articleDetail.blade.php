70fbff86f330eb5252c14dadbd44df4e25b8eb5a
Mike Byrne <mike@thirteentwelve.com>
2018-02-22 15:34:07 +0000

    Events page and day listings responsive sizes added

--- resources/views/site/articleDetail.blade.php
+++ resources/views/site/articleDetail.blade.php
@@ -321,6 +321,30 @@
         @foreach ($item->relatedEventsByDay as $date)
             @component('components.molecules._m-date-listing')
                 @slot('date', $date)
+                @slot('imageSettings', array(
+                    'fit' => 'crop',
+                    'ratio' => '16:9',
+                    'srcset' => array(200,400,600),
+                    'sizes' => aic_imageSizes(array(
+                          'xsmall' => '58',
+                          'small' => '13',
+                          'medium' => '13',
+                          'large' => '13',
+                          'xlarge' => '13',
+                    )),
+                ))
+                @slot('imageSettingsOnGoing', array(
+                    'fit' => 'crop',
+                    'ratio' => '16:9',
+                    'srcset' => array(200,400,600),
+                    'sizes' => aic_imageSizes(array(
+                          'xsmall' => '58',
+                          'small' => '7',
+                          'medium' => '7',
+                          'large' => '7',
+                          'xlarge' => '7',
+                    )),
+                ))
             @endcomponent
         @endforeach
     @endcomponent
