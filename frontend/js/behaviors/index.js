008b753e8ccb58d4f5823d87b5e90db2fe96c693
Mike Byrne <mike@thirteentwelve.com>
2018-02-23 10:57:32 +0000

    commented out ref to stickyHeader js

--- frontend/js/behaviors/index.js
+++ frontend/js/behaviors/index.js
@@ -37,4 +37,4 @@ export { default as newsletter } from './newsletter';
 export { default as openImageFullScreen } from './openImageFullScreen';
 export { default as blurMyBackground } from './blurMyBackground';
 export { default as loadMore } from './loadMore';
-export { default as stickyHeader } from './stickyHeader';
+//export { default as stickyHeader } from './stickyHeader';
