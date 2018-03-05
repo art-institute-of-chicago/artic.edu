c0beb8f8e33ea5e6d4d259c59faa2e3021db7034
Quentin Renard <quentin@area17.com>
2018-03-05 06:25:48 +0100

    Unify CMS page missing error message

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -8,6 +8,8 @@ use Session;
 
 class PageController extends ModuleController
 {
+    const MISSING_CMS_PAGE_MESSAGE = "CMS home page doesn't exist, make sure to migrate the database and seed it first (php artisan migrate & php artisan db:seed)";
+
     protected $moduleName = 'pages';
     protected $formWith = ['slugs'];
     protected $formWithCount = [];
@@ -19,42 +21,42 @@ class PageController extends ModuleController
 
     public function home(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Home'), 500, "CMS home page doesn't exist, make sure to migrate the database first (php artisan migrate)");
+        abort_unless($page = $pages->byName('Home'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.home'));
         return view('admin.pages.form', $this->form($page->id));
     }
 
     public function exhibitions(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Exhibitions and Events'), 500, "CMS home page doesn't exist, make sure to migrate the database first (php artisan migrate)");
+        abort_unless($page = $pages->byName('Exhibitions and Events'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.exhibitions'));
         return view('admin.pages.form', $this->form($page->id));
     }
 
     public function art(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Art and Ideas'), 500, "CMS home page doesn't exist, make sure to migrate the database first (php artisan migrate)");
+        abort_unless($page = $pages->byName('Art and Ideas'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.art'));
         return view('admin.pages.form', $this->form($page->id));
     }
 
     public function visit(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Visit'), 500, "CMS home page doesn't exist, make sure to migrate the database first (php artisan migrate)");
+        abort_unless($page = $pages->byName('Visit'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.visit'));
         return view('admin.pages.form', $this->form($page->id));
     }
 
     public function articles(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Articles'), 500, "CMS home page doesn't exist, make sure to migrate the database and seed it first (php artisan migrate & php artisan db:seed)");
+        abort_unless($page = $pages->byName('Articles'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.articles'));
         return view('admin.pages.form', $this->form($page->id));
     }
 
     public function exhibitionHistory(PageRepository $pages)
     {
-        abort_unless($page = $pages->byName('Exhibition History'), 500, "CMS home page doesn't exist, make sure to migrate the database first (php artisan migrate)");
+        abort_unless($page = $pages->byName('Exhibition History'), 500, self::MISSING_CMS_PAGE_MESSAGE);
         Session::put("pages_back_link", route('admin.landing.exhibition_history'));
         return view('admin.pages.form', $this->form($page->id));
     }
