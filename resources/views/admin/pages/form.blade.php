f49151d4e742dac3ea47dc98a97ef25b95366796
Quentin Renard <quentin@area17.com>
2018-03-06 03:41:35 +0100

    Add sticky nav to visit form

--- resources/views/admin/pages/form.blade.php
+++ resources/views/admin/pages/form.blade.php
@@ -1,3 +1,15 @@
-@extends('cms-toolkit::layouts.form')
+@php
+    $pageType = App\Models\Page::$types[$item->type];
+@endphp
 
-@include('admin.pages.form_' . snake_case(App\Models\Page::$types[$item->type]))
+@extends('cms-toolkit::layouts.form', [
+    'additionalFieldsets' =>
+    $pageType == 'Visit' ? [
+        ['fieldset' => 'locations', 'label' => 'Locations'],
+        ['fieldset' => 'admissions', 'label' => 'Admissions'],
+        ['fieldset' => 'featured_hours', 'label' => 'Featured hours'],
+        ['fieldset' => 'dinning_hours', 'label' => 'Dinning hours'],
+    ] : []
+])
+
+@include('admin.pages.form_' . snake_case($pageType))
