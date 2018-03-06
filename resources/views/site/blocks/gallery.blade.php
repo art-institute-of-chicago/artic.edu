1ba69dd44696dca2ac368f152e46e7a984ccb912
Quentin Renard <quentin@area17.com>
2018-03-06 04:28:27 +0100

    Fix gallery block view (wip)

--- resources/views/site/blocks/gallery.blade.php
+++ resources/views/site/blocks/gallery.blade.php
@@ -1,11 +1,11 @@
 @php
-    $subtype = $block->input('layout') == 1 ? 'mosaic' : 'slider'
+    $subtype = $block->input('layout') == 1 ? 'mosaic' : 'slider';
     $image = $block->imageAsArray('image', 'desktop');
 @endphp
 
 @component('components.organisms._o-gallery----'.$subtype)
     @slot('variation', 'o-blocks__block')
-    @slot('title', $block->input('label'));
-    @slot('caption', $block->input('subhead'));
-    @slot('items', $image;
+    @slot('title', $block->input('label'))
+    @slot('caption', $block->input('subhead'))
+    @slot('items', $image)
 @endcomponent
