e6935c53e28d632fedc4792ea5fe5752e4417f69
Mubashar Iqbal <me@mubs.me>
2018-03-07 14:53:38 -0500

    embed code for youtube/vimeo

--- app/Libraries/EmbedConverterService.php
+++ app/Libraries/EmbedConverterService.php
@@ -12,6 +12,58 @@ class EmbedConverterService
 
     public function convertUrl($url)
     {
-        return " THIS IS THE EMBED HTML";
+        if (!empty($url)) {
+            $parts = parse_url($url);
+            if (isset($parts['host'])) {
+                if (stripos($parts['host'], 'youtube') !== false) {
+                    return $this->getYouTubeEmbedCode($url);
+                } elseif (stripos($parts['host'], 'vimeo') !== false) {
+                    return $this->getVimeoEmbedCode($url);
+                } elseif (stripos($parts['host'], 'soundcloud') !== false) {
+                    return $this->getSoundCloudEmbedCode($url);
+                }
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * getYouTubeEmbedCode description
+     * @param  string $url YouTube video url, eg: https://www.youtube.com/watch?v=SZFVhIji7sY
+     * @return [type]      [description]
+     */
+    private function getYouTubeEmbedCode($url)
+    {
+        preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
+        $videoId = $match[1];
+        return '<iframe src="https://www.youtube.com/embed/'.$videoId.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    }
+
+    /**
+     * [getSoundCloudEmbedCode description]
+     * @param  [type] $url [description]
+     * @return [type]      [description]
+     */
+    private function getSoundCloudEmbedCode($url)
+    {
+        return $url;
+    }
+
+    /**
+     * [getVimeoEmbedCode description]
+     * @param  [type] $url [description] eg: https://vimeo.com/182466118
+     * @return [type]      [description]
+     */
+    private function getVimeoEmbedCode($url)
+    {
+
+        if (preg_match('~^http[s]://(?:www\.)?vimeo\.com/(?:clip:)?(\d+)~', $url, $match)) {
+            $videoId = $match[1];
+        } else {
+            $videoId = substr($url,10,strlen($url));
+        }
+
+        return '<iframe src="https://player.vimeo.com/video/'.$videoId.'?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
     }
 }
--- resources/views/site/blocks/media_embed.blade.php
+++ resources/views/site/blocks/media_embed.blade.php
@@ -1,27 +1,29 @@
 @php
+    $source_url = $block->input('source_url');
     $image = $block->imageAsArray('image', 'desktop');
-    $embed_code = EmbedConverter::convertUrl($block->input('source_url'))
+    $embed_code = EmbedConverter::convertUrl($source_url);
 @endphp
 
-@component('components.molecules._m-media')
-    @slot('variation', 'o-blocks__block')
-    @slot('item', [
-        'type' => 'embed',
-        'size' => 'm',
-        'media' => [
-            "embed" => $embed_code,
-            "src" => $image['src'],
-            "srcset" => $image['src'],
-            "width" => $image['width'],
-            "height" => $image['height'],
-            "shareUrl" => '#',
-            "shareTitle" => '',
-            "downloadUrl" => $block->input('source_url'),
-            "downloadName" => '',
-            "credit" => '',
-            "creditUrl" => '',
-        ],
-        'caption' => ''
-    ])
-@endcomponent
-
+@if ($source_url)
+    @component('components.molecules._m-media')
+        @slot('variation', 'o-blocks__block')
+        @slot('item', [
+            'type' => 'embed',
+            'size' => 'm',
+            'media' => [
+                "embed" => $embed_code,
+                "src" => (isset($image['src'])) ? $image['src'] : '',
+                "srcset" => (isset($image['src'])) ? $image['src'] : '',
+                "width" => (isset($image['width'])) ? $image['width'] : '',
+                "height" => (isset($image['height'])) ? $image['height'] : '',
+                "shareUrl" => '#',
+                "shareTitle" => '',
+                "downloadUrl" => $source_url,
+                "downloadName" => '',
+                "credit" => '',
+                "creditUrl" => '',
+            ],
+            'caption' => ''
+        ])
+    @endcomponent
+@endif
