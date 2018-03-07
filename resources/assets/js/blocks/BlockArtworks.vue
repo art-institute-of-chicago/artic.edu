fd933aebadaa2a015c06362cebd5cb130bb87ad4
Mubashar Iqbal <me@mubs.me>
2018-03-07 09:35:28 -0500

    Add additional artwork gallery block fields

--- resources/assets/js/blocks/BlockArtworks.vue
+++ resources/assets/js/blocks/BlockArtworks.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="block__body">
-        <a17-inputframe label="Artworks" name="browsers.artworks"> <a17-browserfield :name="fieldName('artworks')" item-label="artworks" :max="100" endpoint="/whatson/artworks/browser" modal-title="Attach artworks" >Add up to 100 artworks</a17-browserfield></a17-inputframe>
+        <a17-inputframe label="Artworks" name="browsers.artworks"> <a17-browserfield :name="fieldName('artworks')" item-label="artworks" :max="100" endpoint="/whatson/artworks/browser" modal-title="Attach artworks" >Add up to 100 artworks</a17-browserfield></a17-inputframe><a17-vselect label="Layout" :name="fieldName('layout')" :options='[{"value":1,"label":"Layout 1 (Mosaic)"},{"value":2,"label":"Layout 2 (Carousel)"}]' placeholder="Select layout" :has-default-store="true" size="large" in-store="inputValue" ></a17-vselect><a17-textfield label="Description" :name="fieldName('description')" type="textarea" :rows="4" in-store="value" ></a17-textfield><a17-textfield label="Image Subhead" :name="fieldName('subhead')" type="text" :maxlength="150" in-store="value" ></a17-textfield><a17-textfield label="Image Label" :name="fieldName('label')" type="text" :maxlength="150" in-store="value" ></a17-textfield>
     </div>
 </template>
 
