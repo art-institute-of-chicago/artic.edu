e794a547fe068181b963a0d470049938f1549f2e
Mike Byrne <mike@thirteentwelve.com>
2018-03-12 20:17:09 +0000

    Updating multilink listings to have image and title click areas

--- resources/views/components/molecules/_m-listing----multi-links.blade.php
+++ resources/views/components/molecules/_m-listing----multi-links.blade.php
@@ -1,32 +1,41 @@
 <{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}">
 
-    @if ( !empty( $item['image'] ) )
-        <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}">
-            @component('components.atoms._img')
-                @slot('image', $item['image'])
-                @slot('settings', $imageSettings ?? '')
-            @endcomponent
-        </span>
+    @if (isset($item['titleLink']) and $item['titleLink'])
+        <a href="{{ $item['titleLink'] }}" class="m-listing__link">
     @endif
 
-    <span class="m-listing__meta">
-        @if (isset($item['title']) and $item['title'])
-            @if (isset($item['titleLink']) and $item['titleLink'])
-                @component('components.atoms._arrow-link')
-                    @slot('variation', 'title')
-                    @slot('font', $titleFont ?? 'f-list-3')
-                    @slot('href', $item['titleLink'])
-                    {{ $item['title'] }}
+        @if ( !empty( $item['image'] ) )
+            <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}">
+                @component('components.atoms._img')
+                    @slot('image', $item['image'])
+                    @slot('settings', $imageSettings ?? '')
                 @endcomponent
-            @else
+            </span>
+        @endif
+
+    @if (isset($item['titleLink']) and $item['titleLink'])
+
+        @if (isset($item['title']) and $item['title'])
+            <span class="m-listing__meta">
                 @component('components.atoms._title')
                     @slot('font', $titleFont ?? 'f-list-3')
                     {{ $item['title'] }}
                 @endcomponent
-            @endif
+            </span>
+        @endif
+
+        </a>
+    @endif
+
+    <span class="m-listing__meta">
+        @if (empty($item['titleLink']))
+            @component('components.atoms._title')
+                @slot('font', $titleFont ?? 'f-list-3')
+                {{ $item['title'] }}
+            @endcomponent
+            <br>
         @endif
         @if (isset($item['text']) and $item['text'])
-            <br>
             @component('components.blocks._text')
                 @slot('font', 'f-secondary')
                 @slot('tag', 'span')
@@ -37,7 +46,7 @@
             <br>
             @foreach ($item['links'] as $link)
                 @component('components.atoms._arrow-link')
-                    @slot($link['href'], '#')
+                    @slot('href', $link['href'])
                     {{ $link['label'] }}
                 @endcomponent
             @endforeach
