3ba9d5bff555209309bd94d43371ad500cc7e79c
Quentin Renard <quentin@area17.com>
2018-03-14 08:15:32 +0100

    Cleanup HomeFeature CMS module

--- app/Http/Controllers/Admin/HomeFeatureController.php
+++ app/Http/Controllers/Admin/HomeFeatureController.php
@@ -8,38 +8,7 @@ class HomeFeatureController extends ModuleController
 {
     protected $moduleName = 'homeFeatures';
 
-    protected $indexColumns = [
-        'title' => [
-            'title' => 'Title',
-            'field' => 'title',
-            'sort' => true
-        ],
+    protected $indexOptions = [
+        'permalink' => false,
     ];
-
-    /*
-     * Relations to eager load for the index view
-     */
-    protected $indexWith = [];
-
-    /*
-     * Relations to eager load for the form view
-     */
-    protected $formWith = [];
-
-    /*
-     * Filters mapping ('fFilterName' => 'filterColumn')
-     * In the indexData function, name your lists with the filter name + List (fFilterNameList)
-     */
-    protected $filters = [];
-
-    protected function indexData($request)
-    {
-        return [];
-    }
-
-    protected function formData($request)
-    {
-        return [];
-    }
-
 }
