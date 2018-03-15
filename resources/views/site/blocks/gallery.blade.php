38e02282d8bd7d336e112d2c2d3019057c530524
Fernando Petrelli <fernando@area17.com>
2018-03-15 14:49:31 -0400

    Implement gallery block

--- resources/views/site/blocks/gallery.blade.php
+++ resources/views/site/blocks/gallery.blade.php
@@ -1,19 +1,19 @@
 @php
     $subtype = $block->input('layout') == 1 ? 'mosaic' : 'slider';
-    $images = $block->imagesAsArrays('image', 'desktop');
 
     $items = [];
-    foreach($images as $image) {
-        $item = [];
-        $item['type'] = 'image';
-        $item['media'] = $image;
+    foreach ($block->childs as $item) {
+        $item->type = 'image';
+        $item->media = $item->imageAsArray('image', 'desktop');
+        $item->captionTitle = $item->input('captionTitle');
+        $item->caption = $item->input('caption');
         $items[] = $item;
     }
 @endphp
 
 @component('components.organisms._o-gallery----'.$subtype)
     @slot('variation', 'o-blocks__block')
-    @slot('title', $block->input('label'))
-    @slot('caption', $block->input('subhead'))
+    @slot('title', $block->input('title'))
+    @slot('caption', $block->input('description'))
     @slot('items', $items)
 @endcomponent
