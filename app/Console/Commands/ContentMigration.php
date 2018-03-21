df6e18db67c4b43e5e33983b65efc2a34c185b7f
Area 17 <area17@MacBook-Pro-4.local>
2018-03-21 17:10:58 -0400

    content migration script work

---
+++ app/Console/Commands/ContentMigration.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ContentMigration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:content {type="article"}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to migrate content from the old system to the new';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $type = $this->argument('type');
+
+       
+
+    }
+}
