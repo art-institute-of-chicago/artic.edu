85081579c9af163e5a0383d7bb232a86177eaf58
Quentin Renard <quentin@area17.com>
2018-03-22 15:07:16 +0100

    Improve home features form

--- resources/views/admin/homeFeatures/form.blade.php
+++ resources/views/admin/homeFeatures/form.blade.php
@@ -6,28 +6,66 @@
         'name' => 'hero'
     ])
 
-    @formField('browser', [
-        'routePrefix' => 'collection.articles_publications',
-        'moduleName' => 'articles',
-        'name' => 'articles',
-        'label' => 'Article'
+    @formField('radios', [
+        'name' => '_featureType',
+        'label' => 'Feature type',
+        'default' => 'articles',
+        'inline' => true,
+        'options' => [
+            [
+                'value' => 'articles',
+                'label' => 'Article'
+            ],
+            [
+                'value' => 'events',
+                'label' => 'Events'
+            ],
+            [
+                'value' => 'exhibitions',
+                'label' => 'Exhibitions'
+            ],
+        ]
     ])
 
-    @formField('browser', [
-        'routePrefix' => 'exhibitions_events',
-        'moduleName' => 'events',
-        'name' => 'events',
-        'label' => 'Events'
+
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'articles'
     ])
+        @formField('browser', [
+            'routePrefix' => 'collection.articles_publications',
+            'moduleName' => 'articles',
+            'name' => 'articles',
+            'label' => 'Article'
+        ])
+    @endcomponent
 
-    @formField('browser', [
-        'routePrefix' => 'exhibitions_events',
-        'moduleName' => 'exhibitions',
-        'name' => 'exhibitions',
-        'label' => 'Exhibitions'
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'events'
     ])
+        @formField('browser', [
+            'routePrefix' => 'exhibitions_events',
+            'moduleName' => 'events',
+            'name' => 'events',
+            'label' => 'Event'
+        ])
+    @endcomponent
 
-
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'exhibitions'
+    ])
+        @formField('browser', [
+            'routePrefix' => 'exhibitions_events',
+            'moduleName' => 'exhibitions',
+            'name' => 'exhibitions',
+            'label' => 'Exhibition'
+        ])
+    @endcomponent
 {{--
     #2 Removed for now?
      @formField('browser', [
