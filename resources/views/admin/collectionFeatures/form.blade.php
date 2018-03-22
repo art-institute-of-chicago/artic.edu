6635e71da3a39e22012f4f2af82eee703292e7c9
Quentin Renard <quentin@area17.com>
2018-03-22 16:05:27 +0100

    Improve collection features

--- resources/views/admin/collectionFeatures/form.blade.php
+++ resources/views/admin/collectionFeatures/form.blade.php
@@ -1,24 +1,72 @@
 @extends('cms-toolkit::layouts.form')
 
 @section('contentFields')
-    @formField('browser', [
-        'routePrefix' => 'collection.articles_publications',
-        'moduleName' => 'articles',
-        'name' => 'articles',
-        'label' => 'Article'
+    @php
+        $selectedFeature = 'articles';
+        foreach (['articles', 'artworks', 'selections'] as $featureType) {
+            if (isset($form_fields['browsers'][$featureType]) && !empty($form_fields['browsers'][$featureType])) {
+                $selectedFeature = $featureType;
+            }
+        }
+    @endphp
+
+    @formField('radios', [
+        'name' => '_featureType',
+        'label' => 'Feature type',
+        'default' => $selectedFeature,
+        'inline' => true,
+        'options' => [
+            [
+                'value' => 'articles',
+                'label' => 'Article'
+            ],
+            [
+                'value' => 'artworks',
+                'label' => 'Artwork'
+            ],
+            [
+                'value' => 'selections',
+                'label' => 'Selection'
+            ],
+        ]
     ])
 
-    @formField('browser', [
-        'routePrefix' => 'collection',
-        'moduleName' => 'artworks',
-        'name' => 'artworks',
-        'label' => 'Artworks'
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'articles'
     ])
+        @formField('browser', [
+            'routePrefix' => 'collection.articles_publications',
+            'moduleName' => 'articles',
+            'name' => 'articles',
+            'label' => 'Article'
+        ])
+    @endcomponent
 
-    @formField('browser', [
-        'routePrefix' => 'collection',
-        'moduleName' => 'selections',
-        'name' => 'selections',
-        'label' => 'Selections'
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'artworks'
     ])
+        @formField('browser', [
+            'routePrefix' => 'collection',
+            'moduleName' => 'artworks',
+            'name' => 'artworks',
+            'label' => 'Artworks'
+        ])
+    @endcomponent
+
+    @component('cms-toolkit::partials.form.utils._connected_fields', [
+        'fieldName' => '_featureType',
+        'renderForBlocks' => false,
+        'fieldValues' => 'selections'
+    ])
+        @formField('browser', [
+            'routePrefix' => 'collection',
+            'moduleName' => 'selections',
+            'name' => 'selections',
+            'label' => 'Selection'
+        ])
+    @endcomponent
 @stop
