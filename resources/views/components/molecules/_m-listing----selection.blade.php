d79dd566c440e4560be003b0d842492894d764c5
Fernando Petrelli <fernando@area17.com>
2018-03-22 14:21:01 -0400

    Fix selections detail page link

--- resources/views/components/molecules/_m-listing----selection.blade.php
+++ resources/views/components/molecules/_m-listing----selection.blade.php
@@ -1,5 +1,5 @@
 <{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}">
-    <a href="{{ $item->slug }}" class="m-listing__link">
+    <a href="{!! route('selections.show', $item) !!}" class="m-listing__link">
         <span class="m-listing__img m-listing__img--no-bg{{ (isset($imgVariation)) ? ' '.$imgVariation : ' m-listing__img--square' }}">
             @component('components.molecules._m-image-stack')
                 @slot('images', $item->images)
