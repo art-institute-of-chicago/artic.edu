c9a385c11797ed8fb217e60052b3c31e8c0be7b2
Fernando Petrelli <fernando@area17.com>
2018-03-22 12:53:47 -0400

    Fix galleries datahub_type

---
+++ database/migrations/2018_03_22_124637_change_dataahub_id_on_gallery.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDataahubIdOnGallery extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('gallery_slugs');
+        Schema::dropIfExists('galleries');
+
+        Schema::create('galleries', function (Blueprint $table) {
+            createDefaultTableFields($table, true, false);
+
+            $table->text('intro')->nullable();
+            $table->string('datahub_id');
+        });
+
+        Schema::create('gallery_slugs', function (Blueprint $table) {
+            createDefaultSlugsTableFields($table, 'gallery');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
