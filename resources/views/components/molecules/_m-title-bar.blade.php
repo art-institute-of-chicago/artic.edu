fc683386a0b3521d249c631e3f426c237eeabfb2
Mike Byrne <mike@thirteentwelve.com>
2018-04-12 14:48:03 +0100

    ticket #466 - updated some hover states

--- resources/views/components/molecules/_m-title-bar.blade.php
+++ resources/views/components/molecules/_m-title-bar.blade.php
@@ -5,7 +5,7 @@
     @foreach ($links as $link)
     <li>
         @if (isset($link['href']))
-        <a href="{{ $link['href'] }}" class="f-buttons">
+        <a href="{{ $link['href'] }}" class="f-link">
             {!! $link['label'] !!}&nbsp;&nbsp;&rsaquo;
         </a>
         @else
