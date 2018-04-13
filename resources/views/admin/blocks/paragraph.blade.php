f101e75cd4c95d1148c2730cc9995f91b938c37a
Mubashar Iqbal <me@mubs.me>
2018-04-13 10:35:31 -0400

    #712 cms paragraph styling

--- resources/views/admin/blocks/paragraph.blade.php
+++ resources/views/admin/blocks/paragraph.blade.php
@@ -1,5 +1,5 @@
 @formField('wysiwyg', [
     'name' => 'paragraph',
     'label' => 'Paragraph',
-    'toolbarOptions' => [ 'h2', 'bold', 'italic', 'underline', 'link', 'list-unordered' ],
+    'toolbarOptions' => [ 'h2', 'bold', 'italic', 'underline', 'link', 'list-ordered', 'list-unordered' ],
 ])
