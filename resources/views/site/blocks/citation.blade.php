9b16adf33e998cc1577f5e89205365ee23c95d32
Mubashar Iqbal <me@mubs.me>
2018-04-13 10:05:29 -0400

    #597 fix citations

--- resources/views/site/blocks/citation.blade.php
+++ resources/views/site/blocks/citation.blade.php
@@ -1,3 +1,11 @@
-@component('components.blocks._text', ['tag' => 'div'])
+@component('components.atoms._hr')
+@endcomponent
+@component('components.blocks._text')
+    @slot('font', 'f-subheading-1')
+    @slot('tag', 'h4')
+    Citation
+@endcomponent
+@component('components.blocks._text')
+    @slot('font', 'f-secondary')
     {!! $block->input('citation') !!}
 @endcomponent
