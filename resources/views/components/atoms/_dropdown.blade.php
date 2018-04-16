dccb69cfe51998d52ad9f0303c32b889a53a951f
Mike Byrne <mike@thirteentwelve.com>
2018-04-16 22:07:32 +0100

    ticket #804 - dropdown with active state fix

--- resources/views/components/atoms/_dropdown.blade.php
+++ resources/views/components/atoms/_dropdown.blade.php
@@ -2,7 +2,7 @@
   <button class="dropdown__trigger {{ $font ?? "f-secondary" }}">{{ $prompt ?? 'Select' }}<svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></button>
   <ul class="dropdown__list {{ $font ?? "f-secondary" }}" data-dropdown-list>
     @foreach ($options as $option)
-      <li{!! (isset($option['active'])) ? ' class="s-active"' : '' !!}><a href="{{ $option['href'] ?? '#' }}">{!! $option['label'] ?? '' !!}</a></li>
+      <li{!! (isset($option['active']) and $option['active']) ? ' class="s-active"' : '' !!}><a href="{{ $option['href'] ?? '#' }}">{!! $option['label'] ?? '' !!}</a></li>
     @endforeach
   </ul>
 </span>
