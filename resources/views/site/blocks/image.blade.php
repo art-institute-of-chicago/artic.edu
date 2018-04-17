f20707ff108de2300e55c2fef895c96bd61720da
Mubashar Iqbal <me@mubs.me>
2018-04-17 14:59:30 -0400

    add some checks in image blade

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -2,25 +2,26 @@
     $image = $block->imageAsArray('image', 'desktop');
 @endphp
 
-@component('components.molecules._m-media')
-    @slot('variation', 'o-blocks__block')
-    @slot('item', [
-        'type' => 'image',
-        'size' => $block->input('size'),
-        'caption' => $block->input('caption'),
-        'captionTitle' => $block->input('caption_title'),
-        'media' => [
-            "src" => $image['src'],
-            "srcset" => $image['src'],
-            "width" => $image['width'],
-            "height" => $image['height'],
-            "shareUrl" => '#',
-            "shareTitle" => $block->input('caption'),
-            "downloadUrl" => $image['src'],
-            "downloadName" => '',
-            "credit" => '',
-            "creditUrl" => '',
-        ],
-    ])
-@endcomponent
-
+@if (isset($image['src']))
+    @component('components.molecules._m-media')
+        @slot('variation', 'o-blocks__block')
+        @slot('item', [
+            'type' => 'image',
+            'size' => $block->input('size'),
+            'caption' => $block->input('caption'),
+            'captionTitle' => $block->input('caption_title'),
+            'media' => [
+                "src" => $image['src'],
+                "srcset" => $image['src'],
+                "width" => $image['width'] ?? 0,
+                "height" => $image['height'] ?? 0,
+                "shareUrl" => '#',
+                "shareTitle" => $block->input('caption'),
+                "downloadUrl" => $image['src'],
+                "downloadName" => '',
+                "credit" => '',
+                "creditUrl" => '',
+            ],
+        ])
+    @endcomponent
+@endif
