712b5be6916528e5b87ab31517def4fb516217d0
Fernando Petrelli <fernando@area17.com>
2018-04-30 10:49:35 -0400

    Ticket #850: Remove hardcoded URL's and use routes

--- resources/views/site/collection/index.blade.php
+++ resources/views/site/collection/index.blade.php
@@ -194,7 +194,7 @@
     </div>
 @endif
 
-<div class="o-injected-container" data-behavior="injectContent" data-injectContent-url="/artworks/recentlyViewed" data-user-artwork-history></div>
+<div class="o-injected-container" data-behavior="injectContent" data-injectContent-url="{!! route('artworks.recentlyViewed') !!}" data-user-artwork-history></div>
 
 </section>
 
