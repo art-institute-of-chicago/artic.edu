51506bd7f4bd0ce3138dd391f6c9b09012097f4c
Mike Byrne <mike@thirteentwelve.com>
2018-05-02 18:44:40 +0100

    ticket #889 - explore further modules 4 cols large+

--- resources/views/site/articleDetail.blade.php
+++ resources/views/site/articleDetail.blade.php
@@ -538,8 +538,8 @@
     @component('components.organisms._o-pinboard')
         @slot('cols_small','2')
         @slot('cols_medium','3')
-        @slot('cols_large','3')
-        @slot('cols_xlarge','3')
+        @slot('cols_large','4')
+        @slot('cols_xlarge','4')
         @slot('maintainOrder','false')
         @foreach ($item->exploreFuther['items'] as $item)
             @component('components.molecules._m-listing----'.$item->type)
@@ -553,8 +553,8 @@
                           'xsmall' => '1',
                           'small' => '2',
                           'medium' => '3',
-                          'large' => '3',
-                          'xlarge' => '3',
+                          'large' => '4',
+                          'xlarge' => '4',
                     )),
                 ))
             @endcomponent
--- resources/views/site/artworkDetail.blade.php
+++ resources/views/site/artworkDetail.blade.php
@@ -144,8 +144,8 @@
     @component('components.organisms._o-pinboard')
         @slot('cols_small','2')
         @slot('cols_medium','3')
-        @slot('cols_large','3')
-        @slot('cols_xlarge','3')
+        @slot('cols_large','4')
+        @slot('cols_xlarge','4')
         @slot('maintainOrder','false')
         @foreach ($exploreFurther as $item)
             @component('components.molecules._m-listing----'.$item->type)
@@ -159,8 +159,8 @@
                           'xsmall' => '1',
                           'small' => '2',
                           'medium' => '3',
-                          'large' => '3',
-                          'xlarge' => '3',
+                          'large' => '4',
+                          'xlarge' => '4',
                     )),
                 ))
             @endcomponent
