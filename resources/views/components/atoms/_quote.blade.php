c35f812ea30a1b64754bced4d4ca7eaf7b1f3b43
Tim Brook <tim@area17.com>
2018-05-02 13:28:13 -0500

    #707 - item 7 - update quote icon

--- resources/views/components/atoms/_quote.blade.php
+++ resources/views/components/atoms/_quote.blade.php
@@ -1,4 +1,4 @@
 <blockquote class="quote{{ (isset($variation)) ? ' '.$variation : '' }}">
     <p class="{{ $font ?? 'f-quote' }}">{{ $slot }}</p>
-    <svg class="quote__icon icon--quote--76" aria-hidden="true"><use xlink:href="#icon--quote--76" /></svg>
+    <svg class="quote__icon icon--quote--68" aria-hidden="true"><use xlink:href="#icon--quote--68" /></svg>
 </blockquote>
