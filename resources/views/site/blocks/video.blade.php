94b680a439b7d3d0f3137bd8cc2df25d10e6ffe4
Mubashar Iqbal <mubashariqbal.com@gmail.com>
2018-05-03 12:09:21 -0400

    Integrate video block thumbnail

--- resources/views/site/blocks/video.blade.php
+++ resources/views/site/blocks/video.blade.php
@@ -9,23 +9,11 @@
 
 @if ($source_url)
     @component('components.molecules._m-media')
-        @slot('variation', 'o-blocks__block')
         @slot('item', [
             'type' => 'embed',
             'size' => $size,
-            'media' => [
-                "embed" => $embed_code,
-                "src" => (isset($image['src'])) ? $image['src'] : '',
-                "srcset" => (isset($image['src'])) ? $image['src'] : '',
-                "width" => (isset($image['width'])) ? $image['width'] : '',
-                "height" => (isset($image['height'])) ? $image['height'] : '',
-                "shareUrl" => '#',
-                "shareTitle" => '',
-                "downloadUrl" => $source_url,
-                "downloadName" => '',
-                "credit" => '',
-                "creditUrl" => '',
-            ],
+            'media' => ['embed' => $embed_code],
+            'poster' => $image,
             'captionTitle' => $captionTitle,
             'caption' => $caption
         ])
