0db00978b40ed4efa09f140ad73935fbe6819ae5
Mubashar Iqbal <mubashariqbal.com@gmail.com>
2018-05-04 16:35:43 -0400

    Adds redirect URL to Generic Pages

--- resources/views/components/molecules/_m-listing----generic.blade.php
+++ resources/views/components/molecules/_m-listing----generic.blade.php
@@ -1,5 +1,5 @@
 <{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}">
-    <a href="{{ $item->slug }}" class="m-listing__link">
+    <a href="{{ $item->url }}" class="m-listing__link">
         <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}">
             @if (isset($video) || $item->videoFront)
                 @component('components.atoms._video')
