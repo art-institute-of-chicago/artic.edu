6a08c4025746b5352fb82dc0d9def99dbbad1369
Tim Brook <tim@area17.com>
2018-05-07 16:14:57 -0500

    #707 - add smaller icon to quote module to use on small breakpoint

--- resources/views/components/atoms/_quote.blade.php
+++ resources/views/components/atoms/_quote.blade.php
@@ -1,4 +1,5 @@
 <blockquote class="quote{{ (isset($variation)) ? ' '.$variation : '' }}">
     <p class="{{ $font ?? 'f-quote' }}">{{ $slot }}</p>
-    <svg class="quote__icon icon--quote--68" aria-hidden="true"><use xlink:href="#icon--quote--68" /></svg>
+    <svg class="quote__icon icon--quote--68 u-show@small+" aria-hidden="true"><use xlink:href="#icon--quote--68" /></svg>
+    <svg class="quote__icon icon--quote--48 u-show@xsmall" aria-hidden="true"><use xlink:href="#icon--quote--48" /></svg>
 </blockquote>
