cc6916ba7cd79e6f6f6d96dad5ffa4158ea7db91
Area 17 <area17@MacBook-Pro-4.local>
2018-05-09 15:30:36 -0400

    update google map link

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -34,7 +34,7 @@
         <ul class="g-footer__link-list g-footer__link-list--spaced">
           <li>
             Historic Building <br>
-            <a href="https://www.google.co.uk/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">111 South Michigan Avenue, <br>Chicago, IL 60603</a>
+            <a href="https://www.google.com/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713" target="_blank">111 South Michigan Avenue, <br>Chicago, IL 60603</a>
           </li>
 
           <li>
