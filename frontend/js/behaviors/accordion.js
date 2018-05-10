3306153a75c14f6e88e34658a3174d165c139928
Mike Byrne <mike@thirteentwelve.com>
2018-05-10 12:22:03 +0100

    ticket #742 - accordion won’t tab focus on hidden elements, focuses on next available tab focus

--- frontend/js/behaviors/accordion.js
+++ frontend/js/behaviors/accordion.js
@@ -1,6 +1,50 @@
-import { purgeProperties, triggerCustomEvent, setFocusOnTarget } from '@area17/a17-helpers';
+import { purgeProperties, triggerCustomEvent, setFocusOnTarget, forEach } from '@area17/a17-helpers';
 
 const accordion = function(container) {
+
+  function _findAncestor(el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+  }
+
+  function _focusNextElement(activeElement) {
+    if (activeElement) {
+      activeElement.blur();
+      let activeElementPanelParent = _findAncestor(activeElement,'o-accordion__panel');
+      let activeElementPanelParentTabIndexSet = false;
+      if (activeElementPanelParent && !activeElementPanelParent.getAttribute('tabindex')) {
+        activeElementPanelParent.setAttribute('tabindex','0');
+        activeElementPanelParentTabIndexSet = true;
+      }
+      let focussableElements = [];
+      forEach(document.querySelectorAll('a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])'), function(index, el) {
+        let push = true;
+        if (el.offsetWidth <= 0 || el.offsetWidth <= 0 || el.getAttribute('aria-hidden') === 'true') {
+          push = false;
+        }
+        let parentPanel = _findAncestor(el,'o-accordion__panel');
+        if (parentPanel && parentPanel.getAttribute('aria-hidden') === 'true') {
+          push = false;
+        }
+        if (el === activeElementPanelParent) {
+          push = true;
+        }
+        if (push) {
+          focussableElements.push(el);
+        }
+      });
+      if (activeElementPanelParentTabIndexSet) {
+        activeElementPanelParent.removeAttribute('tabindex');
+      }
+      if (focussableElements.length > 0) {
+        let index = focussableElements.indexOf(activeElementPanelParent);
+        if(index > -1) {
+          let nextElement = focussableElements[index + 1] || focussableElements[0];
+          nextElement.focus();
+        }
+      }
+    }
+  }
 
   function _getHeightAndSet(target) {
     let targetHeight = target.firstElementChild.offsetHeight;
@@ -21,6 +65,10 @@ const accordion = function(container) {
       let trigger = event.target.classList.contains('o-accordion__trigger') ? event.target : event.target.parentNode;
       let target = trigger.nextElementSibling;
       let validTarget = (target.classList.contains('o-accordion__panel'));
+      //
+      if (trigger.classList.contains('s-inactive')) {
+        return;
+      }
       //
       if (validTarget) {
         trigger.blur();
@@ -59,8 +107,14 @@ const accordion = function(container) {
       container.querySelector('.o-accordion__trigger.s-focus').classList.remove('s-focus');
     } catch(err) {
     }
-    if (event.keyCode === 9 && container.contains(document.activeElement) && document.activeElement.classList.contains('o-accordion__trigger')) {
-      document.activeElement.classList.add('s-focus');
+    if (event.keyCode === 9 && container.contains(document.activeElement)) {
+      if (document.activeElement.classList.contains('o-accordion__trigger')) {
+        document.activeElement.classList.add('s-focus');
+      }
+      let parentPanel = _findAncestor(document.activeElement,'o-accordion__panel');
+      if (parentPanel && parentPanel.getAttribute('aria-hidden') === 'true') {
+        _focusNextElement(document.activeElement);
+      }
     }
   }
 
