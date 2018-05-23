19de1f4a9523ab4160c27fd5a23201a756443310
Mike Byrne <mike@thirteentwelve.com>
2018-05-23 12:33:27 +0100

    ticket #563 - artist listing item props added

--- resources/views/components/molecules/_m-listing----artist.blade.php
+++ resources/views/components/molecules/_m-listing----artist.blade.php
@@ -1,5 +1,5 @@
-<{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-behavior="blurMyBackground"' : '' !!}>
-    <a href="{!! route('artists.show', $item) !!}" class="m-listing__link">
+<{{ $tag or 'li' }} class="m-listing{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-behavior="blurMyBackground"' : '' !!} itemscope itemtype="http://schema.org/Person">
+    <a href="{!! route('artists.show', $item) !!}" class="m-listing__link" itemprop="url">
         @if ($item->imageFront('hero'))
         <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '  m-listing__img--square' }}"{{ (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-blur-img' : '' }}>
             @if ($item->videoFront)
