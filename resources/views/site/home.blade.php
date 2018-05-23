cae9f21253905bfca4480039db95778f96cf59c1
Mike Byrne <mike@thirteentwelve.com>
2018-05-23 12:53:04 +0100

    ticket #563 - added JSON-LD to homepage

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -198,4 +198,16 @@
     @slot('linksPrimary', array(array('label' => 'Explore the shop', 'href' => $_pages['shop'], 'variation' => 'btn btn--secondary')))
 @endcomponent
 
+<script type="application/ld+json">
+{
+        "@context": "http://schema.org",
+        "@type": "TouristAttraction",
+        "name": "The Art Institute of Chicago",
+        "additionalType": "Museum, LandmarksOrHistoricalBuildings, LocalBusiness",
+        "description": '{{ strip_tags($intro) }}',
+        "url": "http://www.artic.edu/",
+        "isAccessibleForFree": true
+}
+</script>
+
 @endsection
