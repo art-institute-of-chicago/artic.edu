31793a4126c680613c306ab83edc83ed988fddb7
Mike Byrne <mike@thirteentwelve.com>
2018-05-31 12:00:03 +0100

    ticket #744 - more molecules can accept GTM attributes

--- resources/views/components/molecules/_m-title-bar.blade.php
+++ resources/views/components/molecules/_m-title-bar.blade.php
@@ -5,7 +5,7 @@
     @foreach ($links as $link)
     <li>
         @if (isset($link['href']))
-        <a href="{{ $link['href'] }}" class="f-link"{!! (isset($link['dataAttributes'])) ? ' '.$link['dataAttributes'] : '' !!}>
+        <a href="{{ $link['href'] }}" class="f-link"{!! (isset($link['dataAttributes'])) ? ' '.$link['dataAttributes'] : '' !!}{!! (isset($link['gtmAttributes'])) ? ' '.$link['gtmAttributes'].'' : '' !!}>
             {!! $link['label'] ?? '' !!}{!! (!isset($link['dataAttributes'])) ? '&nbsp;&nbsp;&rsaquo;' : '' !!}
         </a>
         @else
