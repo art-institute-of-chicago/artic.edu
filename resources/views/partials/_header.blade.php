78825813a0e120ae9b3287f96aefe0b65826cd90
Mike Byrne <mike@thirteentwelve.com>
2018-05-31 14:30:39 +0100

    ticket #744 - seems I got the gtm fields switched in _header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -21,25 +21,25 @@
       <nav class="g-header__nav-primary">
         <ul class="f-main-nav">
           <li class='u-hide@small+'>
-            <a href="{{ $_pages['buy'] }}" data-gtm-event="top-nav" data-gtm-event-category="tickets-button">Buy Tickets</a>
+            <a href="{{ $_pages['buy'] }}" data-gtm-event-category="top-nav" data-gtm-event="tickets-button">Buy Tickets</a>
           </li>
           <li{!! (isset($primaryNavCurrent) && $primaryNavCurrent == 'visit') ? ' class="s-current"' : '' !!}>
-            <a href="{{ $_pages['visit'] }}" data-gtm-event="top-nav" data-gtm-event-category="visit">Visit</a>
+            <a href="{{ $_pages['visit'] }}" data-gtm-event-category="top-nav" data-gtm-event="visit">Visit</a>
           </li>
           <li class="u-show@small+{{ (isset($primaryNavCurrent) && $primaryNavCurrent == 'exhibitions_and_events') ? ' s-current' : '' }}">
-            <a href="{{ $_pages['exhibitions'] }}" data-gtm-event="top-nav" data-gtm-event-category="exhibitions-and-events">Exhibitions &amp; Events</a>
+            <a href="{{ $_pages['exhibitions'] }}" data-gtm-event-category="top-nav" data-gtm-event="exhibitions-and-events">Exhibitions &amp; Events</a>
           </li>
           <li class="u-show@small+{{ (isset($primaryNavCurrent) && $primaryNavCurrent == 'collection') ? ' s-current' : '' }}">
-            <a href="{{ $_pages['collection'] }}" data-gtm-event="top-nav" data-gtm-event-category="collection">The Collection</a>
+            <a href="{{ $_pages['collection'] }}" data-gtm-event-category="top-nav" data-gtm-event="collection">The Collection</a>
           </li>
           <li class="u-show@small+"><button data-behavior="globalSearchOpen"><svg class="icon--search--24" aria-label="Search site"><use xlink:href="#icon--search--24" /></svg></button></li>
         </ul>
       </nav>
       <nav class="g-header__nav-secondary">
         <ul class="f-secondary">
-          <li><a href="{{ $_pages['buy'] }}" data-gtm-event="top-nav" data-gtm-event-category="tickets-button">Buy Tickets</a></li>
-          <li><a href="{{ $_pages['become-a-member'] }}" data-gtm-event="top-nav" data-gtm-event-category="member-button">Become a Member</a></li>
-          <li><a href="{{ $_pages['shop'] }}" data-gtm-event="top-nav" data-gtm-event-category="shop-button">Shop</a></li>
+          <li><a href="{{ $_pages['buy'] }}" data-gtm-event-category="top-nav" data-gtm-event="tickets-button">Buy Tickets</a></li>
+          <li><a href="{{ $_pages['become-a-member'] }}" data-gtm-event-category="top-nav" data-gtm-event="member-button">Become a Member</a></li>
+          <li><a href="{{ $_pages['shop'] }}" data-gtm-event-category="top-nav" data-gtm-event="shop-button">Shop</a></li>
         </ul>
       </nav>
       <p class="g-header__opening-hours f-secondary"><a href="{{ $_pages['visit'] }}">{!! $_hours['general'] !!}</a></p>
