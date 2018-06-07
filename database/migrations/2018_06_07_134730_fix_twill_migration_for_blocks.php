6401c3180d864324383d1fdc1668b930fb8937d5
Fernando Petrelli <fernando@area17.com>
2018-06-07 13:50:37 -0400

    Hotfix: Modify blocks mapping at the database

---
+++ database/migrations/2018_06_07_134730_fix_twill_migration_for_blocks.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixTwillMigrationForBlocks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::update("update mediables set mediable_type = 'blocks' where mediable_type = ?", ['A17\CmsToolkit\Models\Block']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::update("update mediables set mediable_type = 'A17\CmsToolkit\Models\Block' where mediable_type = ?", ['blocks']);
+    }
+}
