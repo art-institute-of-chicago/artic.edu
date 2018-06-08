b29317adc6d1d52d653a331cb57100ad0f7c3103
Fernando Petrelli <fernando@area17.com>
2018-06-08 11:16:43 -0400

    Ticket #897: Add a new link for the image and title when showing a family item on Visit

--- resources/assets/js/blocks/BlockOffer.vue
+++ resources/assets/js/blocks/BlockOffer.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- eslint-disable -->
     <div class="block__body">
-        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" ></a17-mediafield> </a17-inputframe><a17-textfield label="Header" :name="fieldName('header')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Subheader" :name="fieldName('subheader')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Description" :name="fieldName('description')" type="textarea" :rows="4" in-store="value" ></a17-textfield><a17-textfield label="Link to Book" :name="fieldName('link_to_book')" type="text" in-store="value" ></a17-textfield>
+        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" >Minimum image width 2000px</a17-mediafield> </a17-inputframe><a17-textfield label="Header" :name="fieldName('header')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Subheader" :name="fieldName('subheader')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Description" :name="fieldName('description')" type="textarea" :rows="4" in-store="value" ></a17-textfield><a17-textfield label="Link to Book" :name="fieldName('link_to_book')" type="text" in-store="value" ></a17-textfield>
     </div>
 </template>
 
