f42493d119c1dccca17c1ec382730de153fdcceb
Mike Byrne <mike@thirteentwelve.com>
2018-06-08 18:18:26 +0100

    ticket #744 - moving GTM JS is in the head
    Stops weird halt on styles being loaded

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,16 +27,6 @@ $pClass = strtolower(preg_replace('/@/i','-',$pClass));
   <link rel="apple-touch-icon-precomposed" type="image/png" href="{{revAsset('images/favicon-120.png')}}" sizes="120x120"> <!-- iPhone retina touch icon -->
   <link rel="apple-touch-icon-precomposed" type="image/png" href="{{revAsset('images/favicon-76.png')}}" sizes="76x76"> <!-- iPad home screen icons -->
 
-    @if (config('services.google_tag_manager.enabled'))
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','{!! config('services.google_tag_manager.id') !!}');</script>
-      <!-- End Google Tag Manager -->
-    @endif
-
   <!--[if lt IE 9]>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
   <![endif]-->
@@ -55,9 +45,18 @@ $pClass = strtolower(preg_replace('/@/i','-',$pClass));
   @font-face {font-family: 'Sabon';src: url({{revAsset('fonts/3545D5_2_0.eot')}});src: url({{revAsset('fonts/3545D5_2_0.eot?#iefix')}}) format('embedded-opentype'),url({{revAsset('fonts/3545D5_2_0.woff2')}}) format('woff2'),url({{revAsset('fonts/3545D5_2_0.woff')}}) format('woff'),url({{revAsset('fonts/3545D5_2_0.ttf')}}) format('truetype');font-weight:normal;font-weight:500;font-style:normal;}
   </style>
   @if ($print)
-  <link href="{{revAsset('styles/print.css')}}" rel="stylesheet" />
+    <link href="{{revAsset('styles/print.css')}}" rel="stylesheet" />
   @else
-  <link href="{{revAsset('styles/app.css')}}" rel="stylesheet" />
+      <link href="{{revAsset('styles/app.css')}}" rel="stylesheet" />
+      @if (config('services.google_tag_manager.enabled'))
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','{!! config('services.google_tag_manager.id') !!}');</script>
+        <!-- End Google Tag Manager -->
+      @endif
   @endif
 </head>
 
