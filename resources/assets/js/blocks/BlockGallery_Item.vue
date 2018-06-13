fe47e7e0f8aac6edc0e79d0f8b878aee88fd3a48
Fernando Petrelli <fernando@area17.com>
2018-06-13 13:48:57 -0400

    Regenerate blocks

--- resources/assets/js/blocks/BlockGallery_Item.vue
+++ resources/assets/js/blocks/BlockGallery_Item.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- eslint-disable -->
     <div class="block__body">
-        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" ></a17-mediafield> </a17-inputframe><a17-textfield label="Caption title" :name="fieldName('captionTitle')" type="text" :maxlength="150" in-store="value" ></a17-textfield><a17-textfield label="Caption" :name="fieldName('caption')" type="text" :maxlength="150" in-store="value" ></a17-textfield>
+        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" ></a17-mediafield> </a17-inputframe><a17-textfield label="Caption title" :name="fieldName('captionTitle')" type="text" :maxlength="80" in-store="value" ></a17-textfield><a17-textfield label="Caption" :name="fieldName('caption')" type="text" :maxlength="200" in-store="value" ></a17-textfield>
     </div>
 </template>
 
--- resources/assets/js/blocks/BlockList_Item.vue
+++ resources/assets/js/blocks/BlockList_Item.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- eslint-disable -->
     <div class="block__body">
-        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" ></a17-mediafield> </a17-inputframe><a17-textfield label="Tag" :name="fieldName('tag')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Header" :name="fieldName('header')" type="text" in-store="value" ></a17-textfield><a17-textfield label="Description" :name="fieldName('description')" type="textarea" :rows="4" in-store="value" ></a17-textfield><a17-textfield label="Link" :name="fieldName('external_link')" type="text" in-store="value" ></a17-textfield>
+        <a17-inputframe label="Image" name="medias.image" > <a17-mediafield :name="fieldName('image')" crop-context="image" ></a17-mediafield> </a17-inputframe><a17-textfield label="Tag" :name="fieldName('tag')" type="text" :maxlength="20" in-store="value" ></a17-textfield><a17-textfield label="Header" :name="fieldName('header')" type="text" :maxlength="60" in-store="value" ></a17-textfield><a17-textfield label="Description" :name="fieldName('description')" type="textarea" :maxlength="100" :rows="4" in-store="value" ></a17-textfield><a17-textfield label="Link" :name="fieldName('external_link')" type="text" in-store="value" ></a17-textfield>
     </div>
 </template>
 
