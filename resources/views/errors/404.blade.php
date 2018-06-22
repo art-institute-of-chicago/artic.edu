d14a5379858ebe135560ffa828647d37077844db
Tim Brook <tim@area17.com>
2018-06-22 09:25:42 -0500

    #779 - update 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
 
-<div class="o-application-error">
-    <h2 class="f-headline">Looking for something?</h2>
-    <p class="f-body">Sorry, whatever you are looking for is no longer here or you typed in the wrong URL. Please double-check and try again.</p>
-    <p class="f-body"><a href="/">&lsaquo; Back to Homepage</a></p>
-</div>
+@component('components.molecules._m-header-block')
+    Looking for something?
+@endcomponent
+
+@component('components.molecules._m-intro-block')
+    Sorry, whatever you are looking for is no longer here or you typed in the wrong URL. Please double-check and try again.
+@endcomponent
+
+<p>
+    <a href="/" class="btn f-buttons btn--secondary">To Homepage</a>
+    <a href="/search" class="btn f-buttons btn--secondary">Perform a Search</a>
+</p>
 
 @endsection
