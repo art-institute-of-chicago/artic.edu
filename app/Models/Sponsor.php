cc7f9017d9350022f65a5f775167682935a0edd4
Illya Moskvin <imoskvin@artic.edu>
2018-06-26 16:21:37 -0500

    Add block editor to sponsors

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use A17\Twill\Models\Behaviors\HasBlocks;
 use A17\Twill\Models\Model;
 
 class Sponsor extends Model
 {
+    use HasBlocks;
+
     protected $fillable = [
         'published',
         'title',
--- resources/views/admin/sponsors/form.blade.php
+++ resources/views/admin/sponsors/form.blade.php
@@ -1,4 +1,9 @@
 @extends('twill::layouts.form')
 
 @section('contentFields')
+    @formField('block_editor', [
+        'blocks' => [
+            'paragraph', 'image'
+        ]
+    ])
 @stop
