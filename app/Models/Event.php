1cb4a51bf5559a9936c3c2f84a2a6a17e3bb3b59
Area 17 <area17@Iains-MacBook-Pro.local>
2018-06-27 13:51:24 -0400

    work done on event date formatting

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -171,7 +171,7 @@ class Event extends Model
     {
         $dates_string = '';
         foreach ($this->all_dates as $date) {
-            $dates_string .= $date['date']->format('F j, Y h:ia') . ' - ' . $date['date_end']->format('F j, Y h:ia') . "\n";
+            $dates_string .= $date['date']->format('F j, Y h:i') . ' - ' . $date['date_end']->format('F j, Y h:i') . "\n";
         }
 
         return $dates_string;
