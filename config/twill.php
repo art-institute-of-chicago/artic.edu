77c4befe65ae9aa87b719ff648e13990eb7ea494
Area 17 <area17@Iains-MacBook-Pro.local>
2018-06-28 14:21:23 -0400

    updating the image ratio for a dining image

--- config/twill.php
+++ config/twill.php
@@ -253,7 +253,7 @@ return [
                 'default' => [
                     [
                         'name' => 'default',
-                        'ratio' => 3 / 4,
+                        'ratio' => 16 / 9,
                     ],
                 ]
             ],
