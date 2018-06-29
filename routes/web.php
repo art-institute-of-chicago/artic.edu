57d5944cd20746db15ca6ce8e46f665d951d602a
Area 17 <area17@Iains-MacBook-Pro.local>
2018-06-29 09:02:24 -0400

    updating the digital catalogues text

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::name('collection.categorySearch')->get('/collection/categorySearch/{categ
 Route::name('collection.publications.printed-catalogs')->get('/collection/publications/printed-catalogs', 'PrintedCatalogsController@index');
 Route::name('collection.publications.printed-catalogs.show')->get('/collection/publications/printed-catalogs/{id}', 'PrintedCatalogsController@show');
 // Collection Publications Digital Catalogs
-Route::name('collection.publications.digital-catalogs')->get('/collection/publications/digital-catalogs', 'DigitalCatalogsController@index');
-Route::name('collection.publications.digital-catalogs.show')->get('/collection/publications/digital-catalogs/{id}', 'DigitalCatalogsController@show');
+Route::name('collection.publications.digital-catalogs')->get('/collection/publications/digital-catalogues', 'DigitalCatalogsController@index');
+Route::name('collection.publications.digital-catalogs.show')->get('/collection/publications/digital-catalogues/{id}', 'DigitalCatalogsController@show');
 
 // Collection Research and Resources ???
 Route::name('collection.research_resources')->get('/collection/research_resources', 'ResearchController@index');
