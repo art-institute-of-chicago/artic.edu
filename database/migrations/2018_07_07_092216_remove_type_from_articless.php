8d9dc1761ada57996f49d47a69274f818b27ea21
Fernando Petrelli <fernando@area17.com>
2018-07-07 12:20:33 -0400

    Ticket #1007: Refresh migration names

--- database/migrations/2018_07_07_092216_remove_type_from_articless.php
+++ database/migrations/2018_07_07_092216_remove_type_from_articless.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RemoveTypeFromArticles extends Migration
+class RemoveTypeFromArticless extends Migration
 {
     /**
      * Run the migrations.
