adbbfd58e8d0b203282531ce449f55f932ba52d9
Area 17 <area17@Iains-MacBook-Pro.local>
2018-07-09 11:28:54 -0400

    updating the exhibition dates per the client's logic

--- resources/views/components/molecules/_m-article-header----default.blade.php
+++ resources/views/components/molecules/_m-article-header----default.blade.php
@@ -12,6 +12,13 @@
             @slot('tag','p')
             {!! $formattedDate !!}
         @endcomponent
+     @elseif (empty($dateEnd))
+           @component('components.atoms._date')
+            @slot('tag','p')
+            <time datetime="{{ $dateStart->format("Y-m-d") }}" itemprop="startDate">{{ $dateStart->format('M j, Y') }}</time> 
+            @endcomponent
+      @elseif (empty($dateStart))
+      @elseif (empty($dateStart) and empty($dateEnd))
     @elseif ($dateStart and $dateEnd)
         @component('components.atoms._date')
             @slot('tag','p')
