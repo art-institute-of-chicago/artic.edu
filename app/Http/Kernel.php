cb1880563c64a3f6590291e040d88dc687fbc139
Fernando Petrelli <fernando@area17.com>
2018-07-19 15:56:25 -0400

    Ticket #459: Implement httpauth at route level to protect specific url's

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'httpauth' => \App\Http\Middleware\BasicHttpAuth::class,
     ];
 }
