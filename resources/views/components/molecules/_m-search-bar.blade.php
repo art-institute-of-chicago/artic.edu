37a53e6d8d769506e8da8b9192257e3dcea0755f
Mike Byrne <mike@thirteentwelve.com>
2018-07-23 14:13:10 +0100

    ticket #1187 - search aria labelling updated

--- resources/views/components/molecules/_m-search-bar.blade.php
+++ resources/views/components/molecules/_m-search-bar.blade.php
@@ -5,15 +5,15 @@
 
         <input class="f-secondary{{ (isset($value) and $value and isset($clearLink) and $clearLink) ? ' s-populated' : '' }}" id="{{ $name ?? '' }}" name="{{ $name ?? '' }}" placeholder="{{ $placeholder ?? '' }}" type="text" value="{{ $value ?? '' }}" autocomplete="off">
 
-        <button class="m-search-bar__submit" type="submit"><svg aria-label="Search" class="icon--search--24"><use xlink:href="#icon--search--24" /></svg></button>
+        <button class="m-search-bar__submit" type="submit" aria-label="Search"><svg aria-hidden="true" class="icon--search--24"><use xlink:href="#icon--search--24" /></svg></button>
 
         @if (isset($clearLink))
-        <a class="m-search-bar__clear" href="{{ $clearLink }}"><svg aria-label="Clear search" class="icon--close"><use xlink:href="#icon--close" /></svg></a>
+        <a class="m-search-bar__clear" href="{{ $clearLink }}" aria-label="Clear search"><svg aria-hidden="true" class="icon--close"><use xlink:href="#icon--close" /></svg></a>
         @endif
 
         @if (isset($behaviors) && strrpos($behaviors, "autocomplete") > -1)
-        <button class="m-search-bar__clear" data-autocomplete-clear>
-            <svg aria-label="Clear search" class="icon--close"><use xlink:href="#icon--close" /></svg>
+        <button class="m-search-bar__clear" aria-label="Clear search" data-autocomplete-clear>
+            <svg aria-hidden="true" class="icon--close"><use xlink:href="#icon--close" /></svg>
         </button>
         @endif
 
