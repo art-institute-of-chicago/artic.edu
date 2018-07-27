ec32448210fc0020eff7676337c4af628f0077cc
Illya Moskvin <imoskvin@artic.edu>
2018-07-27 15:27:07 -0500

    Add sr-only labels to next/prev on artwork detail [WEB-322]

--- resources/views/components/molecules/_m-article-header----gallery.blade.php
+++ resources/views/components/molecules/_m-article-header----gallery.blade.php
@@ -42,6 +42,7 @@ if ($maxZoomWindowSize >= 843) {
         @if ($prevNextObject->next)
         <li class="m-article-header__img-nav-next-artwork">
           <a href="{!! route('artworks.show', ['id' => $prevNextObject->next->id, 'slug' => $prevNextObject->next->titleSlug ] + $prevNextObject->nextParams) !!}" class="m-article-header__img-nav-artwork-preview">
+            <span class="sr-only">Next: </span>
             <span class="m-article-header__img-nav-artwork-preview-img">
               @component('components.atoms._img')
                   @slot('image', $prevNextObject->next->imageFront())
@@ -62,6 +63,7 @@ if ($maxZoomWindowSize >= 843) {
         @if ($prevNextObject->prev)
         <li class="m-article-header__img-nav-prev-artwork">
           <a href="{!! route('artworks.show', ['id' => $prevNextObject->prev->id, 'slug' => $prevNextObject->prev->titleSlug ] + $prevNextObject->prevParams) !!}" class="m-article-header__img-nav-artwork-preview">
+            <span class="sr-only">Previous: </span>
             <span class="m-article-header__img-nav-artwork-preview-img">
               @component('components.atoms._img')
                   @slot('image', $prevNextObject->prev->imageFront())
