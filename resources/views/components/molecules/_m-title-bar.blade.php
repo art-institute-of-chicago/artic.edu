f8f22672c03b74c8353add75867a2bf38c4370ff
nikhiltri <ntrivedi@artic.edu>
2018-07-27 11:54:12 -0500

    When a block’s secondary links only contain one link, don’t output them in a <ul> [WEB-300]

--- resources/views/components/molecules/_m-title-bar.blade.php
+++ resources/views/components/molecules/_m-title-bar.blade.php
@@ -1,9 +1,9 @@
 <div class="m-title-bar {{ $variation ?? '' }}"{!! isset($id) ? ' id="'.$id.'"' : '' !!}>
   <h2 class="title {{ $titleFont ?? 'f-module-title-2' }}">{{ $slot }}</h2>
   @if (isset($links) and $links)
-  <ul class="m-title-bar__links">
+  <{!! count($links) > 1 ? 'ul' : 'span' !!} class="m-title-bar__links">
     @foreach ($links as $link)
-    <li>
+    {!! count($links) > 1 ? '<li>' : '<span>' !!}
         @if (isset($link['href']))
         <a href="{{ $link['href'] }}" class="f-link"{!! (isset($link['dataAttributes'])) ? ' '.$link['dataAttributes'] : '' !!}{!! (isset($link['gtmAttributes'])) ? ' '.$link['gtmAttributes'].'' : '' !!}>
             {!! $link['label'] ?? '' !!}{!! (!isset($link['dataAttributes'])) ? '<span aria-hidden="true">&nbsp;&nbsp;&rsaquo;</span>' : '' !!}
@@ -11,8 +11,8 @@
         @else
             <span class="f-secondary">{!! $link['label'] ?? '' !!}</span>
         @endif
-    </li>
+    {!! count($links) > 1 ? '</li>' : '</span>' !!}
     @endforeach
-  </ul>
+  {!! count($links) > 1 ? '</ul>' : '</span>' !!}
   @endif
 </div>
