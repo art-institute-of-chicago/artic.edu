5135cd410cbc21ffdc468d314affdca47c051911
nikhiltri <ntrivedi@artic.edu>
2018-08-03 17:11:38 -0500

    Take out removed form fields from validation in Educator Request form

--- app/Http/Requests/Form/EducatorAdmissionRequest.php
+++ app/Http/Requests/Form/EducatorAdmissionRequest.php
@@ -10,16 +10,6 @@ class EducatorAdmissionRequest extends FormRequest
             'name' => 'required',
             'email' => 'required',
             'visit_date' => 'required',
-            'phone_number' => 'required',
-            'address_1' => 'required',
-            'address_2' => 'sometimes',
-            'city' => 'required',
-            'state' => 'required',
-            'zipcode' => 'required',
-
-            'school_name' => 'required',
-            'school_city' => 'required',
-            'school_state' => 'required',
             'school_location' => 'required',
 
             'type_of_educator' => 'required',
