31bbb574153ffb76b49f8ccb35610c44c27b3085
nikhiltri <ntrivedi@artic.edu>
2018-08-06 22:11:47 -0500

    Artwork Detail: Accordions: Remove all tabpanel markup [WEB-331]

--- resources/views/components/organisms/_o-accordion.blade.php
+++ resources/views/components/organisms/_o-accordion.blade.php
@@ -1,13 +1,13 @@
-<div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }}" role="tablist" multiselectable="true" data-behavior="accordion">
+<div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="accordion">
     @foreach ($items as $item)
-    <h3 id="{{ getUtf8Slug($item['title']) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" aria-selected="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}" aria-controls="panel_{{ getUtf8Slug($item['title']) }}" aria-expanded="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}" role="tab" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!}>
+    <h3 id="{{ getUtf8Slug($item['title']) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!}>
         {{ $item['title'] }}
         <span class="o-accordion__trigger-icon">
             <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
             <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
         </span>
     </h3>
-    <div id="panel_{{ getUtf8Slug($item['title']) }}" class="o-accordion__panel" aria-labelledby="{{ getUtf8Slug($item['title']) }}" aria-hidden="{{ (isset($item['active']) and $item['active']) ? 'false' : 'true' }}" role="tabpanel">
+    <div id="panel_{{ getUtf8Slug($item['title']) }}" class="o-accordion__panel" aria-labelledby="{{ getUtf8Slug($item['title']) }}" aria-hidden="{{ (isset($item['active']) and $item['active']) ? 'false' : 'true' }}">
         <div class="o-accordion__panel-content o-blocks">
             @component('components.blocks._blocks')
                 @slot('blocks', $item['blocks'])
