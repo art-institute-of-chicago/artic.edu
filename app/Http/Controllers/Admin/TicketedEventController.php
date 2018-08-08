67d3445c6857b0e7deff958b59cc3d9b8921ca49
nikhiltri <ntrivedi@artic.edu>
2018-08-08 16:07:29 -0500

    Add event date and ID to Ticketed Event browser in Event [WEB-405]

--- app/Http/Controllers/Admin/TicketedEventController.php
+++ app/Http/Controllers/Admin/TicketedEventController.php
@@ -5,4 +5,7 @@ namespace App\Http\Controllers\Admin;
 class TicketedEventController extends BaseApiController
 {
     protected $moduleName = 'ticketedEvents';
+
+    protected $titleColumnKey = 'cmsTitle';
+
 }
