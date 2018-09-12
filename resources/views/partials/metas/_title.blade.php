4bb91a6d5caf1860a1083a85aeb870df8ba4a236
nikhiltri <ntrivedi@artic.edu>
2018-09-12 10:30:45 -0500

    Hotfix for broken previews in CMS
    On all pages we are setting the `og:site_name` tag to a globally
    defined variable, but this variable is not defined in preview modes.

--- resources/views/partials/metas/_title.blade.php
+++ resources/views/partials/metas/_title.blade.php
@@ -3,7 +3,9 @@
 @endphp
 
 <title>{{ $title }}</title>
+@if (isset($globalSuffix))
 <meta property="og:site_name"  content="{{ $globalSuffix }}" />
+@endif
 <meta property="og:title"      content="{{ $title }}" />
 <meta name="twitter:title"     content="{{ $title }}" />
 <meta itemprop="name"          content="{{ $title }}" />
