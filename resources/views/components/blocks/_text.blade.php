28178a25a44cedef31cc13f4bd0fb47c599e575b
nikhiltri <ntrivedi@artic.edu>
2018-09-12 14:10:47 -0500

    Header text for Events rendering wrong font size [WEB-530]
    This bug is due to the wysiwyg editor wrapping text in <p> tags, and
    the view component also wrapping the text in a <p> tag. This caused
    an invalid HTML structure, and incorrect output on the frontend. It's
    possible this change could have unforeseen affects, so we should keep
    an eye out on the accuracy of rendered paragraph blocks.

--- resources/views/components/blocks/_text.blade.php
+++ resources/views/components/blocks/_text.blade.php
@@ -1,1 +1,5 @@
+@php
+    $slot = preg_replace('/<p\b[^>]*>/i', '', $slot);
+    $slot = str_ireplace('</p>', '', $slot);
+@endphp
 <{{ $tag ?? 'p' }} class="{{ $font ?? 'f-body' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($id)) ? ' id="'.$id.'"' : '' !!}{!! (isset($itemprop)) ? ' itemprop="'.$itemprop.'"' : '' !!}>{!! $slot !!}</{{ $tag ?? 'p' }}>
