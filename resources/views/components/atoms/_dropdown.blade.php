0755c0c78688515fff36ea822d83c0460e658b50
nikhiltri <ntrivedi@artic.edu>
2018-09-13 16:59:59 -0500

    Wrap Select Language button in a heading, and apply the aria-labelledby pattern to the list it displays

--- resources/views/components/atoms/_dropdown.blade.php
+++ resources/views/components/atoms/_dropdown.blade.php
@@ -1,6 +1,6 @@
 <div aria-label="{{ $ariaTitle }}" class="dropdown{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="dropdown">
-  <button class="dropdown__trigger {{ $font ?? "f-secondary" }}" aria-expanded="false">{{ $prompt ?? 'Select' }}<svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></button>
-  <ul class="dropdown__list{{ (isset($listVariation)) ? ' '.$listVariation : '' }} f-secondary" data-dropdown-list>
+  <h2><button class="dropdown__trigger {{ $font ?? "f-secondary" }}" aria-expanded="false" id="h-select-language">{{ $prompt ?? 'Select' }}<svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></button></h2>
+  <ul class="dropdown__list{{ (isset($listVariation)) ? ' '.$listVariation : '' }} f-secondary" data-dropdown-list aria-labelledby="h-select-language">
     @foreach ($options as $option)
       <li{!! (isset($option['active']) and $option['active']) ? ' class="s-active"' : '' !!}><a href="{{ $option['href'] ?? '#' }}"{!! (isset($option['ajaxScrollTarget']) and $option['ajaxScrollTarget']) ? ' data-ajax-scroll-target="'.$option['ajaxScrollTarget'].'"' : '' !!}{!! (!empty($option['ajaxTabTarget'])) ? ' data-ajax-tab-target="'.$option['ajaxTabTarget'].'"' : '' !!}{!! (isset($link['gtmAttributes'])) ? ' '.$link['gtmAttributes'].'' : '' !!}>{!! $option['label'] ?? '' !!}</a></li>
     @endforeach
