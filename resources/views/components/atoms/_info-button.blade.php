dfc7595acca6d93720eec83b941f57bcb095746c
nikhiltri <ntrivedi@artic.edu>
2018-09-14 16:43:24 -0500

    Visit: Admission: Update aria-labelledby to a more readable label for tooltip buttons

--- resources/views/components/atoms/_info-button.blade.php
+++ resources/views/components/atoms/_info-button.blade.php
@@ -1,4 +1,4 @@
-<button class="info-button-trigger" data-behavior="infoButtonTrigger" aria-label="Info" aria-expanded="false" {!! (isset($id)) ? ' aria-labelledby="h-'.$id.'" data-tooltip-id="'.$id.'"' : '' !!}>
+<button class="info-button-trigger" data-behavior="infoButtonTrigger" aria-label="Info" aria-expanded="false" {!! (isset($id)) ? ' aria-labelledby="b-'.$id.' h-'.$id.'" data-tooltip-id="'.$id.'" id="b-'.$id.'"' : '' !!}>
   <svg class="icon--info"><use xlink:href="#icon--info" /></svg>
 </button>
 <span class="info-button-info s-hidden" id="info-button-info-{{ $id ?? '' }}" data-behavior="infoButtonInfo">
