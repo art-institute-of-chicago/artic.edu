c2b8228748e1dce7387a5cef52498fb1fc6bdfe4
nikhiltri <ntrivedi@artic.edu>
2018-10-29 15:53:43 -0500

    Add redirect to canonical URL for artworks

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -25,6 +25,11 @@ class ArtworkController extends BaseScopedController
             ->include(['artist_pivots', 'place_pivots', 'dates', 'catalogue_pivots'])
             ->findOrFail((Integer) $id);
 
+        // Redirect to the canonical page if it wasn't requested
+        if ('/' .request()->path() != route('artworks.show', ['id' => $item->id, 'slug' => $item->titleSlug ], false)) {
+            return redirect(route('artworks.show', ['id' => $item->id, 'slug' => $item->titleSlug ], false), 301);
+        }
+
         $this->seo->setTitle($item->meta_title ?? $item->title);
         $this->seo->setDescription($item->meta_description ?? $item->fullTitle);
         $this->seo->setImage($item->imageFront('hero'));
