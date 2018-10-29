c671f21ebab4dfca7bae97f442669858c5c720a4
Illya Moskvin <imoskvin@artic.edu>
2018-10-29 16:31:43 -0500

    Refactor member preview dates into organism [WEB-687]

--- resources/views/components/molecules/_m-article-header----super-hero.blade.php
+++ resources/views/components/molecules/_m-article-header----super-hero.blade.php
@@ -22,32 +22,10 @@
       @endif
 
       {{-- Preview dates --}}
-      @if ((!empty($previewDateStart) and $previewDateStart->gte(\Carbon\Carbon::today())) or (!empty($previewDateEnd) and $previewDateEnd->gte(\Carbon\Carbon::today())))
-      @if (empty($previewDateStart) and empty($previewDateEnd))
-      @elseif (empty($previewDateEnd) and !empty($previewDateStart))
-           @component('components.atoms._date')
-            @slot('tag','p')
-            @slot('variation','f-previewdate')
-            Member preview: <time datetime="{{ $previewDateStart->format("Y-m-d") }}">{{ $previewDateStart->format('M j, Y') }}</time>
-            @endcomponent
-      @elseif (empty($previewDateStart))
-      @elseif ($previewDateStart and $previewDateEnd)
-        @component('components.atoms._date')
-            @slot('tag','p')
-            @slot('variation','f-previewdate')
-            Member previews:
-            @if($previewDateStart->format("Y") == $previewDateEnd->format("Y"))
-                @if($previewDateStart->format("M") == $previewDateEnd->format("M"))
-                <time datetime="{{ $previewDateStart->format("Y-m-d") }}">{{ $previewDateStart->format('M j') }}</time>&ndash;<time datetime="{{ $previewDateEnd->format("Y-m-d") }}">{{ $previewDateEnd->format('j, Y') }}</time>
-              @else
-                <time datetime="{{ $previewDateStart->format("Y-m-d") }}">{{ $previewDateStart->format('M j') }}</time>&ndash;<time datetime="{{ $previewDateEnd->format("Y-m-d") }}">{{ $previewDateEnd->format('M j, Y') }}</time>
-              @endif
-            @else
-            <time datetime="{{ $previewDateStart->format("Y-m-d") }}">{{ $previewDateStart->format('M j, Y') }}</time>&ndash;<time datetime="{{ $previewDateEnd->format("Y-m-d") }}">{{ $previewDateEnd->format('M j, Y') }}</time>
-            @endif
-        @endcomponent
-      @endif
-      @endif
+      @component('components.organisms._o-preview-dates')
+        @slot('previewDateStart', $previewDateStart ?? null)
+        @slot('previewDateEnd', $previewDateEnd ?? null)
+      @endcomponent
 
       {{-- Regular dates --}}
       @if (!empty($dateStart) and empty($dateEnd))
