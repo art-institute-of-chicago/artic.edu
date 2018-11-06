f19eb5a8f03ccda4303d1dd33aa50311f4505c7b
nikhiltri <ntrivedi@artic.edu>
2018-11-06 09:36:11 -0600

    Update CMS footer with website version number

---
+++ resources/views/vendor/twill/partials/footer.blade.php
@@ -0,0 +1,27 @@
+@if($errors->any())
+    <div class="notif notif--error">
+        <div class="notif__inner">{{ $errors->first() }}</div>
+    </div>
+@elseif (session('status'))
+    <div class="notif notif--success">
+        <div class="notif__inner">
+            <button type="button" class="notif__close" aria-label="alertClose" onclick="this.parentNode.parentNode.remove()">
+                <span class="icon icon--close_modal">
+                    <svg><title>Close</title><use xlink:href="#close_modal"></use></svg>
+                </span>
+            </button>
+            {{ session('status') }}
+        </div>
+    </div>
+@elseif (session('restoreMessage'))
+    <div class="notif notif--warning">
+        <div class="notif__inner">{{ session('restoreMessage') }}</div>
+    </div>
+@endif
+
+<footer class="footer">
+    <div class="container">
+        <span class="footer__copyright">Version {{ config('aic.version') }}</span>
+        <span class="footer__version"><a href="https://twill.io" target="_blank" class="f--light-hover" tabindex="0">Made with Twill</a></span>
+    </div>
+</footer>
