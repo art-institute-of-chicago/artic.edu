e9b8e772878be9895c99238461a47168bd0f2b7c
Illya Moskvin <imoskvin@artic.edu>
2018-11-19 16:09:05 -0600

    Fix HTML5 audio render on AJAX load [WEB-644]

--- frontend/js/functions/videojsActivate.js
+++ frontend/js/functions/videojsActivate.js
@@ -1,52 +1,60 @@
 const videojsActivate = function() {
 
-    // https://stackoverflow.com/questions/39121463/videojs-5-plugin-add-button
-    var vjsButtonComponent = videojs.getComponent('Button');
+  // https://stackoverflow.com/questions/39121463/videojs-5-plugin-add-button
+  function _registerDownloadButton() {
+    var vjsButtonComponent = window.videojs.getComponent('Button');
 
     window.videojs.registerComponent('DownloadButton', videojs.extend(vjsButtonComponent, {
-        constructor: function () {
-            vjsButtonComponent.apply(this, arguments);
-            this.controlText('Download audio');
-        },
-        handleClick: function () {
-            window.open(this.player_.cache_.src);
-        },
-        buildCSSClass: function () {
-            return 'vjs-control vjs-download-button';
-        },
+      constructor: function () {
+        vjsButtonComponent.apply(this, arguments);
+        this.controlText('Download audio');
+      },
+      handleClick: function () {
+        window.open(this.player_.cache_.src);
+      },
+      buildCSSClass: function () {
+        return 'vjs-control vjs-download-button';
+      },
     }));
+  }
 
-    // activate all .video-js elements
+  function _activateAudioPlayers() {
     var audios = document.getElementsByClassName('video-js')
 
     for (var i=0, max=audios.length; i < max; i++) {
-        window.videojs( audios[i], {
+      window.videojs( audios[i], {
+        "children": [
+          "MediaLoader",
+          // "PosterImage",
+          // "TextTrackDisplay",
+          // "LoadingSpinner",
+          // "BigPlayButton",
+          {
+            "name": "controlBar",
             "children": [
-                "MediaLoader",
-                // "PosterImage",
-                // "TextTrackDisplay",
-                // "LoadingSpinner",
-                // "BigPlayButton",
-                {
-                    "name": "controlBar",
-                    "children": [
-                        "PlayToggle",
-                        "CurrentTimeDisplay",
-                        "TimeDivider",
-                        "DurationDisplay",
-                        "ProgressControl",
-                        "MuteToggle",
-                        "VolumeControl",
-                        "DownloadButton", // our addition
-                        "LiveDisplay"
-                    ]
-                },
-                "ErrorDisplay",
-                // "TextTrackSettings",
-                "ResizeManager"
+              "PlayToggle",
+              "CurrentTimeDisplay",
+              "TimeDivider",
+              "DurationDisplay",
+              "ProgressControl",
+              "MuteToggle",
+              "VolumeControl",
+              "DownloadButton", // our addition
+              "LiveDisplay"
             ]
-        });
+          },
+          "ErrorDisplay",
+          // "TextTrackSettings",
+          "ResizeManager"
+        ]
+      });
     }
+  }
+
+  document.addEventListener('ajaxPageLoad:complete', _activateAudioPlayers, false);
+
+  _registerDownloadButton();
+  _activateAudioPlayers();
 }
 
 export default videojsActivate;
