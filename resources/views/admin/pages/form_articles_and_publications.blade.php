2bde2028e78b3ce50aca3cc87a3d507e8a4e0dff
Kyle Wayne Luck <kylewayneluck@gmail.com>
2018-12-06 18:04:57 -0500

    Clean up old articles browser

--- resources/views/admin/pages/form_articles_and_publications.blade.php
+++ resources/views/admin/pages/form_articles_and_publications.blade.php
@@ -1,13 +1,4 @@
 @section('contentFields')
-
-    {{-- @formField('browser', [
-        'routePrefix' => 'collection.articles_publications',
-        'max' => 5,
-        'moduleName' => 'articles',
-        'name' => 'articles',
-        'label' => 'Featured articles'
-    ]) --}}
-
     @formField('browser', [
         'routePrefix' => 'collection.articles_publications',
         'max' => 5,
