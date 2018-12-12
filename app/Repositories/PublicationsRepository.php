35a7b5581f0d8d34c12ef89ab403e971f6091802
Illya Moskvin <imoskvin@artic.edu>
2018-12-12 13:05:03 -0600

    Hide draft publications from search results [WEB-771]

--- app/Repositories/PublicationsRepository.php
+++ app/Repositories/PublicationsRepository.php
@@ -2,14 +2,13 @@
 
 namespace App\Repositories;
 
-use App\Models\GenericPage;
 use App\Models\Api\Search;
 
 class PublicationsRepository
 {
     public function searchApi($string, $perPage = null, $page = null, $columns = [])
     {
-        $search = Search::query()->search($string)->resources(["digital-catalogs", "printed-catalogs"]);
+        $search = Search::query()->search($string)->published()->resources(["digital-catalogs", "printed-catalogs"]);
 
         $results = $search->getSearch($perPage, $columns, null, $page);
 
