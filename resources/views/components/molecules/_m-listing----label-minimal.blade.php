f712c4adf866c25adb8794ce5d1a4fab39f751c6
Chris Hale <design@chrishale.co.uk>
2019-01-15 15:00:58 +0000

    adds closer look eye to collection landing featured items

--- resources/views/components/molecules/_m-listing----label-minimal.blade.php
+++ resources/views/components/molecules/_m-listing----label-minimal.blade.php
@@ -1,4 +1,4 @@
-<{{ $tag or 'li' }} class="m-listing m-listing--w-meta-bottom{{ (isset($variation)) ? ' '.$variation : '' }}">
+<{{ $tag or 'li' }} class="m-listing m-listing--closer-look m-listing--w-meta-bottom{{ (isset($variation)) ? ' '.$variation : '' }}">
     <a href="{!! $href ?? route('digitalLabels.show', $item) !!}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
         <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}{{ ($item->videoFront) ? ' m-listing__img--video' : '' }}">
             @if ($item->imageFront('hero'))
@@ -23,6 +23,10 @@
             @if ($item->isVideo)
                 <svg class="icon--play--48"><use xlink:href="#icon--play--48" /></svg>
             @endif
+
+            <span class="m-listing__img__overlay">
+                <svg class="icon--closer-look"><use xlink:href="#icon--closer-look"></use></svg>
+            </span>
         </span>
         <span class="m-listing__meta">
             <em class="type f-tag">Digital Label</em>
