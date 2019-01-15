8f4c50d0fd0c102a9eb8d8979a200500ddf69524
Kyle Wayne Luck <kylewayneluck@gmail.com>
2019-01-15 17:25:56 -0500

    Add digital_label.blade.php for block editor, and format block variation title text for _m-listing--closer-look

---
+++ resources/views/site/blocks/digital_label.blade.php
@@ -0,0 +1,26 @@
+@php
+    
+$ids = $block->browserIds('digitalLabels');
+$digitalLabel = \App\Models\Api\DigitalLabel::query()->ids($ids)->get()->first();
+
+@endphp
+
+@if (isset($digitalLabel))
+    @component('components.molecules._m-listing----label')
+        @slot('variation', 'o-blocks__block')
+        @slot('item', $digitalLabel)
+        @slot('titleFont', 'f-list-4')
+        @slot('imageSettings', array(
+            'fit' => 'crop',
+            'ratio' => '16:9',
+            'srcset' => array(200,400,600,1000),
+            'sizes' => aic_imageSizes(array(
+                    'xsmall' => '58',
+                    'small' => '58',
+                    'medium' => '38',
+                    'large' => '28',
+                    'xlarge' => '28',
+            )),
+        ))
+    @endcomponent
+@endif
