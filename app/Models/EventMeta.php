500274f31d06f6bdd64e8aab20a81dc02eb127f4
Illya Moskvin <imoskvin@artic.edu>
2019-01-16 15:04:39 -0600

    Fix EventMetas to extend Eloquent model [WEB-674]

--- app/Models/EventMeta.php
+++ app/Models/EventMeta.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EventMeta extends AbstractModel
+class EventMeta extends Model
 {
     protected $fillable = [
         'date',
