a23ee9ae646fef4dfdcdd2b5d189217fea45929a
Illya Moskvin <imoskvin@artic.edu>
2019-01-16 14:50:15 -0600

    Use publish dates for all `published` calls [WEB-674]

--- app/Models/AbstractModel.php
+++ app/Models/AbstractModel.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use A17\Twill\Models\Model;
 
+/**
+ * This is a place for us to override default Twill model functionality.
+ * Currently, it only affects bona-fide Models, not Slugs, Translations, Revisions, etc.
+ */
+
 class AbstractModel extends Model
 {
+
+    /**
+     * Don't just check the `published` column! Also check
+     * `publish_start_date` and `publish_end_date`. See
+     * `scopeVisible` on `\A17\Twill\Models\Model`.
+     */
+    public function scopePublished($query)
+    {
+        return $query->visible()->wherePublished(true);
+    }
 
 }
