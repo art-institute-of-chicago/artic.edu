4f55a7f394b9f29d98c9370e5de6632c6ed0ac2f
Kyle Wayne Luck <kylewayneluck@gmail.com>
2019-02-06 12:21:48 -0500

    Add item type to articles grid

--- resources/views/site/articles.blade.php
+++ resources/views/site/articles.blade.php
@@ -84,7 +84,7 @@
       @slot('cols_large','4')
       @slot('cols_xlarge','4')
       @foreach ($articles as $item)
-          @component('components.molecules._m-listing----article-minimal')
+          @component('components.molecules._m-listing----' . strtolower($item->type) . '-minimal')
             @slot('item', $item)
             @slot('imageSettings', array(
                 'fit' => 'crop',
