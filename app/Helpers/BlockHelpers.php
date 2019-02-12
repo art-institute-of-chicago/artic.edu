5413af848961618ff7efd870049059cf8e517c8e
Illya Moskvin <imoskvin@artic.edu>
2019-02-12 12:58:51 -0600

    Remove `ddd` function
    Unfortunately, it don't work for Chrome 72. Instead, downgrade to
    Chrome 71 using the following link:
    
    https://www.slimjet.com/chrome/google-chrome-old-version.php
    
    Then, add this to your `/etc/hosts` to prevent auto-updates:
    
    127.0.0.1   tools.google.com

--- app/Helpers/BlockHelpers.php
+++ app/Helpers/BlockHelpers.php
@@ -9,19 +9,3 @@ if (!function_exists('getBlocksForEditor')) {
         return array_intersect(array_keys(config('twill.block_editor.blocks')), $toUse);
     }
 }
-
-/**
- * Putting this here because it fixes a blocker.
- *
- * @link https://medium.com/@radicalloop/laravel-dd-var-function-is-not-working-in-new-chrome-9bc2aa689b9a
- * @link https://bugs.chromium.org/p/chromium/issues/detail?id=930061
- */
-if (!function_exists('ddd')) {
-    function ddd(...$args)
-    {
-        if (!headers_sent()) {
-            header('HTTP/1.1 500 Internal Server Error');
-        }
-        call_user_func_array('dd', $args);
-    }
-}
