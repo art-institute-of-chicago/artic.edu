bf3e7783a896396747d3a146cc575d6a60b99cc1
nikhiltri <ntrivedi@artic.edu>
2019-02-14 14:38:07 -0600

    Truncate download filename to first 50 characters of artwork title [WEB-572]

--- app/Libraries/LakeviewImageService.php
+++ app/Libraries/LakeviewImageService.php
@@ -36,7 +36,7 @@ class LakeviewImageService implements ImageServiceInterface
         $credit = null;
         $creditUrl = null;
         $shareTitle = null;
-        $downloadName = $object->main_reference_number .' - ' .truncateStr($object->title, 100) .'.jpg';
+        $downloadName = $object->main_reference_number .' - ' .truncateStr($object->title, 50) .'.jpg';
 
         $preLoadedInfo = $this->getInfo($object, $imageField);
 
