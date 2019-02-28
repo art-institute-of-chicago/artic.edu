1a030a75ddc59542913999c0ccc48b0710f1c3bc
nikhiltri <ntrivedi@artic.edu>
2019-02-28 10:29:31 -0600

    Fix preview not working for Exhibition Press Rooms [WEB-878]

--- app/Http/Controllers/Admin/ExhibitionPressRoomController.php
+++ app/Http/Controllers/Admin/ExhibitionPressRoomController.php
@@ -7,6 +7,7 @@ use A17\Twill\Http\Controllers\Admin\ModuleController;
 class ExhibitionPressRoomController extends ModuleController
 {
     protected $moduleName = 'exhibitionPressRooms';
+    protected $previewView = 'site.genericPage.show';
 
     protected function formData($request)
     {
@@ -15,5 +16,10 @@ class ExhibitionPressRoomController extends ModuleController
         return [
             'baseUrl' => $baseUrl,
         ];
+    }
+
+    protected function previewData($item)
+    {
+        return $this->repository->getShowData($item);
     }
 }
--- app/Repositories/ExhibitionPressRoomRepository.php
+++ app/Repositories/ExhibitionPressRoomRepository.php
@@ -17,4 +17,22 @@ class ExhibitionPressRoomRepository extends ModuleRepository
     {
         $this->model = $model;
     }
+
+    // Show data, moved here to allow preview
+    public function getShowData($item, $slug = null, $previewPage = null)
+    {
+        return [
+            'borderlessHeader' => !(empty($item->imageFront('banner'))),
+            'subNav' => null,
+            'nav' => null,
+            'intro' => $item->short_description,
+            'headerImage' => $item->imageFront('banner'),
+            "title" => $item->title,
+            "breadcrumb" => [],
+            "blocks" => null,
+            'featuredRelated' => [],
+            'nav' => [],
+            'page' => $item,
+        ];
+    }
 }
