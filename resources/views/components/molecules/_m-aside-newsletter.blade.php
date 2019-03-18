690675201baca895b6c7a43e7998334f32a0bf51
Illya Moskvin <imoskvin@artic.edu>
2019-03-18 11:36:00 -0500

    Fix indentation in newsletter molecule

--- resources/views/components/molecules/_m-aside-newsletter.blade.php
+++ resources/views/components/molecules/_m-aside-newsletter.blade.php
@@ -2,20 +2,20 @@
     $rand = rand(1, 1000);
 @endphp
 <form class="m-aside-newsletter{{ (isset($variation)) ? ' '.$variation : ' ' }}{{ (isset($error) and $error) ? ' s-error' : '' }}{{ (isset($success) and $success) ? ' s-success' : '' }}" action="/subscribe" data-behavior="newsletter" novalidate>
-  <fieldset>
-       <legend><h2 class="title f-list-3">{{ $copy ?? 'Sign up for our enewsletter to receive updates.' }}</h2></legend>
-    <span class="m-aside-newsletter__field">
-      <label for="email{{ $rand }}" class="f-secondary">Email address</label>
-      {{ csrf_field() }}
-      <input type="hidden" name="list" id="list{{ $rand }}" value="{{ $list ?? '' }}">
-      <input type="email" name="email" id="email{{ $rand }}" class="f-secondary" placeholder="{{ $placeholder ?? '' }}">
-      <button type="submit" class="{{ $btnFont ?? 'f-buttons' }}" data-gtm-event-category="subscribe" data-gtm-event-action="{{$seo->title}}" data-gtm-event="email">Subscribe</button>
-    </span>
-    @if (isset($error) and $error)
-    <em class="m-aside-newsletter__msg m-aside-newsletter__msg--error f-buttons">{{ $error }}</em>
-    @endif
-    @if (isset($success) and $success)
-    <em class="m-aside-newsletter__msg m-aside-newsletter__msg--success f-buttons">{{ $success }}</em>
-    @endif
-  </fieldset>
+    <fieldset>
+        <legend><h2 class="title f-list-3">{{ $copy ?? 'Sign up for our enewsletter to receive updates.' }}</h2></legend>
+        <span class="m-aside-newsletter__field">
+            <label for="email{{ $rand }}" class="f-secondary">Email address</label>
+            {{ csrf_field() }}
+            <input type="hidden" name="list" id="list{{ $rand }}" value="{{ $list ?? '' }}">
+            <input type="email" name="email" id="email{{ $rand }}" class="f-secondary" placeholder="{{ $placeholder ?? '' }}">
+            <button type="submit" class="{{ $btnFont ?? 'f-buttons' }}" data-gtm-event-category="subscribe" data-gtm-event-action="{{$seo->title}}" data-gtm-event="email">Subscribe</button>
+        </span>
+        @if (isset($error) and $error)
+        <em class="m-aside-newsletter__msg m-aside-newsletter__msg--error f-buttons">{{ $error }}</em>
+        @endif
+        @if (isset($success) and $success)
+        <em class="m-aside-newsletter__msg m-aside-newsletter__msg--success f-buttons">{{ $success }}</em>
+        @endif
+    </fieldset>
 </form>
