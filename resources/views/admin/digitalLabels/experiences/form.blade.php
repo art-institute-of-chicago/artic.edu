3b808fac7e0192c0376152a463fb694c9696e32c
Yanhao Li <hi@yanhaoli.com>
2019-03-20 12:15:33 -0400

    Working on experiences form

--- resources/views/admin/digitalLabels/experiences/form.blade.php
+++ resources/views/admin/digitalLabels/experiences/form.blade.php
@@ -22,3 +22,43 @@
         'label' => 'Archived'
     ])
 @stop
+
+@section('fieldsets')
+<a17-fieldset title="Attract" id="attract" :open="true">
+    @formField('input', [
+        'name' => 'attract_title',
+        'label' => 'Title'
+    ])
+
+    @formField('input', [
+        'name' => 'youtube_url',
+        'label' => 'Youtube URL'
+    ])
+
+    @formField('input', [
+        'name' => 'attract_subhead',
+        'label' => 'Subhead'
+    ])
+</a17-fieldset>
+
+<a17-fieldset title="End" id="end" :open="true">
+    @formField('input', [
+        'name' => 'media_title',
+        'label' => 'Media Title'
+    ])
+
+    <br />
+
+    <a17-fieldset title="Credit" id="end" :open="true">
+        @formField('input', [
+            'name' => 'end_credit_subhead',
+            'label' => 'Subhead'
+        ])
+
+        @formField('input', [
+            'name' => 'end_credit_copy',
+            'label' => 'Copy'
+        ])
+    </a17-fieldset>
+</a17-fieldset>
+@stop
