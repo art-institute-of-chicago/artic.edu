8957081bbd4647f84178e3f74dff1495b8b79d01
Kyle Wayne Luck <kylewayneluck@gmail.com>
2019-04-03 15:03:52 -0400

    Remove further reading module from interactive feature kiosk view

--- resources/views/site/digitalLabelKiosk.blade.php
+++ resources/views/site/digitalLabelKiosk.blade.php
@@ -19,34 +19,4 @@
         </script>
     </section>
 
-    <section>
-        @component('components.molecules._m-title-bar')
-            Further Reading
-        @endcomponent
-        @component('components.organisms._o-grid-listing')
-            @slot('variation', 'o-grid-listing--single-row o-grid-listing--scroll@xsmall o-grid-listing--scroll@small o-grid-listing--hide-extra@medium o-grid-listing--gridlines-cols o-grid-listing--gridlines-rows')
-            @slot('cols_medium','3')
-            @slot('cols_large','4')
-            @slot('cols_xlarge','4')
-            @slot('behavior','dragScroll')
-            @foreach ($furtherReading as $item)
-                @component('components.molecules._m-listing----article')
-                    @slot('item', $item)
-                    @slot('imageSettings', array(
-                        'fit' => 'crop',
-                        'ratio' => '16:9',
-                        'srcset' => array(200,400,600),
-                        'sizes' => aic_imageSizes(array(
-                            'xsmall' => '216px',
-                            'small' => '216px',
-                            'medium' => '18',
-                            'large' => '13',
-                            'xlarge' => '13',
-                        )),
-                    ))
-                @endcomponent
-            @endforeach
-        @endcomponent
-    </section>
-
 @endsection
