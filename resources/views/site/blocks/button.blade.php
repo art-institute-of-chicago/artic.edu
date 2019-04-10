a5216dbdac77fcced0ad5a7db25365ec6c612105
nikhiltri <ntrivedi@artic.edu>
2019-04-10 17:10:43 -0500

    Alter Button module to standard size [WEB-972]

--- resources/views/site/blocks/button.blade.php
+++ resources/views/site/blocks/button.blade.php
@@ -1,6 +1,6 @@
 @component('components.atoms._btn')
     @slot('tag', 'a')
-    @slot('variation', 'o-blocks__block')
+    @slot('variation', 'o-blocks__block btn-module')
     @slot('href', $block->input('link'))
     {{ $block->input('title') }}
 @endcomponent
