cc4918d4cae5562df0a1267a9e6f5c18b1b8f125
Yanhao Li <hi@yanhaoli.com>
2019-04-16 15:45:30 -0400

    Added nested module shortcut to experience and digitallabel form

--- resources/views/admin/digitalLabels/experiences/form.blade.php
+++ resources/views/admin/digitalLabels/experiences/form.blade.php
@@ -5,8 +5,9 @@
 @extends('twill::layouts.form')
 
 @section('contentFields')
+    <br/><strong><a href="{{ url('collection/experiences/' . $item->id . '/slides') }}">{{ $item->slides->count() }} Slides</a></strong></br>
     <br/><h1><strong>iPad URL:</strong> <a href={{ $ipad_url }}>{{ $ipad_url }}</a></h1>
-    <br/ ><h1><strong>Bundle ID: </strong>{{ $item->id }}</h1>
+    <br/><h1><strong>Bundle ID: </strong>{{ $item->id }}</h1>
     @formField('checkbox', [
         'name' => 'archived',
         'label' => 'Archived'
