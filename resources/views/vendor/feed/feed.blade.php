2206aaa3a9baf048e10ab2498a923add402febee
nikhiltri <ntrivedi@artic.edu>
2019-04-19 17:05:21 -0500

    Update feed syntax to pass RSS 2.0 validation [WEB-957]

--- resources/views/vendor/feed/feed.blade.php
+++ resources/views/vendor/feed/feed.blade.php
@@ -1,30 +1,34 @@
 <?=
     /* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
-    '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
+    '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>'.PHP_EOL
 ?>
-<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     @foreach($meta as $key => $metaItem)
         @if($key === 'link')
             <{{ $key }}>{{ url($metaItem) }}</{{ $key }}>
+            <atom:link href="{{ url($metaItem) }}" rel="self" type="application/rss+xml" />
         @elseif($key === 'title')
             <{{ $key }}><![CDATA[{{ $metaItem }}]]></{{ $key }}>
+        @elseif($key === 'updated')
+            <pubDate>{{ \Carbon\Carbon::parse($metaItem)->toRssString() }}</pubDate>
+        @elseif($key === 'id')
+
         @else
             <{{ $key }}>{{ $metaItem }}</{{ $key }}>
         @endif
     @endforeach
     @foreach($items as $item)
         <item>
-            <id>{{ url($item->id) }}</id>
             <title><![CDATA[{{ $item->title }}]]></title>
             <link>{{ url($item->link) }}</link>
             <guid>{{ url($item->link) }}</guid>
             <dc:creator>{{ $item->author }}</dc:creator>
-            <description type="html">
+            <description>
                 <![CDATA[{!! $item->summary !!}]]>
             </description>
             <enclosure url="{{ url($item->enclosure) }}" length="{{ $item->enclosureLength }}" type="{{ $item->enclosureType }}" />
-            <pubDate>{{ $item->updated->toAtomString() }}</pubDate>
+            <pubDate>{{ $item->updated->toRssString() }}</pubDate>
             @if ($item->category)
             <category>{{ $item->category }}</category>
             @endif
