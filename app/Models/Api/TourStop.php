8e2dd9c3ad0eee14f577ce245a32c35416fc431f
Illya Moskvin <imoskvin@artic.edu>
2019-04-25 13:04:00 -0500

    Enable "Audio Tour Stop" module to target `mobile-sounds` [WEB-79]

--- app/Models/Api/TourStop.php
+++ app/Models/Api/TourStop.php
@@ -7,8 +7,8 @@ use App\Libraries\Api\Models\BaseApiModel;
 class TourStop extends BaseApiModel
 {
     protected $endpoints = [
-        'collection' => '/api/v1/tour-stops',
-        'resource'   => '/api/v1/tour-stops/{id}',
-        'search'     => '/api/v1/tour-stops/search'
+        'collection' => '/api/v1/mobile-sounds',
+        'resource'   => '/api/v1/mobile-sounds/{id}',
+        'search'     => '/api/v1/mobile-sounds/search'
     ];
 }
