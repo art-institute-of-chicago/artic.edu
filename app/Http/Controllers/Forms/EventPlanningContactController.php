3667afa882a00d4306aca3acdf2e828d4a2f487b
nikhiltri <ntrivedi@artic.edu>
2019-05-15 17:18:53 -0500

    Updates to Venue rental inquiry web form [WEB-974]

--- app/Http/Controllers/Forms/EventPlanningContactController.php
+++ app/Http/Controllers/Forms/EventPlanningContactController.php
@@ -23,6 +23,7 @@ class EventPlanningContactController extends FormController
         $blocks = array();
         $formBlocks = array();
         $inquiryFields = array();
+        $daytimeFields = array();
 
         $errors = session('errors');
 
@@ -42,10 +43,116 @@ class EventPlanningContactController extends FormController
                   'textCount' => false,
                   'value' => old('name'),
                   'error' => (!empty($errors) && $errors->first('name')) ? $errors->first('name') : null,
+                  'optional' => false,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => 'Name *',
+                ),
+            ),
+        );
+
+        $inquiryFields[]= array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'input',
+                  'variation' => null,
+                  'id' => 'address_1',
+                  'placeholder' => '',
+                  'textCount' => false,
+                  'value' => old('address_1'),
+                  'error' => (!empty($errors) && $errors->first('address_1')) ? $errors->first('address_1') : null,
                   'optional' => null,
                   'hint' => null,
                   'disabled' => false,
-                  'label' => 'Name',
+                  'label' => 'Street address',
+                ),
+            ),
+        );
+
+        $inquiryFields[]= array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'input',
+                  'variation' => null,
+                  'id' => 'address_2',
+                  'placeholder' => '',
+                  'textCount' => false,
+                  'value' => old('address_2'),
+                  'error' => (!empty($errors) && $errors->first('address_2')) ? $errors->first('address_2') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => 'Apartment or suite',
+                ),
+            ),
+        );
+
+        $inquiryFields[]= array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'input',
+                  'variation' => null,
+                  'id' => 'city',
+                  'placeholder' => '',
+                  'textCount' => false,
+                  'value' => old('city'),
+                  'error' => (!empty($errors) && $errors->first('city')) ? $errors->first('city') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => 'City',
+                ),
+            ),
+        );
+
+        $inquiryFields[] = array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'select',
+                  'variation' => 'm-fieldset__input-narrow-x3',
+                  'id' => 'state',
+                  'error' => (!empty($errors) && $errors->first('state')) ? $errors->first('state') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'value' => old('state'),
+                  'label' => 'State',
+                  'options' => $this->getStatesArray(),
+                ),
+                array(
+                  'type' => 'input',
+                  'variation' => 'm-fieldset__input-narrow-x3',
+                  'id' => 'zipcode',
+                  'placeholder' => '',
+                  'textCount' => false,
+                  'value' => old('zipcode'),
+                  'error' => (!empty($errors) && $errors->first('zipcode')) ? $errors->first('zipcode') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => 'Zipcode',
+                ),
+            ),
+        );
+
+        $inquiryFields[] = array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'select',
+                  'variation' => null,
+                  'id' => 'country',
+                  'error' => (!empty($errors) && $errors->first('country')) ? $errors->first('country') : null,
+                  'value' => old('country'),
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => 'Country',
+                  'options' => $this->getCountriesArray(),
                 ),
             ),
         );
@@ -83,7 +190,7 @@ class EventPlanningContactController extends FormController
                   'optional' => false,
                   'hint' => null,
                   'disabled' => false,
-                  'label' => 'Email',
+                  'label' => 'Email *',
                 ),
             ),
         );
@@ -99,10 +206,10 @@ class EventPlanningContactController extends FormController
                   'textCount' => false,
                   'value' => old('phone_number'),
                   'error' => (!empty($errors) && $errors->first('phone_number')) ? $errors->first('phone_number') : null,
-                  'optional' => null,
+                  'optional' => false,
                   'hint' => null,
                   'disabled' => false,
-                  'label' => 'Phone number',
+                  'label' => 'Phone number *',
                 ),
             ),
         );
@@ -125,61 +232,158 @@ class EventPlanningContactController extends FormController
         }
         $inquiryFields[] = $preferredContactFields;
 
-        $inquiryFields[] = [
-            'variation' => null,
+        $howDidYouHearFields = [
+            'variation' => 'm-fieldset__field--group',
             'blocks' => array(
                 array(
-                  "type" => 'textarea',
-                  'variation' => null,
-                  'id' => 'event_description',
-                  'placeholder' => '',
-                  'value' => old('event_description'),
-                  'error' => (!empty($errors) && $errors->first('event_description')) ? $errors->first('event_description') : null,
-                  'optional' => null,
-                  'hint' => '',
-                  'disabled' => false,
-                  'label' => 'Are you currently planning an event? If so, what type of event do you have in mind?',
+                    "type" => 'label',
+                    'variation' => 'm-fieldset__group-label',
+                    'error' => (!empty($errors) && $errors->first('how_did_you_hear')) ? $errors->first('how_did_you_hear') : null,
+                    'optional' => null,
+                    'hint' => 'Select all that apply',
+                    'label' => 'How did you hear about us?',
                 ),
             ),
         ];
+        $items = $this->getHowDidYouHearArray(old('how_did_you_hear'));
+        foreach($items as $key => $d) {
+            array_push($howDidYouHearFields['blocks'], $d);
+        }
+        $inquiryFields[] = $howDidYouHearFields;
 
-        $inquiryFields[] = [
+        $areYouPlanningFields = [
+            'variation' => 'm-fieldset__field--group',
+            'blocks' => array(
+                array(
+                  'type' => 'label',
+                  'variation' => 'm-fieldset__group-label',
+                  'error' => (!empty($errors) && $errors->first('are_you_currently_planning')) ? $errors->first('are_you_currently_planning') : null,
+                  'optional' => false,
+                  'hint' => null,
+                  'label' => 'Are you currently planning an event?',
+                )
+            ),
+        ];
+        foreach($this->getYesNoArray(old('are_you_currently_planning'), 'are_you_currently_planning') as $t) {
+            array_push($areYouPlanningFields['blocks'], $t);
+        }
+        $inquiryFields[] = $areYouPlanningFields;
+
+        $typeOfEventFields = [
+            'variation' => 'm-fieldset__field--group',
+            'blocks' => array(
+                array(
+                  "type" => 'label',
+                  'variation' => 'm-fieldset__group-label',
+                  'error' => (!empty($errors) && $errors->first('type_of_event')) ? $errors->first('type_of_event') : null,
+                  'optional' => null,
+                  'hint' => 'Select all that apply',
+                  'label' => 'If so, what type of event do you have in mind?',
+                )
+            ),
+        ];
+        foreach($this->getTypeOfEventArray(old('type_of_event')) as $d) {
+            array_push($typeOfEventFields['blocks'], $d);
+        }
+        $inquiryFields[] = $typeOfEventFields;
+
+
+        /*
+         *
+         *  Daytime information
+         *
+         */
+        $daytimeFields[]= array(
             'variation' => null,
             'blocks' => array(
                 array(
-                  "type" => 'textarea',
+                  'type' => 'input',
                   'variation' => null,
-                  'id' => 'dates',
+                  'id' => 'no_of_expected_guests',
                   'placeholder' => '',
-                  'value' => old('dates'),
-                  'error' => (!empty($errors) && $errors->first('dates')) ? $errors->first('dates') : null,
-                  'optional' => null,
-                  'hint' => '',
+                  'textCount' => false,
+                  'value' => old('no_of_expected_guests'),
+                  'error' => (!empty($errors) && $errors->first('no_of_expected_guests')) ? $errors->first('no_of_expected_guests') : null,
+                  'optional' => false,
+                  'hint' => null,
                   'disabled' => false,
-                  'label' => 'Are you looking for a specific date? If so, what date?',
+                  'label' => 'Number of expected guests',
                 ),
             ),
-        ];
+        );
 
-        $inquiryFields[] = [
+        $daytimeFields[] = array(
             'variation' => null,
             'blocks' => array(
                 array(
-                  "type" => 'textarea',
-                  'variation' => null,
-                  'id' => 'know_more_about',
-                  'placeholder' => '',
-                  'value' => old('know_more_about'),
-                  'error' => (!empty($errors) && $errors->first('know_more_about')) ? $errors->first('know_more_about') : null,
-                  'optional' => null,
-                  'hint' => '',
-                  'disabled' => false,
-                  'label' => 'Did you find the information you were looking for? If not, what would you like to know more about?',
+                    'type' => 'text',
+                    'content' => '<span class="input s-disabled"><label class="label f-secondary">Possible Dates *'
+                    .'<em class="label__hint">Please select 3 options for the date of your event.</em></label></span>'
                 ),
             ),
-        ];
+        );
 
-        $inquiryFields[] = array(
+        $daytimeFields[] = array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                  'type' => 'date-select',
+                  'variation' => 'm-fieldset__input-narrow-x3',
+                  'id' => 'possible_date_1',
+                  'placeholder' => 'mm/dd/yy',
+                  'value' => old('possible_date_1'),
+                  'error' => (!empty($errors) && $errors->first('possible_date_1')) ? $errors->first('possible_date_1') : null,
+                  'optional' => false,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => '',
+                ),
+                array(
+                  'type' => 'date-select',
+                  'variation' => 'm-fieldset__input-narrow-x3',
+                  'id' => 'possible_date_2',
+                  'placeholder' => 'mm/dd/yy',
+                  'value' => old('possible_date_2'),
+                  'error' => (!empty($errors) && $errors->first('possible_date_2')) ? $errors->first('possible_date_2') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => '',
+                ),
+                array(
+                  'type' => 'date-select',
+                  'variation' => 'm-fieldset__input-narrow-x3',
+                  'id' => 'possible_date_3',
+                  'placeholder' => 'mm/dd/yy',
+                  'value' => old('possible_date_3'),
+                  'error' => (!empty($errors) && $errors->first('possible_date_3')) ? $errors->first('possible_date_3') : null,
+                  'optional' => null,
+                  'hint' => null,
+                  'disabled' => false,
+                  'label' => '',
+                ),
+            ),
+        );
+
+        $daytimeFields[] = array(
+            'variation' => null,
+            'blocks' => array(
+                array(
+                    "type" => 'textarea',
+                    'variation' => null,
+                    'id' => 'other_info',
+                    'placeholder' => '',
+                    'value' => old('other_info'),
+                    'error' => (!empty($errors) && $errors->first('other_info')) ? $errors->first('other_info') : null,
+                    'optional' => null,
+                    'hint' => null,
+                    'disabled' => false,
+                    'label' => 'Please share any other important information',
+                ),
+            ),
+        );
+
+        $daytimeFields[] = array(
             'variation' => null,
             'blocks' => array(
                 array(
@@ -200,6 +404,13 @@ class EventPlanningContactController extends FormController
           'variation' => null,
           'fields' => $inquiryFields,
           'legend' => 'Inquiry Form',
+        ));
+
+        array_push($formBlocks, array(
+          'type' => 'fieldset',
+          'variation' => null,
+          'fields' => $daytimeFields,
+          'legend' => 'Daytime event',
         ));
 
         array_push($blocks, array(
@@ -251,13 +462,25 @@ class EventPlanningContactController extends FormController
 
         $eventPlanningContact = new EventPlanningContact;
         $eventPlanningContact->name = $validated['name'] ?? '';
+        $eventPlanningContact->address_1 = $validated['address_1'] ?? '';
+        $eventPlanningContact->address_2 = $validated['address_2'] ?? '';
+        $eventPlanningContact->city = $validated['city'] ?? '';
+        $eventPlanningContact->state = $validated['state'] ?? '';
+        $eventPlanningContact->zipcode = $validated['zipcode'] ?? '';
+        $eventPlanningContact->country = $validated['country'] ?? '';
         $eventPlanningContact->company = $validated['company'] ?? '';
         $eventPlanningContact->email = $validated['email'] ?? '';
         $eventPlanningContact->phone_number = $validated['phone_number'] ?? '';
         $eventPlanningContact->preferred_contact = $validated['preferred_contact'] ?? '';
-        $eventPlanningContact->event_description = $validated['event_description'] ?? '';
-        $eventPlanningContact->dates = $validated['dates'] ?? '';
-        $eventPlanningContact->know_more_about = $validated['know_more_about'] ?? '';
+
+        $eventPlanningContact->how_did_you_hear = $validated['how_did_you_hear'] ?? '';
+        $eventPlanningContact->are_you_currently_planning = $validated['are_you_currently_planning'] ?? '';
+        $eventPlanningContact->type_of_event = $validated['type_of_event'] ?? '';
+        $eventPlanningContact->no_of_expected_guests = $validated['no_of_expected_guests'] ?? '';
+        $eventPlanningContact->possible_date_1 = $this->getDateField($validated, 'possible_date_1');
+        $eventPlanningContact->possible_date_2 = $this->getDateField($validated, 'possible_date_2');
+        $eventPlanningContact->possible_date_3 = $this->getDateField($validated, 'possible_date_3');
+        $eventPlanningContact->other_info = $validated['other_info'] ?? '';
 
         Mail::to(config('forms.email_event_planning_to'))
             ->send(new FormEventPlanningContact($eventPlanningContact));
@@ -288,6 +511,71 @@ class EventPlanningContactController extends FormController
 
             if ($selected == $value) {
                 $item['checked'] = 'checked';
+            }
+
+            $list[] = $item;
+        }
+
+        return $list;
+    }
+
+    private function getHowDidYouHearArray($selected)
+    {
+        $hows = ['Open Table', 'TripAdvisor', 'Website search', 'Special event', 'Museum visit', 'The Knot', 'Chicago Social',
+                 'Party Slate', 'Choose Chicago', 'Other'];
+
+        $list = [];
+        foreach($hows as $value) {
+            $item = [
+              'type' => 'checkbox',
+              'variation' => '',
+              'id' => 'how_did_you_hear-'.$value,
+              'name' => 'how_did_you_hear[]',
+              'value' => $value,
+              'error' => null,
+              'optional' => null,
+              'hint' => null,
+              'disabled' => false,
+              'checked' => false,
+              'label' => $value
+            ];
+
+            if ($selected) {
+                if (in_array($value, $selected)) {
+                    $item['checked'] = 'checked';
+                }
+            }
+
+            $list[] = $item;
+        }
+
+        return $list;
+    }
+
+    private function getTypeOfEventArray($selected)
+    {
+        $types = ['Reception', 'Seated dinner', 'Wedding', 'Meeting', 'Performance Program', 'Nonprofit fundraiser', 'Other'];
+
+        $list = [];
+        foreach($types as $value) {
+            $item = [
+              'type' => 'checkbox',
+              'variation' => '',
+              'id' => 'type_of_event-'.$value,
+              'name' => 'type_of_event[]',
+              'value' => $value,
+              'error' => null,
+              'optional' => null,
+              'hint' => null,
+              'disabled' => false,
+              'checked' => false,
+              'label' => $value
+            ];
+
+            if ($selected) {
+                if (in_array($value, $selected)) {
+                    $item['checked'] = 'checked';
+                }
             }
 
             $list[] = $item;
--- app/Http/Requests/Form/EventPlanningContactRequest.php
+++ app/Http/Requests/Form/EventPlanningContactRequest.php
@@ -7,14 +7,25 @@ class EventPlanningContactRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'sometimes',
+            'name' => 'required',
+            'address_1' => 'sometimes',
+            'address_2' => 'sometimes',
+            'city' => 'sometimes',
+            'state' => 'sometimes',
+            'zipcode' => 'sometimes',
+            'country' => 'sometimes',
             'company' => 'sometimes',
-            'email' => 'sometimes',
-            'phone_number' => 'sometimes',
+            'email' => 'required',
+            'phone_number' => 'required',
             'preferred_contact' => 'sometimes',
-            'event_description' => 'sometimes',
-            'dates' => 'sometimes',
-            'know_more_about' => 'sometimes',
+            'how_did_you_hear' => 'sometimes',
+            'are_you_currently_planning' => 'sometimes',
+            'type_of_event' => 'sometimes',
+            'no_of_expected_guests' => 'sometimes',
+            'possible_date_1' => 'sometimes',
+            'possible_date_2' => 'sometimes',
+            'possible_date_3' => 'sometimes',
+            'other_info' => 'sometimes',
         ];
     }
 
