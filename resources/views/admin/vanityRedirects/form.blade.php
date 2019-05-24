55cc601f1f6f621c6945583433753d53b90c63b1
Illya Moskvin <imoskvin@artic.edu>
2019-05-24 16:14:01 -0500

    Make vanity redirects treat vanity paths as case insensitive [WEB-1031]

--- resources/views/admin/vanityRedirects/form.blade.php
+++ resources/views/admin/vanityRedirects/form.blade.php
@@ -1,5 +1,7 @@
 @extends('twill::layouts.form')
 
+<p>The vanity path must be defined in lowercase. Once defined, they are case insensitive during use.</p>
+
 @section('contentFields')
     @formField('input', [
         'name' => 'destination',
