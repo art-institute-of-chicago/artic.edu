c5348ae48806b879121bcb183f9fa732ede8a489
Yanhao Li <hi@yanhaoli.com>
2019-06-25 10:14:48 -0400

    Added migration to clean up legacy digitalLabel records from db

---
+++ database/migrations/2019_06_25_090845_cleanup_realted_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CleanupRealtedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('related')->where('related_type', 'digitalLabels')->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
