97dbb008ab530a2bea5d7fa265fee5450ac1eaa3
Illya Moskvin <imoskvin@artic.edu>
2019-07-26 16:57:23 -0500

    Move "Related" admin fieldset to partial [WEB-1179]

--- resources/views/admin/articles/form.blade.php
+++ resources/views/admin/articles/form.blade.php
@@ -161,23 +161,7 @@
         ])
     </a17-fieldset>
 
-    @php
-        $relatedTos = $item->relatedTos;
-    @endphp
-    @if ($relatedTos->isNotEmpty())
-        <a17-fieldset id="related" title="Related">
-
-            <p>The following content has been related to this one. These will be used to bring this item into global search results.</p>
-
-            <ol style="margin: 1em 0; padding-left: 40px">
-                @foreach($relatedTos as $related)
-                    <li style="list-style-type: decimal; margin-bottom: 0.5em">
-                        {!! Str::title($related->subject_type) !!}: <a href="{!! route($related->subject_type .'.show', $related->subject) !!}">{{ $related->subject->title }}</a>
-                    </li>
-                @endforeach
-            </ol>
-        </a17-fieldset>
-    @endif
+    @include('admin.partials.related')
 
     @include('admin.partials.meta')
 
