b162019b687e8657a5f67664a85aa098a90c51df
Illya Moskvin <imoskvin@artic.edu>
2019-08-08 14:21:04 -0500

    Remove leftover @dump call

--- resources/views/components/molecules/_m-listing----offer.blade.php
+++ resources/views/components/molecules/_m-listing----offer.blade.php
@@ -1,5 +1,4 @@
 <{{ $tag ?? 'li' }} class="m-listing m-listing--w-meta-bottom{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-behavior="blurMyBackground"' : '' !!}>
-  @dump($item)
   <a href="{!! $item->url !!}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
     <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}"{{ (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-blur-img' : '' }}>
         @if (isset($image) || $item->imageFront('hero'))
