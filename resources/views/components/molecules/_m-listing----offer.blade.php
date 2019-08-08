2975705846725fa247f3cdaa17654f8852373d3f
Illya Moskvin <imoskvin@artic.edu>
2019-08-08 13:47:48 -0500

    Fix links for related offers on exhibition detail [WEB-1243]

--- resources/views/components/molecules/_m-listing----offer.blade.php
+++ resources/views/components/molecules/_m-listing----offer.blade.php
@@ -1,5 +1,6 @@
 <{{ $tag ?? 'li' }} class="m-listing m-listing--w-meta-bottom{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-behavior="blurMyBackground"' : '' !!}>
-  <a href="{!! $item->web_url !!}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
+  @dump($item)
+  <a href="{!! $item->url !!}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
     <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}"{{ (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-blur-img' : '' }}>
         @if (isset($image) || $item->imageFront('hero'))
             @component('components.atoms._img')
