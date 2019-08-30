fec44c8972357a3db2194ea57970591b1a035ed9
Illya Moskvin <imoskvin@artic.edu>
2019-08-30 10:40:12 -0500

    Restore `app.debug` config

--- config/app.php
+++ config/app.php
@@ -28,6 +28,18 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+     */
+
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
