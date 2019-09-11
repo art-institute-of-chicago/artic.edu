d701b546ce8416f6060e071b07bf2eb933f01111
nikhiltri <ntrivedi@artic.edu>
2019-09-11 12:03:32 -0500

    Correct log for when the 'restrict' image parameter is not present [WEB-1304]

--- resources/views/components/atoms/_img.blade.php
+++ resources/views/components/atoms/_img.blade.php
@@ -38,13 +38,13 @@
 @endphp
 <img
     alt="{{ $image['alt'] ?? '' }}{{ $alt ?? '' }}"
-    class="{{ $image['class'] ?? '' }} {{ $class ?? '' }} {{ $image['restrict'] ? 'restrict' : '' }}"
+    class="{{ $image['class'] ?? '' }} {{ $class ?? '' }} {{ isset($image['restrict']) && $image['restrict'] ? 'restrict' : '' }}"
     @if (empty($srcset) and isset($src))
         data-src="{{ $src ?? '' }}"
     @else
         src="{{ $src ?? '' }}"
     @endif
-    @if ($image['restrict'])
+    @if (isset($image['restrict']) && $image['restrict'])
        data-behavior="restrictDownload"
     @endif
     @if (isset($_GET['print']) or (isset($settings['lazyload']) and $settings['lazyload'] === false))
