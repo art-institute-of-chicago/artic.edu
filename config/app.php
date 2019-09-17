300453b787a2261abda4191ee7c4cfe66cbdde6b
nikhiltri <ntrivedi@artic.edu>
2019-09-17 11:56:51 -0500

    Add VendorServiceProvider back in [WEB-1304, WEB-927]
    In April we found that the work related to WEB-927 which overrode Twill's
    Block model with our own broke the saving of images in the CMS. We ran into
    a similar issue with the work in WEB-1304, which resurfaced the need for using
    our own custom Block model. The same bug from last April popped up, and
    reapplying the fix start in WEB-927 fixed the issue.

--- config/app.php
+++ config/app.php
@@ -192,7 +192,7 @@ return [
         /*
          * Application Service Providers...
          */
-        // App\Providers\VendorServiceProvider::class, // Commented our because it breaks saving images in image blocks
+        App\Providers\VendorServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
