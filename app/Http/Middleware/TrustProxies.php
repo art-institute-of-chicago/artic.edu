c2e11aca412039294478560ce926be4dfefa8e6d
Illya Moskvin <imoskvin@artic.edu>
2019-09-18 17:24:48 -0500

    Check CloudFront IPs for TrustProxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
+use Closure;
+use Cache;
 
 class TrustProxies extends Middleware
 {
@@ -12,7 +14,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies = '*';
+    protected $proxies = [];
 
     /**
      * The headers that should be used to detect proxies.
@@ -20,4 +22,26 @@ class TrustProxies extends Middleware
      * @var string
      */
     protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    /**
+     * Add remote address to trusted proxy list
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $ips = Cache::remember('list-cloudfront-ips', 60, function () {
+            return file_get_contents('http://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips');
+        });
+
+        $ips = json_decode($ips);
+
+        $this->proxies = array_merge(
+            $this->proxies,
+            $ips->{"CLOUDFRONT_GLOBAL_IP_LIST"},
+            $ips->{"CLOUDFRONT_REGIONAL_EDGE_IP_LIST"}
+        );
+
+        array_push($this->proxies, $request->server->get('REMOTE_ADDR'));
+        parent::handle($request, $next);
+        return $next($request);
+    }
 }
