fbd54a6314b56524123b5e7f415c5ae9347fdbc9
Illya Moskvin <imoskvin@artic.edu>
2019-09-19 15:27:35 -0500

    Move geotarget action to controller

--- routes/api.php
+++ routes/api.php
@@ -317,29 +317,7 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'v1'], function () {
 
-    Route::get('geotarget', function() {
-
-        // Will be "" for https://ipinfo.io/bogon
-        // Response is a string that ends with a newline
-        $url = 'https://ipinfo.io/' . request()->ip() . '/city?token=' . config('app.ipinfo');
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        // Set so curl_exec returns the result instead of outputting it.
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Disable SSL cert verification
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        // Get the response and close the channel.
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        return [
-            'is_local' => (empty($response) ? null : (strpos($response, 'Chicago') !== false)),
-        ];
-    });
+    Route::get('geotarget', 'API\GeotargetController@geotarget');
 
     Route::get('/', function () {
         return "API";
