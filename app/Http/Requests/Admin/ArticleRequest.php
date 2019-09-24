f0714def58be9831b8ccb38ad14172eca7c79e83
Illya Moskvin <imoskvin@artic.edu>
2019-09-24 10:57:43 -0500

    Restrict article display date requirement to updates [WEB-1316, WEB-1278]

--- app/Http/Requests/Admin/ArticleRequest.php
+++ app/Http/Requests/Admin/ArticleRequest.php
@@ -6,16 +6,11 @@ use A17\Twill\Http\Requests\Admin\Request;
 
 class ArticleRequest extends Request
 {
-    public function rules()
+    public function rulesForUpdate()
     {
-        $rules = [
-            'title' => 'required',
+        return [
             'date' => 'required',
             'heading' => 'max:255',
         ];
-
-        // $rules['date'] = 'date_format:m/d/Y H:i';
-
-        return $rules;
     }
 }
