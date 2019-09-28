8084e01065956e94934416d17b066e0a55902608
nikhiltri <ntrivedi@artic.edu>
2019-09-28 01:03:26 -0500

    Get PageTest to work [WEB-354]

---
+++ database/migrations/2019_09_27_173037_add_basic_pages.php
@@ -0,0 +1,92 @@
+<?php
+
+use App\Models\Page;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBasicPages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (App::environment('testing')) {
+            Page::create([
+                'published' => true,
+                'position' => 7,
+                'title' => 'Research and Resources',
+                'type' => 7,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 8,
+                'title' => 'Articles and Publications',
+                'type' => 8,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 0,
+                'title' => 'Home',
+                'type' => 0,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 1,
+                'title' => 'Exhibitions and Events',
+                'type' => 1,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 2,
+                'title' => 'Art and Ideas',
+                'type' => 2,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 3,
+                'title' => 'Visit',
+                'type' => 3,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 4,
+                'title' => 'Articles',
+                'type' => 4,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 5,
+                'title' => 'Exhibition History',
+                'type' => 5,
+            ]);
+
+            Page::create([
+                'published' => true,
+                'position' => 6,
+                'title' => 'Collection',
+                'type' => 6,
+            ]);
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
--- tests/Feature/PageTest.php
+++ tests/Feature/PageTest.php
@@ -10,13 +10,9 @@ use App\Models\Page;
 class PageTest extends TestCase
 {
 
-    use RefreshDatabase;
-
     /** @test */
     public function it_loads_the_home_page()
     {
-        $page = factory(Page::class)->create(['type' => 0]);
-
         $response = $this->get('/');
         $response->assertStatus(200);
     }
