8d29464de3e5a5ef741306b52563cfc7c2ec4879
nikhiltri <ntrivedi@artic.edu>
2019-10-09 16:51:10 -0500

    Have users enter a leading slash into header hours links [WEB-1261]

--- database/migrations/2019_10_08_123127_refactor_hours_table.php
+++ database/migrations/2019_10_08_123127_refactor_hours_table.php
@@ -35,7 +35,7 @@ class RefactorHoursTable extends Migration
         $hour = new Hour;
         $hour->type = 0;        // Museum
         $hour->title = "Open daily 10:30–5:00, Thursdays until 8:00";
-        $hour->url = "visit";
+        $hour->url = "/visit";
         $hour->published = true;
         $hour->save();
 
