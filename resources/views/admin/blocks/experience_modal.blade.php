8aa78eae4b43b92b2789f02c70cd2a23ab8ec2c4
Yanhao Li <hi@yanhaoli.com>
2019-10-15 11:33:40 -0400

    Before refactoring the 3d vue component

--- resources/views/admin/blocks/experience_modal.blade.php
+++ resources/views/admin/blocks/experience_modal.blade.php
@@ -81,7 +81,16 @@
         'keepAlive' => true
 ])
     <br />
-    <a17-block-if_3d_model name="aic_3d_model" />
+    <a17-block-if_3d_model name="aic_split_3d_model" />
+    @push('vuexStore')
+        @php($aic3DModel = $item->AIC3DModel)
+        @foreach (['model_url', 'model_id', 'camera_position', 'camera_target', 'annotation_list'] as $name)
+            window.STORE.form.fields.push({
+                name: "{{ 'aic_3d_model[' . $name . ']' }}",
+                value: {!! json_encode($aic3DModel ? $aic3DModel->$name : '') !!}
+            })
+        @endforeach
+    @endpush
 @endcomponent
 
 @formField('wysiwyg', [
