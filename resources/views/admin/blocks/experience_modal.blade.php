46eff089e53b6099e8a27745ffae1a411c0a781d
Yanhao Li <hi@yanhaoli.com>
2019-10-17 17:03:30 -0400

    Fixed some typo on experience modal which block the 3d model form get rendered

--- resources/views/admin/blocks/experience_modal.blade.php
+++ resources/views/admin/blocks/experience_modal.blade.php
@@ -76,21 +76,12 @@
 
 @component('twill::partials.form.utils._connected_fields', [
         'fieldName' => 'modal_type',
-        'fieldValues' => '3d_modal',
+        'fieldValues' => '3d_model',
         'renderForBlocks' => true,
         'keepAlive' => true
 ])
     <br />
     <a17-block-aic_3d_model :name="fieldName('aic_split_3d_model')" :enhanced="false" />
-    {{-- @push('vuexStore')
-        @php($aic3DModel = $item->AIC3DModel)
-        @foreach (['model_url', 'model_id', 'camera_position', 'camera_target', 'annotation_list'] as $name)
-            window.STORE.form.fields.push({
-                name: "{{ 'aic_3d_model[' . $name . ']' }}",
-                value: {!! json_encode($aic3DModel ? $aic3DModel->$name : '') !!}
-            })
-        @endforeach
-    @endpush --}}
 @endcomponent
 
 @formField('wysiwyg', [
