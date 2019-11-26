415d52e45c38166f53fd63ac34c8c4585ebaacdb
Nicolas Davi <hello@nicolasdavi.fr>
2019-11-26 14:27:00 +0100

    A3-122 — restrict modal size if not cc0

--- resources/views/components/molecules/_m-media.blade.php
+++ resources/views/components/molecules/_m-media.blade.php
@@ -112,7 +112,7 @@
         }
     }
 @endphp
-<{{ $tag ?? 'figure' }} data-type="{{ $type }}" class="m-media m-media--{{ $size }}{{ (isset($item['variation'])) ? ' '.$item['variation'] : '' }}{{ (isset($variation)) ? ' '.$variation : '' }}">
+<{{ $tag ?? 'figure' }} data-type="{{ $type }}"{!! isset($media['cc0']) && $media['cc0'] ? ' data-restricted="true"' : '' !!} class="m-media m-media--{{ $size }}{{ (isset($item['variation'])) ? ' '.$item['variation'] : '' }}{{ (isset($variation)) ? ' '.$variation : '' }}">
     <span class="m-media__img{{ ($type === 'embed' || $type === 'video') ? ' m-media__img--video' : '' }}"{!! ($mediaBehavior) ? ' data-behavior="'.$mediaBehavior.'" aria-label="Media embed, click to play" tabindex="0"' : '' !!}{!! !empty($embed_height) ? ' style="height: ' . $embed_height . '"' : '' !!}{!! isset($media['restrict']) && $media['restrict'] ? ' data-restrict="true"' : '' !!}>
         @if ($type == 'image')
             @if ($showUrlFullscreen)
