018373cbec6ac1b1ddab00296f85c51246d5c409
nikhiltri <ntrivedi@artic.edu>
2019-12-09 17:19:00 -0600

    Take time off of hours admin forms [WEB-1443]

--- resources/views/admin/hours/form.blade.php
+++ resources/views/admin/hours/form.blade.php
@@ -15,10 +15,14 @@
     @formField('date_picker', [
         'name' => 'valid_from',
         'label' => 'Valid From',
+        'withTime' => false,
+        'required' => true
     ])
 
     @formField('date_picker', [
         'name' => 'valid_through',
         'label' => 'Valid Through',
+        'withTime' => false,
+        'required' => true
     ])
 @stop
