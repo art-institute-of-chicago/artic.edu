229dd2c93ca70e4617752edd876265c597e9d737
nikhiltri <ntrivedi@artic.edu>
2020-03-02 12:03:50 -0600

    Add "nofollow" to old events [WEB-1556]

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -134,6 +134,9 @@ class EventsController extends FrontController
         $this->seo->setTitle($item->meta_title ?: $item->title);
         $this->seo->setDescription($item->meta_description ?: $item->short_description ?: $item->list_description);
         $this->seo->setImage($item->imageFront('hero'));
+        if (!$item->is_future) {
+            $this->seo->nofollow = true;
+        }
 
         return view('site.events.detail', [
             'contrastHeader' => $item->present()->contrastHeader,
