71025b41be13844257816d294445b6df30d11bb5
Illya Moskvin <imoskvin@artic.edu>
2020-03-20 13:55:23 -0500

    Finish responsive editor's note layout [WEB-1140]

--- resources/views/components/organisms/_o-editors-note.blade.php
+++ resources/views/components/organisms/_o-editors-note.blade.php
@@ -6,7 +6,7 @@
 
     <div class="o-editors-note__inner f-deck">
         <div class="o-editors-note__issue-number">
-            <span class="deck--black">Issue {{ $issueNumber }}</span>
+            <span class="deck--black">Issue&nbsp;{{ $issueNumber }}</span>
         </div>
         <div class="o-editors-note__title-lockup">
             <span class="deck--black">{{ $title }}:</span> {!! $description !!}&nbsp;<svg class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
