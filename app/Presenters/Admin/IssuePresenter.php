29611961afdf7901dd6e3f47fdcee220c9e61fef
Illya Moskvin <imoskvin@artic.edu>
2020-03-24 22:43:19 -0500

    Implement article grid for issue pages [WEB-1140]

--- app/Presenters/Admin/IssuePresenter.php
+++ app/Presenters/Admin/IssuePresenter.php
@@ -9,11 +9,21 @@ class IssuePresenter extends BasePresenter
 
     private $editorsNote;
 
+    private $articlesForLanding;
+
     public function date()
     {
         if ($this->entity->date) {
             return $this->entity->date->format('M j, Y');
         }
+    }
+
+    public function articlesForLanding()
+    {
+        return $this->articlesForLanding ?? $this->articlesForLanding = $this->entity->articles()
+            ->where('type', '!=' ,'editors-note')
+            ->published()
+            ->get();
     }
 
     public function editorsNote()
--- resources/views/site/issueDetail.blade.php
+++ resources/views/site/issueDetail.blade.php
@@ -29,8 +29,35 @@
                 @endcomponent
             </div>
         @endif
-    </div>
 
+        @if ($item->present()->articlesForLanding)
+            @component('components.organisms._o-grid-listing')
+                @slot('variation', 'o-grid-listing--journal')
+                @slot('cols_xsmall','1')
+                @slot('cols_small','2')
+                @slot('cols_medium','2')
+                @slot('cols_large','2')
+                @slot('cols_xlarge','2')
+                @foreach ($item->present()->articlesForLanding as $item)
+                    @component('components.molecules._m-listing----journal')
+                        @slot('item', $item)
+                        @slot('imageSettings', array(
+                            'fit' => 'crop',
+                            'ratio' => '16:9',
+                            'srcset' => array(200,400,600),
+                            'sizes' => aic_imageSizes(array(
+                                  'xsmall' => '216px',
+                                  'small' => '216px',
+                                  'medium' => '18',
+                                  'large' => '13',
+                                  'xlarge' => '13',
+                            )),
+                        ))
+                    @endcomponent
+                @endforeach
+            @endcomponent
+        @endif
+    </div>
 </article>
 
 @endsection
