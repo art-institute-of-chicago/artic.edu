4b29e718232b1fbda58f1a25f32e11d8b6dbd395
Illya Moskvin <imoskvin@artic.edu>
2020-03-26 16:31:46 -0500

    Add `update:cancelled` to fix event title prefix [WEB-1602, WEB-1603]

---
+++ app/Console/Commands/UpdateCancelled.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+
+use Illuminate\Console\Command;
+
+class UpdateCancelled extends Command
+{
+
+    protected $signature = 'update:cancelled';
+
+    protected $description = 'Prepend cancelled to titles of cancelled events';
+
+    public function handle()
+    {
+        foreach (Event::cursor() as $event) {
+            if ($event->title_display) {
+                if (strpos($event->title_display, 'CANCELED') !== false) {
+                    if (strpos($event->title, 'CANCELED') === false) {
+                        $event->title = 'CANCELED | ' . $event->title;
+                        dump($event->title, $event->title_display);
+                        $event->save();
+                    }
+                }
+            }
+        };
+    }
+
+}
+
+
