6ae8f26e88a0589b7403412660af7fa8281ecf79
Illya Moskvin <imoskvin@artic.edu>
2020-03-30 22:51:43 -0500

    Add image link field to split block [WEB-1605]

--- resources/views/admin/blocks/split_block.blade.php
+++ resources/views/admin/blocks/split_block.blade.php
@@ -21,4 +21,10 @@
     'max' => 1
 ])
 
+@formField('input', [
+    'name' => 'image_link',
+    'label' => 'Link (optional)',
+    'note' => 'Makes image clickable',
+])
+
 @include('admin.blocks.paragraph')
