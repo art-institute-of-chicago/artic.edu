928d3a5065650a3b4b3f1e7cad433ba8f12ef0d2
Illya Moskvin <imoskvin@artic.edu>
2020-04-06 12:58:29 -0500

    Fix request() to return correct URI in preview mode [WEB-1296]
    This prevents pages from redirecting to their canonical URIs.

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -12,9 +12,25 @@ class PreviewController extends BaseController
 
     public function show($hash)
     {
+        // Structured like //host.example/path?query=foobar
         $route = decrypt($hash);
+
+        // Check this config when determining published status
         config(['aic.is_preview_mode' => true]);
-        $request = Request::create($route);
+
+        // We just want to extract the /path, starting with /
+        $path = parse_url($route, PHP_URL_PATH);
+
+        // Get ServerBag of current reqest, rewrite URI, convert to array
+        $server = request()->server;
+        $server->set('REQUEST_URI', $path);
+        $server = $server->all();
+
+        // Rewrite the current request with one containing the new path
+        $request = request()->duplicate(null, null, null, null, null, $server);
+        app()->request = $request;
+
+        // Now, calling request() returns the new request!
         return Route::dispatch($request)->getContent();
     }
 
