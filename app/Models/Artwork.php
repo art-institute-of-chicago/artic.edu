2e6f945b64befea0d09c0b0eccfa92e2357f0db5
surreal8 <tinashah8@gmail.com>
2020-04-17 15:58:01 -0500

    add 360 to artwork admin form

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -7,6 +7,7 @@ use App\Models\Behaviors\HasRelated;
 use App\Models\Behaviors\HasApiRelations;
 use App\Models\Behaviors\HasFeaturedRelated;
 use App\Models\Behaviors\HasMedias;
+use App\Models\SeamlessImage;
 
 use Illuminate\Support\Str;
 
@@ -51,5 +52,26 @@ class Artwork extends AbstractModel
     public function getSlugAttribute()
     {
         return ['en' => getUtf8Slug($this->title)];
+    }
+
+    public function getAssetLibraryAttribute()
+    {
+        // Include image sequence
+        if ($this->fileObject('image_sequence_file')) {
+            $images = SeamlessImage::where('zip_file_id', $this->fileObject('image_sequence_file')->id)->get();
+            $asset = [
+                'type' => 'sequence',
+                'id' => $this->fileObject('image_sequence_file')->id,
+                'width' => $images->first() ? $images->first()->width : 0,
+                'height' => $images->first() ? $images->first()->height : 0,
+                'src' => $images->map(function ($image) {
+                    return [
+                        'src' => 'https://' . config('twill.imgix_source_host') . '/seq/' . $image->file_name,
+                        'frame' => $image->frame,
+                    ];
+                })->toArray(),
+            ];
+        }
+        return $asset;
     }
 }
