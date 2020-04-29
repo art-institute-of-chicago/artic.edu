96d36aa81f6ddb3c3668aed61643f4e6387ac685
surreal8 <tinashah8@gmail.com>
2020-04-29 11:55:32 -0500

    Pass is360 variable to component via slot

--- resources/views/site/artworkDetail.blade.php
+++ resources/views/site/artworkDetail.blade.php
@@ -18,7 +18,7 @@
   {{-- Gallery-type _m-article-header never renders title --}}
   <h1 class="sr-only">{{ $item->title }}</h1>
 
-  @component('components.molecules._m-article-header', ['is360' => $item->assetLibrary])
+  @component('components.molecules._m-article-header')
     {{-- @slot('editorial', false) --}}
     @slot('headerType', $item->present()->headerType)
     {{-- @slot('variation', ($item->headerVariation ?? null)) --}}
@@ -33,6 +33,7 @@
     @slot('maxZoomWindowSize', $item->max_zoom_window_size)
     @slot('prevNextObject', $prevNextObject ?? null)
     @slot('module3d', $model3d ?? null)
+    @slot('is360', $item->assetLibrary !== null)
   @endcomponent
 
   <div class="o-article__primary-actions o-article__primary-actions--inline-header u-show@large+" aria-label="Additional information">
