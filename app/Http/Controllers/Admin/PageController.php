494ea00dc0518c9b75f125ab67dd8e052c0a4e83
nikhiltri <ntrivedi@artic.edu>
2020-05-11 20:53:02 -0500

    Reorder subnav in homepage landing CMS page

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -30,15 +30,15 @@ class PageController extends ModuleController
         Session::put("pages_back_link", route('admin.homepage.landing'));
 
         $additionalFieldsets = [
+            ['fieldset' => 'plan-your-visit', 'label' => 'Plan Your Visit'],
             ['fieldset' => 'video-carousel', 'label' => 'Video Carousel'],
             ['fieldset' => 'call-to-action', 'label' => 'Call to Action'],
             ['fieldset' => 'highlights', 'label' => 'Highlights'],
             ['fieldset' => 'artists', 'label' => 'Artists'],
-            ['fieldset' => 'interactive-features', 'label' => 'Interactive Features'],
-            ['fieldset' => 'plan-your-visit', 'label' => 'Plan Your Visit'],
-            ['fieldset' => 'exhibitions-and-events', 'label' => 'Exhibitions and Events'],
             ['fieldset' => 'from-the-collection', 'label' => 'From the Collection'],
             ['fieldset' => 'from-the-shop', 'label' => 'From the Shop'],
+            ['fieldset' => 'exhibitions-and-events', 'label' => 'Exhibitions and Events'],
+            // ['fieldset' => 'interactive-features', 'label' => 'Interactive Features'],
         ];
         $fields = $this->form($page->id);
         $fields['additionalFieldsets'] = $additionalFieldsets;
