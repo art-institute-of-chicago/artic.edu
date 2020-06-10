fa89a7111c0d16c47aa7c8ba530638e78b48a440
Illya Moskvin <ivmoskvin@gmail.com>
2020-06-10 16:47:17 -0500

    Fix magazine CTA spacing on mobile [WEB-1701]
    We use the :after element to emulate how big the image would be.
    By using flex with nowrap, whichever is taller (the title or the :after)
    is what will be the final height of the .m-listing__title

--- resources/views/components/molecules/_m-listing----publication-call-to-action.blade.php
+++ resources/views/components/molecules/_m-listing----publication-call-to-action.blade.php
@@ -1,17 +1,23 @@
 <{{ $tag ?? 'li' }} class="m-listing m-listing--publication m-listing--publication-call-to-action m-listing--w-meta-bottom{{ (isset($variation)) ? ' '.$variation : '' }}">
   <a href="{{ $href ?? '' }}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
     <span class="m-listing__meta">
-        @component('components.atoms._type')
-            {!! $type !!}
-        @endcomponent
-        <br>
-        @component('components.atoms._title')
-            @slot('font', 'f-headline')
-            @slot('title_display', $title_display)
-            @slot('itemprop', 'name')
-        @endcomponent
-        <br>
-        @if ($link_text)
+        @if (isset($type))
+            @component('components.atoms._type')
+                {!! $type !!}
+            @endcomponent
+            <br>
+        @endif
+        <div class="m-listing__title">
+            @if (isset($title_display))
+                @component('components.atoms._title')
+                    @slot('font', 'f-headline')
+                    @slot('title_display', $title_display)
+                    @slot('itemprop', 'name')
+                @endcomponent
+            @endif
+        </div>
+        @if (isset($link_text))
+            <br>
             <span class="m-listing__meta-bottom">
                 <span class="btn f-buttons btn--magazine">{{ $link_text }}</span>
             </span>
