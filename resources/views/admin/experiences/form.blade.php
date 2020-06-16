4eaa310d87ad3a462998d9e2a16ef7e5a74683a5
nikhiltri <ntrivedi@artic.edu>
2020-06-16 14:34:33 -0500

    Correct link to slides from experiences in CMS

--- resources/views/admin/experiences/form.blade.php
+++ resources/views/admin/experiences/form.blade.php
@@ -10,7 +10,7 @@
 @extends('twill::layouts.form')
 
 @section('contentFields')
-    <br/><strong><a href="{{ url('collection/experiences/' . $item->id . '/slides') }}">{{ $item->slides->count() }} Slides</a></strong><br/>
+    <br/><strong><a href="{{ url('collection/interactive_features/experiences/' . $item->id . '/slides') }}">{{ $item->slides->count() }} Slides</a></strong><br/>
     <br/><h1><strong>Kiosk URL:</strong> <a href={{ $kiosk_url }}>{{ $kiosk_url }}</a></h1>
     <br/><h1><strong>Bundle ID: </strong>{{ $item->id }}</h1>
 
