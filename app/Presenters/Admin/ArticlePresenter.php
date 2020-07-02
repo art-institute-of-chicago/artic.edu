8f6862928a1f28f3f9c1ef6fc2c6b0e0f307faab
Illya Moskvin <ivmoskvin@gmail.com>
2020-07-02 16:19:37 -0500

    Prioritize author display field for articles [WEB-1714]

--- app/Presenters/Admin/ArticlePresenter.php
+++ app/Presenters/Admin/ArticlePresenter.php
@@ -41,10 +41,13 @@ class ArticlePresenter extends BasePresenter
 
     public function author()
     {
+        if ($this->entity->author_display) {
+            return $this->entity->author_display;
+        }
+
         if ($this->entity->authors->isNotEmpty()) {
             $array = $this->entity->authors->pluck('title')->all();
             return join(' and ', array_filter(array_merge(array(join(', ', array_slice($array, 0, -1))), array_slice($array, -1)), 'strlen'));
         }
-        return $this->entity->author_display;
     }
 }
