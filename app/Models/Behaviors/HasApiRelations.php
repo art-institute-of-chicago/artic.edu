ce1e0f9b534ad3a71b012776a696beb175043835
nikhiltri <ntrivedi@artic.edu>
2020-07-09 17:02:49 -0500

    Fix related content not displaying [WEB-1715]
    This was caused by WEB-1667. We had added logic to check that content
    is listed before displaying it. But for content that didn't use the
    `HasUnlisted` behavior it was falling back to not displaying it.
    This was a `!$var` vs. `$var === false` issue.

--- app/Models/Behaviors/HasApiRelations.php
+++ app/Models/Behaviors/HasApiRelations.php
@@ -78,11 +78,10 @@ trait HasApiRelations
                     return $items->map(function ($relatedElement) {
                         $element = $relatedElement->related;
                         $element->position = $relatedElement->position;
-
-                        if (!$element->isListed ?? false) {
+                        if ($element->isListed === false) {
                             return false;
                         }
-                        if (!$element->isPublished ?? false) {
+                        if ($element->isPublished === false) {
                             return false;
                         }
 
