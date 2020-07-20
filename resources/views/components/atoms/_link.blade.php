e5468d0ebae18d6201454fb8c54c7bf8eadcd799
nikhiltri <ntrivedi@artic.edu>
2020-07-20 14:16:37 -0500

    Correct output of gtmAttributes when passed through two slots [WEB-1755]

--- resources/views/components/atoms/_link.blade.php
+++ resources/views/components/atoms/_link.blade.php
@@ -1,2 +1,2 @@
-<{{ $tag ?? 'a' }} class="link {{ (isset($font)) ? $font : 'f-link' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{{ (isset($gtmAttributes)) ? ' '.$gtmAttributes : '' }}{!! (isset($href)) ? ' href="'.$href. '"' : '' !!}>{{ $slot }}</{{ $tag ?? 'a' }}>
+<{{ $tag ?? 'a' }} class="link {{ (isset($font)) ? $font : 'f-link' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes : '' !!}{!! (isset($href)) ? ' href="'.$href. '"' : '' !!}>{{ $slot }}</{{ $tag ?? 'a' }}>
 
