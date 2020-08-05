d81605023767790474f7a8c68baf0565a1f6692e
Illya Moskvin <ivmoskvin@gmail.com>
2020-08-04 21:27:23 -0500

    Add ability to override title to "Audio Tour Stop" block [WEB-1785]

--- resources/views/admin/blocks/tour_stop.blade.php
+++ resources/views/admin/blocks/tour_stop.blade.php
@@ -6,6 +6,12 @@
     'max' => 1
 ])
 
+@formField('input', [
+    'name' => 'title_display',
+    'label' => 'Title override (optional)',
+    'note' => 'Use <i> tag to add italics. e.g. <i>Nighthawks</i>'
+])
+
 @formField('checkbox', [
     'name' => 'hidePromoText',
     'label' => 'Hide promo text'
--- resources/views/site/blocks/tour_stop.blade.php
+++ resources/views/site/blocks/tour_stop.blade.php
@@ -6,7 +6,7 @@
 @if (isset($tourStop))
     @component('components.molecules._m-listing----sound')
         @slot('item', (object) [
-            'title' => $tourStop->present()->title,
+            'title' => $block->input('title_display') ?: $tourStop->present()->title,
             'href' => $tourStop->web_url,
             'transcript' => $tourStop->present()->transcript,
             'subtitle' => $block->input('hidePromoText') === true ? '' : 'Hear the full tour on our app, available for <a href="https://itunes.apple.com/us/app/art-institute-of-chicago-app/id1130366814?mt=8">Apple</a> and <a href="https://play.google.com/store/apps/details?id=edu.artic">Android</a>',
