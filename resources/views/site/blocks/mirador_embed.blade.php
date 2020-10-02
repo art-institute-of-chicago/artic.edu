5d4c926dcf6a0dc5aafee8434670105194fd0e8f
surreal8 <tinashah8@gmail.com>
2020-10-02 15:29:50 -0500

    Embed mirador in article - FE [WEB-1843

---
+++ resources/views/site/blocks/mirador_embed.blade.php
@@ -0,0 +1,36 @@
+@php
+    $datahub_id = $block->input('objectId');
+	$uploaded_manifest = $block->file('upload_manifest_file');
+	$final_manifest;
+	if ($uploaded_manifest OR $datahub_id) {
+		if ($uploaded_manifest) {
+			$final_manifest = $uploaded_manifest;
+		} else {
+			$final_manifest = 'http://aggregator-data.artic.edu/api/v1/artworks/'.$datahub_id.'/manifest.json';
+		}
+	}
+    $caption_title = $block->input('caption_title');
+	$caption = $block->input('caption');
+@endphp
+
+@if ($final_manifest)
+    <div class="m-media m-media--s o-blocks__block">
+        <div class="m-media__img m-media--mirador-embed" data-behavior="fitText">
+        @component('components.molecules._m-viewer-mirador')
+            @slot('type', 'standalone')
+            @slot('title', isset($pageTitle) ? $pageTitle.' - Inline Mirador' : 'Inline Mirador')
+            @slot('manifest', $final_manifest);
+        @endcomponent
+        </div>
+        @if ($caption_title || $caption)
+            <figcaption>
+                @if (isset($caption_title))
+                    <div class="f-caption-title">{!! $caption_title !!}</div><br>
+                @endif
+                @if (isset($caption))
+                    <div class="f-caption">{!! $caption !!}</div>
+                @endif
+            </figcaption>
+        @endif
+    </div>
+@endif
