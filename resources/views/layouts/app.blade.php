f96331b9ff3152695f9a70b0936430714d9c43f9
nikhiltri <ntrivedi@artic.edu>
2021-02-04 17:27:36 -0600

    Hide Google Tag Manager in print mode [WEB-1595]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@ $pClass = strtolower(preg_replace('/@/i','-',$pClass));
 
 <body{!! $print ? ' onload="window.print();"' : '' !!}>
 
-    @if (config('services.google_tag_manager.enabled'))
+    @if (config('services.google_tag_manager.enabled') && !$print)
         <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={!! config('services.google_tag_manager.id') !!}"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
