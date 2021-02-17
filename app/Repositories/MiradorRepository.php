9d609863198eb0c226ae22a3aeae32a3a7341ced
surreal8 <tinashah8@gmail.com>
2021-02-17 16:48:13 -0600

    Create mirador module [WEB-1951]

---
+++ app/Repositories/MiradorRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use A17\Twill\Repositories\Behaviors\HandleSlugs;
+use A17\Twill\Repositories\Behaviors\HandleMedias;
+use A17\Twill\Repositories\Behaviors\HandleFiles;
+use A17\Twill\Repositories\Behaviors\HandleRevisions;
+use A17\Twill\Repositories\ModuleRepository;
+use App\Models\Mirador;
+
+class MiradorRepository extends ModuleRepository
+{
+    use HandleSlugs, HandleMedias, HandleFiles, HandleRevisions;
+
+    public function __construct(Mirador $model)
+    {
+        $this->model = $model;
+    }
+}
