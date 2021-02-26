3fcb10526103323060c74dd75e90929b6b769d1c
Illya Moskvin <imoskvin@artic.edu>
2021-02-26 01:57:22 -0600

    Remove `maxlength` from quote block fields for citations [WEB-1954]

--- resources/views/admin/blocks/quote.blade.php
+++ resources/views/admin/blocks/quote.blade.php
@@ -2,12 +2,10 @@
     'name' => 'quote',
     'type' => 'textarea',
     'label' => 'Quote text',
-    'maxlength' => 250,
     'rows' => 4
 ])
 
 @formField('input', [
     'name' => 'attribution',
     'label' => 'Attribution',
-    'maxlength' => 50
 ])
