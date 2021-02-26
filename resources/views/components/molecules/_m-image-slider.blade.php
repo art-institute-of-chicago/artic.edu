abe12bbada6072b0203a695e10feb90133d69be4
Illya Moskvin <imoskvin@artic.edu>
2021-02-26 02:50:51 -0600

    Add dividing line to image slider [WEB-1958]

--- resources/views/components/molecules/_m-image-slider.blade.php
+++ resources/views/components/molecules/_m-image-slider.blade.php
@@ -27,7 +27,10 @@
                     @endcomponent
                 @endif
                 <div class="m-image-slider__handle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="#FFF" d="M13 21l-5-5 5-5m6 0l5 5-5 5"/></svg>
+                    <div class="m-image-slider__handle__divider"></div>
+                    <div class="m-image-slider__handle__circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="#FFF" d="M13 21l-5-5 5-5m6 0l5 5-5 5"/></svg>
+                    </div>
                 </div>
             </div>
 
