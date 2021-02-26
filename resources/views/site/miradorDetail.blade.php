dec85722c82c5f65b562dbf3f19d2ca8244d630d
surreal8 <tinashah8@gmail.com>
2021-02-26 16:13:28 -0600

    Update presentation for mirador kiosk [WEB-1951]

--- resources/views/site/miradorDetail.blade.php
+++ resources/views/site/miradorDetail.blade.php
@@ -1,41 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.kiosk')
 
 @section('content')
 
-    <article class="o-article o-article--video">
-
-        <div class="o-article__primary-actions o-article__primary-actions--video">
-            @component('components.atoms._title')
-                @slot('tag','p')
-                @slot('font', 'f-tag-2')
-                Mirador Kiosk
-            @endcomponent
-
-            @component('components.molecules._m-article-actions')
-                @slot('articleType', 'video')
-            @endcomponent
-        </div>
-
-        @component('components.molecules._m-article-header')
-            @slot('title', $item->present()->title)
-            @slot('formattedDate', $item->present()->date)
-        @endcomponent
-
-        <div class="o-article__body o-blocks">
-            @component('components.molecules._m-media')
-                @slot('variation', 'o-blocks__block')
-                @slot('item', [
-                    'type' => 'miradorKiosk',
-                    'size' => 'l',
-                    'media' => [
-                        'miradorManifest' => $item->getMiradorManifest(),
-                    ],
-                    'default_view' => $item->getMiradorView(),
-                    'fullscreen' => false,
-                ])
-            @endcomponent
-        </div>
-
-    </article>
+    @component('components.molecules._m-viewer-mirador')
+        @slot('type', 'miradorKiosk')
+        @slot('manifest', $item->getMiradorManifest())
+        @slot('defaultView', $item->getMiradorView())
+    @endcomponent
 
 @endsection
