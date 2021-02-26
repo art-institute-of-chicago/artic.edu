31fa69519d48ca447032643e8dcbb0e4e85a06df
Illya Moskvin <imoskvin@artic.edu>
2021-02-26 01:34:24 -0600

    Split off `_m-table` from table block [WEB-1972]

---
+++ resources/views/components/molecules/_m-table.blade.php
@@ -0,0 +1,69 @@
+@php
+    use Symfony\Component\DomCrawler\Crawler;
+
+    $crawler = new Crawler($tableHtml);
+
+    $table = $crawler->filter('table:first-child')->first();
+@endphp
+
+@if ($table)
+    <div class="m-table">
+        <table>
+            @if (!empty($title))
+                <caption>
+                    @component('components.molecules._m-title-bar')
+                        @slot('id', 'admission')
+                        @lang($title)
+                    @endcomponent
+                </caption>
+            @endif
+            <thead>
+                <tr>
+                    @foreach ($crawler->filter('table:first-child > thead > tr > th') as $cellElement)
+                        @php
+                            $cellStyle = $cellElement->getAttribute('style');
+                        @endphp
+                        <th style="{!! !empty($cellStyle) ? $cellStyle : '' !!}">
+                            @component('components.blocks._text')
+                                @slot('font', 'f-module-title-1')
+                                @slot('tag', 'span')
+                                {!! innerHTML($cellElement) !!}
+                            @endcomponent
+                        </th>
+                        @php
+                            unset($cellStyle);
+                        @endphp
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($crawler->filter('table:first-child > tbody > tr') as $rowElement)
+                    <tr>
+                        @php
+                            $rowCrawler = new Crawler(innerHTML($rowElement));
+                        @endphp
+                        @foreach ($rowCrawler->filter('td') as $i => $cellElement)
+                            @php
+                                $cellStyle = $cellElement->getAttribute('style');
+                                $isCellHeader = $i === 0 && ($hasSideHeader ?? false);
+                            @endphp
+                            <{!! $isCellHeader ? 'th' : 'td' !!} style="{!! !empty($cellStyle) ? $cellStyle : '' !!}">
+                                @component('components.blocks._text')
+                                    @slot('font', $isCellHeader ? 'f-module-title-1' : 'f-secondary')
+                                    @slot('tag', 'span')
+                                    {!! innerHTML($cellElement) !!}
+                                @endcomponent
+                            </{!! $isCellHeader ? 'th' : 'td' !!}>
+                            @php
+                                unset($cellStyle);
+                            @endphp
+                        @endforeach
+                        @php
+                            unset($rowCrawler);
+                        @endphp
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endif
