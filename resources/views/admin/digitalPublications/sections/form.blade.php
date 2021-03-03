9b649ad2603b4fef176ad8b45d6a37cb05a1bf65
nikhiltri <ntrivedi@artic.edu>
2021-03-03 10:58:36 -0600

    Remove deprecated gallery blocks from digital publication section [WEB-1937]

--- resources/views/admin/digitalPublications/sections/form.blade.php
+++ resources/views/admin/digitalPublications/sections/form.blade.php
@@ -91,7 +91,7 @@
             'list', 'artwork', 'hr', 'citation', 'split_block',
             'membership_banner', 'digital_label', 'tour_stop', 'button', 'mobile_app',
             '3d_model', '3d_tour', '3d_embed', '360_embed', '360_modal',
-            'gallery', 'artworks', 'gallery_new',
+            'gallery_new',
             'image_slider', 'mirador_embed', 'mirador_modal',
         ])
     ])
