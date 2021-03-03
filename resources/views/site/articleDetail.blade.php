1190f5eea17bbe91f421fb73ebb7ea31d2bbfeb0
Illya Moskvin <imoskvin@artic.edu>
2021-03-03 00:59:07 -0600

    Abstract related items into shared view [WEB-1975]

--- resources/views/site/articleDetail.blade.php
+++ resources/views/site/articleDetail.blade.php
@@ -329,36 +329,12 @@
 
 @foreach (['featuredArticles', 'alsoInThisIssue'] as $relatedType)
     @if (isset($$relatedType) && $$relatedType)
-        @component('components.molecules._m-title-bar')
-            {{ $relatedType == 'featuredArticles' ? 'Further Reading' : 'Also in this Issue' }}
-        @endcomponent
-        @component('components.organisms._o-grid-listing')
-            @slot('variation', 'o-grid-listing--single-row o-grid-listing--scroll@xsmall o-grid-listing--scroll@small o-grid-listing--hide-extra@medium o-grid-listing--gridlines-cols o-grid-listing--gridlines-rows')
-            @slot('cols_medium','3')
-            @slot('cols_large','4')
-            @slot('cols_xlarge','4')
-            @slot('behavior','dragScroll')
-            @foreach ($$relatedType as $item)
-                @component('components.molecules._m-listing----' . strtolower($item->type))
-                    @slot('item', $item)
-                    @slot('subtype', $item->subtype ?? $item->type)
-                    @slot('hideDescription', true)
-                    @slot('imgVariation', ' ')
-                    @slot('imageSettings', array(
-                        'fit' => 'crop',
-                        'ratio' => '16:9',
-                        'srcset' => array(200,400,600),
-                        'sizes' => aic_imageSizes(array(
-                            'xsmall' => '216px',
-                            'small' => '216px',
-                            'medium' => '18',
-                            'large' => '13',
-                            'xlarge' => '13',
-                        )),
-                    ))
-                @endcomponent
-            @endforeach
-        @endcomponent
+        <section>
+            @component('site.shared._relatedItems')
+                @slot('title', $relatedType == 'featuredArticles' ? 'Further Reading' : 'Also in this Issue')
+                @slot('relatedItems', $$relatedType)
+            @endcomponent
+        </section>
     @endif
 @endforeach
 
