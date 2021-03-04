5ba4cbf14b073199b13a723d01360c43dd10edd9
surreal8 <tinashah8@gmail.com>
2021-03-04 16:06:59 -0600

    Update type name to follow class convention and add mirador-kiosk css [WEB-1951]

--- resources/views/site/miradorDetail.blade.php
+++ resources/views/site/miradorDetail.blade.php
@@ -1,9 +1,11 @@
+<link href="{{revAsset('styles/mirador-kiosk.css')}}" rel="stylesheet" />
+
 @extends('layouts.kiosk')
 
 @section('content')
 
     @component('components.molecules._m-viewer-mirador')
-        @slot('type', 'miradorKiosk')
+        @slot('type', 'mirador-kiosk')
         @slot('manifest', $item->getMiradorManifest())
         @slot('defaultView', $item->getMiradorView())
     @endcomponent
