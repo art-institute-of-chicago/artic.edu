373c57d891339e25d08f7b501af08ad01a23cc17
Illya Moskvin <imoskvin@artic.edu>
2021-03-12 17:46:03 -0600

    Show manual digpub order on frontend [WEB-1976]

--- app/Presenters/Admin/DigitalPublicationPresenter.php
+++ app/Presenters/Admin/DigitalPublicationPresenter.php
@@ -45,6 +45,7 @@ class DigitalPublicationPresenter extends BasePresenter
             $this->sections['all'] = $this->entity
                 ->sections()
                 ->published()
+                ->ordered()
                 ->get();
         }
 
