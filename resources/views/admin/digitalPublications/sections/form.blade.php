a66de34ecfa9193d6c4ce80f9e972d0891b97500
nikhiltri <ntrivedi@artic.edu>
2021-04-09 18:08:39 -0500

    Add Links Bar block as submenu of subheadings [PUB-40]

--- resources/views/admin/digitalPublications/sections/form.blade.php
+++ resources/views/admin/digitalPublications/sections/form.blade.php
@@ -87,6 +87,7 @@
             '3d_model', '3d_tour', '3d_embed', '360_embed', '360_modal',
             'gallery_new',
             'image_slider', 'mirador_embed', 'mirador_modal',
+            'links-bar'
         ])
     ])
 
