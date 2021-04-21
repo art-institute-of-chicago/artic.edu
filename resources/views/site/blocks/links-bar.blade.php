ae5871d56504f45672a590a46e0b04e50dfa3941
Illya Moskvin <imoskvin@artic.edu>
2021-04-21 10:31:22 -0500

    Remove &nbsp; from link bar anchors [PUB-40]

--- resources/views/site/blocks/links-bar.blade.php
+++ resources/views/site/blocks/links-bar.blade.php
@@ -14,15 +14,15 @@
     foreach ($blocks as $b) {
         if ($b->type == 'paragraph') {
             if (preg_match_all('/<h([23])[^>]*>(.*)<\/h\\1>/', $b->content['paragraph'], $matches)) {
-                if ($title = strip_tags($matches[2][0]) ?? null) {
-                    $links[] = ['label' => $title, 'href' => '#' .getUtf8Slug($title)];
+                if ($title = html_entity_decode(strip_tags($matches[2][0]) ?? null)) {
+                    $links[] = ['label' => $title, 'href' => '#' . Str::slug($title)];
                 }
             }
         }
         // galery or gallery-new
         else {
-            if ($title = $b->content['title'] ?? null) {
-                $links[] = ['label' => $title, 'href' => '#' .getUtf8Slug($title)];
+            if ($title = html_entity_decode($b->content['title'] ?? null)) {
+                $links[] = ['label' => $title, 'href' => '#' . Str::slug($title)];
             }
         }
     }
