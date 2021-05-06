a6adada48c4a79c3452eb8e3d7aeecd818e311b0
nikhiltri <ntrivedi@artic.edu>
2021-05-06 18:04:32 -0500

    Add frontend view for Feature 2x block, and style the layout across all breakpoints [WEB-2075]

---
+++ resources/views/site/blocks/feature_2x.blade.php
@@ -0,0 +1,34 @@
+@php
+    $ids = $block->browserIds('experiences');
+    $items = \App\Models\Experience::find($ids);
+@endphp
+@if ($items->count() > 0)
+    @component('components.organisms._o-grid-listing')
+        @slot('variation', 'o-grid-listing--gridlines-cols o-grid-listing--gridlines-top o-grid-listing--feature-2x m-media--l')
+        @slot('cols_small','2')
+        @slot('cols_medium','2')
+        @slot('cols_large','2')
+        @slot('cols_xlarge','2')
+        @php ($count = 0)
+        @foreach ($items as $item)
+            @php ($count += 1)
+            @component('components.molecules._m-listing----exhibition')
+                @slot('titleFont', 'f-list-4')
+                @slot('item', $item)
+                @slot('imageSettings', array(
+                    'fit' => 'crop',
+                    'ratio' => '16:9',
+                    'srcset' => array(200,400,600,1000,1500),
+                    'sizes' => aic_gridListingImageSizes(array(
+                          'xsmall' => '1',
+                          'small' => '2',
+                          'medium' => '2',
+                          'large' => '2',
+                          'xlarge' => '2',
+                    )),
+                ))
+                @slot('gtmAttributes', 'data-gtm-event="' . $item->title . '" data-gtm-event-category="exhibition-' . $count . '"')
+            @endcomponent
+        @endforeach
+    @endcomponent
+@endif
