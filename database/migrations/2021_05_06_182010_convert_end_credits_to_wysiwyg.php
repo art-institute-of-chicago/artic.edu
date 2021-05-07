56e925efac56b4dc3eb0a1493da16a86dde57008
nikhiltri <ntrivedi@artic.edu>
2021-05-07 09:28:10 -0500

    Convert End Slide credits field to WYSIWYG in CMS [IP-107]

---
+++ database/migrations/2021_05_06_182010_convert_end_credits_to_wysiwyg.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use App\Models\Slide;
+
+class ConvertEndCreditsToWysiwyg extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $slides = Slide::where('module_type', 'end')->get();
+
+        foreach ($slides as $slide) {
+            if ($slide->end_credit_copy) {
+                $slide->end_credit_copy = '<p>' .$slide->end_credit_copy .'</p>';
+                $slide->save();
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('wysiwyg', function (Blueprint $table) {
+            //
+        });
+    }
+}
