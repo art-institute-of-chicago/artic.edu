099a4a63d3331ab7e1bf340d07cc770437324b06
nikhiltri <ntrivedi@artic.edu>
2021-05-13 17:07:01 -0500

    Remove unused global variable

--- resources/views/site/blocks/paragraph.blade.php
+++ resources/views/site/blocks/paragraph.blade.php
@@ -56,29 +56,29 @@
         libxml_use_internal_errors($oldInternalErrors);
     }
 
-    global $_addHeadingIds;
+    $oldInternalErrors = libxml_use_internal_errors(true);
 
-    if ($_addHeadingIds ?? true) {
-        $oldInternalErrors = libxml_use_internal_errors(true);
+    $dom = new DomDocument();
+    $dom->loadHTML('<?xml encoding="utf-8" ?>' . $content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
-        $dom = new DomDocument();
-        $dom->loadHTML('<?xml encoding="utf-8" ?>' . $content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+    // Give IDs to H2s, H3s, and H4s
+    $xpath = new DOMXpath($dom);
 
-        // Give IDs to H2s and H3s
-        $xpath = new DOMXpath($dom);
-
-        foreach($xpath->query('//h2') as $node) {
-            $node->setAttribute('id', Str::slug($node->nodeValue));
-        }
-
-        foreach($xpath->query('//h3') as $node) {
-            $node->setAttribute('id', Str::slug($node->nodeValue));
-        }
-
-        $content = $dom->saveHTML($dom);
-
-        libxml_clear_errors();
-        libxml_use_internal_errors($oldInternalErrors);
+    foreach($xpath->query('//h2') as $node) {
+        $node->setAttribute('id', Str::slug($node->nodeValue));
     }
+
+    foreach($xpath->query('//h3') as $node) {
+        $node->setAttribute('id', Str::slug($node->nodeValue));
+    }
+
+    foreach($xpath->query('//h4') as $node) {
+        $node->setAttribute('id', Str::slug($node->nodeValue));
+    }
+
+    $content = $dom->saveHTML($dom);
+
+    libxml_clear_errors();
+    libxml_use_internal_errors($oldInternalErrors);
 @endphp
 {!! $content !!}
