89c77f103b4880f39c3f3711bce896f4a1c5e30e
Illya Moskvin <imoskvin@artic.edu>
2021-06-11 11:20:45 -0500

    Add image toolbar wrapper element [PUB-30]

--- resources/views/partials/_fullscreenImage.blade.php
+++ resources/views/partials/_fullscreenImage.blade.php
@@ -5,29 +5,31 @@
     @endcomponent
     <img class="o-fullscreen-image__img" alt="" src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=">
     <div class="o-fullscreen-image__osd" id="openseadragon"></div>
-    <h2 class="sr-only" id="h-img_action">Image actions</h2>
-    <ul class="o-fullscreen-image__img-actions" aria-labelledby="h-img_action">
-      <li>
-        @component('components.atoms._btn')
-            @slot('variation', 'btn--septenary btn--icon-sq')
-            @slot('font', '')
-            @slot('icon', 'icon--zoom-in--24')
-            @slot('id','osd_zoomInButton')
-            @slot('dataAttributes', 'data-fullscreen-zoom-in')
-            @slot('ariaLabel', 'Zoom in')
-        @endcomponent
-      </li>
-      <li>
-        @component('components.atoms._btn')
-            @slot('variation', 'btn--septenary btn--icon-sq')
-            @slot('font', '')
-            @slot('icon', 'icon--zoom-out--24')
-            @slot('id','osd_zoomOutButton')
-            @slot('dataAttributes', 'data-fullscreen-zoom-out')
-            @slot('ariaLabel', 'Zoom out')
-        @endcomponent
-      </li>
-    </ul>
+    <div class="o-fullscreen-image__img-toolbar">
+        <h2 class="sr-only" id="h-img_action">Image actions</h2>
+        <ul class="o-fullscreen-image__img-actions" aria-labelledby="h-img_action">
+            <li>
+                @component('components.atoms._btn')
+                    @slot('variation', 'btn--septenary btn--icon-sq')
+                    @slot('font', '')
+                    @slot('icon', 'icon--zoom-in--24')
+                    @slot('id','osd_zoomInButton')
+                    @slot('dataAttributes', 'data-fullscreen-zoom-in')
+                    @slot('ariaLabel', 'Zoom in')
+                @endcomponent
+            </li>
+            <li>
+                @component('components.atoms._btn')
+                    @slot('variation', 'btn--septenary btn--icon-sq')
+                    @slot('font', '')
+                    @slot('icon', 'icon--zoom-out--24')
+                    @slot('id','osd_zoomOutButton')
+                    @slot('dataAttributes', 'data-fullscreen-zoom-out')
+                    @slot('ariaLabel', 'Zoom out')
+                @endcomponent
+            </li>
+        </ul>
+    </div>
     @component('components.atoms._btn')
         @slot('variation', 'btn--octonary btn--icon btn--icon-circle-48 o-fullscreen-image__close')
         @slot('font', '')
