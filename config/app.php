b122952c289e23434058b0958c387bd75c34fd67
nikhiltri <ntrivedi@artic.edu>
2021-08-02 15:22:05 -0500

    Delete unused commented out code in `config/` [WEB-2184]

--- config/app.php
+++ config/app.php
@@ -219,7 +219,6 @@ return [
         App\Providers\VendorServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\InvalidationServiceProvider::class,
