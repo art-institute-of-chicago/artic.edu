ef7b5c952abea2d45f75e61b0eb94471b2b00d0d
nikhiltri <ntrivedi@artic.edu>
2022-02-01 16:55:24 -0600

    Prevent font CSS from blocking rendering on Avenir [WEB-2368, WEB-2374]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@ $pClass = strtolower(preg_replace('/@/i','-',$pClass));
   <link rel="preload" href="{{FrontendHelpers::revAsset('fonts/3545D5_0_0.woff2')}}" as="font" type="font/woff2" crossorigin="anonymous" />
   <link rel="preload" href="{{FrontendHelpers::revAsset('fonts/3545D5_1_0.woff2')}}" as="font" type="font/woff2" crossorigin="anonymous" />
   <link rel="preload" href="{{FrontendHelpers::revAsset('fonts/3545D5_2_0.woff2')}}" as="font" type="font/woff2" crossorigin="anonymous" />
-  <style>
+  <style media="print" onload="this.media='all'">
   @import url("//hello.myfonts.net/count/3545d5");
   @font-face {font-family: 'Sabon';src: url({{FrontendHelpers::revAsset('fonts/3545D5_0_0.woff2')}}) format('woff2');font-weight:normal;font-weight:400;font-style:normal;}
   @font-face {font-family: 'Sabon';src: url({{FrontendHelpers::revAsset('fonts/3545D5_1_0.woff2')}}) format('woff2');font-weight:normal;font-weight:400;font-style:italic;}
