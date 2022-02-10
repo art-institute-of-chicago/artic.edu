f23490986c610969bc06c6d5159046dc4334e992
nikhiltri <ntrivedi@artic.edu>
2022-02-10 17:15:04 -0600

    Allow mp4 uploads to the media library [WEB-2371]

--- config/twill.php
+++ config/twill.php
@@ -290,5 +290,20 @@ return [
         ],
     ],
 
-    'imgix_source_host' => env('IMGIX_SOURCE_HOST', 'artic-web.imgix.net')
+    'imgix_source_host' => env('IMGIX_SOURCE_HOST', 'artic-web.imgix.net'),
+
+    'media_library' => [
+        'disk' => 'twill_media_library',
+        'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
+        'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
+        'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads'),
+        'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Imgix'),
+        'acl' => env('MEDIA_LIBRARY_ACL', 'private'),
+        'filesize_limit' => env('MEDIA_LIBRARY_FILESIZE_LIMIT', 50),
+        'allowed_extensions' => ['svg', 'jpg', 'gif', 'png', 'jpeg', 'mp4'],
+        'init_alt_text_from_filename' => true,
+        'prefix_uuid_with_local_path' => config('twill.file_library.prefix_uuid_with_local_path', false),
+        'translated_form_fields' => false,
+        'show_file_name' => false,
+    ]
 ];
