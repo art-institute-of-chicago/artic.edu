1735e9e33f89535354af2440b291375c0e8c0db6
nikhiltri <ntrivedi@artic.edu>
2022-02-23 11:49:56 -0600

    Take `async` attribute off of app.js [WEB-2368]

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="{{FrontendHelpers::revAsset('scripts/app.js')}}" async></script>
+<script src="{{FrontendHelpers::revAsset('scripts/app.js')}}"></script>
 
 @if (!config('aic.disable_extra_scripts'))
     @yield('extra_scripts')
