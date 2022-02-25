de8fa25c99169fed495626b5a078b5587be30e45
nikhiltri <ntrivedi@artic.edu>
2022-02-25 15:39:53 -0600

    Move statics view that's still being used back to codebase [WEB-2185]

---
+++ resources/views/site/exhibitions_load_more.blade.php
@@ -0,0 +1,32 @@
+@if( !empty( $items ) )
+    @foreach ($items as $date => $item)
+        @component('components.molecules._m-date-listing')
+            @slot('date', $date)
+            @slot('events', $item)
+            @slot('imageSettings', array(
+                'fit' => 'crop',
+                'ratio' => '16:9',
+                'srcset' => array(200,400,600),
+                'sizes' => ImageHelpers::aic_imageSizes(array(
+                      'xsmall' => '58',
+                      'small' => '13',
+                      'medium' => '13',
+                      'large' => '13',
+                      'xlarge' => '13',
+                )),
+            ))
+            @slot('imageSettingsOnGoing', array(
+                'fit' => 'crop',
+                'ratio' => '16:9',
+                'srcset' => array(200,400,600),
+                'sizes' => ImageHelpers::aic_imageSizes(array(
+                      'xsmall' => '58',
+                      'small' => '7',
+                      'medium' => '7',
+                      'large' => '7',
+                      'xlarge' => '7',
+                )),
+            ))
+        @endcomponent
+    @endforeach
+@endif
