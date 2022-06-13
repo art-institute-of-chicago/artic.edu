3bc747ba36e706d1ec23b8434153356063064a38
Illya Moskvin <imoskvin@artic.edu>
2022-06-13 12:18:17 -0500

    Flatten `_footer--primary` back into footer [WEB-2419]

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -14,7 +14,21 @@
     <h2 class="sr-only" id="h-footer-grid">Footer content</h2>
     <ul class="g-footer__grid {!! $showHourSummary ? 'g-footer__grid--hours' : '' !!}" aria-labelledby="h-footer-grid">
       <li>
-        @include('partials._footer--primary')
+        <nav aria-label="primary">
+          <h3 class="sr-only" id="h-footer-nav-primary">Primary Navigation</h3>
+
+          <a class="g-footer__logo" aria-label="Art Institute of Chicago" href="/">
+            <svg aria-hidden="true">
+              <use xlink:href="#icon--logo--outline--92" />
+            </svg>
+          </a>
+
+          <ul class="g-footer__link-list" aria-labelledby="h-footer-nav-primary">
+            <li><a href="{{ $_pages['visit'] }}">Visit</a></li>
+            <li><a href="{{ $_pages['exhibitions'] }}">Exhibitions and Events</a></li>
+            <li><a href="{{ $_pages['collection'] }}">The Collection</a></li>
+          </ul>
+        </nav>
       </li>
 
       @if (!$showHourSummary)
