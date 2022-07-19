10f5f21c9b362a7384cbc635e9f3b5305884c805
Illya Moskvin <imoskvin@artic.edu>
2022-07-19 14:36:33 -0500

    Update new footer design with hours [WEB-2437]

---
+++ resources/views/partials/_footer--follow.blade.php
@@ -0,0 +1,8 @@
+<h3 class="f-list-6" id="h-footer-follow-us">Follow us</h3>
+
+<ul class="g-footer__link-list" aria-labelledby="h-footer-follow-us">
+  <li><a href="{{ $_pages['follow-facebook'] }}" data-gtm-event="facebook" data-gtm-event-category="follow" target="_blank">Facebook</a></li>
+  <li><a href="{{ $_pages['follow-twitter'] }}" data-gtm-event="twitter" data-gtm-event-category="follow" target="_blank">Twitter</a></li>
+  <li><a href="{{ $_pages['follow-instagram'] }}" data-gtm-event="instagram" data-gtm-event-category="follow" target="_blank">Instagram</a></li>
+  <li><a href="{{ $_pages['follow-youtube'] }}" data-gtm-event="youtube" data-gtm-event-category="follow" target="_blank">YouTube</a></li>
+</ul>
---
+++ resources/views/partials/_footer--learn.blade.php
@@ -0,0 +1,8 @@
+<h3 class="f-list-6" id="h-footer-learn-with-us"><a href="{{ $_pages['learn'] }}">Learn with us<svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg></a></h3>
+
+<ul class="g-footer__link-list" aria-labelledby="h-footer-learn-with-us">
+  <li><a href="{{ $_pages['learn-families'] }}">Families</a></li>
+  <li><a href="{{ $_pages['learn-teens'] }}">Teens</a></li>
+  <li><a href="{{ $_pages['learn-educators'] }}">Educators</a></li>
+  <li><a href="{{ $_pages['learn-adults'] }}">Adults</a></li>
+</ul>
