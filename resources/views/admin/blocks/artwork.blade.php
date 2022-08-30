59746864c88a158bf656c868eeba24f807211d30
nikhil trivedi <ntrivedi@artic.edu>
2022-08-30 16:09:30 -0500

    Add ability to publish Artwork block in SM, M, LG sizes [ART-56]

--- resources/views/admin/blocks/artwork.blade.php
+++ resources/views/admin/blocks/artwork.blade.php
@@ -1,6 +1,29 @@
 @twillBlockTitle('Artwork')
 @twillBlockIcon('image')
 
+@formField('select', [
+    'name' => 'size',
+    'label' => 'Size',
+    'placeholder' => 'Select size',
+    'default' => 'm',
+    'options' => [
+        [
+            'value' => 's',
+            'label' => 'Small'
+        ],
+        [
+            'value' => 'm',
+            'label' => 'Medium'
+        ],
+        [
+            'value' => 'l',
+            'label' => 'Large'
+        ]
+    ]
+])
+
+<p>Note: if the chosen artwork does not have rights to be viewed at a large size, it will display as size small</p>
+
 @formField('browser', [
     'routePrefix' => 'collection',
     'name' => 'artworks',
