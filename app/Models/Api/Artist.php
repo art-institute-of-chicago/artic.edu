18f9dbf1075654884d83ac0c2e0c0717a0c00405
Illya Moskvin <imoskvin@artic.edu>
2022-09-06 11:12:15 -0500

    Fix empty metadata fields on artist artworks pinboard [WEB-2436]

--- app/Models/Api/Artist.php
+++ app/Models/Api/Artist.php
@@ -35,7 +35,15 @@ class Artist extends BaseApiModel
             ->aggregationClassifications(2)
             ->aggregationPlaces(1)
             ->aggregationStyles(1)
-            ->getSearch($perPage, ['id', 'title', 'image_id', 'main_reference_number', 'thumbnail', 'date_display', 'artist_title'], 'page', null, ['do-not-extract' => true]);
+            ->getSearch(
+                $perPage,
+                \App\Models\Api\Artwork::SEARCH_FIELDS,
+                'page',
+                null,
+                [
+                    'do-not-extract' => true
+                ]
+            );
     }
 
     public function getTitleSlugAttribute()
