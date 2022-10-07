2a15def8960ef5c4c043c3714b02bb5998e4f078
nikhil trivedi <ntrivedi@artic.edu>
2022-10-07 13:39:31 -0500

    Correct logic of condition to include for FareHarbor JS [WEB-2433]

--- resources/views/site/genericPage/show.blade.php
+++ resources/views/site/genericPage/show.blade.php
@@ -99,7 +99,7 @@
     <script src="{{FrontendHelpers::revAsset('scripts/virtualTour.js')}}"></script>
     <script src="/virtual-tours/tour.js"></script>
     <script src="{{FrontendHelpers::revAsset('scripts/videojs.js')}}"></script>
-    @if ($addFareHarborJS)
+    @if (!empty($addFareHarborJS))
         <!-- FareHarbor Lightframe API - do not remove - see: https://fareharbor.com/help/website/resources/lightframe-api/ -->
         <script src="https://fareharbor.com/embeds/api/v1/?autolightframe=yes"></script>
     @endif
