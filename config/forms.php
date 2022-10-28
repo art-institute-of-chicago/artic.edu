4aed36d83e948746ec42eb18b5dec455f480f3fc
nikhil trivedi <ntrivedi@artic.edu>
2022-10-27 21:06:51 -0500

    Remove Event Planning form [WEB-2469]

--- config/forms.php
+++ config/forms.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'email_event_planning_to' => env('EMAIL_EVENT_PLANNING_TO', 'bacatering@artic.edu'),
     'email_filming_proposal_to' => env('EMAIL_FILMING_PROPOSAL_TO', 'aic.publicaffairs@artic.edu'),
 
     'recaptcha_key' => env('RECAPTCHA_KEY', ''),
