b93063f7e6b9896bc8f551eb8ab12b59bc90d732
Hannah Baker <hannahb@cogapp.com>
2023-01-11 12:19:37 +0000

    Remove unneeded code from molecule file

--- resources/views/components/molecules/_m-layered-image-viewer.blade.php
+++ resources/views/components/molecules/_m-layered-image-viewer.blade.php
@@ -1,9 +1,3 @@
-<!-- @if (!empty($item->title))
-    <div>
-        <h2 style="margin-top:40px;font-size:20px;text-transform:uppercase;">{!! $item->title !!}</h2>
-    </div>
-@endif -->
-
 @php
 
     $type = isset($item['type']) ? $item['type'] : 'video';
@@ -21,18 +15,6 @@
 
     $loop = isset($item['loop']) && $item['loop'];
     $loop_or_once = isset($item['loop_or_once']) ? $item['loop_or_once'] : 'loop';
-
-    // WEB-912: For Gallery Items; image module is an array, but gallery item is object?
-    if (isset($item['videoUrl']) || (!is_array($item) && !empty($item->present()->input('videoUrl'))))
-    {
-        $type = 'embed';
-        $poster = $media;
-        $media['embed'] = \App\Facades\EmbedConverterFacade::convertUrl($item['videoUrl'] ?? $item->present()->input('videoUrl'));
-    }
-
-    if ($type === 'embed' and strrpos($media['embed'],'api.soundcloud.com')) {
-        $size = 's';
-    }
 
     $defaultSrcset = array(300,600,800,1200,1600,2000,3000,4500);
 
