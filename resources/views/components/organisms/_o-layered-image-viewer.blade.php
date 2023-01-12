62eddf89002cf5dde45177fcf6b153fc24a093cf
Hannah Baker <hannahb@cogapp.com>
2023-01-12 11:50:38 +0000

    Remove unneeded code so only left with images

--- resources/views/components/organisms/_o-layered-image-viewer.blade.php
+++ resources/views/components/organisms/_o-layered-image-viewer.blade.php
@@ -1,26 +1,4 @@
 <div class="o-gallery o-gallery--mosaic{{ (isset($variation)) ? ' '.$variation : '' }}{{ empty($title) ? ' o-gallery----headerless' : '' }}">
-    @if (!empty($title))
-        <h3 id="{{ Str::slug(html_entity_decode($title)) }}" class="o-gallery__title f-module-title-2">{!! $title !!}</h3>
-    @endif
-    @if (!empty($allLink))
-    <p class="o-gallery__all-link f-buttons">
-        @component('components.atoms._arrow-link')
-            @slot('href', $allLink['href'])
-            {{ $allLink['label'] }}
-        @endcomponent
-    </p>
-    @endif
-    @if (!empty($title) && !empty($caption))
-        <div class="o-gallery__caption">
-            @component('components.atoms._hr')
-            @endcomponent
-            @component('components.blocks._text')
-                @slot('font','f-caption')
-                @slot('tag', 'div')
-                {!! $caption !!}
-            @endcomponent
-        </div>
-    @endif
     <div class="o-gallery__media o-gallery__media--2-col@small o-gallery__media--2-col@medium o-gallery__media--2-col@large o-gallery__media--2-col@xlarge" data-behavior="pinboard">
         @component('site.shared._mediaitems')
             @slot('items', $items)
@@ -36,14 +14,4 @@
             ))
         @endcomponent
     </div>
-    @if (!empty($allLink))
-    <p class="o-gallery__all-link-btn">
-        @component('components.atoms._btn')
-            @slot('variation', 'btn--contrast')
-            @slot('tag', 'a')
-            @slot('href', $allLink['href'])
-            {{ $allLink['label'] }}
-        @endcomponent
-    </p>
-    @endif
 </div>
