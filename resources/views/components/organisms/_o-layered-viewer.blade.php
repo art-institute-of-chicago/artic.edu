bb55838984de86cd7456d291e4cc6ee7faf5c167
Hannah Baker <hannahb@cogapp.com>
2023-01-13 11:34:09 +0000

    Rename files as names were getting too long

--- resources/views/components/organisms/_o-layered-viewer.blade.php
+++ resources/views/components/organisms/_o-layered-viewer.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div>
-        @component('components.molecules._m-layered-image-viewer-img')
+        @component('components.molecules._m-layered-viewer-img')
             @slot('items', $items)
             @slot('imageSettings', $imageSettings ?? array(
                 'srcset' => array(200,400,600,1000,1500,3000),
