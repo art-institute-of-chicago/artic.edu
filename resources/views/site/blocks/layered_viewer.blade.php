db9067cac35fb9890739f8cd001f3e2fcf437dcc
Hannah Baker <hannahb@cogapp.com>
2023-01-13 11:42:15 +0000

    Remove molecule layer as not needed

--- resources/views/site/blocks/layered_viewer.blade.php
+++ resources/views/site/blocks/layered_viewer.blade.php
@@ -23,5 +23,15 @@
     @component('components.organisms._o-layered-viewer')
         @slot('variation', 'o-blocks__block')
         @slot('items', $items)
+        @slot('imageSettings', $imageSettings ?? array(
+                'srcset' => array(200,400,600,1000,1500,3000),
+                'sizes' => ImageHelpers::aic_imageSizes(array(
+                    'xsmall' => '58',
+                    'small' => '28',
+                    'medium' => '28',
+                    'large' => '28',
+                    'xlarge' => '21',
+                )),
+            ))
     @endcomponent
 @endif
