64034f85f12e4266295e5cab3c107752f4944ec2
Hannah Baker <hannahb@cogapp.com>
2023-01-16 11:16:22 +0000

    Remove comment and change figcaption to div

--- resources/views/components/organisms/_o-layered-image-viewer.blade.php
+++ resources/views/components/organisms/_o-layered-image-viewer.blade.php
@@ -64,8 +64,7 @@
     @endforeach
 @endif
 
-<!-- figcaption should be inside figure, but that doesn't translate to repeater fields...  -->
-<figcaption>
+<div>
     @if (isset($captionTitle))
         <div class="{{ isset($caption) ? 'f-caption-title' : 'f-caption' }}"><div>
             @if(isset($item['urlTitle']) && $item['urlTitle'])
@@ -78,6 +77,6 @@
     @if (isset($caption))
         <div class="f-caption">{!! $caption !!}</div>
     @endif
-</figcaption>
+</div>
 
 
