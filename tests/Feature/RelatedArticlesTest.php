7c68ab939b7be7116855f7ae826e0d80d9f8e95c
Zach Garwood <iam@zachgarwood.com>
2023-02-01 11:54:54 -0600

    Move related article data into related article seeder [WEB-2509]

--- tests/Feature/RelatedArticlesTest.php
+++ tests/Feature/RelatedArticlesTest.php
@@ -3,16 +3,11 @@
 namespace Tests\Feature;
 
 use Aic\Hub\Foundation\Testing\FeatureTestCase as BaseTestCase;
-use Illuminate\Support\Facades\DB;
 
 class RelatedArticlesTest extends BaseTestCase
 {
     public function test_related_articles_exist()
     {
-        $allArticlesCount = DB::table('article_article')->select('*')->count()
-            + DB::table('article_page')->select('*')->count()
-            + DB::table('page_art_article')->select('*')->count()
-            + DB::table('article_artist')->select('*')->get();
-        $this->assertDatabaseCount('related', $allArticlesCount);
+        $this->markTestIncomplete("Test related articles load on the correct page(s).");
     }
 }
