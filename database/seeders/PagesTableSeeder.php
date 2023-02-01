f5b022a05276970f676d1ae2426e99e250abe29c
Zach Garwood <iam@zachgarwood.com>
2023-02-01 10:41:16 -0600

    Move basic page data into pages table seeder [WEB-2509]

--- database/seeders/PagesTableSeeder.php
+++ database/seeders/PagesTableSeeder.php
@@ -18,5 +18,68 @@ class PagesTableSeeder extends Seeder
 
             $page->save();
         }
+
+        Page::create([
+            'published' => true,
+            'position' => 7,
+            'title' => 'Research and Resources',
+            'type' => 7,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 8,
+            'title' => 'Articles and Publications',
+            'type' => 8,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 0,
+            'title' => 'Home',
+            'type' => 0,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 1,
+            'title' => 'Exhibitions and Events',
+            'type' => 1,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 2,
+            'title' => 'Art and Ideas',
+            'type' => 2,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 3,
+            'title' => 'Visit',
+            'type' => 3,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 4,
+            'title' => 'Articles',
+            'type' => 4,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 5,
+            'title' => 'Exhibition History',
+            'type' => 5,
+        ]);
+
+        Page::create([
+            'published' => true,
+            'position' => 6,
+            'title' => 'Collection',
+            'type' => 6,
+        ]);
     }
 }
