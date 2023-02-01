e18211ecb419401a0e7ec55176399cc8d4095bcf
Zach Garwood <iam@zachgarwood.com>
2023-02-01 15:03:32 -0600

    Comment on necessity of publish start date seeder

--- database/seeders/PublishStartDateSeeder.php
+++ database/seeders/PublishStartDateSeeder.php
@@ -11,11 +11,14 @@ class PublishStartDateSeeder extends Seeder
     private $tableNames = [
         'articles',
         'events',
-        'selections',
+        'highlights',
     ];
 
     public function run(): void
     {
+        // XXX I think I was mistaken, the migration this came from:
+        // https://github.com/art-institute-of-chicago/artic.edu/pull/5/files#diff-7870073f898417541722fbad31dd4e28f0d780f7c9b28a1b8773ca2852067665
+        // looks like it's renaming/recasting the published column. nikhil, is that correct?
         foreach ($this->tableNames as $tableName) {
             DB::table($tableName)->where('published', 1)->update(['publish_start_date' => Carbon::now()]);
         }
