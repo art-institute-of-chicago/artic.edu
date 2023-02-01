ef7497a25eef5fbf51869b7b97a14f9719d302b9
Zach Garwood <iam@zachgarwood.com>
2023-02-01 15:21:02 -0600

    Question necessity of highlight seeder

--- database/seeders/HighlightSeeder.php
+++ database/seeders/HighlightSeeder.php
@@ -9,6 +9,7 @@ class HighlightSeeder extends Seeder
 {
     public function run(): void
     {
+        // XXX I don't think this is necessary anymore. In the original migration a default was set for this field.
         DB::table('selections')->update(['highlight_type' => 0]);
     }
 }
