5999d28a7e254d8650f6c444091350dc67c573c2
jonw-cogapp <jonw@cogapp.com>
2023-02-08 18:00:11 +0000

    Improve no-js rendering

--- resources/views/components/organisms/_o-layered-image-viewer.blade.php
+++ resources/views/components/organisms/_o-layered-image-viewer.blade.php
@@ -1,9 +1,9 @@
 <div class="o-layered-image-viewer{{ (isset($variation)) ? ' '.$variation : '' }}" data-size="{{ (isset($size)) ? $size : 'm' }}" data-behavior="layeredImageViewer">
     @if (isset($images) && !empty($images))
-        <div class="o-layered-image-viewer__images">
+        <div class="o-layered-image-viewer__images o-blocks__block">
             @foreach ($images as $image)
                 {{-- Repeat div for every image (figure inside is just hardcoded copy of what seems to come out of molecule) --}}
-                <div class="o-layered-image-viewer__image">
+                <div class="o-layered-image-viewer__image o-blocks__block">
                     @component('components.molecules._m-layered-image-viewer-layer')
                         @slot('item', $image)
                         @slot('size', $size)
@@ -15,10 +15,10 @@
     @endif
 
     @if (isset($overlays) && !empty($overlays))
-        <div class="o-layered-image-viewer__annotations">
+        <div class="o-layered-image-viewer__overlays o-blocks__block">
             @foreach ($overlays as $overlay)
                 {{-- Repeat div for every overlay --}}
-                <div class="o-layered-image-viewer__annotation">
+                <div class="o-layered-image-viewer__overlay o-blocks__block">
                     @component('components.molecules._m-layered-image-viewer-layer')
                         @slot('item', $overlay)
                         @slot('size', $size)
@@ -31,11 +31,11 @@
 
     @if (isset($captionTitle))
         <div class="o-layered-image-viewer__caption">
-            <div class="o-layered-image-viewer__caption-title">
+            <div class="o-layered-image-viewer__caption-title f-caption-title">
                 {!! $captionTitle !!}
             </div>
             @if (isset($captionText))
-                <div class="o-layered-image-viewer__caption-text">
+                <div class="o-layered-image-viewer__caption-text f-caption">
                     {!! $captionText !!}
                 </div>
             @endif
