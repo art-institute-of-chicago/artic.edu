ad68903aebde2200359add910cf7167bf098ed2b
jonw-cogapp <jonw@cogapp.com>
2023-02-13 12:03:15 +0000

    Output data attribute on starting items

--- resources/views/components/molecules/_m-layered-image-viewer-layer.blade.php
+++ resources/views/components/molecules/_m-layered-image-viewer-layer.blade.php
@@ -64,7 +64,7 @@
     global $_allowAdvancedModalFeatures;
 @endphp
 
-<div class="o-layered-image-viewer__{{ $layerType }} o-blocks__block">
+<div class="o-layered-image-viewer__{{ $layerType }} o-blocks__block" {{ $layerType == 'overlay' && $startingView ? 'data-active-overlay' : '' }}>
     <figure class="m-media m-media--{{ (isset($size)) ? $size : 'm' }} m-media--layered-image-viewer-embed m-media--contain">
         <div class="m-media__img" data-behavior="fitText">
             @if ($size === 'm' || $size === 'l')
