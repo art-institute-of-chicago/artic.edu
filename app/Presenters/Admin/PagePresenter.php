1344dd6ba97014527bb73d903ad02f4ed29997ec
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-03-17 11:19:53 -0500

    Remove Issue and IssueArticle render from PagePresenter [WEB-2533]

--- app/Presenters/Admin/PagePresenter.php
+++ app/Presenters/Admin/PagePresenter.php
@@ -2,9 +2,6 @@
 
 namespace App\Presenters\Admin;
 
-use App\Models\Issue;
-use App\Repositories\IssueRepository;
-
 use App\Presenters\BasePresenter;
 
 class PagePresenter extends BasePresenter
@@ -29,30 +26,5 @@ class PagePresenter extends BasePresenter
             'media' => $this->entity->imageFront('exhibition_history_intro'),
             'hideCaption' => true
         ];
-    }
-
-    public function getFeaturedJournalIssue()
-    {
-        return $this->featuredJournalIssue ?? $this->featuredJournalIssue = (new IssueRepository(new Issue()))->getLatestIssue();
-    }
-
-    public function getFeaturedJournalArticles()
-    {
-        $featuredJournalArticles = $this->entity->getRelated('featuredJournalArticles')->where('published', true);
-
-        if ($featuredJournalArticles->count() > 0) {
-            return $featuredJournalArticles;
-        }
-
-        $featuredJournalIssue = $this->getFeaturedJournalIssue();
-
-        if (!$featuredJournalIssue) {
-            return;
-        }
-
-        return $featuredJournalIssue
-            ->present()
-            ->articlesForLanding()
-            ->slice(0, 4);
     }
 }
