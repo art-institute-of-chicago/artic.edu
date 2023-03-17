eb51090f77e29ca7bd44a1364569876ce2fcb367
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-03-17 11:27:57 -0500

    Remove IssueArticles from browser on publications block [WEB-2533]

--- resources/views/admin/pages/form_articles_and_publications.blade.php
+++ resources/views/admin/pages/form_articles_and_publications.blade.php
@@ -18,15 +18,6 @@
     ])
 
     @formField('browser', [
-        'max' => 4,
-        'endpoint' => route('admin.collection.issues.articles.subbrowser',[
-            'issue' => $item->present()->getFeaturedJournalIssue()->id ?? 0,
-        ]),
-        'name' => 'featuredJournalArticles',
-        'label' => 'Featured journal articles',
-    ])
-
-    @formField('browser', [
         'routePrefix' => 'collection.interactive_features',
         'max' => 4,
         'moduleName' => 'experiences',
