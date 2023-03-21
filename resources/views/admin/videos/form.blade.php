167d4cfe0aad74600b332d403aa6b51c9931ee97
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-03-21 15:18:26 -0500

    Render CMS for Metadata fields [WEB-2306]

--- resources/views/admin/videos/form.blade.php
+++ resources/views/admin/videos/form.blade.php
@@ -60,6 +60,20 @@
 
 @section('fieldsets')
 
+    <a17-fieldset id="related_to" title="Related">
+
+        <p>If this is left blank, we will show the four most recently published videos.</p>
+
+        @formField('browser', [
+            'routePrefix' => 'collection.articles_publications',
+            'name' => 'related_videos',
+            'moduleName' => 'videos',
+            'label' => 'Related videos',
+            'max' => 4,
+        ])
+
+    </a17-fieldset>
+
     <a17-fieldset id="metadata" title="Overwrite default metadata (optional)">
         @formField('input', [
             'name' => 'meta_title',
@@ -79,20 +93,6 @@
         ])
 
     <p>Comma-separatated list of words or phrases. Don't worry about grammar or similar word variations. This field is intended to assist our internal search engine in finding your content. These tags will not be shown to website users and will have no effect on external search engines, e.g. Google.</p>
-
-    </a17-fieldset>
-
-    <a17-fieldset id="related_to" title="Related">
-
-        <p>If this is left blank, we will show the four most recently published videos.</p>
-
-        @formField('browser', [
-            'routePrefix' => 'collection.articles_publications',
-            'name' => 'related_videos',
-            'moduleName' => 'videos',
-            'label' => 'Related videos',
-            'max' => 4,
-        ])
 
     </a17-fieldset>
 
