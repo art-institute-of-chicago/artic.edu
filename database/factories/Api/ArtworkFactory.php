46231cb3395035c0d59d7d504bc3f8217da6a955
nikhil trivedi <ntrivedi@artic.edu>
2023-04-05 16:59:02 -0500

    Add main images to artwork factory [WEB-2563]

--- database/factories/Api/ArtworkFactory.php
+++ database/factories/Api/ArtworkFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories\Api;
 
 use App\Models\Api\Artwork;
+use App\Models\Api\Image;
 
 class ArtworkFactory extends ApiFactory
 {
@@ -15,5 +16,15 @@ class ArtworkFactory extends ApiFactory
             'title' => ucfirst($this->faker->words(nb: 5, asText: true)),
             'edition' => "{$this->faker->randomNumber()} of {$this->faker->randomNumber(nbDigits: 2, strict: true)}",
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Artwork $artwork) {
+            $artwork->mainImage()->attach(
+                Image::create(['uuid' => $this->faker->uuid()]),
+                ['crop' => 'default', 'role' => 'hero'],
+            );
+        });
     }
 }
