801e5544d4b36864dd19c3df967446052b382140
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-04-12 14:16:55 -0500

    Add enclosed item hero image to fallback order [WEB-2328]

--- resources/views/components/molecules/_m-listing----article.blade.php
+++ resources/views/components/molecules/_m-listing----article.blade.php
@@ -10,7 +10,7 @@
                             @slot('class', 'img-hero-desktop')
                         @endcomponent
                         @component('components.atoms._img')
-                            @slot('image', $imageMobile ?? $item->imageFront('mobile_hero') ?? $item->imageFront('hero'))
+                            @slot('image', $imageMobile ?? $item->imageFront('mobile_hero') ?? $image ?? $item->imageFront('hero'))
                             @slot('settings', $imageSettings ?? '')
                             @slot('class', 'img-hero-mobile')
                         @endcomponent
