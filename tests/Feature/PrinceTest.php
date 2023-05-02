61e62d6d8fa02d65de5322f506f8deadb5ebf07f
Zach Garwood <iam@zachgarwood.com>
2023-05-02 16:57:37 -0500

    Add error handling and cleanup to PDF generation [WEB-2569]

---
+++ tests/Feature/PrinceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Aic\Hub\Foundation\Testing\FeatureTestCase as BaseTestCase;
+use App\Models\DigitalPublication;
+use App\Models\DigitalPublicationSection;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+
+class PrinceTest extends BaseTestCase
+{
+    protected Collection $sections;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->sections = DigitalPublicationSection::factory()
+            ->count(2)
+            ->published()
+            ->for(DigitalPublication::factory()->published())
+            ->create();
+    }
+
+    public function test_errors_when_prince_binary_not_present()
+    {
+        Http::fake();
+        $noPrinceCommand = '/dev/null';
+        Config::set('aic.prince_command', $noPrinceCommand);
+        $this->artisan('pdfs:generate')->assertFailed()->expectsOutput("Prince could not be found at $noPrinceCommand");
+    }
+
+    public function test_errors_when_prince_cannot_generate_pdf()
+    {
+        Http::fake(['*' => Http::response('<link rel="stylesheet" href="missing resource">')]);
+        $id = $this->sections->first()->id;
+        $this->artisan('pdfs:generate-one', [
+            'model' => DigitalPublicationSection::class,
+            'id' => $id,
+        ])
+            ->assertFailed()
+            ->expectsOutput("Prince was unable to generate a PDF for DigitalPublicationSection with ID {$id}");
+    }
+}
