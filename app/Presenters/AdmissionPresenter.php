546372f1ebb57146ff3c022e96bc16859c2f2521
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-05-18 15:02:16 -0500

    Create `AdmissionPresenter` [WEB-2606]

---
+++ app/Presenters/AdmissionPresenter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Models\Admission;
+use App\Models\Fee;
+use App\Models\FeeAge;
+use App\Models\FeeCategory;
+use Illuminate\Support\Str;
+
+class AdmissionPresenter extends BasePresenter
+{
+    public function feeTitles()
+    {
+        $titles = [];
+
+        foreach (FeeCategory::ordered()->get() as $category) {
+            $title = [
+                'title' => $category->title,
+                'id' => Str::slug($category->title),
+                'tooltip' => $category->tooltip
+            ];
+
+            $titles[$category->id] = $title;
+        }
+
+        return $titles;
+    }
+
+    public function feePrices()
+    {
+        $fees = Fee::all();
+        $prices = [];
+
+        foreach (FeeCategory::ordered()->get() as $category) {
+            foreach (FeeAge::ordered()->get() as $age) {
+                $fee = $fees->where('fee_age_id', $age->id)->where('fee_category_id', $category->id)->first();
+
+                if ($fee) {
+                    $price = [
+                        'title' => $age->title,
+                        'title_en' => $age->title,
+                        $category->id => $fee->price
+                    ];
+
+                    $prices[$category->id][$age->id] = $price;
+                }
+            }
+        }
+
+        return $prices;
+    }
+}
