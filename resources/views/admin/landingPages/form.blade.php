748373114a58ebd3d16a082b1ee604c54948349f
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-05-18 15:06:36 -0500

    Update `LandingPage` CMS form with hard-coded fields [WEB-2606]

--- resources/views/admin/landingPages/form.blade.php
+++ resources/views/admin/landingPages/form.blade.php
@@ -1,130 +1,114 @@
+{{-- 
+    // Values & Labels for Page Types
+    // 1 -> Home
+    // 2 -> Exhibitions and Events
+    // 3 -> Collection
+    // 4 -> Visit
+    // 5 -> Articles
+    // 6 -> Exhibition History
+    // 7 -> Art and Ideas
+    // 8 -> Research and Resources
+    // 9 -> Articles and Publications
+    // 10 -> Custom 
+--}}
+
 @extends('twill::layouts.form')
 
 @section('contentFields')
+
     @formField('select', [
         'name' => 'type',
         'label' => 'Page type',
-        'default' => \App\Models\LandingPage::PAGE_TYPE_HOME,
+        'default' => null,
         'inline' => true,
-        'options' => [
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_HOME,
-                'label' => 'Home'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_EXHIBITIONS_AND_EVENTS,
-                'label' => 'Exhibitions and Events'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_THE_COLLECTION,
-                'label' => 'The Collection'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_VISIT,
-                'label' => 'Visit'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_ARTICLES,
-                'label' => 'Articles landing'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_EXHIBITION_HISTORY,
-                'label' => 'Exhibition History'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_COLLECTION,
-                'label' => 'Collection landing'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_RESEARCH_LANDING,
-                'label' => 'Research landing'
-            ],
-            [
-                'value' => \App\Models\LandingPage::PAGE_TYPE_WRITINGS_LANDING,
-                'label' => 'Writings landing'
-            ],
-        ]
+        'options' => $typesOptions,
     ])
 
-    @component('twill::partials.form.utils._connected_fields', [
-        'fieldName' => 'type',
-        'fieldValues' => \App\Models\LandingPage::PAGE_TYPE_HOME,
-        'renderForBlocks' => false
+@stop
+
+@section('fieldsets')
+
+@component('twill::partials.form.utils._connected_fields', [
+    'fieldName' => 'type',
+    'fieldValues' => 1, //Home
+    'renderForBlocks' => false
+])
+    @formField('input', [
+        'name' => 'home_intro',
+        'label' => 'Intro text',
+        'type' => 'textarea'
     ])
-        @formField('input', [
-            'name' => 'home_intro',
-            'label' => 'Intro text',
-            'type' => 'textarea'
-        ])
 
-        @formField('browser', [
-            'routePrefix' => 'homepage',
-            'max' => 3,
-            'moduleName' => 'homeFeatures',
-            'name' => 'mainHomeFeatures',
-            'label' => 'Main feature',
-            'note' => 'Queue up to 3 home features for the large hero area',
-        ])
-
-        @formField('browser', [
-            'routePrefix' => 'homepage',
-            'max' => 5,
-            'moduleName' => 'homeFeatures',
-            'name' => 'secondaryHomeFeatures',
-            'label' => 'Secondary features',
-            'note' => 'Queue up to 5 home features for the two smaller hero areas',
-        ])
-
-        @formField('input', [
-            'name' => 'home_visit_button_text',
-            'label' => 'Label for "Visit" button',
-            'note' => 'Defaults to "Visit"',
-        ])
-
-        @formField('input', [
-            'name' => 'home_visit_button_url',
-            'label' => 'Link for "Visit" button',
-            'note' => 'Defaults to "/visit"',
-        ])
-
-        <hr/>
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_1_text',
-            'label' => 'First "Plan your visit" link text',
-        ])
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_1_url',
-            'label' => 'First "Plan your visit" link URL',
-        ])
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_2_text',
-            'label' => 'Second "Plan your visit" link text',
-        ])
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_2_url',
-            'label' => 'Second "Plan your visit" link URL',
-        ])
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_3_text',
-            'label' => 'Third "Plan your visit" link text',
-        ])
-
-        @formField('input', [
-            'name' => 'home_plan_your_visit_link_3_url',
-            'label' => 'Third "Plan your visit" link URL',
-        ])
-    @endcomponent
-
-    @component('twill::partials.form.utils._connected_fields', [
-        'fieldName' => 'type',
-        'fieldValues' => \App\Models\LandingPage::PAGE_TYPE_VISIT,
-        'renderForBlocks' => false
+    @formField('browser', [
+        'routePrefix' => 'homepage',
+        'max' => 3,
+        'moduleName' => 'homeFeatures',
+        'name' => 'mainHomeFeatures',
+        'label' => 'Main feature',
+        'note' => 'Queue up to 3 home features for the large hero area',
     ])
+
+    @formField('browser', [
+        'routePrefix' => 'homepage',
+        'max' => 5,
+        'moduleName' => 'homeFeatures',
+        'name' => 'secondaryHomeFeatures',
+        'label' => 'Secondary features',
+        'note' => 'Queue up to 5 home features for the two smaller hero areas',
+    ])
+
+    @formField('input', [
+        'name' => 'home_visit_button_text',
+        'label' => 'Label for "Visit" button',
+        'note' => 'Defaults to "Visit"',
+    ])
+
+    @formField('input', [
+        'name' => 'home_visit_button_url',
+        'label' => 'Link for "Visit" button',
+        'note' => 'Defaults to "/visit"',
+    ])
+
+    <hr/>
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_1_text',
+        'label' => 'First "Plan your visit" link text',
+    ])
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_1_url',
+        'label' => 'First "Plan your visit" link URL',
+    ])
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_2_text',
+        'label' => 'Second "Plan your visit" link text',
+    ])
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_2_url',
+        'label' => 'Second "Plan your visit" link URL',
+    ])
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_3_text',
+        'label' => 'Third "Plan your visit" link text',
+    ])
+
+    @formField('input', [
+        'name' => 'home_plan_your_visit_link_3_url',
+        'label' => 'Third "Plan your visit" link URL',
+    ])
+@endcomponent
+
+@component('twill::partials.form.utils._connected_fields', [
+    'fieldName' => 'type',
+    'fieldValues' => 4, // Visit
+    'renderForBlocks' => false
+])
+
+    <a17-fieldset title="Header" id="visit_header">
         @formField('medias', [
             'name' => 'visit_hero',
             'label' => 'Hero image',
@@ -142,92 +126,120 @@
             'label' => 'Hero image, mobile',
             'note' => 'Minimum image width 2000px'
         ])
+    </a17-fieldset>
 
-        @formField('checkbox', [
-            'name' => 'visit_hide_hours',
-            'label' => 'Hide hours table and image',
+    <a17-fieldset title="Navigation Menu" id="visit_nav-menu">
+
+        @formField('repeater', ['type' => 'menu_items'])
+
+        @formField('input', [
+            'name' => 'visit_nav_buy_tix_label',
+            'label' => 'Tickets Label'
         ])
 
         @formField('input', [
-            'name' => 'visit_hour_intro',
-            'label' => 'Intro text',
-            'type' => 'textarea'
+            'name' => 'visit_nav_buy_tix_link',
+            'label' => 'Tickets Link'
         ])
 
-        @formField('medias', [
-            'name' => 'visit_featured_hour',
-            'label' => 'Image',
-            'max' => '1',
-            'note' => 'Minimum image width 2000px'
-        ])
+    </a17-fieldset>
+
+    <a17-fieldset title="Hours" id="visit_hours">
         @formField('wysiwyg', [
-            'name' => 'visit_hour_image_caption',
-            'field_name' => 'visit_hour_image_caption',
-            'label' => 'Image caption',
+            'name' => 'visit_hours_intro',
+            'label' => 'Hours Intro',
             'toolbarOptions' => [
-                'italic', 'link'
+            'bold', 'italic', 'link'
             ],
         ])
-        @formField('input', [
-            'name' => 'visit_hour_header',
-            'field_name' => 'visit_hour_header',
-            'label' => 'Header',
-            'required' => true
-        ])
+
         @formField('wysiwyg', [
-            'name' => 'visit_hour_subheader',
-            'field_name' => 'visit_hour_subheader',
-            'label' => 'Description',
-            'required' => true
-        ])
-        @formField('repeater', ['type' => 'featured_hours'])
-    @endcomponent
-
-    @component('twill::partials.form.utils._connected_fields', [
-        'fieldName' => 'type',
-        'fieldValues' => \App\Models\LandingPage::PAGE_TYPE_RESEARCH_LANDING,
-        'renderForBlocks' => false
-    ])
-        @formField('input', [
-            'name' => 'resources_landing_title',
-            'label' => 'Title',
+            'name' => 'visit_members_intro',
+            'label' => 'Member Intro',
+            'toolbarOptions' => [
+            'bold', 'italic', 'link'
+            ],
         ])
 
-        @formField('input', [
-            'name' => 'resources_landing_intro',
-            'label' => 'Intro text',
-            'type' => 'textarea'
+        @formField('checkbox', [
+            'name' => 'is_custom_hours',
+            'label' => 'Override default hours',
         ])
 
+        @component('twill::partials.form.utils._connected_fields', [
+            'fieldName' => 'is_custom_hours',
+            'fieldValues' => true
+        ])
+            @formField('repeater', ['type' => 'featured_hours'])
+        @endcomponent
+    </a17-fieldset>
+
+    <a17-fieldset title="Admission" id="visit_admission">
+        @formField('wysiwyg', [
+            'name' => 'visit_admission_intro',
+            'label' => 'Admission Intro'
+        ])
+    </a17-fieldset>
+
+    <a17-fieldset title="Location" id="visit_hours">
         @formField('medias', [
-            'label' => 'Hero image',
-            'name' => 'research_landing_image'
+            'name' => 'visit_map',
+            'label' => 'Map Image',
+            'note' => 'Minimum image width 3000px'
         ])
 
-        @formField('browser', [
-        'routePrefix' => 'generic',
-            'max' => 9,
-            'moduleName' => 'genericPages',
-            'name' => 'researchResourcesFeaturePages',
-            'label' => 'Featured pages'
-        ])
+        @formField('repeater', ['type' => 'locations', 'max' => 2])
+    </a17-fieldset>
 
-        @formField('browser', [
-        'routePrefix' => 'generic',
-            'max' => 3,
-            'moduleName' => 'genericPages',
-            'name' => 'researchResourcesStudyRooms',
-            'label' => 'Study room pages'
-        ])
+@endcomponent
 
-        @formField('browser', [
-        'routePrefix' => 'generic',
-            'max' => 1,
-            'moduleName' => 'genericPages',
-            'name' => 'researchResourcesStudyRoomMore',
-            'label' => 'Study room more link'
-        ])
-    @endcomponent
+@component('twill::partials.form.utils._connected_fields', [
+    'fieldName' => 'type',
+    'fieldValues' => 8, // Research and Resources
+    'renderForBlocks' => false
+])
+    @formField('input', [
+        'name' => 'resources_landing_title',
+        'label' => 'Title',
+    ])
+
+    @formField('input', [
+        'name' => 'resources_landing_intro',
+        'label' => 'Intro text',
+        'type' => 'textarea'
+    ])
+
+    @formField('medias', [
+        'label' => 'Hero image',
+        'name' => 'research_landing_image'
+    ])
+
+    @formField('browser', [
+    'routePrefix' => 'generic',
+        'max' => 9,
+        'moduleName' => 'genericPages',
+        'name' => 'researchResourcesFeaturePages',
+        'label' => 'Featured pages'
+    ])
+
+    @formField('browser', [
+    'routePrefix' => 'generic',
+        'max' => 3,
+        'moduleName' => 'genericPages',
+        'name' => 'researchResourcesStudyRooms',
+        'label' => 'Study room pages'
+    ])
+
+    @formField('browser', [
+    'routePrefix' => 'generic',
+        'max' => 1,
+        'moduleName' => 'genericPages',
+        'name' => 'researchResourcesStudyRoomMore',
+        'label' => 'Study room more link'
+    ])
+@endcomponent
+
+<a17-fieldset title="Custom Content" id="custom_content">
 
     @formField('block_editor', [
         'blocks' => BlockHelpers::getBlocksForEditor([
@@ -240,4 +252,7 @@
             'feature_2x', 'feature_4x'
         ])
     ])
+
+</a17-fieldset>
+
 @stop
