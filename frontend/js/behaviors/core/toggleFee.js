05eea61d73d36c1c2f745dcefb9f144f83517e94
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-05-23 14:58:11 -0500

    Update `toggleFee` method for Select [WEB-2596]

--- frontend/js/behaviors/core/toggleFee.js
+++ frontend/js/behaviors/core/toggleFee.js
@@ -1,35 +1,48 @@
 const toggleFee = function(container) {
 
+  if (container.tagName === 'SELECT') {
+    container.addEventListener('change', function() {
+      showFee.call(this);
+    });
+  } else {
     container.addEventListener('click', function() {
-        showFee.call(this);
-            selectCategory.call(this);
-        });
+      showFee.call(this);
+      selectCategoryButton.call(this);
+  });
+  }
+    
+  function showFee() {
+
+    let targetAttribute;
+
+    if (this.value) {
+      targetAttribute = this.value;
+    } else {
+      targetAttribute = this.getAttribute('data-target');
+    }
+
+    // Remove "selected" class from all elements with class "fee-ages"
+    const feeAges = document.querySelectorAll('.fee-ages.selected');
+    feeAges.forEach(element => {
+      element.classList.remove('selected');
+    });
       
-        function showFee() {
-          const targetAttribute = this.getAttribute('data-target');
+    // Find the element with the matching ID and add the "selected" class
+    const targetElement = document.getElementById(targetAttribute);
+    if (targetElement) {
+      targetElement.classList.add('selected');
+    }
+  }
+    
+  function selectCategoryButton() {
+    const categories = document.querySelectorAll('[data-behavior="toggleFee"]');
         
-          // Remove "selected" class from all elements with class "fee-ages"
-          const feeAges = document.querySelectorAll('.fee-ages.selected');
-          feeAges.forEach(element => {
-            element.classList.remove('selected');
-          });
-        
-          // Find the element with the matching ID and add the "selected" class
-          const targetElement = document.getElementById(targetAttribute);
-          if (targetElement) {
-            targetElement.classList.add('selected');
-          }
-        }
-      
-        function selectCategory() {
-          const categories = document.querySelectorAll('[data-behavior="toggleFee"]');
-          
-          categories.forEach(element => {
-            element.classList.remove('selected');
-          });
-      
-          container.classList.add('selected');
-        }
-      };      
+    categories.forEach(element => {
+      element.classList.remove('selected');
+    });
+    
+    container.classList.add('selected');
+  }
+}
 
 export default toggleFee;
