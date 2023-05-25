944c86fae4c41845f437f32d2d301ff83a0ee31a
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-05-25 12:04:57 -0500

    Create `Showcase` block [WEB-2618]

---
+++ resources/views/site/blocks/showcase.blade.php
@@ -0,0 +1,35 @@
+@php
+    $image = $block->imageAsArray('image', 'desktop');
+
+    $title = $block->input('title');
+    $subtitle = $block->input('subtitle');
+    $tag = $block->input('tag');
+@endphp
+
+<div class="m-showcase-block">
+    @component('components.atoms._img')
+        @slot('image', $image)
+        @slot('settings', $imageSettings ?? '')
+    @endcomponent
+    <div class="m-showcase-block__text-wrapper">
+        @component('components.atoms._title')
+            @slot('tag', 'span')
+            @slot('font', 'f-tag')
+            @slot('variation', 'showcase-tag')
+            @slot('title', $tag)
+        @endcomponent
+        @component('components.atoms._title')
+            @slot('tag', 'div')
+            @slot('font', 'f-headline-editorial')
+            @slot('variation', 'showcase-title')
+            @slot('title', $title)
+        @endcomponent
+        @component('components.atoms._title')
+            @slot('tag', 'div')
+            @slot('font', 'f-secondary')
+            @slot('variation', 'showcase-subtitle')
+            @slot('title', $subtitle)
+        @endcomponent
+    </div>
+</div>
+    
