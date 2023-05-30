67e58afb2c751416089e2861cc764c2279515a55
nikhil trivedi <ntrivedi@artic.edu>
2023-05-30 12:05:00 -0500

    Refactor menu_items migrations to use Schema facade [WEB-2596]

--- database/migrations/2023_05_12_103223_create_menu_items_table.php
+++ database/migrations/2023_05_12_103223_create_menu_items_table.php
@@ -1,31 +1,25 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateMenuItemsTable extends Migration
 {
     public function up()
     {
-        DB::statement('CREATE SEQUENCE menu_items_id_seq');
-
-        DB::statement('CREATE TABLE menu_items (
-            id INTEGER DEFAULT nextval(\'menu_items_id_seq\') PRIMARY KEY,
-            created_at TIMESTAMP,
-            updated_at TIMESTAMP,
-            deleted_at TIMESTAMP,
-            published BOOLEAN,
-            position INTEGER,
-            label VARCHAR(255),
-            link VARCHAR(255),
-            page_id INTEGER,
-            landing_page_id INTEGER
-        )');
+        Schema::create('menu_items', function (Blueprint $table) {
+            createDefaultTableFields($table);
+            $table->integer('label');
+            $table->string('position');
+            $table->string('link');
+            $table->integer('page_id');
+            $table->integer('landing_page_id');
+        });
     }
 
     public function down()
     {
-        DB::statement('DROP TABLE IF EXISTS menu_items');
-        DB::statement('DROP SEQUENCE IF EXISTS menu_items_id_seq');
+        Schema::dropIfExists('menu_items');
     }
 }
