0d0d2ba76ea1311a646aeb6e9cc260c8c61e4232
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-06-01 07:47:29 -0700

    Update method to get footer to use `LandingPageTypes`

--- resources/views/site/landingPageDetail.blade.php
+++ resources/views/site/landingPageDetail.blade.php
@@ -9,13 +9,7 @@
         {!! $item->renderBlocks(false, [], []) !!}
 
     </div>
-
-    {{-- @if ($item->type === \App\Models\LandingPage::PAGE_TYPE_HOME)
-        @include('partials._fixed-footer--home')
-    @elseif ($item->type === \App\Models\LandingPage::PAGE_TYPE_VISIT)
-        @include('partials._fixed-footer--visit')
-    @elseif ($item->type === \App\Models\LandingPage::PAGE_TYPE_RESEARCH_LANDING)
-        @include('partials._fixed-footer--research-landing')
-    @endif --}}
+    <hr/>
+    @include('site.landingPage._footer----'.$landingPageType)
 
 @endsection
