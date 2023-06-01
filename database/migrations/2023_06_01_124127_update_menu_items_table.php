2d0b6e62610c114f621c5c027797ddde43367b36
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-06-01 12:19:20 -0700

    Create migrations to fix issues [WEB-2596]

---
+++ database/migrations/2023_06_01_124127_update_menu_items_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMenuItemsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            $table->string('label')->change();
+            $table->integer('page_id')->nullable()->change();
+            $table->integer('landing_page_id')->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+
+    }
+}
