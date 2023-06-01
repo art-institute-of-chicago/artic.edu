5f5e7d270468878dc3947ebeb1aca1a33a63c51b
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-06-01 12:19:20 -0700

    Create migrations to fix issues [WEB-2596]

---
+++ database/migrations/2023_06_01_141407_update_landing_pages_tables.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateLandingPagesTables extends Migration
+
+
+{
+
+    private $tablesToAddLandingPageIdTo = [
+        'featured_hours',
+        'home_artists',
+        'locations',
+        'faqs',
+        'families',
+        'what_to_expects',
+        'dining_hours',
+        'page_printed_publication',
+        'page_home_secondary_home_feature',
+        'page_home_main_home_feature',
+        'page_home_home_feature',
+        'page_home_event',
+        'page_article_category',
+        'page_art_article',
+        'article_page',
+        'experience_page',
+        'digital_publication_page',
+        'research_resource_feature_page',
+        'research_resource_study_room_pages',
+        'research_resource_study_room_more_pages'
+    ];
+    public function up()
+    {
+        foreach ($this->tablesToAddLandingPageIdTo as $t) {
+            Schema::table($t, function (Blueprint $table) {
+                $table->integer('page_id')->nullable()->change();
+                $table->integer('landing_page_id')->nullable()->change();
+            });
+        }
+    }
+
+    public function down()
+    {
+        
+    }
+}
