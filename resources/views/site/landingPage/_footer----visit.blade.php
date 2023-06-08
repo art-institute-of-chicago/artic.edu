e0cc9d44d8f5a75feb325415e064bb1f647e0cc2
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-06-08 12:37:57 -0500

    Reference `faq` attributes using object [WEB-2596]

--- resources/views/site/landingPage/_footer----visit.blade.php
+++ resources/views/site/landingPage/_footer----visit.blade.php
@@ -1,18 +1,18 @@
 <div class="o-accordion" data-behavior="accordion">
     <h3 id="faqs" class="title f-module-title-2">FAQs</h3>
     @foreach ($faqs as $faq)
-    <h3><button id="{{ StringHelpers::getUtf8Slug($faq['question']) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!} aria-expanded="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}">
-        {!! $faq['question'] !!}
+    <h3><button id="{{ StringHelpers::getUtf8Slug($faq->question) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!} aria-expanded="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}">
+        {!! $faq->question !!}
         <span class="o-accordion__trigger-icon">
             <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
             <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
         </span>
     </button></h3>
-    <div id="panel_{{ StringHelpers::getUtf8Slug($faq['question']) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($faq['question']) }}">
+    <div id="panel_{{ StringHelpers::getUtf8Slug($faq->question) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($faq['question']) }}">
         <div class="o-accordion__panel-content o-blocks">
             @component('components.blocks._text')
                 @slot('font','f-body')
-                {!! SmartyPants::defaultTransform($faq['answer']) !!}
+                {!! SmartyPants::defaultTransform($faq->answer) !!}
             @endcomponent
         </div>
     </div>
