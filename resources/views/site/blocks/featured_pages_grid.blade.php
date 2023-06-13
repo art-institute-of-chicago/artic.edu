1f176f078ece12cf8212f4372d4f30511dcb3295
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-06-13 07:31:03 -0500

    Create `Featured_Pages_Grid` blade [WEB-2596]

---
+++ resources/views/site/blocks/featured_pages_grid.blade.php
@@ -0,0 +1,37 @@
+<h3 id="{{ StringHelpers::getUtf8Slug($block->input('grid_heading')) }}" class="title f-module-title-2">{{ $block->input('grid_heading') }}</h3>
+<hr/>
+<div class="featured-pages-grid" data-blur-img>
+    @foreach($block->getRelated('genericPages') as $index => $page)
+        @if($index === count($block->getRelated('genericPages')) || $index !== 0 && $index % 4 === 0)
+            </div>
+        @endif
+        @if($loop->first || $index % 4 === 0)
+            <div class="featured-pages-grid_row">
+        @endif
+        <div class="featured-pages-grid_card">
+            <div class="featured-pages-grid_details">
+                <a href="{{ $page->getURL() }}" class="m-listing__link">
+                    @if($page->imageFront('listing') ?? $page->imageFront('hero'))
+                        @component('components.atoms._img')
+                            @slot('class', 'featured-pages-grid_img')
+                            @slot('image', $page->imageFront('listing') ?? $page->imageFront('hero'))
+                            @slot('settings', array(
+                                'fit' => 'crop',
+                                'ratio' => '16:9',
+                                'srcset' => array(300),
+                                'sizes' => '300px', 
+                            ))
+                        @endcomponent
+                    @else
+                        <span class="default-img featured-pages-grid_img"></span>
+                    @endif
+                    <span>
+                        <h3 class="f-list-2">{{ $page->title }}
+                            <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
+                        </h3>
+                    </span>
+                </a>
+            </div>
+        </div>
+    @endforeach
+</div>
