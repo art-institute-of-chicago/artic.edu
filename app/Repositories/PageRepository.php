1db74ae9e1cdb453badf40919a3f5410bd29523e
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-07-20 17:07:13 -0500

    Revert proof of concept work for `pages` [WEB-2596]

--- app/Repositories/PageRepository.php
+++ app/Repositories/PageRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories;
 
-use A17\Twill\Repositories\Behaviors\HandleBlocks;
 use A17\Twill\Repositories\Behaviors\HandleFiles;
 use A17\Twill\Repositories\Behaviors\HandleMedias;
 use A17\Twill\Repositories\Behaviors\HandleRevisions;
@@ -19,7 +18,6 @@ class PageRepository extends ModuleRepository
     use HandleFiles;
     use HandleApiRelations;
     use HandleTranslations;
-    use HandleBlocks;
 
     protected $browsers = [
         // Homepage landing
