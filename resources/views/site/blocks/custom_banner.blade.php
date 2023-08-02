edef77942ead024777b5ca55d3cfe479cba0c727
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-08-02 10:22:36 -0500

    Update app png icon to svg [WEB-2596]

--- resources/views/site/blocks/custom_banner.blade.php
+++ resources/views/site/blocks/custom_banner.blade.php
@@ -40,8 +40,8 @@
             <div class="button-wrapper">
                 @if($btn_type == 'mobile_app')
                     <div class="banner-apps">
-                        <a class="app-store" target=”_blank” href="https://apps.apple.com/us/app/art-institute-of-chicago-app/id1130366814?itsct=apps_box_badge&amp;itscg=30200"><img src="{{FrontendHelpers::revAsset('images/icon_app-store.png')}}" alt="Download on the App Store"></a>
-                        <a class="google-play" target=”_blank” href='https://play.google.com/store/apps/details?id=edu.artic&hl=en_US&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src="{{FrontendHelpers::revAsset('images/icon_google-play.png')}}"/></a>
+                        <a class="app-store" target=”_blank” href="https://apps.apple.com/us/app/art-institute-of-chicago-app/id1130366814?itsct=apps_box_badge&amp;itscg=30200"><img src="{{FrontendHelpers::revAsset('images/icon_app-store.svg')}}" alt="Download on the App Store"></a>
+                        <a class="google-play" target=”_blank” href='https://play.google.com/store/apps/details?id=edu.artic&hl=en_US&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src="{{FrontendHelpers::revAsset('images/icon_google-play.svg')}}"/></a>
                     </div>
                 @endif
 
