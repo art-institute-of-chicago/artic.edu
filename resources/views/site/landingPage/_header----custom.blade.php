490d8963301d137ba54a84fbb5be438fb442bc1e
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-08-02 11:24:11 -0500

    Add static headers and footers for LPB types [WEB-2596]

---
+++ resources/views/site/landingPage/_header----custom.blade.php
@@ -0,0 +1,18 @@
+<section class="custom" itemscope itemtype="http://schema.org/TouristAttraction">
+    <link itemprop="additionalType" href="http://schema.org/Museum" aria-hidden="true"/>
+    <link itemprop="additionalType" href="http://schema.org/LandmarksOrHistoricalBuildings" aria-hidden="true"/>
+    <link itemprop="additionalType" href="http://schema.org/LocalBusiness" aria-hidden="true"/>
+    @component('site.shared._schemaItemProps')
+      @slot('itemprops',$itemprops ?? null)
+    @endcomponent
+
+    @component('components.molecules._m-media')
+        @slot('item', $headerMedia)
+        @slot('tag', 'span')
+        @slot('imageSettings', array(
+            'srcset' => array(300,600,1000,1500,3000),
+            'sizes' => '100vw',
+        ))
+        @slot('variation', 'm-visit-header')
+    @endcomponent
+</section>
