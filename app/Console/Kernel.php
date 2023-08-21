9963f3ec5e576fc0bee4d52b0478e56fa959a0a6
nikhil trivedi <ntrivedi@artic.edu>
2023-08-21 14:01:37 -0500

    Code updates related to Laravel 10 upgrade [WEB-2646]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('cache:prune-stale-tags')->hourly();
         $schedule->command('sitemap:generate')->twiceDaily();
         $schedule->command('update:links')->daily();
         $schedule->command('update:cloudfront-ips')->hourly();
