fddac79c7634004f04a8e827cbe9f07070eb29bc
nikhil trivedi <ntrivedi@artic.edu>
2023-08-21 18:59:56 -0500

    Fix RobotsTxt test [WEB-2646]

--- tests/Feature/RobotsTxtTest.php
+++ tests/Feature/RobotsTxtTest.php
@@ -33,7 +33,6 @@ class RobotsTxtTest extends BaseTestCase
     {
         Config::set('app.env', 'production');
         Config::set('app.debug', false);
-        Config::set('app.url', 'www-dev.artic.edu');
 
         $response = $this->get(route('robots-txt'));
         $this->assertNotEquals("User-agent: *\nDisallow: /", $response->getContent());
