c390f087263541b59658db7b7c725c283c109733
nikhil trivedi <ntrivedi@artic.edu>
2023-08-23 09:48:03 -0500

    Fix typo [WEB-2646]

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,7 +10,7 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * The path to your application's "home" route.
      *
-     * TTypically, users are redirected here after authentication.
+     * Typically, users are redirected here after authentication.
      *
      * @var string
      */
