0399f3f37c1479c62d5d770673ad8e9e998dd3b1
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-09-18 15:44:33 -0500

    Update `Showcase` block to conditionally render fields [WEB-2629]

--- resources/views/site/blocks/showcase.blade.php
+++ resources/views/site/blocks/showcase.blade.php
@@ -15,30 +15,38 @@
             @slot('settings', $imageSettings ?? '')
         @endcomponent
         <div class="m-showcase-block__text-wrapper">
-            @component('components.atoms._title')
-                @slot('tag', 'span')
-                @slot('font', 'f-tag')
-                @slot('variation', 'showcase-tag')
-                @slot('title', $tag)
-            @endcomponent
-            @component('components.atoms._title')
-                @slot('tag', 'div')
-                @slot('font', 'f-headline-editorial')
-                @slot('variation', 'showcase-title')
-                @slot('title', $title)
-            @endcomponent
-            @component('components.blocks._text')
-                @slot('tag', 'div')
-                @slot('font', 'f-secondary')
-                @slot('variation', 'showcase-description')
-                {!! SmartyPants::defaultTransform($description) !!}
-            @endcomponent
-            @component('components.atoms._link')
-                @slot('font', 'f-secondary')
-                @slot('href', $linkUrl)
-                @slot('variation', 'showcase-link')
-                {!! SmartyPants::defaultTransform($linkLabel) !!} <svg class="icon--arrow"><use xlink:href="#icon--arrow"></use></svg>
-            @endcomponent
+            @if ($tag)
+                @component('components.atoms._title')
+                    @slot('tag', 'span')
+                    @slot('font', 'f-tag')
+                    @slot('variation', 'showcase-tag')
+                    @slot('title', $tag)
+                @endcomponent
+            @endif
+            @if ($title)
+                @component('components.atoms._title')
+                    @slot('tag', 'div')
+                    @slot('font', 'f-headline-editorial')
+                    @slot('variation', 'showcase-title')
+                    @slot('title', $title)
+                @endcomponent
+            @endif
+            @if ($description)
+                @component('components.blocks._text')
+                    @slot('tag', 'div')
+                    @slot('font', 'f-secondary')
+                    @slot('variation', 'showcase-description')
+                    {!! SmartyPants::defaultTransform($description) !!}
+                @endcomponent
+            @endif
+            @if ($linkLabel || $linkUrl)
+                @component('components.atoms._link')
+                    @slot('font', 'f-secondary')
+                    @slot('href', $linkUrl)
+                    @slot('variation', 'showcase-link')
+                    {!! SmartyPants::defaultTransform($linkLabel) !!} <svg class="icon--arrow"><use xlink:href="#icon--arrow"></use></svg>
+                @endcomponent
+            @endif
         </div>
     </div>
 </div>
