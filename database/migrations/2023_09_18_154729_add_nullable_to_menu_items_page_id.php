48931a8a0d9aa3a7f98d87d9ce37b04b9021cfd9
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-09-18 15:49:00 -0500

    Add `nullable` to `page_id` field in `menu_items` [WEB-2596]

---
+++ database/migrations/2023_09_18_154729_add_nullable_to_menu_items_page_id.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            $table->integer('page_id')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            //
+        });
+    }
+};
