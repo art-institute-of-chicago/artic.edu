db8d8fdd1f06d3d61437c18a1f6fe745fd010fce
Hannah Baker <hannahb@cogapp.com>
2023-09-22 18:53:25 +0100

    Set up basic content type

---
+++ app/Models/Revisions/CustomToursLandingPageItemRevision.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Revisions;
+
+use A17\Twill\Models\Revision;
+
+class CustomToursLandingPageItemRevision extends Revision
+{
+    protected $table = "custom_tours_landing_page_item_revisions";
+}
