5b07bfbf2dd656e37b4caaf5b538c82bb92b381b
nikhil trivedi <ntrivedi@artic.edu>
2023-09-22 11:57:45 -0500

    Refactor JS admission tooltip logic [WEB-2658]

--- frontend/js/behaviors/core/showAdmissionTooltip.js
+++ frontend/js/behaviors/core/showAdmissionTooltip.js
@@ -1,59 +1,79 @@
 const showAdmissionTooltip = function(container) {
 
-    let tooltipSized = false;
-
-    let targetAttribute = container.getAttribute('data-tooltip-target');
-    let targetTooltip = document.getElementById(targetAttribute);
-
-    container.addEventListener('click', function() {
-        toggleTooltip.call(this);
-    });
-
     function toggleTooltip() {
         hideTooltips();
-        showTooltip(targetTooltip);
-        sizeToolTips();
+        showTooltip();
     }
 
     function hideTooltips() {
         const tooltips = document.querySelectorAll('.admission-info-button-info');
         tooltips.forEach(element => {
-            element.setAttribute('style', 'display: none');
+            element.style.display = 'none';
             element.setAttribute('aria-expanded','false');
             element.setAttribute('aria-hidden','true');
             element.classList.remove('admission-info-visible')
         })
     }
 
-    function showTooltip(targetTooltip) {
+    function showTooltip() {
+        let targetTooltip = document.getElementById(container.dataset.tooltipTarget);
+
         if (targetTooltip) {
             targetTooltip.setAttribute('aria-expanded','true');
             targetTooltip.setAttribute('aria-hidden','false');
-            targetTooltip.setAttribute('style', 'display: block');
+            targetTooltip.style.display = 'block';
             targetTooltip.classList.add('admission-info-visible');
         }
     }
 
     function sizeToolTips() {
-        if (!tooltipSized){
-            tooltipSized = true;
+        const posTooltips = document.querySelectorAll('.admission-info-button-info');
 
-            const posTooltips = document.querySelectorAll('.admission-info-button-info');
-
-            posTooltips.forEach(element => {
+        posTooltips.forEach(element => {
             if (element.clientHeight !== undefined) {
                 const newTopValue = -1 * (element.clientHeight) + 'px';
                 element.setAttribute('style', 'top: ' + newTopValue);
             }
-            });
-        }
+        });
+    }
+
+    function _clicksOutside(event) {
+        hideTooltips();
+    }
+
+    function _handleClicks(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      toggleTooltip();
+      container.blur();
+    }
+
+    function _escape(event) {
+      var isInput = (event.target.tagName === 'INPUT');
+      if (event.keyCode === 27 && !isInput) {
+        hideTooltips();
       }
+    }
 
     function _init() {
         hideTooltips();
-        document.addEventListener('click', hideTooltips, false);
-        window.addEventListener('resize', sizeToolTips, false);
+        sizeToolTips();
+        container.addEventListener('click', _handleClicks, false);
+        document.addEventListener('click', _clicksOutside, false);
+        window.addEventListener('resize', sizeToolTips), false;
+        window.addEventListener('keyup', _escape, false);
     }
+
+    this.destroy = function() {
+      // Remove specific event handlers
+      container.removeEventListener('click', _handleClicks);
+      document.removeEventListener('click', _clicksOutside);
+      window.removeEventListener('resize', sizeToolTips);
+      window.removeEventListener('keyup', _escape);
+
+      // Remove properties of this behavior
+      A17.Helpers.purgeProperties(this);
+    };
 
     this.init = function() {
         _init();
