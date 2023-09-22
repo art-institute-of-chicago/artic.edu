59e5e003f04dfb7db8b946950ffc471414462cd9
Hannah Baker <hannahb@cogapp.com>
2023-09-22 18:53:25 +0100

    Set up basic content type

---
+++ app/Models/CustomToursLandingPageItem.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use A17\Twill\Models\Behaviors\HasBlocks;
+use A17\Twill\Models\Behaviors\HasSlug;
+use A17\Twill\Models\Behaviors\HasMedias;
+use A17\Twill\Models\Behaviors\HasFiles;
+use A17\Twill\Models\Behaviors\HasRevisions;
+use A17\Twill\Models\Model;
+
+class CustomToursLandingPageItem extends Model 
+{
+    use HasBlocks, HasSlug, HasMedias, HasFiles, HasRevisions;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'description',
+    ];
+    
+    public $slugAttributes = [
+        'title',
+    ];
+    
+    public $mediasParams = [
+        'cover' => [
+            'default' => [
+                [
+                    'name' => 'default',
+                    'ratio' => 16 / 9,
+                ],
+            ],
+            'mobile' => [
+                [
+                    'name' => 'mobile',
+                    'ratio' => 1,
+                ],
+            ],
+            'flexible' => [
+                [
+                    'name' => 'free',
+                    'ratio' => 0,
+                ],
+                [
+                    'name' => 'landscape',
+                    'ratio' => 16 / 9,
+                ],
+                [
+                    'name' => 'portrait',
+                    'ratio' => 3 / 5,
+                ],
+            ],
+        ],
+    ];
+}
---
+++ app/Models/Slugs/CustomToursLandingPageItemSlug.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Slugs;
+
+use A17\Twill\Models\Model;
+
+class CustomToursLandingPageItemSlug extends Model
+{
+    protected $table = "custom_tours_landing_page_item_slugs";
+}
