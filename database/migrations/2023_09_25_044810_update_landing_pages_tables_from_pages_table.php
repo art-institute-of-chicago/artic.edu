5a45d7530ee33663e9d858010e706b0b226ce503
Hannah Baker <hannahb@cogapp.com>
2023-09-25 10:59:46 +0100

    Add new tours landing page type and tours header and footer

---
+++ database/migrations/2023_09_25_044810_update_landing_pages_tables_from_pages_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("INSERT INTO landing_page_types (page_type) VALUES ('Tours');");
+    }
+
+    public function down(): void
+    {
+        DB::statement("DELETE FROM landing_page_types WHERE page_type = 'Tours';");
+    }
+};
