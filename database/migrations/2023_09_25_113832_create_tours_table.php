87575976e0b5728cf7feda2c0e625cf609229f11
Hannah Baker <hannahb@cogapp.com>
2023-09-25 17:56:53 +0100

    Add Laravel migration for new tours db

---
+++ database/migrations/2023_09_25_113832_create_tours_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $connection = 'ddev';
+
+    public function up(): void
+    {
+        Schema::connection($this->connection)->create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->json('tour_json');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tours');
+    }
+};
