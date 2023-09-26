9a7e40d4ca48f1857e972398daf776274f3a47ea
Hannah Baker <hannahb@cogapp.com>
2023-09-26 18:17:59 +0100

    WIP Create post route

--- app/Http/Controllers/CustomTourController.php
+++ app/Http/Controllers/CustomTourController.php
@@ -17,22 +17,39 @@ class CustomTourController extends Controller
 
         //handle validation / sanitization / errors
 
-        $tour = new CustomTour;
-        $tour->id =  $request->input('id');;
-        $tour->tour_json = [
-            'title' => 'Custom Tour (Source)',
-            'description' => 'Custom tour description',
+        $tour = new CustomTour();
+        $tour->id =  '1238';
+        ;
+        $tour->tour_json = json_encode([
+            'title' => 'Another custom Tour (Source)',
+            'description' => 'Another custom tour description',
             'artworks' => ['artwork_url_1', 'artwork_url_2'],
-        ];
+        ]);
         $tour->save();
-
-//        return response()->json(['message' => 'Tour created successfully!', 'tour' => $tour], 201);
+//
+//      return response()->json(['message' => 'Tour created successfully!', 'tour' => $tour], 201);
         return redirect('/example')->with('success', 'Item created successfully');
     }
 
     public function show()
     {
-        $models = CustomTour::all();
-        return view('site.example', ['models' => $models]);
+//        Add tours to db within show method
+//        Todo: move to store method
+//        $tour = new CustomTour();
+//        $tour->id =  '1237';
+//
+//        $tour->tour_json = json_encode([
+//            'title' => 'Another custom Tour (Source)',
+//            'description' => 'Another custom tour description',
+//            'artworks' => ['artwork_url_1', 'artwork_url_2'],
+//        ]);
+//        $tour->save();
+
+        $tours = CustomTour::all();
+        return view('site.example', ['tours' => $tours]);
+
+
+
+
     }
 }
