22997af57a23c0726a1d5d5e79353d78a0d65d7f
Hannah Baker <hannahb@cogapp.com>
2023-09-26 11:32:54 +0100

    Add basic tour model

---
+++ app/Models/CustomTour.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomTour extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'tours_db';
+
+    protected $table = 'tours';
+    protected $fillable = ['id', 'tour_json', 'timestamp'];
+}
