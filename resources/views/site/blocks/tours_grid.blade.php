a35bd3711a6bc089cfd17369e0a65fb2bde8ff5c
Hannah Baker <hannahb@cogapp.com>
2023-09-27 11:53:02 +0100

    Tidy up tours view

--- resources/views/site/blocks/tours_grid.blade.php
+++ resources/views/site/blocks/tours_grid.blade.php
@@ -1,21 +1,14 @@
-<div class="featured-pages-grid" data-blur-img>
-    @foreach($block->getRelated('customToursLandingPageItems') as $index => $page)
-        @if($index === count($block->getRelated('customToursLandingPageItems')) || $index !== 0 && $index % 4 === 0)
-            </div>
-        @endif
-        @if($loop->first || $index % 4 === 0)
-             <div class="featured-pages-grid_row">
-        @endif
-            <div class="featured-pages-grid_card">
-                <div class="featured-pages-grid_details">
-                    <span>
-                        <h3 class="f-list-2">
-                            {{ $page->title }}
-                            <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
-                            {{ $page->tour_id }}
-                        </h3>
-                    </span>
-                </div>
-            </div>
-    @endforeach
-</div>
+@foreach($block->getRelated('customToursLandingPageItems') as $index => $page)
+    <div class="featured-pages-grid_card">
+        <div class="featured-pages-grid_details">
+            <span>
+                <h3 class="f-list-2">
+                    {{ $page->title }}
+                    <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
+                    {{ $page->tour_id }}
+                </h3>
+            </span>
+        </div>
+    </div>
+@endforeach
+
