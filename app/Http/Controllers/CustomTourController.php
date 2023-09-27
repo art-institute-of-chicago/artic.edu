a7a1887fb765483e384948689e91a5e301e8cc14
Hannah Baker <hannahb@cogapp.com>
2023-09-27 10:50:17 +0100

    Get api route to work

--- app/Http/Controllers/CustomTourController.php
+++ app/Http/Controllers/CustomTourController.php
@@ -9,26 +9,27 @@ class CustomTourController extends Controller
 {
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'tour_json.title' => 'required|string',
-            'tour_json.description' => 'required|string',
-            'tour_json.artworks' => 'required|array'
-        ]);
+//        $data = $request->validate([
+//            'tour_json.title' => 'required|string',
+//            'tour_json.description' => 'required|string',
+//            'tour_json.artworks' => 'required|array'
+//        ]);
 
         //handle validation / sanitization / errors
 
         $tour = new CustomTour();
-        $tour->id =  '1238';
+        $tour->id =  '1239';
         ;
         $tour->tour_json = json_encode([
             'title' => 'Another custom Tour (Source)',
             'description' => 'Another custom tour description',
             'artworks' => ['artwork_url_1', 'artwork_url_2'],
         ]);
+//        $tour->tour_json = $data['tour_json'];
         $tour->save();
 //
-//      return response()->json(['message' => 'Tour created successfully!', 'tour' => $tour], 201);
-        return redirect('/example')->with('success', 'Item created successfully');
+      return response()->json(['message' => 'Tour created successfully!', 'tour' => $tour], 201);
+//        return redirect('/example')->with('success', 'Item created successfully');
     }
 
     public function show()
