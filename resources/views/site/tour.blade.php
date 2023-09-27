bac85cf4fe2cb17c05d3b9b232495a5ded909ceb
Hannah Baker <hannahb@cogapp.com>
2023-09-27 15:35:12 +0100

    Create new build method and separate view

--- resources/views/site/tour.blade.php
+++ resources/views/site/tour.blade.php
@@ -1,10 +1,4 @@
 <h1>Custom tour view</h1>
 
-<form method="POST" action="{{ url('/api/tour') }}">
-    @csrf
-    <input type="text" name="id" placeholder="Tour ID">
-    <input type="submit" value="Submit">
-</form>
-
 <p>ID: {{ $tour->id }}</p>
 
