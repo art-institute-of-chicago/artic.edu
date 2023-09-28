5213e21925825adfcb52b4d69709b1c79cc4278d
Hannah Baker <hannahb@cogapp.com>
2023-09-28 11:59:30 +0100

    Remove tour builder

--- resources/views/site/tour-builder.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Custom tour builder</h1>
-
-<form method="POST" action="{{ url('/api/tour') }}">
-    @csrf
-{{--    <textarea name="tour_json" row="8"></textarea>--}}
-    <input type="submit" value="Submit">
-</form>
