2e7f030f6e97dd986bd6ae0f883b6598d5037dc3
Hannah Baker <hannahb@cogapp.com>
2023-09-28 11:56:45 +0100

    Access tour_json on blade template

--- resources/views/site/tour.blade.php
+++ resources/views/site/tour.blade.php
@@ -1,4 +1,10 @@
-<h1>Custom tour view</h1>
+<h1>Custom tour view - {{ $tour_json['title'] }} </h1>
 
 <p>ID: {{ $tour->id }}</p>
+
+<p>{{ $tour_json['description'] }}</p>
+
+@foreach ($tour_json['artworks'] as $artwork)
+    <a href="{{ $artwork['url'] }}">{{ $artwork['title'] }}</a>
+@endforeach
 
