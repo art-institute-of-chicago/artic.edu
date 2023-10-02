f0f5a7865b14bf1768cddd34776e3862a52c84bf
Hannah Baker <hannahb@cogapp.com>
2023-10-02 16:00:46 +0100

    Turn image into link

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -11,26 +11,28 @@
                 @php
                     $image = $custom_tour->imageAsArray('teaser_image');
                 @endphp
-
-                @if ($image)
-                    @component('components.atoms._img')
-                        @slot('class', 'featured-pages-grid_img')
-                        @slot('image', $image)
-                        @slot('settings', array(
-                            'fit' => 'crop',
-                            'ratio' => '16:9',
-                            'srcset' => array(300,600,800,1200,1600),
-                            'sizes' => ImageHelpers::aic_imageSizes(array(
-                              'xsmall' => 58,
-                              'small' => 58,
-                              'medium' => 58,
-                              'large' => 38,
-                              'xlarge' => 38,
-                            ))))
-                    @endcomponent
-                @else
-                    <span class="default-img featured-pages-grid_img"></span>
-                @endif
+{{--            Todo: Update the href if the tours url changes    --}}
+                <a href="/tours/{{ $custom_tour->tour_id }}" class="m-listing__link">
+                    @if ($image)
+                        @component('components.atoms._img')
+                            @slot('class', 'featured-pages-grid_img')
+                            @slot('image', $image)
+                            @slot('settings', array(
+                                'fit' => 'crop',
+                                'ratio' => '16:9',
+                                'srcset' => array(300,600,800,1200,1600),
+                                'sizes' => ImageHelpers::aic_imageSizes(array(
+                                  'xsmall' => 58,
+                                  'small' => 58,
+                                  'medium' => 58,
+                                  'large' => 38,
+                                  'xlarge' => 38,
+                                ))))
+                        @endcomponent
+                    @else
+                        <span class="default-img featured-pages-grid_img"></span>
+                    @endif
+                </a>
 
             </span>
         </div>
