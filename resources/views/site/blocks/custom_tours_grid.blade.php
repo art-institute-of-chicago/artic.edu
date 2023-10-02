b9468a623fd110bca10ed7a2b0dfd3e812a6d85f
Hannah Baker <hannahb@cogapp.com>
2023-10-02 15:44:00 +0100

    Utilise the img component and add a placeholder if not image

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -1,22 +1,57 @@
-@foreach($block->getRelated('customToursItems') as $index => $page)
+@foreach($block->getRelated('customToursItems') as $index => $custom_tour)
     <div class="featured-pages-grid_card">
         <div class="featured-pages-grid_details">
             <span>
                 <h3 class="f-list-2">
-                    {{ $page->title }}
+                    {{ $custom_tour->title }}
                     <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
-                    {{ $page->tour_id }}
+                    {{ $custom_tour->tour_id }}
                 </h3>
-                <p>{{ $page->teaser_text }}</p>
+                <p>{{ $custom_tour->teaser_text }}</p>
                 @php
-                    $image = $page->imageAsArray('teaser_image')
+                    $image = $custom_tour->imageAsArray('teaser_image')
                 @endphp
 
-                <img src="{{ $image['src'] }}" alt="{{ $image['alt']  }}"/>
+                @if ($image)
+                    @component('components.atoms._img')
+                        @slot('class', 'featured-pages-grid_img')
+                        @slot('image', $image)
+                        @slot('settings', array(
+                            'fit' => 'crop',
+                            'ratio' => '16:9',
+                            'srcset' => array(300,600,800,1200,1600),
+                            'sizes' => ImageHelpers::aic_imageSizes(array(
+                              'xsmall' => 58,
+                              'small' => 58,
+                              'medium' => 58,
+                              'large' => 38,
+                              'xlarge' => 38,
+                            ))))
+                    @endcomponent
+                @else
+                    <span class="default-img featured-pages-grid_img"></span>
+                @endif
 
-{{--                @component('components.atoms._img')--}}
-{{--                    @slot('img', $page->imageAsArray('teaser_image'))--}}
-{{--                @endcomponent--}}
+{{--                @if ($custom_tour->imageFront('teaser_image'))--}}
+{{--                    @component('components.atoms._img')--}}
+{{--                        @slot('class', 'featured-pages-grid_img')--}}
+{{--                        @slot('image', $image)--}}
+{{--                        @slot('settings', array(--}}
+{{--                            'fit' => 'crop',--}}
+{{--                            'ratio' => '16:9',--}}
+{{--                            'srcset' => array(300,600,800,1200,1600),--}}
+{{--                            'sizes' => ImageHelpers::aic_imageSizes(array(--}}
+{{--                              'xsmall' => 58,--}}
+{{--                              'small' => 58,--}}
+{{--                              'medium' => 58,--}}
+{{--                              'large' => 38,--}}
+{{--                              'xlarge' => 38,--}}
+{{--                            ))))--}}
+{{--                    @endcomponent--}}
+{{--                @else--}}
+{{--                    <span class="default-img featured-pages-grid_img"></span>--}}
+{{--                @endif--}}
+
             </span>
         </div>
     </div>
