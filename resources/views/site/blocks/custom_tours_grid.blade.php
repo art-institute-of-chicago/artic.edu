694b9f73d4e7288a3b1b3a17a0d3858e002accd7
Hannah Baker <hannahb@cogapp.com>
2023-10-02 15:52:05 +0100

    Remove commented out section of code

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -9,7 +9,7 @@
                 </h3>
                 <p>{{ $custom_tour->teaser_text }}</p>
                 @php
-                    $image = $custom_tour->imageAsArray('teaser_image')
+                    $image = $custom_tour->imageAsArray('teaser_image');
                 @endphp
 
                 @if ($image)
@@ -31,26 +31,6 @@
                 @else
                     <span class="default-img featured-pages-grid_img"></span>
                 @endif
-
-{{--                @if ($custom_tour->imageFront('teaser_image'))--}}
-{{--                    @component('components.atoms._img')--}}
-{{--                        @slot('class', 'featured-pages-grid_img')--}}
-{{--                        @slot('image', $image)--}}
-{{--                        @slot('settings', array(--}}
-{{--                            'fit' => 'crop',--}}
-{{--                            'ratio' => '16:9',--}}
-{{--                            'srcset' => array(300,600,800,1200,1600),--}}
-{{--                            'sizes' => ImageHelpers::aic_imageSizes(array(--}}
-{{--                              'xsmall' => 58,--}}
-{{--                              'small' => 58,--}}
-{{--                              'medium' => 58,--}}
-{{--                              'large' => 38,--}}
-{{--                              'xlarge' => 38,--}}
-{{--                            ))))--}}
-{{--                    @endcomponent--}}
-{{--                @else--}}
-{{--                    <span class="default-img featured-pages-grid_img"></span>--}}
-{{--                @endif--}}
 
             </span>
         </div>
