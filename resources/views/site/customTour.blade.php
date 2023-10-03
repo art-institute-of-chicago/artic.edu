89c64af8926e784b1a5dbf6372692dac05f2b2aa
Hannah Baker <hannahb@cogapp.com>
2023-10-03 12:11:02 +0100

    Add validation rules to json

--- resources/views/site/customTour.blade.php
+++ resources/views/site/customTour.blade.php
@@ -7,7 +7,19 @@
     <div class="o-blocks">
         <p>ID: {{ $id }}</p>
 
-        <p>{{ $custom_tour['description'] }}</p>
+    <p>ID: {{ $id }}</p>
+
+    <p>{{ $custom_tour['description'] }}</p>
+
+    @foreach ($custom_tour['artworks'] as $artwork)
+        <div>
+            <h2>{{ $artwork['title'] }}</h2>
+
+            @if (array_key_exists('objectNote', $artwork))
+                <p>{{ $artwork['objectNote'] }}</p>
+            @endif
+        </div>
+    @endforeach
 
         <ul>
             @foreach ($custom_tour['artworks'] as $artwork)
