f8165b116345d20f2caffb3c596ea28d9e086a07
Hannah Baker <hannahb@cogapp.com>
2023-10-05 18:26:40 +0100

    Fix bug by changing  variable name to

--- app/Http/Requests/API/CustomTourRequest.php
+++ app/Http/Requests/API/CustomTourRequest.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace App\Http\Requests\Api;
+namespace App\Http\Requests\API;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class CustomTourRequest extends FormRequest
 {
+
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'title' => 'required|string',
