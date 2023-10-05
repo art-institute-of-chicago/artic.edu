bc5b5b6bb674059c9d177e2f59173c8210ae622e
Hannah Baker <hannahb@cogapp.com>
2023-10-05 11:51:28 +0100

    Re-purpose featured pages grid to improve layout of custom tours grid

--- resources/views/admin/blocks/custom_tours_grid.blade.php
+++ resources/views/admin/blocks/custom_tours_grid.blade.php
@@ -1,6 +1,11 @@
 @twillBlockTitle('Custom Tours Grid')
 @twillBlockIcon('image')
 
+@formField('input', [
+'name' => 'custom_tours_grid_heading',
+'label' => 'Heading',
+])
+
 @formField('browser', [
     'routePrefix' => 'visit',
     'moduleName' => 'customToursItems',
