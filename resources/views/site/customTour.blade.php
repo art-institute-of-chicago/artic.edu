f941b32b87aa92f14d09561df6f6a0ce62328806
Hannah Baker <hannahb@cogapp.com>
2023-10-05 12:02:20 +0100

    Improve layout/styling of single custom tour view

--- resources/views/site/customTour.blade.php
+++ resources/views/site/customTour.blade.php
@@ -2,16 +2,21 @@
 
 @section('content')
 
-    <h1>Custom tour view - {{ $custom_tour['title'] }} </h1>
+    <h2 class="title f-module-title-2">{{ $custom_tour['title'] }} </h2>
+    <hr/>
+    <div class="o-blocks">
+        <p>ID: {{ $id }}</p>
 
-    <p>ID: {{ $id }}</p>
+        <p>{{ $custom_tour['description'] }}</p>
 
-    <p>{{ $custom_tour['description'] }}</p>
-
-    @foreach ($custom_tour['artworks'] as $artwork)
-        <a href="{{ $artwork['url'] }}">{{ $artwork['title'] }}</a>
-    @endforeach
-
+        <ul>
+            @foreach ($custom_tour['artworks'] as $artwork)
+                <li>
+                    <a href="{{ $artwork['url'] }}">{{ $artwork['title'] }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
 
 
