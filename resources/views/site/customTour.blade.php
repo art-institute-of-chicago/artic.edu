86c4170431e0021142d94b64285d7c26d6941a8c
Hannah Baker <hannahb@cogapp.com>
2023-10-05 17:51:34 +0100

    Fix conflict

--- resources/views/site/customTour.blade.php
+++ resources/views/site/customTour.blade.php
@@ -7,20 +7,8 @@
     <div class="o-blocks">
         <p>ID: {{ $id }}</p>
 
-    <p>ID: {{ $id }}</p>
-
-    <p>{{ $custom_tour['description'] }}</p>
-
-    @foreach ($custom_tour['artworks'] as $artwork)
-        <div>
-            <a href="{{ $artwork['url'] }}">{{ $artwork['title'] }}</a>
-
-            @if (array_key_exists('objectNote', $artwork))
-                <p>{{ $artwork['objectNote'] }}</p>
-            @endif
-        </div>
-    @endforeach
-
+        <p>{{ $custom_tour['description'] }}</p>
+        
         <ul>
             @foreach ($custom_tour['artworks'] as $artwork)
                 <li>
