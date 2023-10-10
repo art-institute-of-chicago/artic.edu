14a7857cb6f9054c77c3aab24657f1bbe393e6d9
jonw-cogapp <jonw@cogapp.com>
2023-10-10 19:08:37 +0100

    Catch the header sometimes hiding on reload

--- frontend/js/behaviors/customTourBuilder/customTourBuilder.js
+++ frontend/js/behaviors/customTourBuilder/customTourBuilder.js
@@ -21,7 +21,15 @@ export default function customTourBuilder(container) {
       <CustomTourBuilder
         initMessage="Custom Tour Builder will render here!"
       />,
-      container
+      container,
+      () => {
+        // This is informed by where I think the header scroll hiding is happening
+        // See: frontend/js/functions/core/setScrollDirection.js
+        // Catch if the scroll is below the threshold and remove the class
+        if (window.scrollY < 100) {
+          document.documentElement.classList.remove('s-header-hide');
+        }
+      }
     );
   }
   this.destroy = function () {
