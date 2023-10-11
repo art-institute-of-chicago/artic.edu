f2bed1e4b15770b888d3d3795ee293d0d26ff949
Hannah Baker <hannahb@cogapp.com>
2023-10-11 18:19:48 +0100

    Remove commented out code, fix linting errors

--- database/seeders/CustomTourSeeder.php
+++ database/seeders/CustomTourSeeder.php
@@ -47,39 +47,5 @@ class CustomTourSeeder extends Seeder
         $customTour = new CustomTour();
         $customTour->tour_json = json_encode($tourJson);
         $customTour->save();
-
-//        $customTour = CustomTour::create([
-//            'id' => 1,
-//            'tour_json' => [
-//                "title" => "My custom tour title",
-//                "description" => "My custom tour description",
-//                "artworks" => [
-//                    [
-//                        "id" => 656,
-//                        "image_id" => "6b1edb9c-0f3f-0ee3-47c7-ca25c39ee360",
-//                        "title" => "Lion (One of a Pair, South Pedestal)",
-//                        "description" => "Object description",
-//                        "artist" => "Artist name",
-//                        "date" => "1888",
-//                        "gallery_title" => "Michigan Avenue entrance/steps",
-//                        "latitude" => null,
-//                        "longitude" => null,
-//                        "objectNote" => "note about the lion",
-//                    ],
-//                    [
-//                        "id" => 656,
-//                        "image_id" => "6b1edb9c-0f3f-0ee3-47c7-ca25c39ee360",
-//                        "title" => "Lion (One of a Pair, South Pedestal)",
-//                        "description" => "Object description",
-//                        "artist" => "Artist name",
-//                        "date" => "1888",
-//                        "gallery_title" => "Michigan Avenue entrance/steps",
-//                        "latitude" => null,
-//                        "longitude" => null,
-//                        "objectNote" => "note about the lion",
-//                    ],
-//                ],
-//            ],
-//        ]);
     }
 }
