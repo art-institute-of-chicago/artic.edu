6223e8ee0a65102b4c003a69697b0738e7b4a4ba
nikhil trivedi <ntrivedi@artic.edu>
2023-10-12 13:34:35 -0500

    Lint code [WEB-2629]

--- app/Models/Slugs/LandingPageCategorySlug.php
+++ app/Models/Slugs/LandingPageCategorySlug.php
@@ -4,7 +4,7 @@ namespace App\Models\Slugs;
 
 use A17\Twill\Models\Model;
 
-class LandingPageCategory extends Model
+class LandingPageCategorySlug extends Model
 {
     protected $table = 'landing_page_category_slugs';
 }
