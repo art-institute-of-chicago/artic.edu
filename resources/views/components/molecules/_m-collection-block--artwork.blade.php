efd1672ce706e984765e6bc3d877cb50deedd57c
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-11-01 10:39:51 -0500

    Remove unnecessary variables from `Collection Block` artwork component [WEB-2632]

--- resources/views/components/molecules/_m-collection-block--artwork.blade.php
+++ resources/views/components/molecules/_m-collection-block--artwork.blade.php
@@ -1,4 +1,4 @@
-<{{ $tag ?? 'li' }} class="m-listing{{ (isset($variation) and $variation === 'o-pinboard__item') ? ' m-listing--variable-height' : '' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{!! (isset($variation) and strrpos($variation, "--hero") > -1 and !$item->videoFront) ? ' data-behavior="blurMyBackground"' : '' !!} itemscope itemtype="http://schema.org/CreativeWork">
+<li class="m-listing" itemscope itemtype="http://schema.org/CreativeWork">
     <a href="{!! route('artworks.show', ['id' => $item->id, 'slug' => $item->titleSlug ]) !!}" class="m-listing__link" itemprop="url">
         <span class="m-collection__img m-collection__img--no-bg">
             @if (isset($image) || $item->imageFront())
@@ -15,4 +15,4 @@
             @endif
         </span>
     </a>
-</{{ $tag ?? 'li' }}>
+</li>
