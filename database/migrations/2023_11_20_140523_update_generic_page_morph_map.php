d6c23b959afd5594434acb643605f45949945f1b
nikhil trivedi <ntrivedi@artic.edu>
2023-11-20 14:30:03 -0600

    Update database table references to GenericPage to morphmap alias [WEB-2679]

---
+++ database/migrations/2023_11_20_140523_update_generic_page_morph_map.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("UPDATE blocks SET blockable_type='genericPages' where blockable_type='App\Models\GenericPage'");
+        DB::statement("UPDATE related SET subject_type='genericPages' where subject_type='App\Models\GenericPage'");
+        DB::statement("UPDATE related SET related_type='genericPages' where related_type='App\Models\GenericPage'");
+        DB::statement("UPDATE api_relatables SET api_relatable_type='genericPages' where api_relatable_type='App\Models\GenericPage'");
+        DB::statement("UPDATE mediables SET mediable_type='genericPages' where mediable_type='App\Models\GenericPage'");
+    }
+
+    public function down(): void
+    {
+        DB::statement("UPDATE blocks SET blockable_type='App\Models\GenericPage' where blockable_type='genericPages'");
+        DB::statement("UPDATE related SET subject_type='App\Models\GenericPage' where subject_type='genericPages'");
+        DB::statement("UPDATE related SET related_type='App\Models\GenericPage' where related_type='genericPages'");
+        DB::statement("UPDATE api_relatables SET api_relatable_type='App\Models\GenericPage' where api_relatable_type='genericPages'");
+        DB::statement("UPDATE mediables SET mediable_type='App\Models\GenericPage' where mediable_type='genericPages'");
+    }
+};
