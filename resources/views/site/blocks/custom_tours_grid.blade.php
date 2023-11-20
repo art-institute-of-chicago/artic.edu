1b1e78580bd8499e9d199a18097140211ac8e5f3
Hannah Baker <hannahb@cogapp.com>
2023-11-20 18:08:48 +0000

    Add new text field and style

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -1,7 +1,8 @@
-<h2 id="{{ StringHelpers::getUtf8Slug($block->input('custom_tours_grid_heading')) }}" class="title f-headline">{!! $block->input('custom_tours_grid_heading') !!}</h2>
-{{--Todo: Set up field and remove hard coded text --}}
-<p>Browse a list of existing self-guided tours you can take at the museum, or select one that you can recommend and share with others.</p>
-<div class="aic-ct-grid" data-blur-img>
+<h2 id="{{ StringHelpers::getUtf8Slug($block->input('custom_tours_grid_heading')) }}" class="title f-headline">
+    {!! $block->input('custom_tours_grid_heading') !!}
+</h2>
+<div class="aic-ct-grid-intro">{!! $block->input('custom_tours_grid_text') !!}</div>
+<div class="aic-ct-grid-container" data-blur-img>
     @foreach($block->getRelated('custom_tours_items') as $index => $custom_tour)
         <div class="aic-ct-grid-card">
             <div class="aic-ct-grid-card__top">
