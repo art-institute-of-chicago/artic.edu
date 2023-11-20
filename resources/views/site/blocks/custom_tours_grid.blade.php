311ff53d10de61fc3bee9a7897f8f070f704055c
Hannah Baker <hannahb@cogapp.com>
2023-11-20 15:53:27 +0000

    Switch to using grid instead of flex

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -2,42 +2,40 @@
 {{--Todo: Set up field and remove hard coded text --}}
 <p>Browse a list of existing self-guided tours you can take at the museum, or select one that you can recommend and share with others.</p>
 <div class="aic-ct-grid" data-blur-img>
-    <div class="aic-ct-grid__row">
-        @foreach($block->getRelated('custom_tours_items') as $index => $custom_tour)
-            <div class="aic-ct-grid-card">
-                <div class="aic-ct-grid-card__details">
-                        @if($custom_tour->imageAsArray('teaser_image'))
-                            @component('components.atoms._img')
-                                @slot('class', 'aic-ct-grid-card__img')
-                                @slot('image', $custom_tour->imageAsArray('teaser_image'))
-                                @slot('settings', array(
-                                    'fit' => 'crop',
-                                    'ratio' => '16:9',
-                                    'srcset' => array(300,600,800,1200,1600),
-                                    'sizes' => ImageHelpers::aic_imageSizes(array(
-                                      'xsmall' => 58,
-                                      'small' => 58,
-                                      'medium' => 58,
-                                      'large' => 38,
-                                      'xlarge' => 38,
-                                    ))))
-                            @endcomponent
-                        @else
-                            {{-- Todo: Is "default-img" needed here? e.g. padding: 0; --}}
-                            <span class="default-img aic-ct-grid-card__img"></span>
-                        @endif
-                        <span>
-                            <h3 class="f-list-2">{!! $custom_tour->title !!}
-                                <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
-                            </h3>
-                        </span>
-                        <p>{!! $custom_tour->teaser_text !!}</p>
-                        <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="btn btn--secondary f-buttons">
-                            View tour
-                        </a>
-                </div>
+    @foreach($block->getRelated('custom_tours_items') as $index => $custom_tour)
+        <div class="aic-ct-grid-card">
+            <div class="aic-ct-grid-card__details">
+                    @if($custom_tour->imageAsArray('teaser_image'))
+                        @component('components.atoms._img')
+                            @slot('class', 'aic-ct-grid-card__img')
+                            @slot('image', $custom_tour->imageAsArray('teaser_image'))
+                            @slot('settings', array(
+                                'fit' => 'crop',
+                                'ratio' => '16:9',
+                                'srcset' => array(300,600,800,1200,1600),
+                                'sizes' => ImageHelpers::aic_imageSizes(array(
+                                  'xsmall' => 58,
+                                  'small' => 58,
+                                  'medium' => 58,
+                                  'large' => 38,
+                                  'xlarge' => 38,
+                                ))))
+                        @endcomponent
+                    @else
+                        {{-- Todo: Is "default-img" needed here? e.g. padding: 0; --}}
+                        <span class="default-img aic-ct-grid-card__img"></span>
+                    @endif
+                    <span>
+                        <h3 class="f-list-2">{!! $custom_tour->title !!}
+                            <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
+                        </h3>
+                    </span>
+                    <p>{!! $custom_tour->teaser_text !!}</p>
+                    <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="btn btn--secondary f-buttons">
+                        View tour
+                    </a>
             </div>
-        @endforeach
-    </div>
+        </div>
+    @endforeach
 </div>
 
