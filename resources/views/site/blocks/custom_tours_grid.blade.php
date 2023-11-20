c8ca978024b2b2ce52bad1bbeefcd378636530fa
Hannah Baker <hannahb@cogapp.com>
2023-11-20 16:37:10 +0000

    Style button and text on card

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -4,36 +4,37 @@
 <div class="aic-ct-grid" data-blur-img>
     @foreach($block->getRelated('custom_tours_items') as $index => $custom_tour)
         <div class="aic-ct-grid-card">
+            @if($custom_tour->imageAsArray('teaser_image'))
+                @component('components.atoms._img')
+                    @slot('class', 'aic-ct-grid-card__img')
+                    @slot('image', $custom_tour->imageAsArray('teaser_image'))
+                    @slot('settings', array(
+                        'fit' => 'crop',
+                        'ratio' => '16:9',
+                        'srcset' => array(300,600,800,1200,1600),
+                        'sizes' => ImageHelpers::aic_imageSizes(array(
+                          'xsmall' => 58,
+                          'small' => 58,
+                          'medium' => 58,
+                          'large' => 38,
+                          'xlarge' => 38,
+                        ))))
+                @endcomponent
+            @else
+                {{-- Todo: Is "default-img" needed here? e.g. padding: 0; --}}
+                <span class="default-img aic-ct-grid-card__img"></span>
+            @endif
+
+            <h3 class="f-list-2">{!! $custom_tour->title !!}
+                <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
+            </h3>
             <div class="aic-ct-grid-card__details">
-                    @if($custom_tour->imageAsArray('teaser_image'))
-                        @component('components.atoms._img')
-                            @slot('class', 'aic-ct-grid-card__img')
-                            @slot('image', $custom_tour->imageAsArray('teaser_image'))
-                            @slot('settings', array(
-                                'fit' => 'crop',
-                                'ratio' => '16:9',
-                                'srcset' => array(300,600,800,1200,1600),
-                                'sizes' => ImageHelpers::aic_imageSizes(array(
-                                  'xsmall' => 58,
-                                  'small' => 58,
-                                  'medium' => 58,
-                                  'large' => 38,
-                                  'xlarge' => 38,
-                                ))))
-                        @endcomponent
-                    @else
-                        {{-- Todo: Is "default-img" needed here? e.g. padding: 0; --}}
-                        <span class="default-img aic-ct-grid-card__img"></span>
-                    @endif
-                    <span>
-                        <h3 class="f-list-2">{!! $custom_tour->title !!}
-                            <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
-                        </h3>
-                    </span>
-                    <p>{!! $custom_tour->teaser_text !!}</p>
-                    <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="btn btn--secondary f-buttons">
-                        View tour
-                    </a>
+                <div class="aic-ct-grid-card__text">
+                    {!! $custom_tour->teaser_text !!}
+                </div>
+                <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="btn btn--secondary f-buttons">
+                    View tour
+                </a>
             </div>
         </div>
     @endforeach
