ea2851f5057b54d329169f9b3ed56a8e8b3c22dc
Hannah Baker <hannahb@cogapp.com>
2023-11-20 12:42:10 +0000

    Hard code text, add button

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -1,11 +1,12 @@
-<h2 id="{{ StringHelpers::getUtf8Slug($block->input('custom_tours_grid_heading')) }}" class="title f-module-title-2">{!! $block->input('custom_tours_grid_heading') !!}</h2>
-<hr/>
+<h2 id="{{ StringHelpers::getUtf8Slug($block->input('custom_tours_grid_heading')) }}" class="title f-headline">{!! $block->input('custom_tours_grid_heading') !!}</h2>
+{{--Todo: Set up field and remove hard coded text --}}
+<p>Browse a list of existing self-guided tours you can take at the museum, or select one that you can recommend and share with others.</p>
 <div class="featured-pages-grid" data-blur-img>
     <div class="featured-pages-grid_row">
         @foreach($block->getRelated('custom_tours_items') as $index => $custom_tour)
             <div class="featured-pages-grid_card">
                 <div class="featured-pages-grid_details">
-                    <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="m-listing__link">
+
                         @if($custom_tour->imageAsArray('teaser_image'))
                             @component('components.atoms._img')
                                 @slot('class', 'featured-pages-grid_img')
@@ -30,7 +31,9 @@
                                 <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
                             </h3>
                         </span>
-                    </a>
+                        <a href="{{ route('custom-tours.show', $custom_tour->tour_id) }}" class="btn btn--secondary f-buttons">
+                            View tour
+                        </a>
                 </div>
             </div>
         @endforeach
