e98e4bf9105bde4580a7d9fb5e50b68525c3efa3
nikhil trivedi <ntrivedi@artic.edu>
2023-11-21 13:02:30 -0600

    Remove endpoint array from single-type form browser [WEB-2676]

--- resources/views/admin/blocks/feature_2x.blade.php
+++ resources/views/admin/blocks/feature_2x.blade.php
@@ -5,12 +5,6 @@
     'routePrefix' => 'collection.interactive_features',
     'moduleName' => 'experiences',
     'name' => 'experiences',
-    'endpoints' => [
-        [
-            'label' => 'Interactive feature',
-            'value' => moduleRoute('experiences', 'collection.interactive_features', 'browser', [], false)
-        ]
-    ],
     'max' => 2,
     'label' => 'Featured items',
 ])
