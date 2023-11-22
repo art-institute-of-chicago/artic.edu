17a005a24bdc4746f17c43ed5cf9038d2dc0433d
Hannah Baker <hannahb@cogapp.com>
2023-11-22 12:32:03 +0000

    Add conditionals to cta module

--- resources/views/components/molecules/_m-cta-banner.blade.php
+++ resources/views/components/molecules/_m-cta-banner.blade.php
@@ -10,43 +10,50 @@
     // TODO: Make this an option?
     $isBigLink = isset($image);
 @endphp
-<{{ $tag }} class="m-cta-banner{{ isset($image) ? ' m-cta-banner--with-image' : '' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{!! isset($image) ? ' data-behavior="bannerParallax"' : '' !!}>
-    @if ($isBigLink)
-        <a href="{{ $href }}" class="m-cta-banner__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
-    @endif
-        @if (isset($image))
-            <div class="m-cta-banner__img" data-parallax-img>
-                @component('components.atoms._img')
-                    @slot('image', $image)
-                    @slot('settings', array(
-                        'fit' => 'crop',
-                        'ratio' => '25:4',
-                        'srcset' => array(300,600,1000,1500,2000),
-                        'sizes' => ImageHelpers::aic_imageSizes(array(
-                              'xsmall' => '58',
-                              'small' => '58',
-                              'medium' => '58',
-                              'large' => '58',
-                              'xlarge' => '58',
-                        )),
-                    ))
-                @endcomponent
-            </div>
+
+@if ($header)
+    <{{ $tag }} class="m-cta-banner{{ isset($image) ? ' m-cta-banner--with-image' : '' }}{{ (isset($variation)) ? ' '.$variation : '' }}"{!! isset($image) ? ' data-behavior="bannerParallax"' : '' !!}>
+        @if ($isBigLink)
+            <a href="{{ $href }}" class="m-cta-banner__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
         @endif
-        <div class="m-cta-banner__txt">
-            <div class="m-cta-banner__title f-module-title-2">{!! SmartyPants::defaultTransform($header) !!}</div>
-            <div class="m-cta-banner__msg f-list-2">{!! SmartyPants::defaultTransform($body) !!}</div>
-            <div class="m-cta-banner__action">
-                @if (!$isBigLink)
-                    <a href="{{ $href }}">
+            @if (isset($image))
+                <div class="m-cta-banner__img" data-parallax-img>
+                    @component('components.atoms._img')
+                        @slot('image', $image)
+                        @slot('settings', array(
+                            'fit' => 'crop',
+                            'ratio' => '25:4',
+                            'srcset' => array(300,600,1000,1500,2000),
+                            'sizes' => ImageHelpers::aic_imageSizes(array(
+                                  'xsmall' => '58',
+                                  'small' => '58',
+                                  'medium' => '58',
+                                  'large' => '58',
+                                  'xlarge' => '58',
+                            )),
+                        ))
+                    @endcomponent
+                </div>
+            @endif
+            <div class="m-cta-banner__txt">
+                <div class="m-cta-banner__title f-module-title-2">{!! SmartyPants::defaultTransform($header) !!}</div>
+                @if ($body)
+                    <div class="m-cta-banner__msg f-list-2">{!! SmartyPants::defaultTransform($body) !!}</div>
                 @endif
-                <span class="btn f-buttons{{ isset($image) ? ' btn--contrast' : '' }}">{!! SmartyPants::defaultTransform($button_text) !!}</span>
-                @if (!$isBigLink)
-                    </a>
+                @if ($button_text)
+                    <div class="m-cta-banner__action">
+                        @if (!$isBigLink)
+                            <a href="{{ $href }}">
+                        @endif
+                        <span class="btn f-buttons{{ isset($image) ? ' btn--contrast' : '' }}">{!! SmartyPants::defaultTransform($button_text) !!}</span>
+                        @if (!$isBigLink)
+                            </a>
+                        @endif
+                    </div>
                 @endif
             </div>
-        </div>
-    @if ($isBigLink)
-        </a>
-    @endif
-</{{ $tag }}>
+        @if ($isBigLink)
+            </a>
+        @endif
+    </{{ $tag }}>
+@endif
