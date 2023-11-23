7dcbfd6a6bcefca568cb91a8146f3247a2d8e63b
Hannah Baker <hannahb@cogapp.com>
2023-11-23 11:15:30 +0000

    Use util class for artwork count, remove unnecessary nesting

--- resources/views/site/blocks/custom_tours_grid.blade.php
+++ resources/views/site/blocks/custom_tours_grid.blade.php
@@ -43,7 +43,7 @@
                 @if ($custom_tour->artwork_count)
                     <div class="aic-ct-grid-card__artworks-count-container">
                         <svg aria-hidden="true" class="icon--close"><use xlink:href="#icon--image-stack" /></svg>
-                        <div class="aic-ct-grid-card__artworks-count">
+                        <div class="f-module-title-1">
                             {!! $custom_tour->artwork_count !!}
                         </div>
                     </div>
