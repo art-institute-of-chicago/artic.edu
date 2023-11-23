e86bc643b1faed823723f2a309f81676fad6d76f
Luke Watson-Davies <lukew@cogapp.com>
2023-11-23 09:37:25 +0000

    Switch to using gallery IDs for sorting instead of gallery titles

--- app/Libraries/CustomTour/ArtworkSortingService.php
+++ app/Libraries/CustomTour/ArtworkSortingService.php
@@ -8,8 +8,8 @@ class ArtworkSortingService
     {
         usort($artworks, function ($a, $b) use ($galleryOrder) {
             $defaultPosition = count($galleryOrder);
-            $posA = isset($a['gallery_title']) ? array_search($a['gallery_title'], $galleryOrder) : $defaultPosition;
-            $posB = isset($b['gallery_title']) ? array_search($b['gallery_title'], $galleryOrder) : $defaultPosition;
+            $posA = isset($a['gallery_id']) ? array_search($a['gallery_id'], $galleryOrder) : $defaultPosition;
+            $posB = isset($b['gallery_id']) ? array_search($b['gallery_id'], $galleryOrder) : $defaultPosition;
 
             return $posA <=> $posB;
         });
