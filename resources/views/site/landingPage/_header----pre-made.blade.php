a69224d32370c6fab77fa8ecde047cc10c2380f1
Hannah Baker <hannahb@cogapp.com>
2023-11-27 15:51:57 +0000

    Add conditional logic to custom tours header

--- resources/views/site/landingPage/_header----pre-made.blade.php
+++ resources/views/site/landingPage/_header----pre-made.blade.php
@@ -19,11 +19,20 @@
         <h2 class="f-display-2">Custom Tours</h2>
     </div>
 
-    <div class="f-deck">
-        {!! $header_custom_tours_text !!}
-    </div>
-    <div class="custom-tours-header__btn-container">
-        <a href="{{ $header_custom_tours_primary_button_link }}" class="btn f-buttons">{{ $header_custom_tours_primary_button_label }}</a>
-        <a href="{{ $header_custom_tours_secondary_button_link }}" class="btn btn--secondary f-buttons">{{ $header_custom_tours_secondary_button_label }}</a>
-    </div>
+    @if ($header_custom_tours_text)
+        <div class="f-deck">
+            {!! $header_custom_tours_text !!}
+        </div>
+    @endif
+
+    @if ($header_custom_tours_primary_button_link || $header_custom_tours_secondary_button_link)
+        <div class="custom-tours-header__btn-container">
+            @if ($header_custom_tours_primary_button_link && $header_custom_tours_primary_button_label)
+                <a href="{{ $header_custom_tours_primary_button_link }}" class="btn f-buttons">{{ $header_custom_tours_primary_button_label }}</a>
+            @endif
+            @if ($header_custom_tours_secondary_button_link && $header_custom_tours_secondary_button_label)
+                <a href="{{ $header_custom_tours_secondary_button_link }}" class="btn btn--secondary f-buttons">{{ $header_custom_tours_secondary_button_label }}</a>
+            @endif
+        </div>
+    @endif
 </section>
