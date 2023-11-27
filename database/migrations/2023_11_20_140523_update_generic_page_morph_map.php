dc833195aabe88377d21aee23daa22965d4334f0
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-11-27 11:05:33 -0700

    Lint code [WEB-2676]

--- database/migrations/2023_11_20_140523_update_generic_page_morph_map.php
+++ database/migrations/2023_11_20_140523_update_generic_page_morph_map.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         DB::statement("UPDATE blocks SET blockable_type='genericPages' where blockable_type='App\Models\GenericPage'");
