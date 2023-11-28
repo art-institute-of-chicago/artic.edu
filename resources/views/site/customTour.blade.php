0d9d4bf5ecc52a5439a0877be72e0a80109d9db1
Hannah Baker <hannahb@cogapp.com>
2023-11-28 14:42:12 +0000

    Add cta blocks at the bottom of the page

--- resources/views/site/customTour.blade.php
+++ resources/views/site/customTour.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src=""/>
 
     <h2 class="title f-module-title-2">{!! $custom_tour['title'] !!}</h2>
     <hr/>
@@ -53,4 +54,45 @@
             @endforeach
         </ul>
     </div>
+
+    {{-- Todo: Put the CTA blocks in a separate file? Use local src and downloadUrl? Update alt text. --}}
+    @php
+        $cta_image = [
+            "sourceType" => "imgix",
+            "src" => "https://artic-web-test.imgix.net/a1d91c0c-697b-42ef-b2a3-61a6a3a2da60/Art-Institute-FC-1222-0479.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=0%2C1040%2C6238%2C991",
+            "width" => 6238,
+            "height" => 991,
+            "shareUrl" => "#",
+            "shareTitle" => "",
+            "downloadUrl" => "https://artic-web-test.imgix.net/a1d91c0c-697b-42ef-b2a3-61a6a3a2da60/Art-Institute-FC-1222-0479.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=0%2C1040%2C6238%2C991",
+            "downloadName" => "Art-Institute-FC-1222-0479.jpg",
+            "credit" => "",
+            "creditUrl" => "",
+            "lqip" => null,
+            "alt" => "Art Institute Fc 1222 0479",
+            "caption" => null,
+            "iiifId" => null,
+            "restrict" => false,
+        ];
+    @endphp
+
+    @component('components.molecules._m-cta-banner')
+        @slot('image', $cta_image)
+        @slot('href', 'https://sales.artic.edu/admissions')
+        @slot('header', 'Custom tours are even better in real life')
+        @slot('body', 'Plan a visit and take your tour!')
+        @slot('button_text', 'Buy tickets')
+        @slot('gtmAttributes', 'data-gtm-event="Buy tickets" data-gtm-event-category="internal-ad-click"')
+        @slot('custom_tours', true)
+    @endcomponent
+
+    {{-- Todo: Set $isBigLink to true and add gtmAttributes? --}}
+    @component('components.molecules._m-cta-banner')
+        @slot('header', 'Create your own custom tour')
+        @slot('body', 'Ready to build your own tour? Click the "Create tour" button to search available artworks or to see a list of themes that can help get you started.')
+        @slot('button_text', 'Create your own tour')
+        @slot('custom_tours', true)
+    @endcomponent
 @endsection
+
+
