64fae0e8f84a24cd0de3a701bf117d09e7fa43a8
Hannah Baker <hannahb@cogapp.com>
2023-11-28 13:20:13 +0000

    Use blade class directive

--- resources/views/site/landingPageDetail.blade.php
+++ resources/views/site/landingPageDetail.blade.php
@@ -4,10 +4,13 @@
 
     @include('site.landingPage._header----'.$landingPageType)
 
-    <div id="{{ $landingPageType === 'pre-made' ? 'aic-ct-landingpage' : '' }}" class="o-landingpage__body {{ $landingPageType === 'pre-made' ? 'o-landingpage__body--aic-ct' : '' }} o-blocks">
-
+    <div id="{{ $landingPageType === 'pre-made' ? 'aic-ct-landingpage' : '' }}"
+         @class([
+            'o-landingpage__body',
+            'o-blocks',
+            'o-landingpage__body--aic-ct' => $landingPageType === 'pre-made',
+    ])>
         {!! $item->renderBlocks(false, [], []) !!}
-
     </div>
 
     @include('site.landingPage._footer----'.$landingPageType)
