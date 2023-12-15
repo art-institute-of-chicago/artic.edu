9c512c164efdbc59423745f78a2578a485f899ed
Hannah Baker <hannahb@cogapp.com>
2023-12-15 11:18:38 +0000

    Update url so it's not hardcoded

--- resources/views/site/customTour.blade.php
+++ resources/views/site/customTour.blade.php
@@ -97,7 +97,7 @@
                                         "restrict" => false,
                                     ];
                                 @endphp
-                                <a href="https://www.artic.edu/artworks/{{ $artwork['id'] }}"
+                                <a href="{{ config('app.url') }}/artworks/{{ $artwork['id'] }}"
                                    aria-label="View full artwork page for {{ $artwork['title'] }}{{ isset($artwork['thumbnail']['alt_text']) ? ': ' . $artwork['thumbnail']['alt_text'] : '' }}">
                                     @component('components.atoms._img')
                                         @slot('image', $artwork_image)
@@ -144,7 +144,7 @@
                                 "{{ $artwork['objectNote'] }}"
                             @endcomponent
                         @endisset
-                        <a href="https://www.artic.edu/artworks/{{ $artwork['id'] }}"
+                        <a href="{{ config('app.url') }}/artworks/{{ $artwork['id'] }}"
                            target="_blank"
                            class="external-link f-link"
                            aria-hidden="true">
