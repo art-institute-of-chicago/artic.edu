0f9880dab830eeaae78137c3cd7bf4605b42f3b3
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2023-12-22 10:42:13 -0600

    Remove exploreFurtherTags logic from blade [WEB-1120]

--- resources/views/site/artworkDetail.blade.php
+++ resources/views/site/artworkDetail.blade.php
@@ -148,17 +148,6 @@
 
     <div class="explore-further-injected"></div>
 
-    @if ($exploreFurtherAllTags)
-        @component('components.molecules._m-multi-col-list')
-            @slot('cols_small','2')
-            @slot('cols_medium','3')
-            @slot('cols_large','4')
-            @slot('cols_xlarge','4')
-            @slot('title', 'All tags on this artwork')
-            @slot('items', $exploreFurtherAllTags)
-        @endcomponent
-    @endif
-
     @if ($exploreFurtherCollectionUrl)
         @component('components.molecules._m-links-bar')
             @slot('variation', 'm-links-bar--buttons')
