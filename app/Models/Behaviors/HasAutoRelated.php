2e6603854956e07f31305637696cfd2d2f2fd0d9
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-01-09 12:04:08 -0600

    Create `HasAutoRelated` trait behavior [WEB-2699]

---
+++ app/Models/Behaviors/HasAutoRelated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Behaviors;
+
+use App\Models\Vendor\Block;
+use Illuminate\Support\Str;
+
+trait HasAutoRelated
+{
+    public function related($id)
+    {
+        $modelName = Str::plural(Str::lower(class_basename(__CLASS__)));;
+
+        $blockableTypes = [
+            'highlights',
+            'educatorResources',
+            'articles',
+            'videos',
+            'researchGuides',
+            'genericPages',
+            'events',
+            'exhibitions',
+        ];
+
+        $relatedBlockItems = Block::whereIn('blockable_type', $blockableTypes)
+            ->where('content', 'LIKE', '%"browsers"%')
+            ->where('content', 'LIKE', '%' . $modelName . '%')
+            ->where('content', 'LIKE', '%' . $id . '%')
+            ->get();
+
+        $relatedItems = [];
+        foreach ($relatedBlockItems as $relatedBlockItem) {
+            $relatedItems[] = $relatedBlockItem->blockable;
+        }
+
+        return $relatedItems;
+    }
+}
+
+?>
