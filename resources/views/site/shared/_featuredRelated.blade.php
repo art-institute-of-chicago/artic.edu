88c023a3ec3a3e43a84dd0dc5fa1b11356bbbe19
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-01-09 09:55:14 -0600

    Update `_featuredRelated` to handle auto related items and render accordingly [WEB-2699]

--- resources/views/site/shared/_featuredRelated.blade.php
+++ resources/views/site/shared/_featuredRelated.blade.php
@@ -17,28 +17,50 @@
     if (($behavior ?? '') === 'relatedSidebar') {
         $listingVariation .= ' m-listing--dynamic';
     }
+
+    // Get featured related items
+    $isFeatured = false;
+    $featuredRelated = collect($item->getFeaturedRelated())->map(function ($featuredRelated) {
+        return $featuredRelated['item'];
+    });
+    $featuredRelatedIds = $featuredRelated->pluck('id');
+
+    // Get auto related items & evaluate if they are featured
+
+    $autoRelated = collect($item->related($item->id))->unique('id');
+
+    // Remove featured related items from auto related items
+    if ($featuredRelatedIds->isNotEmpty()) {
+        $autoRelated = $autoRelated->reject(function ($relatedItem) use ($featuredRelatedIds) {
+            return $featuredRelatedIds->contains($relatedItem->id);
+        });
+    }
 @endphp
 
-@if (method_exists($item, 'hasFeaturedRelated') && $item->hasFeaturedRelated())
+@if (method_exists($item, 'hasFeaturedRelated') && $item->hasFeaturedRelated() || count($autoRelated) > 0)
     <aside class="m-inline-aside{{ (isset($variation)) ? ' '.$variation : '' }}" {!! (isset($behavior)) ? 'data-behavior="'.$behavior.'"' : '' !!}>
-        @component('components.atoms._hr')
-        @endcomponent
+            @component('components.atoms._hr')
+            @endcomponent
         @component('components.blocks._text')
             @slot('font', 'f-module-title-1')
             @slot('tag', 'h4')
             {{ $item->getFeaturedRelatedTitle() }}
         @endcomponent
         @component('components.organisms._o-row-listing')
-            @foreach ($item->getFeaturedRelated() as $related)
-                @component('components.molecules._m-listing----' . strtolower($related['type']))
-                    @slot('item', $related['item'])
+            @foreach ($featuredRelated->concat($autoRelated)->take(6) as $related)
+                @php
+                    $isFeatured = $loop->first;
+                @endphp
+                @component('components.molecules._m-listing----auto-related')
+                    @slot('isFeatured', $isFeatured)
+                    @slot('item', $related['item'] ?? $related)
                     @slot('variation',  $listingVariation)
                     @slot('fullscreen', false)
-                    @slot('titleFont', $loop->index > 0 ? 'f-list-1' : 'f-list-3')
+                    @slot('titleFont', $isFeatured ? 'f-list-3' : 'f-list-1')
                     @slot('hideImage', $loop->index > 0)
                     @slot('hideDescription', $loop->index > 0)
                     @slot('imageSettings', $imageSettings ?? null)
-                    @slot('gtmAttributes', $item->getFeaturedRelatedGtmAttributes())
+                    @slot('gtmAttributes', $isFeatured ? ($item->getFeaturedRelatedGtmAttributes() ?? null) : (method_exists($related, 'getFeaturedRelatedGtmAttributes') ? $related->getFeaturedRelatedGtmAttributes() : null))
                 @endcomponent
             @endforeach
         @endcomponent
