4e1def58adf9998add2784e2680acb7ac1fe2f35
Luke Watson-Davies <lukew@cogapp.com>
2024-01-17 11:45:19 +0000

    show gallery location and level data in the viewer and add this data to the galleries config

--- app/Libraries/CustomTour/ArtworkSortingService.php
+++ app/Libraries/CustomTour/ArtworkSortingService.php
@@ -6,12 +6,41 @@ class ArtworkSortingService
 {
     public static function sortArtworksByGallery(&$artworks, $galleryOrder)
     {
-        usort($artworks, function ($a, $b) use ($galleryOrder) {
-            $defaultPosition = count($galleryOrder);
-            $posA = isset($a['gallery_id']) ? array_search($a['gallery_id'], $galleryOrder) : $defaultPosition;
-            $posB = isset($b['gallery_id']) ? array_search($b['gallery_id'], $galleryOrder) : $defaultPosition;
+        $galleryKeys = array_keys($galleryOrder);
+        $defaultPosition = count($galleryOrder);
 
-            return $posA <=> $posB;
+        usort($artworks, function ($a, $b) use ($galleryKeys, $defaultPosition, $galleryOrder) {
+            return self::compareArtworks($a, $b, $galleryKeys, $defaultPosition, $galleryOrder);
         });
+
+        // Append gallery details
+        foreach ($artworks as &$artwork) {
+            self::appendGalleryDetails($artwork, $galleryOrder);
+        }
+    }
+
+    private static function compareArtworks($a, $b, $galleryKeys, $defaultPosition, $galleryOrder)
+    {
+        $posA = self::getArtworkPosition($a, $galleryKeys, $defaultPosition, $galleryOrder);
+        $posB = self::getArtworkPosition($b, $galleryKeys, $defaultPosition, $galleryOrder);
+
+        return $posA <=> $posB;
+    }
+
+    private static function getArtworkPosition($artwork, $galleryKeys, $defaultPosition, $galleryOrder)
+    {
+        return array_key_exists('gallery_id', $artwork) && isset($galleryOrder[$artwork['gallery_id']])
+            ? array_search($artwork['gallery_id'], $galleryKeys)
+            : $defaultPosition;
+    }
+
+    private static function appendGalleryDetails(&$artwork, $galleryOrder)
+    {
+        if (array_key_exists('gallery_id', $artwork) && isset($galleryOrder[$artwork['gallery_id']])) {
+            $galleryId = $artwork['gallery_id'];
+            $artwork['gallery_title'] = $galleryOrder[$galleryId]['gallery_title'];
+            $artwork['gallery_level'] = $galleryOrder[$galleryId]['gallery_level'];
+            $artwork['gallery_location'] = $galleryOrder[$galleryId]['gallery_location'];
+        }
     }
 }
