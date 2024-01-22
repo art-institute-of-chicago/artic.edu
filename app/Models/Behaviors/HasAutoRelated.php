74a124e1cbae9c1f2df75cfb7e5e2e467d0ff0d6
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-01-22 14:19:22 -0600

    Update `HasAutoRelated` method to evaluate `sidebar_items` and weigh them with block types [WEB-2699]

--- app/Models/Behaviors/HasAutoRelated.php
+++ app/Models/Behaviors/HasAutoRelated.php
@@ -11,8 +11,11 @@ trait HasAutoRelated
 {
     public function related($id)
     {
+        $relatedItems = [];
+
         $modelName = Str::plural(Str::lower(class_basename(__CLASS__)));
 
+        // Set scope to only include blockable types that can be related to
         $blockableTypes = [
             'highlights',
             'educatorResources',
@@ -24,16 +27,73 @@ trait HasAutoRelated
             'exhibitions',
         ];
 
+        // Get all blocks that have this model as a related item
         $relatedBlockItems = Block::whereIn('blockable_type', $blockableTypes)
             ->where('content', 'LIKE', '%"browsers"%')
             ->where('content', 'LIKE', '%' . $modelName . '%')
             ->where('content', 'LIKE', '%' . $id . '%')
             ->get();
 
-        $relatedItems = [];
         foreach ($relatedBlockItems as $relatedBlockItem) {
             $relatedItems[] = $relatedBlockItem->blockable;
         }
+
+        // Get all sidebar items that have this model as a related item
+        $editorialTypes = [
+            'articles', 
+            'highlights', 
+            'events', 
+            'exhibitions', 
+            'experiences', 
+            'digitalPublications', 
+            'videos'
+        ];
+
+        // Set scope to only include sidebar items that can be related to
+        $relatedSidebarItems = DB::table('related')
+            ->where('browser_name', 'sidebar_items')
+            ->where('related_id', $id)
+            ->where('related_type', $modelName)
+            ->whereIn('subject_type', $editorialTypes)
+            ->get();
+
+            foreach ($relatedSidebarItems as $item) {
+                // Resolve the model class
+                $modelClass = Relation::getMorphedModel($item->subject_type) ?? $item->subject_type;
+                $itemId = $item->subject_id;
+            
+                if (class_exists($modelClass)) {
+                    $sidebarItem = app($modelClass)->find($itemId);
+            
+                    if ($sidebarItem) {
+                        $relatedItems[] = $sidebarItem;
+                    }
+                }
+            }
+
+        // Set types that should be weighted to the top
+        $weightedTypes = ['article', 'highlight', 'experience'];
+
+        // Compare related items by publish date, with weighted types at the top
+        usort($relatedItems, function ($a, $b) use ($weightedTypes) {
+            $aType = $a->subject_type;
+            $bType = $b->subject_type;
+
+            // If both are weighted or neither are weighted, sort by publish date
+            $bothArePriority = in_array($aType, $weightedTypes) && in_array($bType, $weightedTypes);
+            $neitherIsPriority = !in_array($aType, $weightedTypes) && !in_array($bType, $weightedTypes);
+
+            if ($bothArePriority || $neitherIsPriority) {
+                return $b->publish_start_date <=> $a->publish_start_date;
+            }
+
+            // If only one is weighted, sort it to the top
+            if (in_array($aType, $weightedTypes)) {
+                return -1;
+            }
+
+            return 1;
+        });
 
         return $relatedItems;
     }
