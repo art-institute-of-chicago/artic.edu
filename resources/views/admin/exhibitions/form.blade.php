f6bad33a715a3f5544084b94902ad2bcbb5b6f39
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-01-23 14:17:58 -0600

    Pass $item to featured-related component [WEB-2699]

--- resources/views/admin/exhibitions/form.blade.php
+++ resources/views/admin/exhibitions/form.blade.php
@@ -240,7 +240,7 @@
         ])
     </a17-fieldset>
 
-    @component('admin.partials.featured-related', ['form_fields' => $form_fields])
+    @component('admin.partials.featured-related', ['form_fields' => $form_fields, 'item' => $item])
         @slot('routePrefix', 'exhibitions_events')
         @slot('moduleName', 'exhibitions')
     @endcomponent
--- resources/views/admin/highlights/form.blade.php
+++ resources/views/admin/highlights/form.blade.php
@@ -82,7 +82,7 @@
 @stop
 
 @section('fieldsets')
-    @component('admin.partials.featured-related', ['form_fields' => $form_fields])
+    @component('admin.partials.featured-related', ['form_fields' => $form_fields, 'item' => $item])
         @slot('routePrefix', 'collection')
         @slot('moduleName', 'highlights')
     @endcomponent
