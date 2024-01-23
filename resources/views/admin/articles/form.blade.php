98a9a2905280d259b872a7e0269c19c60686292f
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-01-23 14:17:58 -0600

    Pass $item to featured-related component [WEB-2699]

--- resources/views/admin/articles/form.blade.php
+++ resources/views/admin/articles/form.blade.php
@@ -210,7 +210,7 @@
         <p style="margin-top: 1em">If this logic is satisfactory, there's no need to add exhibitions to the "Custom related items" field. However, if you'd like to control the order of exhibitions relative to other related content, feel free to add them using the field above.</p>
     </a17-fieldset>
 
-    @component('admin.partials.featured-related', ['form_fields' => $form_fields, 'autoRelated' => $autoRelated, 'item' => $item])
+    @component('admin.partials.featured-related', ['form_fields' => $form_fields, 'item' => $item])
         @slot('routePrefix', 'collection.articles_publications')
         @slot('moduleName', 'articles')
     @endcomponent
