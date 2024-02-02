361a42f73f174a1eea7fd314184a07f46d95f92c
nikhil trivedi <ntrivedi@artic.edu>
2024-02-02 10:19:48 -0600

    Finish draft of custom tour PDF in 12-column layout [WEB-2750]

--- resources/views/site/customToursPdfLayout.blade.php
+++ resources/views/site/customToursPdfLayout.blade.php
@@ -13,58 +13,82 @@
     $col4 = 40;
 @endphp
 
-<table>
-    <tr>
-        <td style="width: {{ $col1 }}%">
-            <svg aria-hidden="true">
-                <use xlink:href="#icon--logo--outline--80" />
-                <use xlink:href="#icon--logo--outline--88" />
-                <use xlink:href="#icon--logo--outline--92" />
-              </svg>
-        </td>
-        <td colspan="3">
-            <span class="title f-headline-editorial home-callout-text"><p>{{ $custom_tour['title'] }}</p></span>
-        </td>
-    </tr>
+<table border="1">
+    <thead>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td colspan="2">
+                <svg aria-hidden="true">
+                    <use xlink:href="#icon--logo--outline--80" />
+                    <use xlink:href="#icon--logo--outline--88" />
+                    <use xlink:href="#icon--logo--outline--92" />
+                </svg>
+            </td>
+            <td colspan="10">
+                <span class="title f-headline-editorial home-callout-text"><p>{{ $custom_tour['title'] }}</p></span>
+            </td>
+        </tr>
 
-    <tr>
-        <td style="width: {{ $col1 }}%">
-            <span class="f-body">{{ count($custom_tour['artworks']) }} artworks</strong><br/><em>from</em> <strong>{{ $unique_artists_count }} artists</strong></br><em>across</em> <strong>{{ $unique_galleries_count }} galleries</span>
-        </td>
-        <td style="width: {{ $col2 }}%">
-            <span class="f-body">Tour made by</span>
-        </td>
-        <td colspan="2">
-            <span class="f-quote">{{ $custom_tour['description'] }}</span>
-        </td>
-    </tr>
+        <tr>
+            <td colspan="2">
+                <span class="f-body">{{ count($custom_tour['artworks']) }} artworks</strong><br/><em>from</em> <strong>{{ $unique_artists_count }} artists</strong></br><em>across</em> <strong>{{ $unique_galleries_count }} galleries</span>
+            </td>
+            <td colspan="3">
+                <span class="f-body">Tour made by</span>
+            </td>
+            <td colspan="7">
+                <span class="f-quote">{{ $custom_tour['description'] }}</span>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
 @foreach ($custom_tour['artworks'] as $artwork)
-    <tr class="artwork">
-        <td colspan="2" rowspan="2">
-            <img src="https://www.artic.edu/iiif/2/{{ $artwork['image_id'] }}/full/!1087,700/0/default.jpg"/>
-        </td>
-        <td style="width: {{ $col3 }}%">
-            <p><span class="f-headline-editorial">{{ $artwork['title'] }}</span></p>
-            @isset($artwork['gallery_title'])
-                <p><span class="f-body">{{ $artwork['gallery_title'] }}</span></p>
-            @endisset
-        </td>
-        <td style="width: {{ $col4 }}%">
-            @isset($artwork['description'])
-                <span class="f-caption-title">{{ $artwork['description'] }}</span>
-            @endisset
-        </td>
-    </tr>
+<table>
+    <tbody>
+        <tr class="artwork">
+            <td colspan="5" rowspan="2">
+                <div class="image-container">
+                    <img src="https://www.artic.edu/iiif/2/{{ $artwork['image_id'] }}/full/!1087,700/0/default.jpg"/>
+                </div>
+            </td>
+            <td colspan="3">
+                <p><span class="f-subheading-3">{{ $artwork['title'] }}</span></p>
+                @isset($artwork['gallery_title'])
+                    <p><span class="f-body">{{ $artwork['gallery_title'] }}</span></p>
+                @endisset
+            </td>
+            <td colspan="4">
+                @isset($artwork['description'])
+                    <span class="f-body">{{ $artwork['description'] }}</span>
+                @endisset
+            </td>
+        </tr>
 
-    <tr>
-        <td colspan="2">
-            @isset($artwork['objectNote'])
-                <span class="f-quote">"{{ $artwork['objectNote'] }}"</span>
-            @endisset
-        </td>
-    </tr>
-@endforeach
+        <tr>
+            <td colspan="7">
+                @isset($artwork['objectNote'])
+                    <span class="f-quote">"{{ $artwork['objectNote'] }}"</span>
+                @endisset
+            </td>
+        </tr>
+    </tbody>
 </table>
+@endforeach
 
 @endsection
