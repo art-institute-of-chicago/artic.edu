d878b4bf1a44209c0fd2457445e9e5df17d83e02
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-02-02 13:06:59 -0600

    Update `Showcase` form to handle theming logic [WEB-2735]

--- resources/views/admin/blocks/showcase.blade.php
+++ resources/views/admin/blocks/showcase.blade.php
@@ -1,24 +1,134 @@
+@php
+    $currentUrl = explode('/', request()->url());
+    $type = \App\Models\LandingPage::find(intval($currentUrl[4]))->getType();
+
+    switch ($type) {
+        case 'Home':
+            $themes = ['default', 'home'];
+            $mediaTypes = ['image'];
+            break;
+        case 'Visit':
+            $themes = ['default', 'visit'];
+            $mediaTypes = ['image'];
+            break;
+        case 'RLC':
+            $themes = ['default', 'rlc', 'multiple'];
+            $mediaTypes = ['image', 'video'];
+            break;
+        default:
+            $themes = ['default'];
+            $mediaTypes = ['image', 'video'];
+    }
+@endphp
+
 @twillBlockTitle('Showcase')
 @twillBlockIcon('image')
 
-@formField('input', [
-    'name' => 'header',
-    'label' => 'Header',
-])
-
 @formField('select', [
-    'name' => 'media_type',
-    'label' => 'Media Type',
-    'required' => true,
-    'unpack' => true,
-    'options' => collect(['image' => 'Image', 'video' => 'Video']),
+    'name' => 'theme',
+    'label' => 'Theme',
+    'default' => 'default',
+    'options' => collect($themes)->map(function($theme) {
+        return [
+            'value' => $theme,
+            'label' => ucfirst($theme),
+        ];
+    })->toArray(),
 ])
 
 @formConnectedFields([
-    'fieldName' => 'media_type',
-    'fieldValues' => 'image',
+    'fieldName' => 'theme',
+    'fieldValues' => 'multiple',
     'renderForBlocks' => true,
-])
+    ])
+
+    @formField('select', [
+        'name' => 'variation',
+        'label' => 'Variation',
+        'default' => 'default',
+        'options' => [
+            [
+                'value' => 'make-with-us',
+                'label' => 'Make with us',
+            ],
+            [
+                'value' => 'experience-with-us',
+                'label' => 'Experience with us',
+            ],
+            [
+                'value' => 'learn-with-us',
+                'label' => 'Learn with us',
+            ],
+        ],
+    ])
+@endcomponent
+
+@formConnectedFields([
+    'fieldName' => 'theme',
+    'fieldValues' => 'rlc',
+    'renderForBlocks' => true,
+    ])
+
+    @formField('select', [
+        'name' => 'variation',
+        'label' => 'Variation',
+        'default' => 'default',
+        'options' => [
+            [
+                'value' => 'default',
+                'label' => 'Default',
+            ],
+            [
+                'value' => 'about-the-rlc',
+                'label' => 'About the RLC',
+            ],
+        ]
+    ])
+@endcomponent
+
+@if (count($mediaTypes) > 1)
+
+    @formField('select', [
+        'name' => 'media_type',
+        'label' => 'Media Type',
+        'required' => true,
+        'unpack' => true,
+        'options' => collect($mediaTypes)->map(function($media) {
+            return [
+                'value' => $media,
+                'label' => ucfirst($media),
+            ];
+        })->toArray(),
+    ])
+
+    @formConnectedFields([
+        'fieldName' => 'media_type',
+        'fieldValues' => 'image',
+        'renderForBlocks' => true,
+    ])
+        @formField('medias', [
+            'name' => 'image',
+            'label' => 'Image',
+            'max' => 1,
+            'withVideoUrl' => false,
+            'required' => true,
+        ])
+    @endcomponent
+
+    @formConnectedFields([
+        'fieldName' => 'media_type',
+        'fieldValues' => 'video',
+        'renderForBlocks' => true,
+    ])
+        @formField('files', [
+            'name' => 'video',
+            'label' => 'Video',
+            'max' => 1,
+            'required' => true,
+        ])
+    @endcomponent
+
+@else
     @formField('medias', [
         'name' => 'image',
         'label' => 'Image',
@@ -26,49 +136,93 @@
         'withVideoUrl' => false,
         'required' => true,
     ])
-@endcomponent
+@endif
+
+@if ($type == 'RLC')
+
+    @formConnectedFields([
+        'fieldName' => 'variation',
+        'fieldValues' => ['default', 'about-the-rlc'],
+        'renderForBlocks' => true,
+    ])
+
+        @formField('input', [
+            'type' => 'text',
+            'name' => 'header',
+            'label' => 'Header',
+        ])
+
+        @formField('input', [
+            'name' => 'tag',
+            'label' => 'Tag',
+            'type' => 'text',
+            'maxlength' => 100,
+        ])
+
+        @formField('wysiwyg', [
+            'name' => 'title',
+            'label' => 'Title',
+            'maxlength' => 100,
+            'required' => true,
+            'toolbarOptions' => [
+                    'italic'
+            ],
+        ])
+
+        @formField('wysiwyg', [
+            'name' => 'description',
+            'label' => 'Description',
+            'required' => true,
+        ])
+
+        @formField('input', [
+            'name' => 'link_label',
+            'label' => 'Link Label',
+            'type' => 'text',
+        ])
+
+        @formField('input', [
+            'name' => 'link_url',
+            'label' => 'Link Url',
+            'type' => 'text',
+        ])
+
+    @endcomponent
+
+@endif
 
 @formConnectedFields([
-    'fieldName' => 'media_type',
-    'fieldValues' => 'video',
+    'fieldName' => 'theme',
+    'fieldValues' => 'multiple',
     'renderForBlocks' => true,
-])
-    @formField('files', [
-        'name' => 'video',
-        'label' => 'Video',
-        'max' => 1,
-        'required' => true,
     ])
+
+    @formField('input', [
+        'name' => 'header',
+        'label' => 'Header',
+        'type' => 'text',
+    ])
+
+    @formField('input', [
+        'name' => 'intro',
+        'label' => 'Intro',
+        'type' => 'text',
+    ])
+
+    @formConnectedFields([
+        'fieldName' => 'variation',
+        'fieldValues' => ['make-with-us', 'experience-with-us'],
+        'renderForBlocks' => true,
+    ])
+        @formField('repeater', ['type' => 'showcase_item', 'max' => 3])
+    @endcomponent
+
+    @formConnectedFields([
+        'fieldName' => 'variation',
+        'fieldValues' => 'learn-with-us',
+        'renderForBlocks' => true,
+    ])
+        @formField('repeater', ['type' => 'showcase_item', 'max' => 4])
+    @endcomponent
+
 @endcomponent
-
-@formField('input', [
-    'name' => 'tag',
-    'label' => 'Tag',
-    'maxlength' => 100,
-])
-
-@formField('wysiwyg', [
-    'name' => 'title',
-    'label' => 'Title',
-    'maxlength' => 100,
-    'required' => true,
-    'toolbarOptions' => [
-            'italic'
-    ],
-])
-
-@formField('wysiwyg', [
-    'name' => 'description',
-    'label' => 'Description',
-    'required' => true,
-])
-
-@formField('input', [
-    'name' => 'link_label',
-    'label' => 'Link Label'
-])
-
-@formField('input', [
-    'name' => 'link_url',
-    'label' => 'Link Url'
-])
