384345c9f273159ab896836b3acb4b1797c9b42e
nikhil trivedi <ntrivedi@artic.edu>
2024-02-05 08:58:27 -0600

    Implement bifold layout design for My Museum Tour PDF [WEB-2757]

--- resources/views/site/customToursPdfLayout.blade.php
+++ resources/views/site/customToursPdfLayout.blade.php
@@ -5,14 +5,6 @@
 
 @section('content')
 
-{{-- 16% | 24% | 20% | 40% --}}
-@php
-    $col1 = 16;
-    $col2 = 24;
-    $col3 = 20;
-    $col4 = 40;
-@endphp
-
 <table border="1">
     <thead>
         <tr>
@@ -21,8 +13,9 @@
             <td></td>
             <td></td>
             <td></td>
-            <td></td>
-            <td></td>
+            <td class="thin"></td>
+            <td class="crease"></td>
+            <td class="thin"></td>
             <td></td>
             <td></td>
             <td></td>
@@ -32,6 +25,85 @@
     </thead>
     <tbody>
         <tr>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][5]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][5]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
+            </td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][5]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][5]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][5]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][5]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][5]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][5]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+            <td class="crease"></td>
+            <td colspan="6">
+                <div class="heading-image-container">
+                    <img src="https://artic-web-test.imgix.net/a9a0fefa-2101-456b-9afa-cc34dccaf06d/unnamed1.jpg?rect=0%2C65%2C1391%2C783&auto=format%2Ccompress&q=80&fit=crop&crop=faces%2Ccenter&w=1000&h=563"/>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][5]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][5]['objectNote'] }}"</span>
+                @endisset
+            </td>
+            <td class="crease"></td>
+            <td colspan="6">
+                <span class="title f-headline-editorial home-callout-text"><p>{{ $custom_tour['title'] }}</p></span>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="3" rowspan="2">
+                @if(isset($custom_tour['artworks'][6]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][6]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
+            </td>
+            <td colspan="3" rowspan="2">
+                @if(isset($custom_tour['artworks'][6]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][6]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][6]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][6]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][6]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][6]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+            <td class="crease" rowspan="2"></td>
+            <td colspan="5">
+                <span class="f-body">Tour made by</span>
+            </td>
+            <td colspan="1">
+                <span class="f-body">{{ count($custom_tour['artworks']) }} artworks</strong><br/><em>from</em> <strong>{{ $unique_artists_count }} artists</strong></br><em>across</em> <strong>{{ $unique_galleries_count }} galleries</span>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="6">
+                <span class="f-quote">{{ $custom_tour['description'] }}</span>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][6]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][6]['objectNote'] }}"</span>
+                @endisset
+            </td>
+            <td class="crease"></td>
             <td colspan="2">
                 <svg aria-hidden="true">
                     <use xlink:href="#icon--logo--outline--80" />
@@ -39,56 +111,120 @@
                     <use xlink:href="#icon--logo--outline--92" />
                 </svg>
             </td>
-            <td colspan="10">
-                <span class="title f-headline-editorial home-callout-text"><p>{{ $custom_tour['title'] }}</p></span>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-                <span class="f-body">{{ count($custom_tour['artworks']) }} artworks</strong><br/><em>from</em> <strong>{{ $unique_artists_count }} artists</strong></br><em>across</em> <strong>{{ $unique_galleries_count }} galleries</span>
-            </td>
-            <td colspan="3">
-                <span class="f-body">Tour made by</span>
-            </td>
-            <td colspan="7">
-                <span class="f-quote">{{ $custom_tour['description'] }}</span>
-            </td>
         </tr>
     </tbody>
 </table>
 
-@foreach ($custom_tour['artworks'] as $artwork)
-<table>
+<table class="on-new-page">
     <tbody>
-        <tr class="artwork">
-            <td colspan="5" rowspan="2">
-                <div class="image-container">
-                    <img src="https://www.artic.edu/iiif/2/{{ $artwork['image_id'] }}/full/!1087,700/0/default.jpg"/>
-                </div>
+        <tr>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][1]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][1]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
             </td>
             <td colspan="3">
-                <p><span class="f-subheading-3">{{ $artwork['title'] }}</span></p>
-                @isset($artwork['gallery_title'])
-                    <p><span class="f-body">{{ $artwork['gallery_title'] }}</span></p>
+                @if(isset($custom_tour['artworks'][1]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][1]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][1]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][1]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][1]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][1]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+            <td class="crease"></td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][3]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][3]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
+            </td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][3]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][3]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][3]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][3]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][3]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][3]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][1]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][1]['objectNote'] }}"</span>
                 @endisset
             </td>
-            <td colspan="4">
-                @isset($artwork['description'])
-                    <span class="f-body">{{ $artwork['description'] }}</span>
+            <td class="crease"></td>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][3]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][3]['objectNote'] }}"</span>
                 @endisset
             </td>
         </tr>
 
         <tr>
-            <td colspan="7">
-                @isset($artwork['objectNote'])
-                    <span class="f-quote">"{{ $artwork['objectNote'] }}"</span>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][2]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][2]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
+            </td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][2]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][2]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][2]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][2]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][2]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][2]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+            <td class="crease"></td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][4]))
+                    <div class="artwork-image-container">
+                        <img src="https://www.artic.edu/iiif/2/{{ $custom_tour['artworks'][4]['image_id'] }}/full/684,/0/default.jpg"/>
+                    </div>
+                @endif
+            </td>
+            <td colspan="3">
+                @if(isset($custom_tour['artworks'][4]))
+                    <p><span class="f-subheading-3">{{ $custom_tour['artworks'][4]['title'] }}</span></p>
+                    @isset($custom_tour['artworks'][4]['gallery_title'])
+                        <p><span class="f-body">{{ $custom_tour['artworks'][4]['gallery_title'] }}</span></p>
+                    @endisset
+                    @isset($custom_tour['artworks'][4]['description'])
+                        <span class="f-body">{{ $custom_tour['artworks'][4]['description'] }}</span>
+                    @endisset
+                @endif
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][2]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][2]['objectNote'] }}"</span>
+                @endisset
+            </td>
+            <td class="crease"></td>
+            <td colspan="6">
+                @isset($custom_tour['artworks'][4]['objectNote'])
+                    <span class="f-quote">"{{ $custom_tour['artworks'][4]['objectNote'] }}"</span>
                 @endisset
             </td>
         </tr>
     </tbody>
 </table>
-@endforeach
 
 @endsection
