8aadd745bbfaadda6e4555fb18ef87a9e13bf61a
Zach Garwood <iam@zachgarwood.com>
2024-02-06 10:44:05 -0600

    Remove unnecessary code [WEB-2689]

--- app/Http/Controllers/LandingPagesController.php
+++ app/Http/Controllers/LandingPagesController.php
@@ -214,7 +214,6 @@ class LandingPagesController extends FrontController
                         'default' => $item->imageFront('rlc_location'),
                         'mobile' => $item->imageFront('rlc_location', 'mobile'),
                     ],
-                    'donor_info' => $item->labels->get('donor_info'),
                 ];
                 break;
 
