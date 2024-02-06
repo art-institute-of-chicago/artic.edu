9d9a7f28289a261b4d5ad47fda953a00e5ccd275
nikhil trivedi <ntrivedi@artic.edu>
2024-02-06 11:23:52 -0600

    Fix CSS rendering for landing pages [WEB-2758]

--- resources/views/site/landingPageDetail.blade.php
+++ resources/views/site/landingPageDetail.blade.php
@@ -1,10 +1,12 @@
+use Illuminate\Support\Str;
+
 @extends('layouts.app')
 
 @section('content')
 
     @include('site.landingPage._header----'.$landingPageType)
 
-    <div class="o-landingpage__body o-blocks {{StringHelpers::pageBlades($landingPageType)}}" id="{{ $landingPageType === 'pre-made' ? 'aic-ct-landingpage' : '' }}">
+    <div class="o-landingpage__body o-blocks {{Str::kebab($landingPageType)}} {{ $landingPageType === 'my-museum-tours' ? 'o-landingpage__body--aic-ct' : '' }}" id="{{ $landingPageType === 'my-museum-tours' ? 'aic-ct-landingpage' : '' }}">
 
         {!! $item->renderBlocks(false, data: ['landingPageType' => $landingPageType]) !!}
 
