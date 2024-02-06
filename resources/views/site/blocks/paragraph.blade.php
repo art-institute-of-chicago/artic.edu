9dadd988712c9825b4d19922a12d7bdfaa839192
Zach Garwood <iam@zachgarwood.com>
2024-02-06 14:07:18 -0600

    Add `hasWrapper` flag to paragraph block [WEB-2689]

--- resources/views/site/blocks/paragraph.blade.php
+++ resources/views/site/blocks/paragraph.blade.php
@@ -81,6 +81,10 @@
     libxml_clear_errors();
     libxml_use_internal_errors($oldInternalErrors);
 @endphp
+@if (isset($hasWrapper) && $hasWrapper)
 <div class="paragraph-wrapper">
     {!! $content !!}
 </div>
+@else
+    {!! $content !!}
+@endif
