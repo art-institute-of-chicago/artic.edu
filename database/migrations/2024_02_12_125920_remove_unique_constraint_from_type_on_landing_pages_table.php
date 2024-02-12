271fc1023a749d60c707e2f53d76a59ac9fa3e29
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-02-12 13:14:59 -0600

    Remove unique constraint from Landing Pages [WEB-2723]

---
+++ database/migrations/2024_02_12_125920_remove_unique_constraint_from_type_on_landing_pages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('landing_pages', function (Blueprint $table) {
+            $table->dropUnique('landing_pages_type_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('landing_pages', function (Blueprint $table) {
+            $table->unique('type_id', 'landing_pages_type_unique');
+        });
+    }
+};
