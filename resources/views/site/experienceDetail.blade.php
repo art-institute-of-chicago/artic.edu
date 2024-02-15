c4c583c8b950fef86d01ec179b4dfc8d91fb9ae5
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-02-15 16:33:58 -0600

    Add category tags to detail pages [WEB-2711]

--- resources/views/site/experienceDetail.blade.php
+++ resources/views/site/experienceDetail.blade.php
@@ -10,6 +10,28 @@
             </script>
     </section>
 
+    @if (count($item->categories) > 0)
+        @component('components.atoms._hr')
+        @endcomponent
+        @component('components.blocks._text')
+            @slot('font', 'f-subheading-1')
+            @slot('tag', 'h4')
+            @slot('id', 'h-topics')
+            Topics
+        @endcomponent
+        <ul class="m-inline-list" aria-labelledby="h-topics">
+        @foreach ($item->categories as $category)
+            <li class="m-inline-list__item">
+                @if (!empty($category['id']))
+                    <a class="tag f-tag" href="{{ route('articles', ['category' => $category['id']]) }}">{{ $category['name'] }}</a>
+                @else
+                    <span class="tag f-tag">{{ $category['name'] }}</span>
+                @endif
+            </li>
+        @endforeach
+        </ul>
+    @endif
+
     <section>
         @component('site.shared._relatedItems')
             @slot('title', $furtherReadingTitle ?? null)
