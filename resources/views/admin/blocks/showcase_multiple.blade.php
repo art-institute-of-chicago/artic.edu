56d6bdbfdee2ffd8f68a09bb91886f7788e1e5ad
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-02-28 16:31:22 -0600

    Update `showcase_multiple` block to take on theme and variations [WEB-2735]

--- resources/views/admin/blocks/showcase_multiple.blade.php
+++ resources/views/admin/blocks/showcase_multiple.blade.php
@@ -1,20 +1,68 @@
+@php
+    $currentUrl = explode('/', request()->url());
+    $type = \App\Models\LandingPage::find(intval($currentUrl[4]))->type;
+
+    switch ($type) {
+        case 'RLC':
+            $themes = ['rlc'];
+            break;
+        default:
+            $themes = ['default'];
+    }
+@endphp
+
+@formField('select', [
+    'name' => 'theme',
+    'label' => 'Theme',
+    'default' => 'default',
+    'options' => collect($themes)->map(function($theme) {
+        return [
+            'value' => $theme,
+            'label' => ucfirst($theme),
+        ];
+    })->toArray(),
+])
+
+@formConnectedFields([
+    'fieldName' => 'theme',
+    'fieldValues' => 'rlc',
+    'renderForBlocks' => true,
+    ])
+
+    @formField('select', [
+        'name' => 'variation',
+        'label' => 'Variation',
+        'options' => [
+            [
+                'value' => 'make-with-us',
+                'label' => 'Make with us',
+            ],
+            [
+                'value' => 'experience-with-us',
+                'label' => 'Experience with us',
+            ],
+            [
+                'value' => 'learn-with-us',
+                'label' => 'Learn with us',
+            ],
+        ]
+    ])
+
+@endcomponent
+
 @twillBlockTitle('Showcase Multiple')
 @twillBlockIcon('image')
 
 @formField('input', [
-    'name' => 'id',
-    'label' => 'Block ID',
-    'note' => 'Utilized to link to the block',
-])
-
-@formField('input', [
     'name' => 'heading',
     'label' => 'Heading',
+    'type' => 'text',
 ])
 
 @formField('input', [
     'name' => 'intro',
     'label' => 'Intro',
+    'type' => 'text',
 ])
 
 @formField('repeater', ['type' => 'showcase_item'])
