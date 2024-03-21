c59166df7753ab885769260cb7d31794618008b3
Zach Garwood <iam@zachgarwood.com>
2024-03-21 11:00:38 -0500

    Use ajax to pull featured exhibition into menu [WEB-2740]

--- frontend/js/behaviors/core/primaryNavigation.js
+++ frontend/js/behaviors/core/primaryNavigation.js
@@ -7,11 +7,17 @@ export default function(container) {
     const menuItemQuery = 'a[role="menuitem"]'
     const canExpandAttribute = 'aria-haspopup'
     const isExpandedAttribute = 'aria-expanded'
+    const featuredExhibitionData =
+        'https://api.artic.edu/api/v1/exhibitions/search?query[bool][must][][term][is_published]=true&query[bool][must][][range][aic_start_at][lte]=now&query[bool][must][][range][aic_end_at][gte]=now&query[bool][must_not][term][status]=Closed&fields=title,image_url,web_url&sort=position&size=1'
 
     const menuBar = container.querySelector(menuBarQuery)
     const menuItems = menuBar.querySelectorAll(menuItemQuery)
+    const exhibitionsDetails = container.querySelector('.exhibitions .details')
 
     function _init() {
+        fetch(featuredExhibitionData, { cache: 'force-cache' })
+            .then(response => response.json())
+            .then(exhibitionHandler)
         menuItems.forEach(function(menuItem) {
             expandEventTypes.forEach(function(eventType) {
                 menuItem.addEventListener(eventType, expandHandler)
@@ -31,6 +37,42 @@ export default function(container) {
     function collapseHandler(event) {
         collapseMenu()
         document.removeEventListener(event.type, collapseHandler)
+    }
+
+    function exhibitionHandler(json) {
+        let data = json['data'][0]
+
+        const a = document.createElement('a')
+        a.setAttribute('href', data.web_url)
+
+        const img = document.createElement('img')
+        img.setAttribute('src', data.image_url)
+
+        const description = document.createElement('div')
+        description.setAttribute('class', 'description')
+
+        const supertitle = document.createElement('div')
+        supertitle.setAttribute('class', 'supertitle')
+        supertitle.textContent = 'Featured Exhibition'
+
+        const title = document.createElement('div')
+        title.setAttribute('class', 'title')
+        title.textContent = data.title
+
+        const viewAll = document.createElement('p')
+        viewAll.setAttribute('class', 'view-all')
+        viewAll.textContent = 'View all'
+
+        const iconArrow = container.querySelector('svg.icon--arrow').cloneNode(true)
+
+        exhibitionsDetails.querySelector('a').remove()
+        exhibitionsDetails.prepend(a)
+        a.append(img)
+        a.append(description)
+        description.append(supertitle)
+        description.append(title)
+        description.append(viewAll)
+        viewAll.append(iconArrow)
     }
 
     function collapseMenu() {
