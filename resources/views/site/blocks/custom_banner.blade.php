c4c74095d3a311d1e557fada56792ecde7461ec2
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-03-26 14:15:49 -0500

    Adjust structure of custom_banner for tag cloud variation [WEB-2785]

--- resources/views/site/blocks/custom_banner.blade.php
+++ resources/views/site/blocks/custom_banner.blade.php
@@ -47,34 +47,34 @@
                 @endcomponent
             </div>
             <div class="button-wrapper">
-            @if ($variation == 'cloud')
-                <div class="tag-cloud">
-                    @foreach ($tags as $tag)
-                        <a class="tag f-tag" href="{{ route('articles', ['category' => $tag->id]) }}">{{ $tag->name }}</a>                    
-                    @endforeach
-                </div>
-                @if ($link_label && $link_url)
-                    <a class="tag-cloud__link f-tag" href="{{ $link_url }}">{{ $link_label }}
-                        <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
-                    </a>
-                @endif
-            @else
-                @if($btn_type == 'mobile_app')
-                    <div class="banner-apps">
-                        <a class="app-store" target=”_blank” href="https://apps.apple.com/us/app/art-institute-of-chicago-app/id1130366814?itsct=apps_box_badge&amp;itscg=30200"><img src="{{FrontendHelpers::revAsset('images/icon_app-store.svg')}}" alt="Download on the App Store"></a>
-                        <a class="google-play" target=”_blank” href='https://play.google.com/store/apps/details?id=edu.artic&hl=en_US&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src="{{FrontendHelpers::revAsset('images/icon_google-play.svg')}}"/></a>
+                @if ($variation == 'cloud')
+                    <div class="tag-cloud">
+                        @foreach ($tags as $tag)
+                            <a class="tag f-tag" href="{{ route('articles', ['category' => $tag->id]) }}">{{ $tag->name }}</a>                    
+                        @endforeach
                     </div>
-                @endif
-                @if($btn_type == 'custom')
-                    @component('components.atoms._btn')
-                        @slot('variation', 'primary')
-                        @slot('tag', 'a')
-                        @slot('href', $block->input('button_url'))
-                        {!! SmartyPants::defaultTransform($block->input('button_text')) !!}
-                    @endcomponent
+                    @if ($link_label && $link_url)
+                        <a class="tag-cloud__link f-tag" href="{{ $link_url }}">{{ $link_label }}
+                            <svg class='icon--arrow'><use xlink:href='#icon--arrow'></use></svg>
+                        </a>
+                    @endif
+                @else
+                    @if($btn_type == 'mobile_app')
+                        <div class="banner-apps">
+                            <a class="app-store" target=”_blank” href="https://apps.apple.com/us/app/art-institute-of-chicago-app/id1130366814?itsct=apps_box_badge&amp;itscg=30200"><img src="{{FrontendHelpers::revAsset('images/icon_app-store.svg')}}" alt="Download on the App Store"></a>
+                            <a class="google-play" target=”_blank” href='https://play.google.com/store/apps/details?id=edu.artic&hl=en_US&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src="{{FrontendHelpers::revAsset('images/icon_google-play.svg')}}"/></a>
+                        </div>
+                    @endif
+                    @if($btn_type == 'custom')
+                        @component('components.atoms._btn')
+                            @slot('variation', 'primary')
+                            @slot('tag', 'a')
+                            @slot('href', $block->input('button_url'))
+                            {!! SmartyPants::defaultTransform($block->input('button_text')) !!}
+                        @endcomponent
+                    @endif
                 @endif
             </div>
-            @endif
         </div>
     </div>
 </div>
