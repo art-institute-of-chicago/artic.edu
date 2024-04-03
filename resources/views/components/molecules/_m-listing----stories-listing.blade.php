52a2801c18ef73d1ca9ad452d8c3dfd4b0b0fba4
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-04-03 10:04:46 -0500

    Update display order of authors for listing and featured listing [WEB-2823]

--- resources/views/components/molecules/_m-listing----stories-listing.blade.php
+++ resources/views/components/molecules/_m-listing----stories-listing.blade.php
@@ -37,13 +37,36 @@
                 @endcomponent
                 <br>
                 @if ($isFeatured)
+                    @if (isset($item->authors) && (count($item->authors) > 0) || $item->author_display)
+                        <div class="author f-body-editorial">
+                            @if (count($item->authors) > 0)
+                                <span class="author__name">
+                                    by
+                                    @foreach ($item->authors as $author)
+                                        {!! $loop->last && !$loop->first ? 'and ' : ''!!}{!! $author->title !!}{{!$loop->last ? ', ' : ''}}
+                                    @endforeach
+                                </span>
+                            @else
+                                by ⁠{!! $item->author_display !!}
+                            @endif
+                        </div>
+                    @endif
                     @if ($item->present()->list_description)
                         <div class="intro {{ $captionFont ?? 'f-caption' }}">{!! $item->present()->list_description !!}</div>
                     @endif
                 @endif
-                @if ($item->author_display)
+                @if ((isset($item->authors) && (count($item->authors) > 0) || $item->author_display) && !$isFeatured)
                     <div class="author f-body-editorial">
-                        ⁠—{!! $item->present()->author_display !!}
+                        @if (count($item->authors) > 0)
+                            <span class="author__name">
+                                by
+                                @foreach ($item->authors as $author)
+                                    {!! $loop->last && !$loop->first ? 'and ' : ''!!}{!! $author->title !!}{{!$loop->last ? ', ' : ''}}
+                                @endforeach
+                            </span>
+                        @else
+                            <span class="author__name">by ⁠{!! $item->author_display !!}</span>
+                        @endif
                     </div>
                 @endif
             </div>
