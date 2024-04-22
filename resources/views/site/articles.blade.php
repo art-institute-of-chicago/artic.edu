3003c6862cb7f511a69bc54bd7c8fefe5a343669
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-04-22 12:17:15 -0500

    Add no items found message to articles listing page [WEB-2832]

--- resources/views/site/articles.blade.php
+++ resources/views/site/articles.blade.php
@@ -50,21 +50,26 @@
   @component('components.atoms._hr')
   @endcomponent
 
-  @component('components.organisms._o-grid-listing')
-      @slot('variation', 'o-grid-listing--gridlines-cols o-grid-listing--gridlines-top')
-      @slot('cols_small','2')
-      @slot('cols_medium','3')
-      @slot('cols_large','4')
-      @slot('cols_xlarge','4')
-      @foreach ($articles as $item)
-        @if ($item->type == 'video')
-          @component('components.molecules._m-listing----media')
-            @slot('item', $item)
-          @endcomponent  
-        @else
-          @component('components.molecules._m-listing----' . Str::slug(strtolower($item->type)) . '-minimal')
-            @slot('item', $item)
-            @slot('imageSettings', array(
+  @if ($articles->isEmpty())
+    <div class="m-no-results">
+      @component('components.atoms._title')
+          @slot('tag','h2')
+          @slot('font', 'f-list-3')
+          {!! $text ?? 'Sorry, we couldn’t find any results matching your criteria.' !!}
+      @endcomponent
+    </div>
+  @else
+    @component('components.organisms._o-grid-listing')
+        @slot('variation', 'o-grid-listing--gridlines-cols o-grid-listing--gridlines-top')
+        @slot('cols_small','2')
+        @slot('cols_medium','3')
+        @slot('cols_large','4')
+        @slot('cols_xlarge','4')
+        @foreach ($articles as $item)
+          @if ($item->type == 'video')
+            @component('components.molecules._m-listing----media')
+              @slot('item', $item)
+              @slot('imageSettings', array(
                 'fit' => 'crop',
                 'ratio' => '16:9',
                 'srcset' => array(200,400,600,1000),
@@ -76,10 +81,27 @@
                       'xlarge' => '4',
                 )),
             ))
-          @endcomponent
-        @endif
-      @endforeach
-  @endcomponent
+            @endcomponent  
+          @else
+            @component('components.molecules._m-listing----' . Str::slug(strtolower($item->type)) . '-minimal')
+              @slot('item', $item)
+              @slot('imageSettings', array(
+                  'fit' => 'crop',
+                  'ratio' => '16:9',
+                  'srcset' => array(200,400,600,1000),
+                  'sizes' => ImageHelpers::aic_gridListingImageSizes(array(
+                        'xsmall' => '1',
+                        'small' => '2',
+                        'medium' => '3',
+                        'large' => '4',
+                        'xlarge' => '4',
+                  )),
+              ))
+            @endcomponent
+          @endif
+        @endforeach
+    @endcomponent
+  @endif
 
   @php
     $GLOBALS['paginationAjaxScrollTarget'] = 'listing';
