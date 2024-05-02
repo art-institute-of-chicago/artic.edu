6f496c865621f60c263117f206f45937810e7664
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-05-02 13:29:24 -0500

    Create Embedding model for vectors [WEB-2718]

---
+++ app/Models/embedding.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Pgvector\Laravel\Vector;
+
+class Embedding extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'embedding' => Vector::class,
+    ];
+
+    public function model()
+    {
+        return $this->morphTo();
+    }
+}
+
