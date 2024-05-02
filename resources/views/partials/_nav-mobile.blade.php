326ad451956c8889e2f37440f5ce58a17d9bf949
Zach Garwood <iam@zachgarwood.com>
2024-05-02 14:49:27 -0500

    Refactor mobile nav to use common template [WEB-2813]

--- resources/views/partials/_nav-mobile.blade.php
+++ resources/views/partials/_nav-mobile.blade.php
@@ -3,16 +3,16 @@
     <div class="g-nav-mobile__container">
       <div class="g-header">
         <div class="g-header__inner">
-            <a class="g-header__logo" href="/">
-              <svg aria-label="Art Institute of Chicago">
-                <use xlink:href="#icon--logo--outline--80" />
-                <use xlink:href="#icon--logo--outline--88" />
-                <use xlink:href="#icon--logo--outline--92" />
-              </svg>
-            </a>
-            @include('partials._nav-secondary')
-          </div>
+          <a class="g-header__logo" href="/">
+            <svg aria-label="Art Institute of Chicago">
+              <use xlink:href="#icon--logo--outline--80" />
+              <use xlink:href="#icon--logo--outline--88" />
+              <use xlink:href="#icon--logo--outline--92" />
+            </svg>
+          </a>
+          @include('partials._nav-secondary')
         </div>
+      </div>
 
       <div class="g-nav-mobile__search" data-behavior="mobileSearch">
         <form action="/search" role="search">
@@ -26,92 +26,11 @@
       <nav class="g-nav-mobile__nav-wrapper">
         <h2 class="sr-only" id="h-nav-mobile">Primary Navigation</h2>
         <ul class="g-nav-mobile__nav" aria-labelledby="h-nav-mobile">
-          {{-- Nav Level 0 --}}
-          @foreach ($primaryNav as $level_0)
-            <li class="{{ $level_0['class'] ?? '' }}">
-              <a href="{{ $level_0['url'] ?? '#' }}">
-                {!! $level_0['name'] !!}
-
-                @if (array_key_exists('children', $level_0))
-                  <span data-nav-trigger>
-                    <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-                  </span>
-                @endif
-              </a>
-
-              {{-- Nav Level 1 --}}
-              @if (array_key_exists('children', $level_0))
-                <div class="g-nav-mobile__subnav">
-                  <a href="#" class="g-nav-mobile__back arrow-link arrow-link--back" data-nav-back>
-                    <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-                  </a>
-                  <h3 id="h-nav-mobile-sub-{{ Str::slug($level_0['name']) }}">
-                    <a href="{{ $level_0['url'] }}" class="g-nav-mobile__link">
-                      {!! $level_0['name'] !!}
-                    </a>
-                  </h3>
-
-                  <ul aria-labelledby="h-nav-mobile-sub-{{ Str::slug($level_0['name']) }}">
-                    @foreach ($level_0['children'] as $level_1)
-                      <li class="{{ $level_1['class'] ?? '' }}">
-                        <a href="{{ $level_1['url'] ?? '#' }}">
-                          {!! $level_1['name'] !!}
-
-                          @if (array_key_exists('children', $level_1))
-                            <span data-nav-trigger>
-                              <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-                            </span>
-                          @endif
-                        </a>
-
-                        {{-- Nav Level 2 --}}
-                        @if (array_key_exists('children', $level_1))
-                          <div class="g-nav-mobile__subnav">
-                            <a href="#" class="g-nav-mobile__back arrow-link arrow-link--back" data-nav-back>
-                              <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-                            </a>
-                            <h4 id="h-nav-mobile-sub-sub-{{ Str::slug($level_1['name']) }}">
-                              <a href="{{ $level_1['url'] }}" class="g-nav-mobile__link">
-                                {!! $level_1['name'] !!}
-                              </a>
-                            </h4>
-
-                            <ul aria-labelledby="h-nav-mobile-sub-sub-{{ Str::slug($level_1['name']) }}">
-                              @foreach ($level_1['children'] as $level_2)
-                                <li class="{{ $level_2['class'] ?? '' }}">
-                                  <a href="{{ $level_2['url'] ?? '#' }}" {!! array_key_exists('children', $level_2) ? ' class="g-footer-nav__expander-trigger arrow-link arrow-link--down"' : '' !!}>
-                                    <h5 id="h-nav-mobile-sub-sub-sub-{{ Str::slug($level_2['name']) }}">{!! $level_2['name'] !!}</h5>
-
-                                    @if (array_key_exists('children', $level_2))
-                                      <span data-nav-trigger>
-                                        <svg aria-hidden="true" class="icon--arrow"><use xlink:href="#icon--arrow" /></svg>
-                                      </span>
-                                    @endif
-                                  </a>
-
-                                  {{-- Nav Level 3 --}}
-                                  @if (array_key_exists('children', $level_2))
-                                    <div class="g-nav-mobile__expander">
-                                      <ul aria-labelledby="h-nav-mobile-sub-sub-sub-{{ Str::slug($level_2['name']) }}">
-                                        @foreach ($level_2['children'] as $level_3)
-                                          <li><a href="{{ $level_3['url'] ?? '#' }}">{!! $level_3['name'] !!}</a></li>
-                                        @endforeach
-                                      </ul>
-                                    </div>
-                                  @endif
-
-                                </li>
-                              @endforeach
-                            </ul>
-                          </div>
-                        @endif
-                      </li>
-                    @endforeach
-                  </ul>
-                </div>
-              @endif
-            </li>
-          @endforeach
+          @include('partials._navigation-list', [
+            'role' => 'primary',
+            'list' => $primaryNav,
+            'eventCategory' => 'top-nav',
+          ])
         </ul>
 
         <div class="g-nav-mobile__nav-secondary">
