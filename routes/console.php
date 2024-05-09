681929fe98ac081fd03d9236def9aa5c9fe47691
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-05-09 13:57:06 -0500

    Update embed and create search console script

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 use App\Libraries\AIServices\VectorEmbeddingService as EmbeddingService;
+use App\Libraries\AIServices\SemanticSearchService as SearchService;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use App\Models\Vendor\Block;
+use Pgvector\Laravel\Vector;
+use App\Models\Embedding;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,33 +38,77 @@ Artisan::command('embed', function () {
 
     $modelClass = $this->ask('What is the item\'s model?');
     $modelClass = Str::ucfirst(Str::singular($modelClass));
-    if (!class_exists("\\App\\Models\\$modelClass")) {
+    $model = "\\App\\Models\\$modelClass";
+    $morphedModel = app($model)->getMorphClass();
+    
+    if (!class_exists($model)) {
         $this->error("The class {$modelClass} does not exist.");
         return;
     }
+    
     $id = $this->ask('What is the id of the item?');
+    
+    // Get the morph map key for the model class
 
-    $model = $modelClass::find($id);
+    // Get all blocks associated with the model class and ID
+    $blocks = Block::where('blockable_type', $morphedModel)
+        ->where('blockable_id', $id)
+        ->orderBy('position')
+        ->get();
 
-    if (!$model) {
-        $this->error("No model of class {$modelClass} with id {$id} was found.");
+    if ($blocks->isEmpty()) {
+        $this->error("No blocks associated with class {$modelClass} and id {$id} were found.");
         return;
     }
 
-    $data = [];
+    $textContent = "";
+
+    foreach ($blocks as $block) {
+        // Get the 'content' field
+        $content = $block->content;
+    
+        // Check if the 'paragraph' property exists in the 'content'
+        if (isset($content['paragraph'])) {
+            // Get the 'paragraph' property's content
+            $paragraphBlock = $content['paragraph'];
+    
+            // Append the paragraph content to the allParagraphs string
+            $textContent .= $paragraphBlock;
+        }
+    }
+    
+    // Dump the merged paragraph contents
+    $textContent = strip_tags($textContent);
 
     // Add the model data to the data array
     $embeddingService = new EmbeddingService();
-    $response = $embeddingService->getEmbeddings($data);
+    $response = $embeddingService->getEmbeddings($textContent);
 
-    $embeddings = json_decode($response->getContent())->data[0]->embedding;
-
+    // Check if the response has the expected structure
+    if (isset($response['data'][0]['embedding'])) {
+        $embeddingsData = $response['data'][0]['embedding'];
+        $embeddings = new Vector($embeddingsData);
+    } else {
+        $this->error("The response does not have the expected structure.");
+        return;
+    }
+    
     // Save the embeddings to the Embeddings table
-    $embedding = new \App\Models\Embedding();
+    $embedding = new Embedding();
     $embedding->model_id = $id;
-    $embedding->model_type = $modelClass;
-    $embedding->embeddings = $embeddings;
+    $embedding->model_type = $morphedModel;
+    $embedding->embedding = $embeddings;
     $embedding->save();
 
     $this->info("Embedding has been run on {$modelClass} with id {$id}.");
+});
+
+Artisan::command('search', function () {
+    $input = $this->ask('Enter search');
+    $threshold = $this->ask('Enter threshold');
+
+    $semanticSearchHelper = new SearchService(new EmbeddingService());
+
+    $results = $semanticSearchHelper->search($input, $threshold);
+    return $results;
 });
