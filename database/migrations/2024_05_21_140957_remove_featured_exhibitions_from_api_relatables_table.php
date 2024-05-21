4e6696088bcd03b896d7e622efd8994a9aa0589b
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-05-21 14:49:20 -0500

    Use DB statements for migration [WEB-2682]

--- database/migrations/2024_05_21_140957_remove_featured_exhibitions_from_api_relatables_table.php
+++ database/migrations/2024_05_21_140957_remove_featured_exhibitions_from_api_relatables_table.php
@@ -3,14 +3,13 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
     public function up(): void
     {
-        Schema::table('api_relatables', function (Blueprint $table) {
-            $table->where('relation', 'exhibitionUpcomingListing')->delete();
-            $table->where('relation', 'exhibitionExhibitions')->delete();
-        });
+        DB::table('api_relatables')->where('relation', 'exhibitionsUpcomingListing')->delete();
+        DB::table('api_relatables')->where('relation', 'exhibitionsExhibitions')->delete();
     }
 
     public function down(): void
