58afa14fbf6e660b3469ba532084628c8aa32f9a
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-05-29 13:38:43 -0500

    Lint code [PUB-191]

--- database/migrations/2024_05_23_131202_rename_digital_publication_sections_to_digital_publication_articles.php
+++ database/migrations/2024_05_23_131202_rename_digital_publication_sections_to_digital_publication_articles.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         // Drop existing foreign key constraints
@@ -57,35 +56,35 @@ return new class extends Migration
             $table->dropForeign('digital_publication_article_revisions_digital_publication_article_id_foreign');
             $table->renameColumn('digital_publication_article_id', 'digital_publication_section_id');
         });
-    
+
         Schema::table('digital_publication_article_slugs', function (Blueprint $table) {
             $table->dropForeign('digital_publication_article_slugs_digital_publication_article_id_foreign');
             $table->renameColumn('digital_publication_article_id', 'digital_publication_section_id');
         });
-    
+
         Schema::table('digital_publication_articles', function (Blueprint $table) {
             $table->dropForeign('digital_publication_articles_digital_publication_id_foreign');
         });
-    
+
         // Rename the tables back
         Schema::rename('digital_publication_article_revisions', 'digital_publication_section_revisions');
         Schema::rename('digital_publication_article_slugs', 'digital_publication_section_slugs');
         Schema::rename('digital_publication_articles', 'digital_publication_sections');
-    
+
         // Rename the sequences back
         DB::statement('ALTER SEQUENCE digital_publication_article_revisions_id_seq RENAME TO digital_publication_section_revisions_id_seq');
         DB::statement('ALTER SEQUENCE digital_publication_article_slugs_id_seq RENAME TO digital_publication_section_slugs_id_seq');
         DB::statement('ALTER SEQUENCE digital_publication_articles_id_seq RENAME TO digital_publication_sections_id_seq');
-    
+
         // Re-attach the foreign keys to the original table names
         Schema::table('digital_publication_section_revisions', function (Blueprint $table) {
             $table->foreign('digital_publication_section_id')->references('id')->on('digital_publication_sections')->onDelete('cascade');
         });
-    
+
         Schema::table('digital_publication_section_slugs', function (Blueprint $table) {
             $table->foreign('digital_publication_section_id')->references('id')->on('digital_publication_sections')->onDelete('cascade');
         });
-    
+
         Schema::table('digital_publication_sections', function (Blueprint $table) {
             $table->foreign('digital_publication_id')->references('id')->on('digital_publications')->onDelete('cascade');
         });
