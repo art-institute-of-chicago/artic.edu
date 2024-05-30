6ee085a81edf2332efdee29fe53be081cac53349
nikhil trivedi <ntrivedi@artic.edu>
2024-05-30 14:10:51 -0500

    Roll back one more instance of "article" on interactive feature slides [PUB-191]

--- app/Http/Requests/Admin/SlideRequest.php
+++ app/Http/Requests/Admin/SlideRequest.php
@@ -19,7 +19,7 @@ class SlideRequest extends Request
         return [
             'attract_title' => 'max:150',
             'attract_subhead' => 'max:150',
-            'article_title' => 'max:150',
+            'section_title' => 'max:150',
             'caption' => 'max:150',
             'interstitial_headline' => 'max:150',
             'body_copy' => 'max:500',
