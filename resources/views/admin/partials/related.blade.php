8c09ec66f98f2b2e2a7128f60fbe9d276c4ae077
Zach Garwood <iam@zachgarwood.com>
2024-05-30 12:14:52 -0500

    Refactor related partial template [PUB-196]
    Long lines made it difficult to read.

--- resources/views/admin/partials/related.blade.php
+++ resources/views/admin/partials/related.blade.php
@@ -2,16 +2,23 @@
     $relatedTos = $item->relatedTos;
 @endphp
 @if ($relatedTos->isNotEmpty())
-    <a17-fieldset id="related_from" title="Related (inverse)">
-
-        <p>The following content has been related to this one. These will be used to bring this item into global search results.</p>
-
+    @formFieldset([
+        'id' => 'related_from',
+        'title' => 'Related (inverse)',
+    ])
+        <p>
+            The following content has been related to this one. These will be
+            used to bring this item into global search results.
+        </p>
         <ol style="margin: 1em 0; padding-left: 40px">
             @foreach($relatedTos as $related)
                 <li style="list-style-type: decimal; margin-bottom: 0.5em">
-                    {!! Str::title($related->subject_type) !!}: <a href="{!! route($related->subject_type .'.show', $related->subject) !!}">{{ $related->subject->title }}</a>
+                    {!! Str::title($related->subject_type) !!}:
+                    <a href="{!! route($related->subject_type .'.show', $related->subject) !!}">
+                        {{ $related->subject->title }}
+                    </a>
                 </li>
             @endforeach
         </ol>
-    </a17-fieldset>
+    @endformFieldset
 @endif
