cd523487d214fde09c9aafbe56c79f3135acff31
nikhil trivedi <ntrivedi@artic.edu>
2024-06-21 12:53:21 -0500

    Refactor `migrate:osci-publication` command to access sqlite DB using built-it Laravel mechanisms

--- app/Console/Commands/MigrateOSCIPublicationOne.php
+++ app/Console/Commands/MigrateOSCIPublicationOne.php
@@ -3,26 +3,12 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
 use App\Repositories\Api\PublicationRepository;
 use App\Models\DigitalPublication;
 use App\Models\DigitalPublicationArticle;
 use App\Models\Vendor\Block;
-
-/**
- * MigrationDB 
- * 
- * Wrapper around the migration db -- basically just an open()-er?
- * 
- * TODO: construct() should take a migration filename string
- */
-class MigrationDB extends \SQLite3 {
-
-    function __construct()
-    {
-        $this->open('migration.sqlite3');
-    }
-
-}
 
 class MigrateOSCIPublicationOne extends Command
 {
@@ -40,21 +26,6 @@ class MigrateOSCIPublicationOne extends Command
      */
     protected $description = 'Migrate an OSCI publication from a migration file to a website DigitalPublication';
 
-    protected $db;
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     * 
-     * TODO: This should take.. a migration filename?
-     */
-    public function __construct()
-    {
-        $this->db = new MigrationDB();
-        parent::__construct();
-    }
-
     /**
      * Execute the console command.
      *
@@ -66,34 +37,27 @@ class MigrateOSCIPublicationOne extends Command
 
         // NB!: For each of these types in the admin UI it's impossible to validate the save without setting a date!
 
-        $pubQuery = $this->db->prepare("SELECT json_extract(publications.data,'$._title') as title FROM publications LEFT JOIN tocs ON tocs.package=publications.pub_id WHERE pub_id=:id");
-        $pubQuery->bindValue(':id',$pubId);
-
-        $result = $pubQuery->execute();
-        $pub = $result->fetchArray();
+        $pubs = DB::connection('osci_migration')->select("SELECT json_extract(publications.data,'$._title') as title FROM publications LEFT JOIN tocs ON tocs.package=publications.pub_id WHERE pub_id=:id", ['id' => $pubId]);
+        $pub = Arr::first($pubs);
 
         // TODO: Handle italics / etc in the title
         // TODO: Set any other publication level metadata (date?)
 
         $webPub = new DigitalPublication();
-        $webPub->title = 'Migrated ' . date('M j, Y') . ' | ' . $pub["title"];
+        $webPub->title = 'Migrated ' . date('M j, Y') . ' | ' . $pub->title;
         $webPub->published = false;
         $webPub->is_dsc_stub = false;
         $webPub->save();
 
         $webPubId = $webPub->id;
 
-        $textsQuery = $this->db->prepare("SELECT coalesce(json_extract(data,'$._title'),'FIXME') as title,text_id FROM texts WHERE package=:pubId and text_id NOT LIKE '%ncxtoc%'");
-        $textsQuery->bindValue(':pubId',$pubId);
+        $texts = DB::connection('osci_migration')->select("SELECT coalesce(json_extract(data,'$._title'),'FIXME') as title,text_id FROM texts WHERE package=:pubId and text_id NOT LIKE '%ncxtoc%'", ['pubId' => $pubId]);
 
-        $textResult = $textsQuery->execute();
-        $text = $textResult->fetchArray();
+        foreach ($texts as $text) {
 
-        while ($text) {
-            
             $webArticle = new DigitalPublicationArticle();
             $webArticle->type = "text";
-            $webArticle->title = $text['title'];
+            $webArticle->title = $text->title;
             $webArticle->published = false;
             $webArticle->digital_publication_id = $webPubId;
             $webArticle->date = date('M j, Y');
@@ -101,23 +65,17 @@ class MigrateOSCIPublicationOne extends Command
             $webArticle->created_at = date('M j, Y');
 
             // TODO: Join toc position via json_tree against toc data
-            $webArticle->position = 0; 
+            $webArticle->position = 0;
 
             $webArticle->save();
 
             // TODO: Use a multiline string!! Also this can be moved outside the while
 
-            $blocksQuery = $this->db->prepare("SELECT json_extract(blk.value,'$.html') as html, blk.id as position, json_extract(blk.value,'$.blockType') as type, json_extract(blk.value,'$.fallback_url') as figure_url, coalesce(json_extract(blk.value,'$.caption_html'),'') as figure_capt from texts, json_each(texts.data,'$.sections') as sects,json_each(sects.value,'$.blocks') as blk where texts.text_id=:textId");
-
-            $blocksQuery->bindValue(':textId',$text['text_id']);
-
-            $blocksResult = $blocksQuery->execute();
-
-            $blk = $blocksResult->fetchArray();
+            $blocks = DB::connection('osci_migration')->select("SELECT json_extract(blk.value,'$.html') as html, blk.id as position, json_extract(blk.value,'$.blockType') as type, json_extract(blk.value,'$.fallback_url') as figure_url, coalesce(json_extract(blk.value,'$.caption_html'),'') as figure_capt from texts, json_each(texts.data,'$.sections') as sects,json_each(sects.value,'$.blocks') as blk where texts.text_id=:textId", ['textId' => $text->text_id]);
 
             $order = 0;
 
-            while ($blk) {
+            foreach ($blocks as $blk) {
 
                 $block = new Block();
                 $block->blockable_id = $webArticle->id;
@@ -126,14 +84,14 @@ class MigrateOSCIPublicationOne extends Command
                 $block->position = $order;
 
                 // TODO: Adapt Trevin's spec image code here
-                if ($blk['type'] == 'figure') {
-                    
+                if ($blk->type == 'figure') {
+
                     // $figText = '<span>'.$blk['figure_url'].'</span>';
-                    $figText = '<figure><img src="'. $blk['figure_url'] . '" alt /><figcaption>'.$blk['figure_capt'].'</figcaption></figure>';
+                    $figText = '<figure><img src="'. $blk->figure_url . '" alt /><figcaption>'.$blk->figure_capt.'</figcaption></figure>';
 
                     $block->content = [ 'paragraph' => $figText ];
                 } else {
-                    $block->content = [ 'paragraph' => $blk['html'] ];
+                    $block->content = [ 'paragraph' => $blk->html ];
                 }
 
                 $block->type = 'paragraph';
@@ -141,17 +99,13 @@ class MigrateOSCIPublicationOne extends Command
 
                 $webArticle->blocks()->save($block);
 
-                $blk = $blocksResult->fetchArray();
                 $order += 1;
             }
 
             $webArticle->save();
             $webPub->articles()->save($webArticle);
-
-            $text = $textResult->fetchArray();
-
         }
-    
+
         $webPub->save();
     }
 }
