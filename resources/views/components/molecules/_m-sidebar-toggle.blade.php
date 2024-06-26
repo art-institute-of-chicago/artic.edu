982148586e7dad0c03c22250310aa07228a8953f
Zach Garwood <iam@zachgarwood.com>
2024-06-26 10:52:23 -0500

    Set sidebar toggle text to TOC [PUB-190]
    Also, make sidebar the full width of the screen on mobile.

--- resources/views/components/molecules/_m-sidebar-toggle.blade.php
+++ resources/views/components/molecules/_m-sidebar-toggle.blade.php
@@ -1,4 +1,4 @@
-<div class="m-sidebar-toggle u-hide@large+">
+<div class="m-sidebar-toggle">
     <button data-behavior="showStickySidebar" aria-label="Show publication navigation">
         <svg class="icon--menu--24">
             <use xlink:href="#icon--menu--24" />
@@ -6,7 +6,7 @@
         @component('components.blocks._text')
             @slot('tag', 'span')
             @slot('font', 'f-list-1')
-            {!! $title ?? '' !!}
+            Table of Contents
         @endcomponent
     </button>
 </div>
