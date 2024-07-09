3e2993b187e4d29fdb0d95d3d86e8c443b753d30
nikhil trivedi <ntrivedi@artic.edu>
2024-07-09 12:37:16 -0500

    Make nav links in table of contents linkable [PUB-215]

--- resources/views/components/organisms/_o-table-of-contents.blade.php
+++ resources/views/components/organisms/_o-table-of-contents.blade.php
@@ -1,16 +1,22 @@
 <div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="accordion">
     @foreach ($items as $item)
-        <h3>
-            <button id="{{ StringHelpers::getUtf8Slug($item['title']) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!} aria-expanded="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}">
-                {!! $item['title'] !!}
-                @if (count($item['items']) > 0)
+        @if (count($item['items']) > 0)
+            <h3>
+                <button id="{{ StringHelpers::getUtf8Slug($item['title']) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!} aria-expanded="{{ (isset($item['active']) and $item['active']) ? 'true' : 'false' }}">
+                    {!! $item['title'] !!}
                     <span class="o-accordion__trigger-icon">
                         <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
                         <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
                     </span>
-                @endif
-            </button>
-        </h3>
+                </button>
+            </h3>
+        @else
+            <span class="m-link-list__item o-accordion__panel-content">
+                <a class="m-link-list__trigger f-secondary" href="{{ $item['url'] }}"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!}>
+                    <span class="m-link-list__label">{!! $item['title'] !!}</span>
+                </a>
+            </span>
+        @endif
         @if (count($item['items']) > 0)
             <div id="panel_{{ StringHelpers::getUtf8Slug($item['title']) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($item['title']) }}" aria-hidden="{{ (isset($item['active']) and $item['active']) ? 'false' : 'true' }}">
                 {{-- In the same tune of the build nested articles we can just recursively call the same component to build the table of contents. --}}
