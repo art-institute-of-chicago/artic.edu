982404969629634bb40ad94773c4ce549a65d097
nikhil trivedi <ntrivedi@artic.edu>
2024-07-11 13:42:15 -0500

    Update publication landing to show basic heirarchy layout [PUB-206]

--- resources/views/site/digitalPublicationDetail.blade.php
+++ resources/views/site/digitalPublicationDetail.blade.php
@@ -41,83 +41,43 @@
             </div>
         @endif
 
-        @if ($item->present()->hasArticles(DigitalPublicationArticleType::Contributions->value))
+        @foreach ($item->present()->topLevelArticles() as $topLevelArticle)
             @component('components.molecules._m-title-bar', [
                 'variation' => 'm-title-bar--compact m-title-bar--light',
             ])
-                {{ DigitalPublicationArticleType::Contributions->name }}
+                {!! $topLevelArticle->title !!}
             @endcomponent
 
             @component('components.organisms._o-grid-listing')
-                @slot('variation', 'o-grid-listing--journal')
-                @slot('cols_xsmall','1')
-                @slot('cols_small','2')
-                @slot('cols_medium','2')
-                @slot('cols_large','2')
-                @slot('cols_xlarge','2')
-                @foreach ($item->present()->getArticles(DigitalPublicationArticleType::Contributions->value) as $article)
-                    @component('components.molecules._m-listing----publication')
-                        @slot('variation', 'm-listing--journal')
-                        @slot('href', $article->present()->getArticleUrl($item))
-                        @slot('image', $article->imageFront('hero'))
-                        @slot('type', $article->present()->label)
-                        @slot('title', $article->present()->title)
-                        @slot('title_display', $article->present()->title_display)
-                        @slot('list_description', $article->present()->list_description)
-                        @slot('author_display', $article->showAuthors())
-                        @slot('imageSettings', array(
-                            'fit' => 'crop',
-                            'ratio' => '16:9',
-                            'srcset' => array(200,400,600),
-                            'sizes' => ImageHelpers::aic_imageSizes(array(
-                                  'xsmall' => '216px',
-                                  'small' => '216px',
-                                  'medium' => '18',
-                                  'large' => '13',
-                                  'xlarge' => '13',
-                            )),
-                        ))
-                    @endcomponent
+                @slot('variation', 'o-grid-listing--single-row o-grid-listing--scroll@xsmall o-grid-listing--scroll@small o-grid-listing--scroll@medium o-grid-listing--gridlines-cols')
+                @slot('cols_medium','3')
+                @slot('cols_large','4')
+                @slot('cols_xlarge','4')
+
+                @foreach ($topLevelArticle->children as $article)
+                    @if ($loop->iteration <= 3)
+                        @component('components.molecules._m-listing----article')
+                            @slot('imgVariation','')
+                            @slot('item', $article)
+                            @slot('module', 'collection.publications.digital-publications-articles')
+                            @slot('routeParameters', ['pubId' => $item->id, 'pubSlug' => $item->getSlug(), 'id' => $article->id])
+                            @slot('imageSettings', array(
+                                'fit' => 'crop',
+                                'ratio' => '16:9',
+                                'srcset' => array(200,400,600),
+                                'sizes' => ImageHelpers::aic_imageSizes(array(
+                                    'xsmall' => '216px',
+                                    'small' => '216px',
+                                    'medium' => '18',
+                                    'large' => '13',
+                                    'xlarge' => '13',
+                                )),
+                            ))
+                        @endcomponent
+                    @endif
                 @endforeach
             @endcomponent
-        @endif
-
-        @if ($item->present()->hasArticles(DigitalPublicationArticleType::Works->value))
-            @component('components.molecules._m-title-bar', [
-                'variation' => 'm-title-bar--compact m-title-bar--light',
-            ])
-                {{ DigitalPublicationArticleType::Works->name }}
-            @endcomponent
-
-            @component('components.organisms._o-grid-listing')
-                @slot('variation', 'o-grid-listing--journal')
-
-                @foreach ($item->present()->getArticles(DigitalPublicationArticleType::Works->value) as $article)
-                    @component('components.molecules._m-listing----publication')
-                        @slot('variation', 'm-listing--work')
-                        @slot('href', $article->present()->getArticleUrl($item))
-                        @slot('image', $article->imageFront('hero'))
-                        @slot('type', $article->present()->label)
-                        @slot('title', $article->present()->title)
-                        @slot('title_display', $article->present()->title_display)
-                        @slot('list_description', $article->present()->list_description)
-                        @slot('author_display', $article->showAuthors())
-                        @slot('imageSettings', array(
-                            'fit' => 'crop',
-                            'ratio' => '16:9',
-                            'srcset' => array(200,400,600),
-                            'sizes' => ImageHelpers::aic_imageSizes(array(
-                                'xsmall' => '216px',
-                                'small' => '216px',
-                                'medium' => '18',
-                                'large' => '13',
-                                'xlarge' => '13',
-                            )),
-                        ))
-                    @endcomponent
-                @endforeach
-            @endcomponent
-        @endif
+        @endforeach
 
         @if (isset($item->sponsor_display))
             @component('components.molecules._m-title-bar', [
