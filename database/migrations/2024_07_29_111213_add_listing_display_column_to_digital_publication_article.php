e713069a1a832bf30c63bf524eaceb655b6cf465
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-08-08 16:44:38 -0500

    Create migration to add listing_display column [PUB-201]

---
+++ database/migrations/2024_07_29_111213_add_listing_display_column_to_digital_publication_article.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('digital_publication_articles', function (Blueprint $table) {
+            $table->string('listing_display')->nullable()->after('type');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('digital_publication_articles', function (Blueprint $table) {
+            $table->dropColumn('listing_display');
+        });
+    }
+};
