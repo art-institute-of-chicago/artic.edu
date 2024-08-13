f1ed97c5462c8af37badc2fbdd566d547036b12f
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-08-13 16:49:14 -0500

    Fix digipub title bar to render title's and no types [WEB-2911]

--- resources/views/components/molecules/_m-digipub-title-bar.blade.php
+++ resources/views/components/molecules/_m-digipub-title-bar.blade.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 <div class="m-title-bar m-digipub-title-bar"{!! isset($id) ? ' id="'.$id.'"' : '' !!}>
   @if ($item->children && count($item->children) >= 1)
   <a href="{{ $item->present()->url }}" class="f-link">
-    <h2 class="title {{ $titleFont ?? 'f-list-2' }}" id="{{ isset($id) ? $id : Str::snake(strip_tags($slot), '-') }}">{{ $slot }}</h2>
+    <h2 class="title {{ $titleFont ?? 'f-list-2' }}" id="{{ isset($id) ? $id : Str::snake(strip_tags($item->present()->title), '-') }}">{{ $item->present()->title }}</h2>
       <span class="link">{{ 'View all ' . $item->present()->type }}
-        <span aria-hidden="true">&nbsp;&nbsp;&rsaquo;</span>
+        <span aria-hidden="true">&rsaquo;</span>
       </span>
     </a>
   @else
     <a href="{{ $item->present()->url }}" class="f-link">
-      <h2 class="title {{ $titleFont ?? 'f-list-2' }}" id="{{ isset($id) ? $id : Str::snake(strip_tags($slot), '-') }}">{{ $slot }}</h2>
+      <h2 class="title {{ $titleFont ?? 'f-list-2' }}" id="{{ isset($id) ? $id : Str::snake(strip_tags($item->present()->title ), '-') }}">{{ $item->present()->title  }}</h2>
     </a>
   @endif
 
