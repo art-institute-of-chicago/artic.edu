570c964121a8eb986bed5eada45e3cb672f457b4
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-08-15 10:01:18 -0500

    Fix duping of TOC [WEB-2911]

--- resources/views/components/organisms/_o-table-of-contents.blade.php
+++ resources/views/components/organisms/_o-table-of-contents.blade.php
@@ -5,17 +5,14 @@
                 <button id="toc-{{ StringHelpers::getUtf8Slug($item->title) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0"{!! (isset($item->gtmAttributes)) ? ' '.$item->gtmAttributes.'' : '' !!} aria-expanded="true">
                     {!! $item->title !!}
                     <span class="o-accordion__trigger-icon">
-                        <svg class="icon--plus
-                        "><use xlink:href="#icon--plus" /></svg>
+                        <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
                         <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
                     </span>
                 </button>
             </h3>
-            @foreach ($item->children as $child)
-                <div id="panel_{{ StringHelpers::getUtf8Slug($item->title) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($item->title) }}" aria-hidden="false">
-                    @include('components.organisms._o-table-of-contents', ['items' => $item->children->sortBy('position')])
-                </div>
-            @endforeach
+            <div id="panel_{{ StringHelpers::getUtf8Slug($item->title) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($item->title) }}" aria-hidden="false">
+                @include('components.organisms._o-table-of-contents', ['items' => $item->children->sortBy('position')])
+            </div>
         @else
             <span class="m-link-list__item o-accordion__panel-content">
                 <a class="m-link-list__trigger f-secondary" href="{{ $item->url }}"{!! (isset($item->gtmAttributes)) ? ' '.$item->gtmAttributes.'' : '' !!}>
