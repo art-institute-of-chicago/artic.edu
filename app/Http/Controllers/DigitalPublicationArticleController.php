bb84950405c1e1b345a5e717bda71d59059e97fc
nikhil trivedi <ntrivedi@artic.edu>
2024-08-22 10:03:44 -0500

    Make main nav in digital publication articles not sticky [PUB-218]

--- app/Http/Controllers/DigitalPublicationArticleController.php
+++ app/Http/Controllers/DigitalPublicationArticleController.php
@@ -49,7 +49,7 @@ class DigitalPublicationArticleController extends FrontController
             'item' => $item,
             'contrastHeader' => false,
             'borderlessHeader' => false,
-            'unstickyHeader' => false,
+            'unstickyHeader' => true,
             'canonicalUrl' => $canonicalPath,
         ]);
     }
