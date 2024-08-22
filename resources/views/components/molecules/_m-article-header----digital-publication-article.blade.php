32aea77f3eafa4f3a836326b88deccc04bcb656a
nikhil trivedi <ntrivedi@artic.edu>
2024-08-22 15:44:54 -0500

    Rename `m-article-header--journal-article` to `m-article-header--digital-publication-article`[PUB-231]

--- resources/views/components/molecules/_m-article-header----digital-publication-article.blade.php
+++ resources/views/components/molecules/_m-article-header----digital-publication-article.blade.php
@@ -1,4 +1,4 @@
-<header class="m-article-header m-article-header--journal-article">
+<header class="m-article-header m-article-header--feature m-article-header--digital-publication-article">
     <div class="m-article-header__img">
         @if ($img)
             @component('components.atoms._img')
@@ -16,23 +16,6 @@
                     'srcset' => array(300,600,1000,1500,3000),
                     'sizes' => '100vw',
                 ))
-            @endcomponent
-        @endif
-    </div>
-    <div class="m-article-header__text">
-        @if (isset($title))
-            @component('components.atoms._title')
-                @slot('tag', 'h1')
-                @slot('font', 'f-headline-editorial')
-                @slot('itemprop', 'name')
-                @slot('title', $title)
-                @slot('title_display', $title_display ?? null)
-            @endcomponent
-        @endif
-        @if (!empty($credit))
-            @component('components.molecules._m-info-trigger')
-                @slot('isInverted', true)
-                @slot('creditText', $credit)
             @endcomponent
         @endif
     </div>
