7428054cc66f63edfa2368661b9ba47df5df29ce
nikhil trivedi <ntrivedi@artic.edu>
2024-08-23 17:31:42 -0500

    Style xsmall contribution header [PUB-231]

--- resources/views/components/molecules/_m-article-actions----digital-publication.blade.php
+++ resources/views/components/molecules/_m-article-actions----digital-publication.blade.php
@@ -4,7 +4,7 @@
         <svg aria-hidden="true" class="icon--close" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><path fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1.5" d="M3 3l10 10M13 3L3 13"></path></svg>
     </button>
 
-    <div class="m-article-actions--publication__logo">
+    <div class="m-article-actions--publication__logo u-show@large+">
         <a href="{!! $digitalPublication->present()->getCanonicalUrl() !!}">
             {!! $digitalPublication->header_title_display !!}
         </a>
