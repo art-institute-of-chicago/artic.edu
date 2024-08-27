d0593b1f490f7998171b8e406eecbeb8b1e30c5f
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-08-27 12:10:35 -0500

    Wire up image, title, and description to be links [PUB-235]

--- resources/views/components/molecules/_m-showcase.blade.php
+++ resources/views/components/molecules/_m-showcase.blade.php
@@ -1,11 +1,13 @@
 <div class="m-showcase {{ $variation ? $variation : ''}}">
     <div class="m-showcase-wrapper">
             <div class="m-showcase-media">
-                @component('components.atoms._img')
-                    @slot('image', $image)
-                    @slot('settings', $imageSettings ?? '')
-                    @slot('class', 'm-showcase-image')
-                @endcomponent
+                <a href={{ $linkUrl }} class="showcase-link" >
+                    @component('components.atoms._img')
+                        @slot('image', $image)
+                        @slot('settings', $imageSettings ?? '')
+                        @slot('class', 'm-showcase-image')
+                    @endcomponent
+                </a>
             </div>
         <div class="m-showcase-block__text-wrapper">
             @if ($tag)
@@ -17,23 +19,27 @@
                 @endcomponent
             @endif
             @if ($title)
+            <a href={{ $linkUrl }} class="showcase-link" >
                 @component('components.atoms._title')
                     @slot('tag', 'div')
                     @slot('font', 'f-headline-editorial')
                     @slot('variation', 'showcase-title')
                     @slot('title', $title)
                 @endcomponent
+            </a>
             @endif
             @if ($author_display)
                 <span class="f-secondary">by {{ $author_display }}</span>
             @endif
             @if ($description)
-                @component('components.blocks._text')
-                    @slot('tag', 'div')
-                    @slot('font', 'f-secondary')
-                    @slot('variation', 'showcase-description')
-                    {!! SmartyPants::defaultTransform($description) !!}
-                @endcomponent
+                <a href={{ $linkUrl }} class="showcase-link" >
+                    @component('components.blocks._text')
+                        @slot('tag', 'div')
+                        @slot('font', 'f-secondary')
+                        @slot('variation', 'showcase-description')
+                        {!! SmartyPants::defaultTransform($description) !!}
+                    @endcomponent
+                </a>
             @endif
             @if ($linkLabel || $linkUrl)
                 @component('components.atoms._link')
