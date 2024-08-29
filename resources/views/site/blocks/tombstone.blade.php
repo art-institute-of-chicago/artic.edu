5fbe5bfa7b9635c3ff3b4ff5c971a3097853569e
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-08-29 13:28:55 -0500

    Parse footnotes in the Tombstone block [PUB-221]

--- resources/views/site/blocks/tombstone.blade.php
+++ resources/views/site/blocks/tombstone.blade.php
@@ -1,3 +1,60 @@
+@php
+    global $_collectedReferences;
+    if (!isset($_collectedReferences)) {
+        $_collectedReferences = [];
+    }
+
+    // Processing the paragraph content and references
+    list($content, $_collectedReferences) = StringHelpers::convertReferenceLinks($block->present()->input('text'), $_collectedReferences);
+
+    global $_paragraphCount;
+    if (isset($_paragraphCount)) {
+        $oldInternalErrors = libxml_use_internal_errors(true);
+
+        $dom = new DomDocument();
+        $dom->loadHTML('<?xml encoding="utf-8" ?>' . $content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+        $xpath = new DOMXpath($dom);
+        $nodes = $xpath->query('//p');
+
+        foreach ($nodes as $node) {
+            $_paragraphCount++;
+
+            $newNode = $dom->createElement('p');
+            $newNode->setAttribute('id', 'p-' . $_paragraphCount);
+            $newNode->setAttribute('class', 'p--linked');
+
+            $refSpan = $dom->createElement('span');
+            $refSpan->setAttribute('class', 'p--linked__ref');
+
+            $textSpan = $dom->createElement('span');
+            $textSpan->setAttribute('class', 'p--linked__text');
+
+            $refAnchor = $dom->createElement('a');
+            $refAnchor->setAttribute('href', '#p-' . $_paragraphCount);
+            $refAnchor->setAttribute('class', 'reset');
+
+            $refText = $dom->createTextNode($_paragraphCount);
+            $refAnchor->appendChild($refText);
+            $refSpan->appendChild($refAnchor);
+
+            foreach ($node->childNodes as $childNode) {
+                $textSpan->appendChild(clone $childNode);
+            }
+
+            $newNode->appendChild($textSpan);
+            $newNode->appendChild($refSpan);
+
+            $node->parentNode->replaceChild($newNode, $node);
+        }
+
+        $content = $dom->saveHTML($dom);
+
+        libxml_clear_errors();
+        libxml_use_internal_errors($oldInternalErrors);
+    }
+@endphp
+
 <div class="m-tombstone-block">
     @component('components.atoms._title')
         @slot('font', 'f-secondary')
@@ -8,6 +65,6 @@
     @component('components.atoms._short-description')
         @slot('font', 'f-body-editorial')
         @slot('tag', 'div')
-            {!! $block->input('text') !!}
+            {!! $content !!}
     @endcomponent
 </div>
