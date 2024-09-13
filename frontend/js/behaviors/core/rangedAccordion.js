0550d42bdf668f2ea6091dde3fcf9db2299a16ed
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-09-13 14:31:48 -0500

    Fix scroll behavior for RangedAccordion [PUB-195]

--- frontend/js/behaviors/core/rangedAccordion.js
+++ frontend/js/behaviors/core/rangedAccordion.js
@@ -1,4 +1,4 @@
-import { triggerCustomEvent } from '@area17/a17-helpers';
+import { getOffset } from '@area17/a17-helpers';
 
 const rangedAccordion = function(container) {
 
@@ -20,46 +20,24 @@ const rangedAccordion = function(container) {
         const isExpanded = trigger.getAttribute('aria-expanded') === 'true';
         trigger.setAttribute('aria-expanded', !isExpanded);
 
-        // Add the fading effect
-        if (!isExpanded) {
-            // Expanding the panel
-            panel.style.height = 'auto'; // Let content height determine the space
-            panel.style.opacity = '0';   // Set initial opacity to 0 for fade-in
-            panel.style.transition = 'opacity 0.5s ease'; // Add transition for fade-in effect
-
-            // Timeout to ensure the height is calculated before applying opacity
-            setTimeout(() => {
-                panel.style.opacity = '1'; // Fade-in effect
-            }, 10); // Minimal delay to ensure transition kicks in
-        } else {
-            // Collapsing the panel
-            panel.style.transition = 'opacity 0.5s ease'; // Add transition for fade-out effect
-            panel.style.opacity = '0'; // Fade-out effect
-
-            // Set timeout to hide after fade-out completes
-            setTimeout(() => {
-                panel.style.height = '0';  // Collapse the height
-            }, 500); // Match with transition duration (0.5s)
-        }
+        // Toggle the height of the panel
+        panel.style.height = isExpanded ? '0' : 'max-content';
 
         // Toggle the active class on the trigger
         trigger.classList.toggle('is-active');
 
         // Adjust scroll position only if closing the accordion
         if (isExpanded) {
-            setTimeout(() => {
-                window.scrollTo({
-                    top: currentScrollY,
-                    behavior: 'instant'
-                });
-                triggerCustomEvent(document, 'accordion:toggled',);
-            }, 500);
-        } else {
             window.scrollTo({
                 top: currentScrollY,
-                behavior: 'instant'
+                behavior: 'smooth'
             });
-            triggerCustomEvent(document, 'accordion:toggled',);
+        } else {
+            // After expanding, ensure the current scroll position is restored
+            window.scrollTo({
+                top: currentScrollY,
+                behavior: 'smooth'
+            });
         }
     };
 
