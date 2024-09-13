1b6955c2c3c44e89a03e5e50e896d1deed80c043
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-09-13 14:31:48 -0500

    Fix scroll behavior for RangedAccordion [PUB-195]

--- frontend/js/behaviors/core/rangedAccordion.js
+++ frontend/js/behaviors/core/rangedAccordion.js
@@ -2,13 +2,19 @@ import { getOffset } from '@area17/a17-helpers';
 
 const rangedAccordion = function(container) {
 
-    function _toggleAccordion() {
+    function _toggleAccordion(event) {
+
+        // Prevent default behavior
+        event.preventDefault();
 
         // Get the trigger element
         let trigger = container.querySelector('.o-accordion__trigger');
 
         // Get the associated panel
         let panel = document.getElementById('panel_' + trigger.id);
+
+        // Get the current scroll position
+        const currentScrollY = window.scrollY;
 
         // Toggle the expanded state
         const isExpanded = trigger.getAttribute('aria-expanded') === 'true';
@@ -20,9 +26,19 @@ const rangedAccordion = function(container) {
         // Toggle the active class on the trigger
         trigger.classList.toggle('is-active');
 
-        // On close scroll to the top of the accordion
-        const offset = getOffset(container.querySelector('#' + trigger.id));
-        window.scrollTo(0, offset.top);
+        // Adjust scroll position only if closing the accordion
+        if (isExpanded) {
+            window.scrollTo({
+                top: currentScrollY,
+                behavior: 'smooth'
+            });
+        } else {
+            // After expanding, ensure the current scroll position is restored
+            window.scrollTo({
+                top: currentScrollY,
+                behavior: 'smooth'
+            });
+        }
     };
 
     function _init() {
