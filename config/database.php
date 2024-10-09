2ef695851c46329518c6c591687bbb5435e2a0a0
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-10-09 11:00:09 -0500

    Configure vector db in config [WEB-2934]

--- config/database.php
+++ config/database.php
@@ -114,6 +114,19 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+        'vectors' => [
+            'driver' => 'pgsql',
+            'host' => env('VECTORS_DB_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('VECTORS_DB_PORT', env('DB_PORT', '5432')),
+            'database' => env('VECTORS_DB_DATABASE', env('DB_DATABASE', 'forge')),
+            'username' => env('VECTORS_DB_USERNAME', env('DB_USERNAME', 'forge')),
+            'password' => env('VECTORS_DB_PASSWORD', env('DB_PASSWORD', '')),
+            'charset' => 'utf8',
+            'prefix' => env('VECTORS_PREFIX', ''),
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer'
+        ],
 
         'osci_migration' => [
             'driver' => 'sqlite',
