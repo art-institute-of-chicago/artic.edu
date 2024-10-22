c6bb7aea2e360da0e96fab8c740bea869459d7c8
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-10-22 15:57:33 -0500

    Add ranged-accordion to generic pages [WEB-2944]

--- resources/views/site/blocks/ranged_accordion.blade.php
+++ resources/views/site/blocks/ranged_accordion.blade.php
@@ -5,20 +5,19 @@
 
 @endphp
 
-<div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }}" data-behavior="rangedAccordion">
-    @if ($type === 'start')
-    <h3><button id="accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-list-3' }}" tabindex="0">
-        {!! $title !!}
-        <span class="o-accordion__trigger-icon">
-            <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
-            <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
-        </span>
-    </button></h3>
-    <div id="panel_accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($title)}}">
-        <div class="o-accordion__panel-content o-blocks">
-    @else
+@if ($type === 'start')
+    <div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }} o-ranged-accordion" data-behavior="rangedAccordion">
+        <h3><button id="accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-module-title-2' }}" tabindex="0">
+            {!! $title !!}
+            <span class="o-accordion__trigger-icon">
+                <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
+                <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
+            </span>
+        </button></h3>
+        <div id="panel_accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($title)}}">
+            <div class="o-accordion__panel-content o-blocks o-blocks--with-sidebar">
+@else
             </div>
         </div>
     </div>
-    </div>
-    @endif
+@endif
