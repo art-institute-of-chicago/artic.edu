21524c8ab95f10d441d046a0c63f1e13ac7901c9
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-11-15 12:06:12 -0600

    Update _slide-promo with default variation logic [WEB-2940]

--- resources/views/partials/_slider-promo.blade.php
+++ resources/views/partials/_slider-promo.blade.php
@@ -9,18 +9,9 @@
             <div class="g-slider__intro f-list-2">{!! $modal['intro'] !!}</div>
         </div>
 
-        <form class="g-slider__form" action="{{ $modal['action_url'] }}" method="post" enctype="multipart/form-data" accept-charset="UTF-8" target="_blank" data-gtm-event-category="lightbox" data-gtm-event="{{ $modal['lightbox_button_text'] ?? 'Join Now' }}">
-            @if ($modal['variation'] === \App\Models\Lightbox::VARIATION_NEWSLETTER)
-                <div class="g-slider__form-row">
-                    @component('components.atoms._email')
-                        @slot('id', 'edit-submitted-mail')
-                        @slot('name', 'email')
-                        @slot('required', 'required')
-                        Email address
-                    @endcomponent
-                </div>
-            @else
-                @if ($modal['variation'] === \App\Models\Lightbox::VARIATION_EMAIL)
+        @if ($modal['variation'] !== \App\Models\Lightbox::VARIATION_DEFAULT)
+            <form class="g-slider__form" action="{{ $modal['action_url'] }}" method="post" enctype="multipart/form-data" accept-charset="UTF-8" target="_blank" data-gtm-event-category="lightbox" data-gtm-event="{{ $modal['lightbox_button_text'] ?? 'Join Now' }}">
+                @if ($modal['variation'] === \App\Models\Lightbox::VARIATION_NEWSLETTER)
                     <div class="g-slider__form-row">
                         @component('components.atoms._email')
                             @slot('id', 'edit-submitted-mail')
@@ -29,31 +20,53 @@
                             Email address
                         @endcomponent
                     </div>
-                @elseif ($modal['variation'] === \App\Models\Lightbox::VARIATION_TICKETING)
-                    <div class="g-slider__form-row">
-                        @component('components.atoms._select')
-                            @slot('id', 'g-slider--promo__ticketing')
-                            @slot('name', 'g-slider--promo__ticketing')
-                            @slot('options', [
-                                ['value' => '1', 'label' => 'Visit today'],
-                            ])
-                        @endcomponent
-                    </div>
+                @else
+                    @if ($modal['variation'] === \App\Models\Lightbox::VARIATION_EMAIL)
+                        <div class="g-slider__form-row">
+                            @component('components.atoms._email')
+                                @slot('id', 'edit-submitted-mail')
+                                @slot('name', 'submitted[mail]')
+                                @slot('required', 'required')
+                                Email address
+                            @endcomponent
+                        </div>
+                    @elseif ($modal['variation'] === \App\Models\Lightbox::VARIATION_TICKETING)
+                        <div class="g-slider__form-row">
+                            @component('components.atoms._select')
+                                @slot('id', 'g-slider--promo__ticketing')
+                                @slot('name', 'g-slider--promo__ticketing')
+                                @slot('options', [
+                                    ['value' => '1', 'label' => 'Visit today'],
+                                ])
+                            @endcomponent
+                        </div>
+                    @endif
+
+                    <input type="hidden" name="submitted[tlcsource]" id="edit-submitted-tlcsource" value="{{ $modal['form_tlc_source'] }}" >
+                    <input type="hidden" name="form_token" value="{{ $modal['form_token'] }}">
+                    <input type="hidden" name="form_id" value="{{ $modal['form_id'] }}">
                 @endif
 
-                <input type="hidden" name="submitted[tlcsource]" id="edit-submitted-tlcsource" value="{{ $modal['form_tlc_source'] }}" >
-                <input type="hidden" name="form_token" value="{{ $modal['form_token'] }}">
-                <input type="hidden" name="form_id" value="{{ $modal['form_id'] }}">
-            @endif
+                <div class="g-slider__form-row g-slider__form-row--submit">
+                    @component('components.atoms._btn')
+                        @slot('variation', 'btn--secondary')
+                        {{ $modal['lightbox_button_text'] ?? 'Join Now' }}
+                    @endcomponent
+                </div>
 
+            </form>
+        @else
             <div class="g-slider__form-row g-slider__form-row--submit">
+                <a href="{{ $modal['action_url'] }}" 
+                {{ !empty(parse_url($modal['action_url'] ?? '')['host']) && !Str::startsWith($modal['action_url'] ?? '', request()->root()) ? 'target="_blank" rel="noopener noreferrer"' : '' }}
+                >
                 @component('components.atoms._btn')
-                    @slot('variation', 'btn--secondary')
-                    {{ $modal['lightbox_button_text'] ?? 'Join Now' }}
-                @endcomponent
+                        @slot('variation', 'btn--secondary')
+                        {{ $modal['lightbox_button_text'] ?? 'View' }}
+                    @endcomponent
+                </a>
             </div>
-
-        </form>
+        @endif
 
         <button class="g-slider__close" data-behavior="closeRoadblock">
             <svg aria-hidden="true" class="icon--close" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><path fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1.5" d="M3 3l10 10M13 3L3 13"></path></svg>
