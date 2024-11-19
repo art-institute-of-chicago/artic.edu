fd002786edbbe7e54b3f42306eeba4c920336c15
Zach Garwood <iam@zachgarwood.com>
2024-11-19 16:49:02 -0600

    Update Twill configs and routes [WEB-2960]

--- routes/twill.php
+++ routes/twill.php
@@ -11,113 +11,113 @@ use App\Http\Controllers\Admin\FeeController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\PageController;
 
-Route::module('pages');
+TwillRoutes::module('pages');
 
 Route::group(['prefix' => 'homepage'], function () {
     Route::name('homepage.landing')->get('landing', [PageController::class, 'home']);
-    Route::module('homeFeatures');
-    Route::module('lightboxes');
-    Route::module('homeArtists');
+    TwillRoutes::module('homeFeatures');
+    TwillRoutes::module('lightboxes');
+    TwillRoutes::module('homeArtists');
 });
 
 Route::group(['prefix' => 'visit'], function () {
     Route::name('visit.landing')->get('landing', [PageController::class, 'visit']);
-    Route::module('hours');
-    Route::module('buildingClosures');
-    Route::module('questions');
-    Route::module('feeAges');
-    Route::module('feeCategories');
-    Route::module('shopItems');
+    TwillRoutes::module('hours');
+    TwillRoutes::module('buildingClosures');
+    TwillRoutes::module('questions');
+    TwillRoutes::module('feeAges');
+    TwillRoutes::module('feeCategories');
+    TwillRoutes::module('shopItems');
     Route::name('visit.fees')->get('fees', [FeeController::class, 'index']);
     Route::name('visit.fees.update')->post('fees', [FeeController::class, 'update']);
-    Route::module('virtualTours');
-    Route::module('myMuseumTourItems');
+    TwillRoutes::module('virtualTours');
+    TwillRoutes::module('myMuseumTourItems');
 });
 
 Route::group(['prefix' => 'exhibitions_events'], function () {
     Route::name('exhibitions_events.landing')->get('landing', [PageController::class, 'exhibitions']);
 
-    Route::module('exhibitions');
+    TwillRoutes::module('exhibitions');
     Route::get('exhibitions/augment/{datahub_id}', [ExhibitionController::class, 'augment'])->name('exhibitions_events.exhibitions.augment');
 
-    Route::module('events');
-    Route::module('sponsors');
-    Route::module('waitTimes');
+    TwillRoutes::module('events');
+    TwillRoutes::module('sponsors');
+    TwillRoutes::module('waitTimes');
 
-    Route::module('ticketedEvents');
+    TwillRoutes::module('ticketedEvents');
 
     Route::get('history', [PageController::class, 'exhibitionHistory'])->name('exhibitions_events.history');
 
-    Route::module('emailSeries');
+    TwillRoutes::module('emailSeries');
 
-    Route::module('magazineIssues');
+    TwillRoutes::module('magazineIssues');
 
-    Route::module('miradors');
+    TwillRoutes::module('miradors');
 });
 
 Route::group(['prefix' => 'collection'], function () {
     Route::get('landing', [PageController::class, 'art'])->name('collection.landing');
-    Route::module('artworks');
+    TwillRoutes::module('artworks');
     Route::get('artworks/augment/{datahub_id}', [ArtworkController::class, 'augment'])->name('collection.artworks.augment');
-    Route::module('artists');
+    TwillRoutes::module('artists');
     Route::get('artists/augment/{datahub_id}', [ArtistController::class, 'augment'])->name('collection.artists.augment');
 
     Route::group(['prefix' => 'interactive_features'], function () {
-        Route::module('interactiveFeatures');
-        Route::module('experiences');
-        Route::module('experiences.slides');
+        TwillRoutes::module('interactiveFeatures');
+        TwillRoutes::module('experiences');
+        TwillRoutes::module('experiences.slides');
     });
 
-    Route::module('authors');
+    TwillRoutes::module('authors');
 
-    Route::module('categoryTerms');
+    TwillRoutes::module('categoryTerms');
     Route::get('categoryTerms/augment/{datahub_id}', [CategoryTermController::class, 'augment'])->name('collection.categoryTerms.augment');
 
     Route::group(['prefix' => 'research_resources'], function () {
         Route::get('landing', [PageController::class, 'research'])->name('collection.research_resources.landing');
-        Route::module('educatorResources');
+        TwillRoutes::module('educatorResources');
     });
 
     Route::group(['prefix' => 'articles_publications'], function () {
         Route::get('landing', [PageController::class, 'articles_publications'])->name('collection.articles_publications.landing');
         Route::get('articles_landing', [PageController::class, 'articles'])->name('collection.articles_publications.articles_landing');
-        Route::module('articles');
-        Route::module('categories');
-        Route::module('videos');
-        Route::module('printedPublications');
-        Route::module('digitalPublications');
-        Route::module('digitalPublications.articles');
+        TwillRoutes::module('articles');
+        TwillRoutes::module('categories');
+        TwillRoutes::module('videos');
+        TwillRoutes::module('printedPublications');
+        TwillRoutes::module('digitalPublications');
+        TwillRoutes::module('digitalPublications.articles');
 
         // WEB-1963: Browser for nested modules must be implemented manually
         Route::get('/digitalPublicationsBrowser/articles/browser?digitalPublication={digitalPublication}', [DigitalPublicationArticleController::class, 'browser'])->name('collection.articles_publications.digitalPublications.articles.subbrowser');
         Route::get('/digitalPublicationsBrowser/articles/browser', [DigitalPublicationArticleController::class, 'browser'])->name('collection.articles_publications.digitalPublications.articles.browser');
     });
 
-    Route::module('galleries');
+    TwillRoutes::module('galleries');
     Route::get('galleries/augment/{datahub_id}', [GalleryController::class, 'augment'])->name('collection.galleries.augment');
 
-    Route::module('departments');
+    TwillRoutes::module('departments');
     Route::get('departments/augment/{datahub_id}', [DepartmentController::class, 'augment'])->name('collection.departments.augment');
 
-    Route::module('highlights');
+    TwillRoutes::module('highlights');
 });
 
 Route::group(['prefix' => 'generic'], function () {
-    Route::module('genericPages');
-    Route::module('landingPages');
-    Route::module('pageFeatures');
-    Route::module('pressReleases');
-    Route::module('exhibitionPressRooms');
+    TwillRoutes::module('genericPages');
+    TwillRoutes::module('landingPages');
+    TwillRoutes::module('pageFeatures');
+    TwillRoutes::module('pressReleases');
+    TwillRoutes::module('exhibitionPressRooms');
 });
 
 Route::group(['prefix' => 'general'], function () {
-    Route::module('siteTags');
-    Route::module('searchTerms');
-    Route::module('pageCategories');
-    Route::module('catalogCategories');
-    Route::module('resourceCategories');
-    Route::module('shopItems');
-    Route::module('eventPrograms');
-    Route::module('tourStops');
-    Route::module('vanityRedirects');
+    TwillRoutes::module('siteTags');
+    TwillRoutes::module('searchTerms');
+    TwillRoutes::module('pageCategories');
+    TwillRoutes::module('catalogCategories');
+    TwillRoutes::module('resourceCategories');
+    TwillRoutes::module('shopItems');
+    TwillRoutes::module('eventPrograms');
+    TwillRoutes::module('tourStops');
+    TwillRoutes::module('vanityRedirects');
 });
