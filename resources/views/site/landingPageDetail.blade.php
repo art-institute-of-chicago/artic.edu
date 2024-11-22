8ba3f9dba159e5bb15f81d2dde30c1bc9c9432c5
Zach Garwood <iam@zachgarwood.com>
2024-11-22 16:24:50 -0600

    Update calls to renderBlocks [WEB-2960]
    See https://github.com/area17/twill/blob/3.x/UPGRADE.md#block-editor-render-children

--- resources/views/site/landingPageDetail.blade.php
+++ resources/views/site/landingPageDetail.blade.php
@@ -6,7 +6,7 @@
 
     <div class="o-landingpage__body o-blocks {{Str::kebab($landingPageType)}} {{ $landingPageType === 'my-museum-tour' ? 'o-landingpage__body--aic-ct' : '' }}" id="{{ $landingPageType === 'my-museum-tour' ? 'aic-ct-landingpage' : '' }}">
 
-        {!! $item->renderBlocks(false, data: ['landingPageType' => $landingPageType]) !!}
+        {!! $item->renderBlocks(data: ['landingPageType' => $landingPageType]) !!}
 
     </div>
 
