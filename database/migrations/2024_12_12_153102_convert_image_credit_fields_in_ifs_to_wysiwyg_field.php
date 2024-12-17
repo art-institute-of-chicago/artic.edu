2c25d24a4e8fccb358b8bd012bed8a6ea65b49ec
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-12-17 15:16:50 -0600

    Lint code [IP-231]

--- database/migrations/2024_12_12_153102_convert_image_credit_fields_in_ifs_to_wysiwyg_field.php
+++ database/migrations/2024_12_12_153102_convert_image_credit_fields_in_ifs_to_wysiwyg_field.php
@@ -13,9 +13,9 @@ return new class extends Migration
         Schema::table('experience_images', function (Blueprint $table) {
             $table->text('image_credits')->nullable()->after('inline_credits');
         });
-        
+
         $experienceImages = ExperienceImage::all();
-        
+
         foreach ($experienceImages as $instance) {
             $artist = $instance?->artist;
             $title = $instance?->credit_title;
@@ -25,20 +25,32 @@ return new class extends Migration
             $creditLine = $instance?->credit_line;
             $referenceNumber = $instance?->main_reference_number;
             $type = $instance?->credits_input;
-        
+
             $imageCredit = '';
-            
+
             $parts = [];
-            
-            if ($artist) $parts[] = "<p>{$artist}</p><br class=\"softbreak\">";
-            if ($title) $parts[] = "<p>{$title}</p><br class=\"softbreak\">";
-            if ($date) $parts[] = "<p>{$date}</p><br class=\"softbreak\">";
-            if ($medium) $parts[] = "<p>{$medium}</p><br class=\"softbreak\">";
-            if ($dimensions) $parts[] = "<p>{$dimensions}</p><br class=\"softbreak\">";
-            if ($referenceNumber) $parts[] = "<p>{$referenceNumber}</p><br class=\"softbreak\">";
-            
+
+            if ($artist) {
+                $parts[] = "<p>{$artist}</p><br class=\"softbreak\">";
+            }
+            if ($title) {
+                $parts[] = "<p>{$title}</p><br class=\"softbreak\">";
+            }
+            if ($date) {
+                $parts[] = "<p>{$date}</p><br class=\"softbreak\">";
+            }
+            if ($medium) {
+                $parts[] = "<p>{$medium}</p><br class=\"softbreak\">";
+            }
+            if ($dimensions) {
+                $parts[] = "<p>{$dimensions}</p><br class=\"softbreak\">";
+            }
+            if ($referenceNumber) {
+                $parts[] = "<p>{$referenceNumber}</p><br class=\"softbreak\">";
+            }
+
             $imageCredit = !empty($parts) ? implode('', $parts) : null;
-        
+
             $instance->update(['image_credits' => $imageCredit]);
         }
     }
