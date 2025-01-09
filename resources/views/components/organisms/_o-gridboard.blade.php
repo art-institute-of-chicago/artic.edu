25f97f98ac71a01bf8e5b13773957d5dd01a5770
nikhil trivedi <ntrivedi@artic.edu>
2025-01-09 09:23:33 -0600

    More cleanup [WEB-2976]

--- resources/views/components/organisms/_o-gridboard.blade.php
+++ resources/views/components/organisms/_o-gridboard.blade.php
@@ -3,10 +3,10 @@
     <h3 class="sr-only" id="h-{{ Str::slug($title) }}">{{ $title }}</h3>
 @endif
 
-  <button data-hash="=110100000000010101000111001010000001110100001011010100011100011111110010111101001110111010000010001111101" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 1</button>
-  <button data-hash="=0001011101001101001001001011000101101111010000000000011110100010000010111011111010011010100101110110111" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 2</button>
-  <button data-hash="=111001110101100111111001010001111101100110110010000111000011010100110101001101111110011" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 3</button>
-  <button data-hash="=1011111111010111110111111010010011101101001110101000100110110001001010110110011011" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 4</button>
+  <button data-hash="110100000000010101000111001010000001110100001011010100011100011111110010111101001110111010000010001111101" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 1</button>
+  <button data-hash="0001011101001101001001001011000101101111010000000000011110100010000010111011111010011010100101110110111" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 2</button>
+  <button data-hash="111001110101100111111001010001111101100110110010000111000011010100110101001101111110011" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 3</button>
+  <button data-hash="1011111111010111110111111010010011101101001110101000100110110001001010110110011011" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 4</button>
   <button data-hash="01101011010000111100100010011101011000100101000100000111011011001100001010001111001011101110000010010101011" class="o-gridboard__btn-random btn btn--tertiary f-buttons">Tag 5</button>
   <button class="o-gridboard__btn-page btn btn--tertiary f-buttons">1</button>
   <button class="o-gridboard__btn-page btn btn--tertiary f-buttons">2</button>
