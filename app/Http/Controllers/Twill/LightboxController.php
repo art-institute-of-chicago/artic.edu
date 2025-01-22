1cbec7c8485878c8aa1ea5c52fe1fb8d7a1cf955
Zach Garwood <iam@zachgarwood.com>
2025-01-22 16:19:55 -0600

    Lightbox [WEB-2982]

--- app/Http/Controllers/Twill/LightboxController.php
+++ app/Http/Controllers/Twill/LightboxController.php
@@ -2,29 +2,42 @@
 
 namespace App\Http\Controllers\Twill;
 
-class LightboxController extends \App\Http\Controllers\Twill\ModuleController
-{
-    protected $moduleName = 'lightboxes';
+use A17\Twill\Services\Listings\Columns\Text;
+use A17\Twill\Services\Listings\TableColumns;
+use App\Models\Lightbox;
 
-    protected $indexColumns = [
-        'title' => [
-            'title' => 'Title',
-            'field' => 'title',
-            'sort' => true,
-        ],
-        'lightboxStartDate' => [
-            'title' => 'Start Date',
-            'field' => 'lightboxStartDate',
-            'present' => true,
-            'sortKey' => 'lightbox_start_date',
-            'sort' => true,
-        ],
-        'lightboxEndDate' => [
-            'title' => 'End Date',
-            'field' => 'lightboxEndDate',
-            'present' => true,
-            'sortKey' => 'lightbox_end_date',
-            'sort' => true,
-        ]
-    ];
+class LightboxController extends BaseController
+{
+    public function setUpController(): void
+    {
+        parent::setUpController();
+        $this->setModuleName('lightboxes');
+    }
+
+    public function additionalIndexTableColumns(): TableColumns
+    {
+        $columns = TableColumns::make();
+        $columns->add(
+            Text::make()
+                ->title('Start Date')
+                ->field('lightbox_start_date')
+                ->optional()
+                ->sortable()
+                ->customRender(function (Lightbox $lightbox): string {
+                    return $lightbox?->lightbox_start_date->format('M j, Y') ?? '';
+                })
+        );
+        $columns->add(
+            Text::make()
+                ->title('End Date')
+                ->field('lightbox_end_date')
+                ->optional()
+                ->sortable()
+                ->customRender(function (Lightbox $lightbox): string {
+                    return $lightbox?->lightbox_end_date->format('M j, Y') ?? '';
+                })
+        );
+
+        return $columns;
+    }
 }
