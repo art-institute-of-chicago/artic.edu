02feecaa4f05c25c141ce4ac28bc7defb97318d7
Zach Garwood <iam@zachgarwood.com>
2025-01-22 19:43:36 -0600

    MyMuseumTourItemController [WEB-2982]

--- app/Http/Controllers/Twill/MyMuseumTourItemController.php
+++ app/Http/Controllers/Twill/MyMuseumTourItemController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers\Twill;
 
-use A17\Twill\Http\Controllers\Admin\ModuleController;
+use A17\Twill\Services\Listings\Columns\Text;
+use A17\Twill\Services\Listings\TableColumns;
 
-class MyMuseumTourItemController extends ModuleController
+class MyMuseumTourItemController extends BaseController
 {
-    protected $moduleName = 'myMuseumTourItems';
+    public function setUpController(): void
+    {
+        parent::setUpController();
+        $this->enableShowImage();
+        $this->setModuleName('myMuseumTourItems');
+        $this->setSearchColumns(['title', 'teaser_text', 'tour_id']);
+    }
+
+    public function additionalIndexTableColumns(): TableColumns
+    {
+        $columns = TableColumns::make();
+        $columns->add(
+            Text::make()
+                ->field('teaser_text')
+        );
+        $columns->add(
+            Text::make()
+                ->field('artwork_count')
+                ->optional()
+                ->hide()
+        );
+        $columns->add(
+            Text::make()
+                ->field('tour_id')
+                ->sortable()
+                ->optional()
+                ->hide()
+        );
+
+        return $columns;
+    }
 }
