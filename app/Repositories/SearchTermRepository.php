f89c38de9ee4385a8e8ea767a55c7b2839c1c3ba
Zach Garwood <iam@zachgarwood.com>
2025-01-28 09:56:40 -0600

    Search Terms [WEB-2982]

--- app/Repositories/SearchTermRepository.php
+++ app/Repositories/SearchTermRepository.php
@@ -3,11 +3,20 @@
 namespace App\Repositories;
 
 use App\Models\SearchTerm;
+use Illuminate\Database\Eloquent\Builder;
 
 class SearchTermRepository extends ModuleRepository
 {
     public function __construct(SearchTerm $model)
     {
         $this->model = $model;
+    }
+
+    public function order(Builder $query, array $orders = []): Builder
+    {
+        // Default sort by position instead of created_at.
+        $orders['position'] ??= 'asc';
+        unset($orders['created_at']);
+        return parent::order($query, $orders);
     }
 }
