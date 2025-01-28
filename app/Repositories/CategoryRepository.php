2b17d00936d72a5a544881419f330a391de03394
Zach Garwood <iam@zachgarwood.com>
2025-01-27 21:23:55 -0600

    Category [WEB-2982]

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use A17\Twill\Repositories\Behaviors\HandleSlugs;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Builder;
 
 class CategoryRepository extends ModuleRepository
 {
@@ -12,5 +13,13 @@ class CategoryRepository extends ModuleRepository
     public function __construct(Category $model)
     {
         $this->model = $model;
+    }
+
+    public function order(Builder $query, array $orders = []): Builder
+    {
+        // Default sort by name instead of created_at.
+        $orders['name'] ??= 'asc';
+        unset($orders['created_at']);
+        return parent::order($query, $orders);
     }
 }
