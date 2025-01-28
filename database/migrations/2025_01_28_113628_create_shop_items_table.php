fdbbbd1bdb825824796bd45a5d32e1ff44a0eaf9
Zach Garwood <iam@zachgarwood.com>
2025-01-28 15:45:56 -0600

    Shop [WEB-2982]

---
+++ database/migrations/2025_01_28_113628_create_shop_items_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('shop_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->text('datahub_id')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image_id')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('shop_items');
+    }
+};
