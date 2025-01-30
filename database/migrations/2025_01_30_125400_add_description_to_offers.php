a3c9ed4a8698cb4c4b28ebbac844eea891c5b26a
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-01-30 12:57:09 -0600

    Add description column back to Offers [WEB-3010]

---
+++ database/migrations/2025_01_30_125400_add_description_to_offers.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            $table->string('description')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('offers', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
