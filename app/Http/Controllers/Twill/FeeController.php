322053d25bc87780920f6a643fce4af9d633ac3a
nikhil trivedi <ntrivedi@artic.edu>
2025-02-03 11:14:42 -0600

    Remove commented out code [WEB-2984]

--- app/Http/Controllers/Twill/FeeController.php
+++ app/Http/Controllers/Twill/FeeController.php
@@ -37,42 +37,4 @@ class FeeController extends \App\Http\Controllers\Twill\ModuleController
 
         return $columns;
     }
-
-    // protected FeeRepository $fees;
-
-    // public function __construct(FeeRepository $fees)
-    // {
-    //     $this->fees = $fees;
-    //     $this->request = request();
-    // }
-
-    // public function index($parentModuleId = null): mixed
-    // {
-    //     $feeCategories = FeeCategory::ordered()->get();
-    //     $feeAges = FeeAge::ordered()->get();
-
-    //     return view('twill.fees.index', [
-    //         'customForm' => true,
-    //         'editableTitle' => false,
-    //         'customTitle' => 'Update admission fees',
-    //         'form_fields' => $this->fees->getFormFields(),
-    //         'feeCategories' => $feeCategories,
-    //         'feeAges' => $feeAges,
-    //         'saveUrl' => route('twill.visit.fees.update'),
-    //         'publish' => false,
-    //     ]);
-    // }
-
-    // public function update($id, $submoduleId = null): \Illuminate\Http\JsonResponse
-    // {
-    //     if (array_key_exists('cancel', request()->all())) {
-    //         return redirect()->route('twill.visit.fees.update');
-    //     }
-
-    //     $this->fees->update(request()->except('_token'));
-
-    //     $this->fireEvent();
-
-    //     return redirect()->route('twill.visit.fees.update');
-    // }
 }
--- app/Repositories/FeeRepository.php
+++ app/Repositories/FeeRepository.php
@@ -14,8 +14,8 @@ class FeeRepository extends ModuleRepository
 
     public function order(Builder $query, array $orders = []): Builder
     {
-        // Default sort by name instead of created_at.
         $orders['fee_category_id'] ??= 'asc';
+        $orders['fee_age_id'] ??= 'asc';
         $orders['fee_age_id'] ??= 'asc';
         unset($orders['created_at']);
         return parent::order($query, $orders);
