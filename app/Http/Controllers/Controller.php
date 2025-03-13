8a5c754a2cec2426a2cc5194c91403b77c30521d
nikhil trivedi <ntrivedi@artic.edu>
2025-03-13 18:06:45 -0500

    Code changes to stay up to date with Laravel 11 [WEB-2953]

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
-class Controller extends BaseController
+abstract class Controller
 {
-    use AuthorizesRequests;
-    use ValidatesRequests;
 }
