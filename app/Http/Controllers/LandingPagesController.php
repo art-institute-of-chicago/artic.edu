55ded3bebb460ddbf09870cd12021d5fba5a60a1
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-18 13:33:39 -0500

    Update LandingPagesController for the Publications LP [WEB-3023]

--- app/Http/Controllers/LandingPagesController.php
+++ app/Http/Controllers/LandingPagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admission;
+use App\Models\CatalogCategory;
+use App\Models\DigitalPublication;
 use App\Models\Hour;
 use App\Models\Lightbox;
 use App\Models\LandingPage;
@@ -254,12 +256,65 @@ class LandingPagesController extends FrontController
                 ];
                 break;
 
-            case $types->search('Publications'):
-                $viewData = [
-                    'publications' => PrintedPublication::all(),
-                    'subnav' => collect($blockHeadings)->all(),
-                ];
-                break;
+                case $types->search('Publications'):
+                    $publications = PrintedPublication::published()->get()->merge(DigitalPublication::published()->get())->sortByDesc('publish_start_date');
+                    $filters = $item->labels?->get('filters')
+                        ? collect($item->labels->get('filters'))->map(function ($filterId) {
+                            $category = CatalogCategory::find($filterId);
+                            return $category ? [
+                                'label' => $category->name,
+                                'value' => Str::kebab(Str::lower($category->name))
+                            ] : null;
+                        })->filter()->values()->toArray()
+                        : [];
+                    $primaryFilters = array_merge(
+                        [
+                            [
+                                'label' => 'All Publications',
+                                'value' => 'all'
+                            ],
+                            [
+                                'label' => 'Digital Publications',
+                                'value' => 'digital-publication'
+                            ],
+                            [
+                                'label' => 'Printed Publications',
+                                'value' => 'printed-publication'
+                            ]
+                        ],
+                        $filters
+                    );
+                    
+                    // Get all active categories
+                    $allCategories = $this->getItemCategories($publications);
+                    
+                    // Filter out categories that exist in primaryFilters
+                    $primaryFilterValues = collect($primaryFilters)->pluck('value')->toArray();
+                    $categories = collect($allCategories)->filter(function ($category) use ($primaryFilterValues) {
+                        return !in_array($category['data-button-value'], $primaryFilterValues);
+                    })->values()->toArray();
+                    
+                    $viewData = [
+                        'publications' => $publications,
+                        'primaryFilters' => $primaryFilters,
+                        'categories' => $categories,
+                        'sortOptions' => [
+                            [
+                                'label' => 'Newest To Oldest',
+                                'value' => 'datetime::desc'
+                            ],
+                            [
+                                'label' => 'Oldest To Newest',
+                                'value' => 'datetime::asc'
+                            ],
+                            [
+                                'label' => 'Alphabetically',
+                                'value' => 'title::desc'
+                            ],
+                        ],
+                        'subnav' => collect($blockHeadings)->all(),
+                    ];
+                    break;
 
             default:
                 $viewData = array();
@@ -368,5 +423,30 @@ class LandingPagesController extends FrontController
 
                 break;
         }
+    }
+
+    private function getItemCategories($items)
+    {
+        $categories = collect();
+        $categoryIds = [];
+        
+        foreach ($items as $item) {
+            if ($item->categories) {
+                foreach ($item->categories as $category) {
+                    if (!in_array($category->id, $categoryIds)) {
+                        $categories->push([
+                            'label' => $category->name,
+                            'active' => request()->get('category') == $category->id,
+                            'ajaxScrollTarget' => 'listing',
+                            'id' => $category->id,
+                            'data-button-value' => Str::kebab(Str::lower($category->name))
+                        ]);
+                        $categoryIds[] = $category->id;
+                    }
+                }
+            }
+        }
+        
+        return $categories;
     }
 }
