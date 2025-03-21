9a50dc42b355a58768c6c1bc0932b23fab5ee57e
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-21 15:52:59 -0500

    Lint code [WEB-3030]

--- database/migrations/2025_03_20_143208_update_morph_map_values.php
+++ database/migrations/2025_03_20_143208_update_morph_map_values.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         DB::table(config('twill.mediables_table', 'twill_mediables'))->where('mediable_type', 'A17\Twill\Models\Block')->get()->each(function ($mediable) {
