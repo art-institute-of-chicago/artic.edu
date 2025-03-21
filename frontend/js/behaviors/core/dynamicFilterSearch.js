7c347b8fd111febdf400dc3fc844cf8fe9373233
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-21 13:43:30 -0500

    Create filtering components [WEB-3030]

--- frontend/js/behaviors/core/dynamicFilterSearch.js
+++ frontend/js/behaviors/core/dynamicFilterSearch.js
@@ -1,27 +1,45 @@
 import { triggerCustomEvent } from "@area17/a17-helpers"
-import { dynamicFilterSetup } from ".";
+import generateUUID from '../../functions/core/generateUUID';
 
 const dynamicFilterSearch = function(container) {
+    let id = generateUUID();
     let searchBar = container.querySelector('input');
+
+    container.setAttribute("data-filter-id", id);
 
     function _handleSubmit(event) {
         event.preventDefault();
 
-        triggerCustomEvent(document, 'filter:reset');
-        triggerCustomEvent(document, 'filter:updated', { 
-            filterText: searchBar.value 
+        triggerCustomEvent(document, 'filter:updated', {
+            id: id,
+            parameter: 'search',
+            value: searchBar.value,
         });
     }
 
     function _filterRegister() {
         triggerCustomEvent(document, 'filter:register', {
-
+            id: id,
+            parameter: 'search',
+            value: ''
         })
     }
 
     function _init() {
+        _filterRegister();
         container.addEventListener('submit', _handleSubmit);
-        document.addEventListener('filter:init', _filterRegister())
+        document.addEventListener('filter:castUpdate', function(event) {
+            console.log('search hears castupdate');
+            if (event.data.id === id) {
+                searchBar.value = event.data.value;
+
+                triggerCustomEvent(document, 'filter:updated', {
+                    id: id,
+                    parameter: 'search',
+                    value: searchBar.value,
+                });
+            }
+        });
     }
 
     this.init = function() {
