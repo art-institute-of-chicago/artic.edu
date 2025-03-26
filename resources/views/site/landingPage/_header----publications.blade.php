da493f8a24f1f5cbf21d622fa151e9886f504598
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-26 15:35:31 -0500

    Update publications related blades [WEB-3023]

--- resources/views/site/landingPage/_header----publications.blade.php
+++ resources/views/site/landingPage/_header----publications.blade.php
@@ -1,5 +1,7 @@
+<h2 style="margin-top: 75px" class="title f-display-1">Publications</h2>
+<hr>
 @if (isset($subnav) && (count($subnav) > 0))
-    <div class="stories-subnav">
+    <div class="{{$landingPageType}}-subnav">
         @include('components.molecules._m-auto-subnav', ['subnav' => $subnav])
     </div>
 @endif
