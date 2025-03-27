00a817ff85a7447d3024134fef5b59c5c30aba6c
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-27 16:37:28 -0500

    Add hero_caption to digipub articles [WEB-3053]

---
+++ database/migrations/2025_03_27_162107_add_hero_caption_column_to_digital_publication_articles.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('digital_publication_articles', function (Blueprint $table) {
+            $table->string('hero_caption')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('digital_publication_articles', function (Blueprint $table) {
+            $table->dropColumn('hero_caption');
+        });
+    }
+};
