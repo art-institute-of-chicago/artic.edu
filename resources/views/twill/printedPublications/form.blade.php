0e03be9eef95496ed546348fe67dcef26e540236
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-03-31 12:11:38 -0500

    Add printed publication date field back [WEB-3043]

--- resources/views/twill/printedPublications/form.blade.php
+++ resources/views/twill/printedPublications/form.blade.php
@@ -34,6 +34,12 @@
         :maxlength='255'
     />
 
+    <x-twill::date-picker
+        name='publication_date'
+        label='Publication date'
+        withTime='false'
+    />
+
     <x-twill::multi-select
         name='categories'
         label='Categories'
