a11427db6dfff3fc9be89185499997a7b0d88461
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-04-10 13:19:10 -0500

    Lint code [WEB-3065]

--- database/migrations/2025_04_08_120613_update_editor_name_in_blocks.php
+++ database/migrations/2025_04_08_120613_update_editor_name_in_blocks.php
@@ -3,8 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         DB::update("UPDATE blocks SET editor_name='default' WHERE editor_name is null");
