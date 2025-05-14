2ae8ed750456fafa0fec9734dbc526966fe38e3f
Zach Garwood <iam@zachgarwood.com>
2025-05-14 13:14:21 -0500

    Lint new code [WEB-3071]

--- app/Presenters/Admin/IlluminatedLinkPresenter.php
+++ app/Presenters/Admin/IlluminatedLinkPresenter.php
@@ -6,12 +6,12 @@ use App\Presenters\BasePresenter;
 
 class IlluminatedLinkPresenter extends BasePresenter
 {
-    public function title_display()
+    public function title_display() // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
     {
         return $this->title;
     }
 
-    public function list_description()
+    public function list_description() // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
     {
         return $this->description;
     }
