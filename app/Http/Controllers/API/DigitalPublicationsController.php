a2a7b3a8b6c1ee06f253230edb8c3d26a763ff65
Zach Garwood <iam@zachgarwood.com>
2025-05-19 15:45:44 -0500

    Exclude unlisted digipubs from API results [PUB-315]

--- app/Http/Controllers/API/DigitalPublicationsController.php
+++ app/Http/Controllers/API/DigitalPublicationsController.php
@@ -6,4 +6,15 @@ class DigitalPublicationsController extends BaseController
 {
     protected $model = \App\Models\DigitalPublication::class;
     protected $transformer = \App\Http\Transformers\DigitalPublicationTransformer::class;
+
+    /**
+     * Exclude unlisted digital publications.
+     *
+     * @param int $limit
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    protected function paginate($limit)
+    {
+        return ($this->model)::notUnlisted()->paginate($limit);
+    }
 }
