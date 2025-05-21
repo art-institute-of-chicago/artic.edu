cf517f70543aa84679977d834e80908189925d24
Zach Garwood <iam@zachgarwood.com>
2025-05-21 10:47:11 -0500

    Update existing footer templates with FAQ component [WEB-3074]

--- resources/views/site/landingPage/_footer----conservation-and-science.blade.php
+++ resources/views/site/landingPage/_footer----conservation-and-science.blade.php
@@ -3,11 +3,5 @@
         @slot('faqs', $item->faqs)
         @slot('active', $item['active'])
         @slot('gtmAttributes', $item['gtmAttributes'])
-
-        <div class="spacer-container">
-            <div class="spacer">
-                <div class="semicircle left-hemisphere"></div>
-            </div>
-        </div>
     @endcomponent
 </div>
