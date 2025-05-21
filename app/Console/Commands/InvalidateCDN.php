49a8c0898ac85f9d529df51d644c3add6a153363
nikhil trivedi <ntrivedi@artic.edu>
2025-05-21 09:48:50 -0500

    Lint code [WEB-3107, WEB-3108]

--- app/Console/Commands/InvalidateCDN.php
+++ app/Console/Commands/InvalidateCDN.php
@@ -20,8 +20,7 @@ class InvalidateCDN extends Command
             }
 
             $this->info('Cloudflare invalidation request sent!');
-        }
-        elseif (config('services.cloudfront.enabled')) {
+        } elseif (config('services.cloudfront.enabled')) {
             if (!empty($this->argument('urls'))) {
                 app(CloudfrontCacheService::class)->invalidate($this->argument('urls'));
             }
