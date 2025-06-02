a91c8ebfb9ac30eee2328f5d19d80f9d240d42c4
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-06-02 16:07:20 -0500

    Add static files field for educator resource pdfs [WEB-3095]

--- resources/views/twill/educatorResources/form.blade.php
+++ resources/views/twill/educatorResources/form.blade.php
@@ -37,6 +37,13 @@
         :translated='true'
     />
 
+    <x-twill::files
+        name='file'
+        label='PDF download'
+        note='Upload a .pdf file'
+        :translated='true'
+    />
+
     <x-twill::multi-select
         name='categories'
         label='Categories'
