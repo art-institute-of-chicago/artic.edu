51dc143b2dcbb7304f636cb4c974e20fec561840
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-06-11 11:04:40 -0500

    Lint code [PUB-329]

--- database/migrations/2025_06_10_141823_update_content_column_on_blocks_to_nullable.php
+++ database/migrations/2025_06_10_141823_update_content_column_on_blocks_to_nullable.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
-      Schema::table('blocks', function (Blueprint $table) {
-          $table->json('content')->nullable()->change();
-      });
+        Schema::table('blocks', function (Blueprint $table) {
+            $table->json('content')->nullable()->change();
+        });
     }
 
     public function down(): void
     {
-      Schema::table('blocks', function (Blueprint $table) {
-          $table->json('content')->change();
-      });
+        Schema::table('blocks', function (Blueprint $table) {
+            $table->json('content')->change();
+        });
     }
 };
