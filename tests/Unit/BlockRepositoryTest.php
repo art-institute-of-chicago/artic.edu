5e34d9beb2cc63abd537d8f07becb59a6b29fe02
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-06-11 11:33:26 -0500

    Update name of insubstantial method

--- tests/Unit/BlockRepositoryTest.php
+++ tests/Unit/BlockRepositoryTest.php
@@ -17,49 +17,49 @@ class BlockRepositoryTest extends BaseTestCase
 
     public function test_it_trims_whitespace(): void
     {
-        $result = $this->blockRepository->cleanNonSubstantialContent('   ');
+        $result = $this->blockRepository->cleanInsubstantialContent('   ');
         $this->assertEmpty($result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent('   Not removed   ');
+        $result = $this->blockRepository->cleanInsubstantialContent('   Not removed   ');
         $this->assertEquals('Not removed', $result);
     }
 
     public function test_it_removes_paragraphs_with_only_whitespace(): void
     {
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p> </p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p> </p>');
         $this->assertEmpty($result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent("<p>\n</p>");
+        $result = $this->blockRepository->cleanInsubstantialContent("<p>\n</p>");
         $this->assertEmpty($result);
     }
 
     public function test_it_removes_paragraphs_with_only_breaks(): void
     {
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p><br /></p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p><br /></p>');
         $this->assertEmpty($result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p><br></p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p><br></p>');
         $this->assertEmpty($result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p><br class="softbreak" /></p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p><br class="softbreak" /></p>');
         $this->assertEmpty($result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent("<p><br class='softbreak'></p>");
+        $result = $this->blockRepository->cleanInsubstantialContent("<p><br class='softbreak'></p>");
         $this->assertEmpty($result);
     }
 
     public function test_it_returns_an_empty_string_if_there_is_no_substantial_content(): void
     {
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p><div><b><em>   </em></b></div></p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p><div><b><em>   </em></b></div></p>');
         $this->assertEmpty($result);
     }
 
     public function test_it_does_not_remove_paragraphs_with_substantial_content(): void
     {
-        $result = $this->blockRepository->cleanNonSubstantialContent("<p>\n</p><p>Not removed</p><p><br /></p>");
+        $result = $this->blockRepository->cleanInsubstantialContent("<p>\n</p><p>Not removed</p><p><br /></p>");
         $this->assertEquals('<p>Not removed</p>', $result);
 
-        $result = $this->blockRepository->cleanNonSubstantialContent('<p>Not<br class="softbreak">removed</p>');
+        $result = $this->blockRepository->cleanInsubstantialContent('<p>Not<br class="softbreak">removed</p>');
         $this->assertEquals('<p>Not<br class="softbreak">removed</p>', $result);
     }
 }
