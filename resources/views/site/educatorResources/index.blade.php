4734c40e7b0d5a6eabf653e5a108f057d2dc4c7b
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-06-17 14:05:11 -0500

    Update filtering methods and usage of lables [WEB-3096]

--- resources/views/site/educatorResources/index.blade.php
+++ resources/views/site/educatorResources/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<section class="o-educator-resources">
+<section class="o-educator-resources" data-behavior='dynamicFilter' data-filter-persist='true'>
 
     @component('components.molecules._m-article-header')
         @slot('headerType', 'generic')
@@ -19,73 +19,66 @@
         <div class="dropdown__container">
             <p class="f-secondary">Content</p>
             @component('components.atoms._dropdown')
+                @slot('prompt', 'All')
                 @slot('ariaTitle', 'Filter by content')
                 @slot('variation','m-filters m-filters__dropdown-down-arrow')
                 @slot('options', $contentOptions)
                 @slot('behavior', 'dynamicFilterDropdown')
-                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="content"')
+                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="content" data-filter-persist')
             @endcomponent
         </div>
 
         <div class="dropdown__container">
             <p class="f-secondary">Audience</p>
             @component('components.atoms._dropdown')
+                @slot('prompt', 'All')
                 @slot('ariaTitle', 'Filter by audience')
                 @slot('variation','dropdown--filter__sort m-filters m-filters__dropdown-down-arrow')
                 @slot('options', $audienceOptions)
                 @slot('behavior', 'dynamicFilterDropdown')
-                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="audience"')
+                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="audience" data-filter-persist')
             @endcomponent
         </div>
 
         <div class="dropdown__container">
             <p class="f-secondary">Topic</p>
             @component('components.atoms._dropdown')
+                @slot('prompt', 'All')
                 @slot('ariaTitle', 'Filter by topic')
                 @slot('variation','dropdown--filter__sort f-link m-filters m-filters__dropdown-down-arrow')
                 @slot('options', $topicOptions)
                 @slot('behavior', 'dynamicFilterDropdown')
-                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="topic"')
+                @slot('attribute', 'data-filter-behavior="filter" data-filter-label="topic" data-filter-persist')
             @endcomponent
         </div>
 
-            @component('components.atoms._checkbox')
-                @slot('attribute', 'data-filter-behavior="checkbox"')
-                @slot('id', null)
-                @slot('label', 'Español')
-            @endcomponent
+        <a class="checkbox f-secondary" data-behavior="dynamicFilterCheckbox" data-filter-behavior="filter" data-filter-label="locale" data-filter-value="es" data-filter-persist>
+            Espanol
+        </a>
 
     </div>
 
-    <div data-behavior='dynamicFilter'>
-        @component('components.organisms._o-grid-listing')
-            @slot('behavior', 'data-filter-target')
-            @slot('cols_small','2')
-            @slot('cols_medium','3')
-            @slot('cols_large','3')
-            @slot('cols_xlarge','3')
-            @slot('variation', 'o-grid-listing--gridlines-cols o-grid-listing--gridlines-top')
-
-            @foreach ($items as $item)
-                @component('components.molecules._m-listing----educator-resource')
-                    @slot('item', $item)
-                    @slot('hideDescription', false)
-                    @slot('imageSettings', array(
-                        'fit' => 'crop',
-                        'ratio' => '16:9',
-                        'srcset' => array(200,400,600),
-                        'sizes' => ImageHelpers::aic_gridListingImageSizes(array(
-                              'xsmall' => '1',
-                              'small' => '2',
-                              'medium' => '3',
-                              'large' => '4',
-                              'xlarge' => '4',
-                        )),
-                    ))
-                @endcomponent
-            @endforeach
-
-        @endcomponent
+    <div data-filter-target>
+        <div class="o-educator-resources__listing-grid">
+          @foreach ($items as $item)
+              @component('components.molecules._m-listing----educator-resource')
+                  @slot('item', $item)
+                  @slot('hideDescription', false)
+                  @slot('imageSettings', array(
+                      'fit' => 'crop',
+                      'ratio' => '16:9',
+                      'srcset' => array(200,400,600),
+                      'sizes' => ImageHelpers::aic_gridListingImageSizes(array(
+                            'xsmall' => '1',
+                            'small' => '2',
+                            'medium' => '3',
+                            'large' => '4',
+                            'xlarge' => '4',
+                      )),
+                  ))
+              @endcomponent
+          @endforeach
+        </div>
     </div>
 
 
