f47ac5954441d8181f1cff21d6281333ed74aa2b
Zach Garwood <iam@zachgarwood.com>
2025-07-10 16:53:50 -0500

    Write tests for StringHelpers::getLastWord() method [WEB-3155]

---
+++ tests/Unit/StringHelpersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Helpers\StringHelpers;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class StringHelpersTest extends BaseTestCase
+{
+    #[DataProvider('htmlEntities')]
+    public function test_getLastWord_finds_last_html_entity(string $htmlEntity): void
+    {
+        [$beforeLastWord, $lastWord] = StringHelpers::getLastWord('Text before HTML entity ' . $htmlEntity . ' ');
+        $this->assertEquals('Text before HTML entity ', $beforeLastWord);
+        $this->assertEquals($htmlEntity, $lastWord);
+    }
+
+    public function test_getLastWord_finds_last_tag(): void
+    {
+        [$beforeLastWord, $lastWord] = StringHelpers::getLastWord('Text not in a tag <em>Text in ending tag</em> ');
+        $this->assertEquals('Text not in a tag ', $beforeLastWord);
+        $this->assertEquals('<em>Text in ending tag</em>', $lastWord);
+    }
+
+    public function test_getLastWord_finds_non_html_non_space_character(): void
+    {
+        [$beforeLastWord, $lastWord] = StringHelpers::getLastWord('Text ending with a non-html, non-space character. ');
+        $this->assertEquals('Text ending with a non-html, non-space character', $beforeLastWord);
+        $this->assertEquals('.', $lastWord);
+    }
+
+    public static function htmlEntities(): array
+    {
+        return array_map(
+            fn ($htmlEntity) => [$htmlEntity],
+            get_html_translation_table(HTML_ENTITIES),
+        );
+    }
+}
