c579281eb60ad988c195a1545f8c80abef71c232
nikhil trivedi <ntrivedi@artic.edu>
2025-07-17 13:11:38 -0500

    Add check for controller not being set on 404s [WEB-3168]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@ $print = app('printservice')->isPrintMode();
 $pClass = 'App\Http\Controllers\GenericPagesController';
 if (env('APP_ENV') != 'testing') {
     $action = request()->route()?->getAction();
-    if (is_array($action) && $action['controller']) {
+    if (is_array($action) && isset($action['controller'])) {
         $pClass = $action['controller'];
     }
 }
