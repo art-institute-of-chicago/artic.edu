67f816c703259176e678a026b363d9571f0bcf10
nikhil trivedi <ntrivedi@artic.edu>
2025-07-22 12:16:11 -0500

    Only return the hours record active today in the API [WEB-416]

--- app/Http/Controllers/API/HoursController.php
+++ app/Http/Controllers/API/HoursController.php
@@ -6,4 +6,15 @@ class HoursController extends BaseController
 {
     protected $model = \App\Models\Hour::class;
     protected $transformer = \App\Http\Transformers\HourTransformer::class;
+
+    /**
+     * Only include currently active hours
+     *
+     * @param int $limit
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    protected function paginate($limit)
+    {
+        return ($this->model)::today()->limit(1)->paginate($limit);
+    }
 }
