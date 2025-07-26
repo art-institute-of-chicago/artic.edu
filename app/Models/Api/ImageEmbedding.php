e23423756628c3cb1acf718c9f489d5a29aff97a
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-07-25 22:58:53 -0500

    Add TODO's for weights [LAB-24]

--- app/Models/Api/ImageEmbedding.php
+++ app/Models/Api/ImageEmbedding.php
@@ -5,6 +5,7 @@ namespace App\Models\Api;
 use Illuminate\Database\Eloquent\Model;
 use Pgvector\Laravel\Vector;
 
+// TODO: Utilize Embedding weights
 // use ImageEmbeddingWeight;
 
 class ImageEmbedding extends Model
@@ -43,6 +44,8 @@ class ImageEmbedding extends Model
     {
         return $this->morphTo('artwork', 'model_name', 'model_id');
     }
+
+    // TODO: Create Embedding weight models
 
     /**
      * Get the weights for the image embedding.
