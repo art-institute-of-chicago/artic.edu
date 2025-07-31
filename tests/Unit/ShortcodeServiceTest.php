8ba21e1000907e653ff5b45c71d74fe73aaad404
Zach Garwood <iam@zachgarwood.com>
2025-07-31 14:10:16 -0500

    Rewrite ShortcodeService tests to rest method
    ...instead of regex directly

--- tests/Unit/ShortcodeServiceTest.php
+++ tests/Unit/ShortcodeServiceTest.php
@@ -7,73 +7,48 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 class ShortcodeServiceTest extends BaseTestCase
 {
-    // Implementing: https://regex101.com/r/5dOPYU/1
-    public function test_REF_REGEXP(): void
+    public function test_parse_ref(): void
     {
-        $shortcodedHtml = <<<'HTML'
+        [$parsedRef] = ShortcodeService::parse_ref('<p>[ref]text[/ref]</p>');
+        $this->assertArrayHasKey('shortcode', $parsedRef, 'The shortcode is a [tag]...[/tag] with content');
+        $this->assertArrayHasKey('name', $parsedRef, 'The tag has a name');
+        $this->assertArrayHasKey('content', $parsedRef, 'There is content inside the tag');
+    }
+
+    // Implementing: https://regex101.com/r/5dOPYU/1
+    public function test_parse_ref_with_multiple_tags(): void
+    {
+        $htmlWithRefs = <<<'HTML'
             <p>Let&#8217;s give him a friend too.[ref]FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.[/ref] Everybody needs a friend. From all of us here, I want to wish you happy painting and God bless, my friends. Everybody&#8217;s different. Trees are different.[ref]FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.[/ref] Let them all be individuals.</p><p>That&#8217;s a son of a gun of a cloud. There comes a nice little fluffer. I get carried away with this brush cleaning.</p>
         HTML;
-        $matches = array();
-        preg_match_all(ShortcodeService::REF_REGEXP, $shortcodedHtml, $matches, PREG_SET_ORDER);
 
+        $parsedRefs = ShortcodeService::parse_ref($htmlWithRefs);
+        $this->assertCount(2, $parsedRefs);
 
-        $this->assertRefRegexpMatches(
-            [
-                0 => "[ref]FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.[/ref]",
-                1 => "[ref]FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.[/ref]",
-                2 => "ref",
-                3 => "FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.",
-            ],
-            $matches[0],
-        );
-
-        $this->assertRefRegexpMatches(
-            [
-                0 => "[ref]FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.[/ref]",
-                1 => "[ref]FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.[/ref]",
-                2 => "ref",
-                3 => "FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.",
-            ],
-            $matches[1],
-        );
-    }
-
-    public function assertRefRegexpMatches(array $expected, array $matches): void
-    {
-        foreach (array_keys($expected) as $index) {
-            $this->assertArrayHasKey($index, $matches, "Match $index exists");
-            $this->assertRefRegexpMatchHasGroups($matches);
-        }
-
+        [$firstRef, $secondRef] = $parsedRefs;
         $this->assertEquals(
-            $expected[0],
-            $matches['shortcode'],
-            'The full match is the shortcode',
+            '[ref]FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.[/ref]',
+            $firstRef['shortcode'],
         );
-
         $this->assertEquals(
-            $expected[1],
-            $matches['shortcode'],
-            'The first capture group is a [ref]...[/ref] tag',
+            'ref',
+            $firstRef['name'],
         );
-
         $this->assertEquals(
-            $expected[2],
-            $matches['name'],
-            'The second capture group is the name of the [ref] tag',
+            'FPO Franklin Rosemont, “Manifesto on the Position and Direction of the Surrealist Movement in the United States,” in&nbsp;<em>Arsenal: Surrealist Subversion</em>, vol. 1, edited by Franklin Rosemont (Chicago: Black Swan Press: 1970), 13.',
+            $firstRef['content'],
         );
-
         $this->assertEquals(
-            $matches[3],
-            $matches['content'],
-            'The third capture group is the content inside the tag',
+            '[ref]FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.[/ref]',
+            $secondRef['shortcode'],
         );
-    }
-
-    public function assertRefRegexpMatchHasGroups(array $match): void
-    {
-        foreach (['shortcode', 'name', 'content'] as $group) {
-            $this->assertArrayHasKey($group, $match, "Match group $group exists");
-        }
+        $this->assertEquals(
+            'ref',
+            $secondRef['name'],
+        );
+        $this->assertEquals(
+            'FPO Sometimes titled&nbsp;<em>The Secret Voyage (with portrait of Dori and Pancho) (A viagem secreta [com retrato de Dori e Pancho])</em>, my co-curators and I saw this painting in Guedes’s home in Sintra, Portugal. It was not included in&nbsp;<em>Malangatana: Mozambique Modern</em>&nbsp;due to the conservation that would have been required.',
+            $secondRef['content'],
+        );
     }
 }
