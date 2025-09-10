6a38344cd750774d32f6475b67d49326f4f9fcf7
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-09-10 11:46:36 -0500

    Add block ids to ranged_accordions [WEB-3201]

--- resources/views/site/blocks/ranged_accordion.blade.php
+++ resources/views/site/blocks/ranged_accordion.blade.php
@@ -4,14 +4,14 @@
 @endphp
 
 <div class="o-accordion{{ (isset($variation)) ? ' '.$variation : '' }} o-ranged-accordion" data-behavior="rangedAccordion">
-    <h3><button id="accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-module-title-2' }}" tabindex="0">
+    <h3><button id="accordion_{{ StringHelpers::getUtf8Slug($title.'-'.$block->id) }}" class="o-accordion__trigger {{ $titleFont ?? 'f-module-title-2' }}" tabindex="0">
         {!! $title !!}
         <span class="o-accordion__trigger-icon">
             <svg class="icon--plus"><use xlink:href="#icon--plus" /></svg>
             <svg class="icon--minus"><use xlink:href="#icon--minus" /></svg>
         </span>
     </button></h3>
-    <div id="panel_accordion_{{ StringHelpers::getUtf8Slug($title) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($title)}}">
+    <div id="panel_accordion_{{ StringHelpers::getUtf8Slug($title.'-'.$block->id) }}" class="o-accordion__panel" aria-labelledby="{{ StringHelpers::getUtf8Slug($title)}}">
         <div class="o-accordion__panel-content o-blocks o-blocks--with-sidebar">
             {!! $renderData->renderChildren('accordion_items') !!}
         </div>
