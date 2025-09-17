39a77d27eedb16d4012877966ae65a96ae82f28a
Zach Garwood <iam@zachgarwood.com>
2025-09-17 09:19:38 -0500

    Create command to import YouTube data [WEB-3179]

---
+++ app/Models/Playlist.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Playlist extends AbstractModel
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'youtube_id',
+    ];
+
+    protected $casts = [
+        'published' => 'boolean',
+    ];
+
+    public $attributes = [
+        'published' => false,
+    ];
+
+    public function videos()
+    {
+        return $this->belongsToMany(Video::class, 'playlists_videos')
+            ->withTimestamps()
+            ->withPivot('position')
+            ->orderByPivot('position');
+    }
+}
