e3d698b75ee2eafe819c05141998468a3466e528
Zach Garwood <iam@zachgarwood.com>
2025-10-06 06:49:00 -0500

    Create functionality to download caption files [WEB-3207]

--- app/Http/Controllers/Twill/VideoCaptionController.php
+++ app/Http/Controllers/Twill/VideoCaptionController.php
@@ -60,13 +60,6 @@ class VideoCaptionController extends BaseController
                 ->optional()
                 ->hide()
         );
-        $columns->add(
-            Text::make()
-                ->field('youtube_id')
-                ->title('YouTube ID')
-                ->optional()
-                ->hide()
-        );
 
         return $columns;
     }
