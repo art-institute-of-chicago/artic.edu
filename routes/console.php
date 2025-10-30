09a7c1ea1c57eec5bedb315f7ae3b43f661c5181
Zach Garwood <iam@zachgarwood.com>
2025-10-30 16:48:46 -0500

    Add scheduled tasks for syncing youtube data [WEB-3207]

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 Schedule::command('cache:prune-stale-tags')->hourly();
 Schedule::command('sitemap:generate')->twiceDaily();
@@ -15,3 +9,7 @@ Schedule::command('update:cdn-ips')->hourly();
 Schedule::command('fix:galleries')->everyMinute();
 Schedule::command('send:confirmations')->everyTwoMinutes()->withoutOverlapping();
 Schedule::command('exhibitions:featured')->dailyAt('00:00');
+// TODO: Remove after getting caught up with the YouTube api
+Schedule::command('youtube:captions', ['--downloads-only'])->daily();
+Schedule::command('youtube:videos-and-playlists')->hourly();
+Schedule::command('youtube:captions')->hourly();
