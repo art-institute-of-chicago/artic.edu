68759d9e7b0c07e7b6598e56f74182c33f7bfa2e
Zach Garwood <iam@zachgarwood.com>
2025-10-30 17:04:44 -0500

    Re-include `php artisan inspire` console command

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
 
 Schedule::command('cache:prune-stale-tags')->hourly();
 Schedule::command('sitemap:generate')->twiceDaily();
