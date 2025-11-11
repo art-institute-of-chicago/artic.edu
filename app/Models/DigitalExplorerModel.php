ae390c518572c29fd428bddfbebad3e98d7ff4e3
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2025-11-11 14:23:11 -0600

    Build website views for Digital Explorers [IP-243] [IP-244] [IP-245] [IP-246] [IP-247]

---
+++ app/Models/DigitalExplorerModel.php
@@ -0,0 +1,66 @@
+<?php
+// app/Models/DigitalExplorerModel.php
+
+namespace App\Models;
+
+use A17\Twill\Models\Behaviors\HasFiles;
+
+class DigitalExplorerModel extends AbstractModel
+{
+    use HasFiles;
+
+    protected $fillable = [
+        'digital_explorer_id',
+        'position',
+        'settings',
+        'model_settings',
+    ];
+
+    protected $casts = [
+        'settings' => 'array',
+        'model_settings' => 'array',
+    ];
+
+    public $filesParams = [
+        'modelFile' => [
+            'role' => 'modelFile',
+        ]
+    ];
+
+    public function digitalExplorer()
+    {
+        return $this->belongsTo(DigitalExplorer::class);
+    }
+
+    public function toRepeaterArray(): array
+    {
+        $data = [
+            'id' => $this->id,
+            'position' => $this->position,
+        ];
+
+        // Flatten settings
+        if ($this->settings && is_array($this->settings)) {
+            foreach ($this->settings as $key => $value) {
+                $data["settings.{$key}"] = $value;
+            }
+        }
+
+        // Handle model_settings array (for multi-select)
+        if ($this->model_settings && is_array($this->model_settings)) {
+            $data['modelSettings'] = $this->model_settings;
+        }
+
+        // Handle files
+        if (method_exists($this, 'filesParams')) {
+            foreach ($this->filesParams as $role => $params) {
+                $data['files'][$role] = $this->getRelated('files')
+                    ->where('role', $role)
+                    ->pluck('id')
+                    ->toArray();
+            }
+        }
+
+        return $data;
+    }
+}
--- app/Repositories/ModuleRepository.php
+++ app/Repositories/ModuleRepository.php
@@ -58,10 +58,12 @@ abstract class ModuleRepository extends BaseModuleRepository
         // Remove trailing newlines from block content (for `HasBlocks` only)
         if (isset($fields['blocks'])) {
             foreach ($fields['blocks'] as $blockKey => $block) {
-                foreach ($block['content'] as $contentKey => $content) {
+                if (isset($block['content']) && is_array($block['content'])) {
+                  foreach ($block['content'] as $contentKey => $content) {
                     $fields['blocks'][$blockKey]['content'][$contentKey] =
                         StringHelpers::rightTrim($content, '<p><br class="softbreak"></p>');
-                }
+                  }
+              }
             }
         }
 
