d928b7ee6948419602405ac8971569112e2cb77a
nikhil trivedi <ntrivedi@artic.edu>
2025-11-21 13:36:49 -0600

    Update cookie handler function syntax [WEB-3205]

--- frontend/js/functions/core/cookieHandler.js
+++ frontend/js/functions/core/cookieHandler.js
@@ -1,41 +1,49 @@
-const cookieHandler = function(container) {
-  // Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-create
-  // Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-delete
-  // Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-read
+// Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-create
+// Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-delete
+// Doc: https://code.area17.com/a17/a17-helpers/wikis/cookieHandler-read
 
-  function cookieCreate(name, value, days) {
-    var expires = '';
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      expires = '; expires='+date.toGMTString();
-    }
-    document.cookie = name+'='+value+expires+'; path=/; '+(A17.env !== 'local' ? 'Secure; ' : '')+'HttpOnly';
+function cookieCreate(name, value, days) {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    expires = '; expires='+date.toGMTString();
   }
+  document.cookie = name+'='+value+expires+'; path=/; '+(A17.env !== 'local' ? 'Secure; ' : '')+'HttpOnly';
+}
 
-  function cookieDelete(name) {
-    if (name) {
-      this.create(name, '', -1);
-    }
+function cookieDelete(name) {
+  if (name) {
+    cookieCreate(name, '', -1);
   }
+}
 
-  function cookieRead(name) {
-    if (name) {
-      var nameEQ = name + '=';
-      var ca = document.cookie.split(';');
-      for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') {
-          c = c.substring(1, c.length);
-        }
-        if (c.indexOf(nameEQ) === 0) {
-          return c.substring(nameEQ.length, c.length);
-        }
+function cookieRead(name) {
+  if (name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1, c.length);
       }
-      return null;
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length, c.length);
+      }
     }
     return null;
   }
+  return null;
+}
+
+const cookieHandler = {
+  cookieCreate,
+  cookieDelete,
+  cookieRead,
+  // convenience aliases used in some codepaths
+  create: cookieCreate,
+  delete: cookieDelete,
+  read: cookieRead,
 };
 
 export default cookieHandler;
