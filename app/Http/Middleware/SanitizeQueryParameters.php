19db1535cd4d4db0bdc26165113006d4dfda3ba1
nikhil trivedi <ntrivedi@artic.edu>
2025-11-25 16:51:51 -0600

    Move sanitize query logic out of the controllers and into a dedicated middleware [WEB-3206]

---
+++ app/Http/Middleware/SanitizeQueryParameters.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Sanitize incoming query params: only allow valid search params to be returned in the response.
+ * If disallowed params are present, redirect to the same URL with only allowed keys.
+ */
+class SanitizeQueryParameters
+{
+    const ROUTE_PARAM_KEY = 'allowed_query_params';
+
+    protected $allowedQueryParameters = [
+        'q',
+        // Checking to see if these are used by marketing
+        // 'utm',
+        // 'utm_campaign',
+        // 'utm_content',
+        // 'utm_medium',
+        // 'utm_source',
+        // 'utm_term',
+    ];
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $allowedParams = $request->route()->getAction(self::ROUTE_PARAM_KEY) ?? $this->allowedQueryParameters;
+
+        $currentQuery = $request->query();
+        $sanitizedQuery = array_intersect_key($currentQuery, array_flip($allowedParams));
+
+        if ($sanitizedQuery !== $currentQuery) {
+            return redirect()->to($request->url() . (empty($sanitizedQuery) ? '' : ('?' . http_build_query($sanitizedQuery))));
+        }
+
+        return $next($request);
+    }
+}
