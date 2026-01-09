e034d694d3fff03248aa23490177577a28133e1f
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-01-09 10:43:11 -0600

    Updates to digital explorer website setup [IP-246]

--- resources/views/twill/digitalExplorers/form.blade.php
+++ resources/views/twill/digitalExplorers/form.blade.php
@@ -1,141 +1,125 @@
 @extends('twill::layouts.form')
 
 @section('contentFields')
-  <x-twill::medias
-    name='listing'
-    label='Listing Image'
-    note='Minimum image width 2000px'
-  />
+    <x-twill::medias
+        name="listing"
+        label="Listing Image"
+        note="Minimum image width 2000px"
+    />
 
-  <x-twill::select
-    name='type'
-    label='Display Type'
-    :options="[
-      [
-        'value' => '2d',
-        'label' => '2D',
-      ],
-      [
-        'value' => '3d',
-        'label' => '3D',
-      ]
-    ]"
-  />
-
-  <x-twill::multi-select
-    name='settings.sceneSettings'
-    label='Scene Settings'
-    :options="[
-      [
-        'value' => 'antialiasing',
-        'label' => 'Antialiasing'
-      ],
-      [
-        'value' => 'shadows',
-        'label' => 'Shadows'
-      ],
-    ]"
-  />
-
-  <x-twill::formColumns>
-    <x-slot:left>
-      <x-twill::select
-        name='settings.toneMapping'
-        label='Tone Mapping'
+    <x-twill::select
+        name="type"
+        label="Display Type"
         :options="[
-          [
-            'value' => 'None',
-            'label' => 'None'
-          ],
-          [
-            'value' => 'ACESFilmicToneMapping',
-            'label' => 'ACESFilmicToneMapping'
-          ],
-          [
-            'value' => 'LinearToneMapping',
-            'label' => 'LinearToneMapping'
-          ],
-          [
-            'value' => 'ReinhardToneMapping',
-            'label' => 'ReinhardToneMapping'
-          ],
-          [
-            'value' => 'CineonToneMapping',
-            'label' => 'CineonToneMapping'
-          ],
+            ['value' => '2d', 'label' => '2D'],
+            ['value' => '3d', 'label' => '3D']
         ]"
-      />
-    </x-slot>
-    <x-slot:right>
-      <x-twill::select
-        name='settings.colorSpace'
-        label='Color Space'
+    />
+
+    <x-twill::multi-select
+        name="settings.sceneSettings"
+        label="Scene Settings"
         :options="[
-          [
-            'value' => 'SRGB',
-            'label' => 'SRGB'
-          ],
-          [
-            'value' => 'Linear',
-            'label' => 'Linear'
-          ],
+            ['value' => 'antialiasing', 'label' => 'Antialiasing'],
+            ['value' => 'shadows', 'label' => 'Shadows']
         ]"
-      />
-    </x-slot>
-  </x-twill::formColumns>
+    />
+
+    <x-twill::formColumns>
+        <x-slot name="left">
+            <x-twill::select
+                name="settings.toneMapping"
+                label="Tone Mapping"
+                :options="[
+                    ['value' => 'None', 'label' => 'None'],
+                    ['value' => 'ACESFilmicToneMapping', 'label' => 'ACESFilmicToneMapping'],
+                    ['value' => 'LinearToneMapping', 'label' => 'LinearToneMapping'],
+                    ['value' => 'ReinhardToneMapping', 'label' => 'ReinhardToneMapping'],
+                    ['value' => 'CineonToneMapping', 'label' => 'CineonToneMapping']
+                ]"
+            />
+
+            <x-twill::checkbox
+              name="settings.debug"
+              label="Show debugger?"
+            />
+        </x-slot>
+
+        <x-slot name="right">
+            <x-twill::select
+                name="settings.colorSpace"
+                label="Color Space"
+                :options="[
+                    ['value' => 'SRGB', 'label' => 'SRGB'],
+                    ['value' => 'Linear', 'label' => 'Linear']
+                ]"
+            />
+        </x-slot>
+    </x-twill::formColumns>
 @stop
 
 @php
-    $model = BlockHelpers::getBlocksForEditor([
-      'explorer_model'
-    ]);
-
-    $annotation = BlockHelpers::getBlocksForEditor([
-      'explorer_annotation'
-    ]);
-
-    $light = BlockHelpers::getBlocksForEditor([
-      'explorer_light'
-    ]);
+    $model = BlockHelpers::getBlocksForEditor(['explorer_model']);
+    $annotation = BlockHelpers::getBlocksForEditor(['explorer_annotation']);
+    $light = BlockHelpers::getBlocksForEditor(['explorer_light']);
 @endphp
-<br/>
+
 @section('fieldsets')
-  <x-twill::formFieldset title="Models" id="models">
+    <x-twill::formFieldset title="Title Slide" id="title-slide">
 
-    <p>Models can have nested Lights and Annotations which stay attached<br/><br/>The first model in this list will be considered the focus model</p>
+        <x-twill::medias
+            name="title_media"
+            label="Title Media"
+        />
 
-    <x-twill::block-editor
-      name='model_blocks'
-      :blocks='$model'
-      withoutSeparator='true'
-    />
-  </x-twill::formFieldset>
+        <x-twill::wysiwyg
+            name='title_display'
+            label='Title Text'
+            note='Text to display next to the annotation'
+        />
 
-  <x-twill::formFieldset title="Annotations" id="annotations">
+    </x-twill::formFieldset>
 
-    <p>For standalone annotation points, not attached to a model</p>
+    <x-twill::formFieldset title="Models" id="models">
+        <p>Models can have nested Lights and Annotations which stay attached<br/><br/>The first model in this list will be the focused model</p>
+        <x-twill::block-editor
+            name="model_blocks"
+            :blocks="$model"
+            withoutSeparator="true"
+        />
+    </x-twill::formFieldset>
 
-    <x-twill::block-editor
-      name='annotation_blocks'
-      :blocks='$annotation'
-      withoutSeparator='true'
-    />
-  </x-twill::formFieldset>
+    <x-twill::formFieldset title="Annotations" id="annotations">
+        <p>For standalone annotation points, not attached to a model</p>
+        <x-twill::block-editor
+            name="annotation_blocks"
+            :blocks="$annotation"
+            withoutSeparator="true"
+        />
+    </x-twill::formFieldset>
 
-  <x-twill::formFieldset title="Lighting" id="lights">
+    <x-twill::formFieldset title="Lighting" id="lights">
+        <p>For standalone lighting elements, not attached to a model</p>
+        <x-twill::block-editor
+            name="lighting_blocks"
+            :blocks="$light"
+            withoutSeparator="true"
+        />
+    </x-twill::formFieldset>
 
-    <p>For standalone lighting elements, not attached to a model</p>
-
-    <x-twill::block-editor
-      name='lighting_blocks'
-      :blocks='$light'
-      withoutSeparator='true'
-    />
-  </x-twill::formFieldset>
-
-    <x-twill::formFieldset title="Camera" id="camera">
-
-    <p>By default the camera will orbit the first model. If you wish to override you must set camera values manually.</p>
-
-  </x-twill::formFieldset>
+    <x-twill::formFieldset title="Camera Offset" id="camera">
+        <p>By default the camera will orbit the first model. These settings will be used for the initial offset</p>
+        <x-twill::input
+            name="settings.position"
+            label="Position"
+        />
+        <x-twill::input
+            name="settings.rotation"
+            label="Rotation"
+        />
+        <x-twill::input
+            name="settings.scale"
+            label="Scale"
+        />
+    </x-twill::formFieldset>
 @stop
