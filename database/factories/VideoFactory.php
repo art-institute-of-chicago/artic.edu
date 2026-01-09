85bf1dabf4ff3bbcc266c8f94bb4b29ecf660963
Zach Garwood <iam@zachgarwood.com>
2026-01-09 07:49:51 -0600

    Include Video model factory [WEB-3214]

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -7,40 +7,12 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VideoFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Video::class;
 
-    /**
-     * Define the model's default state.
-     */
     public function definition(): array
     {
         return [
-            'title' => $this->faker->sentence(3),
-            'description' => $this->faker->paragraph(),
-            'published' => true,
-            'is_listed' => true,
-            'is_short' => false,
-            'youtube_id' => $this->faker->regexify('[a-zA-Z0-9_-]{11}'),
-            'privacy' => 'public',
+            'youtube_id' => fake()->bothify('???????????'),
         ];
-    }
-
-    public function withYoutubeId(string $youtubeId): self
-    {
-        return $this->state(fn (array $attributes) => [
-            'youtube_id' => $youtubeId,
-        ]);
-    }
-
-    public function unpublished(): self
-    {
-        return $this->state(fn (array $attributes) => [
-            'published' => false,
-        ]);
     }
 }
