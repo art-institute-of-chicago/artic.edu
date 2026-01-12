f1a7192cd03830eaf58cb0143fdf409c31c97e68
Zach Garwood <iam@zachgarwood.com>
2026-01-12 16:37:05 -0600

    Update video detail page style and layout [WEB-3214]

--- resources/views/site/videoDetail.blade.php
+++ resources/views/site/videoDetail.blade.php
@@ -4,22 +4,10 @@
 
     <article class="o-article o-article--video">
 
-        <div class="o-article__primary-actions o-article__primary-actions--video">
-            @component('components.atoms._title')
-                @slot('tag','p')
-                @slot('font', 'f-tag-2')
-                Video
-            @endcomponent
-
-            @component('components.molecules._m-article-actions')
-                @slot('articleType', 'video')
-            @endcomponent
-        </div>
-
         @component('components.molecules._m-article-header')
+            @slot('headerType', 'video')
             @slot('title', $item->present()->title)
             @slot('title_display', $item->present()->title_display)
-            @slot('formattedDate', $item->present()->date)
         @endcomponent
 
         <div class="o-article__body o-blocks">
@@ -38,47 +26,36 @@
                     'fullscreen' => false,
                 ])
             @endcomponent
+
+            @if ($item->heading)
+                <div class="o-article__intro">
+                @component('components.blocks._text')
+                    @slot('font', 'f-deck')
+                    @slot('tag', 'div')
+                    {!! $item->present()->heading !!}
+                @endcomponent
+                </div>
+            @endif
+
+            <x-tabbed-details :tab-count="2">
+                <x-slot name="tab1" title="Info">
+                    <div class="o-article__body o-blocks">
+                        {!! $item->renderBlocks(data: [
+                            'pageTitle' => $item->title
+                        ]) !!}
+                    </div>
+                </x-slot>
+                <x-slot name="tab2" title="Transcript">
+                    @if($transcript)
+                        <h3>Transcript</h3>
+                        {!! $transcript !!}
+                    @else
+                        <p>Sorry, the transcript for this video is not yet available</p>
+                    @endif
+                </x-slot>
+            </x-tabbed-details>
         </div>
-
-        @if ($item->heading)
-            <div class="o-article__intro">
-              @component('components.blocks._text')
-                  @slot('font', 'f-deck')
-                  @slot('tag', 'div')
-                  {!! $item->present()->heading !!}
-              @endcomponent
-            </div>
-        @endif
-
-        <div class="o-article__body o-blocks">
-            {!! $item->renderBlocks(data: [
-                'pageTitle' => $item->title
-            ]) !!}
-        </div>
-
     </article>
-
-    @if ($item->topics && count($item->topics) > 0)
-        @component('components.atoms._hr')
-        @endcomponent
-        @component('components.blocks._text')
-            @slot('font', 'f-subheading-1')
-            @slot('tag', 'h4')
-            @slot('id', 'h-topics')
-            Topics
-        @endcomponent
-        <ul class="m-inline-list" aria-labelledby="h-topics">
-        @foreach ($item->topics as $category)
-            <li class="m-inline-list__item">
-                @if (!empty($category['id']))
-                    <a class="tag f-tag" href="{{ route('articles', ['category' => $category['id']]) }}">{{ $category['name'] }}</a>
-                @else
-                    <span class="tag f-tag">{{ $category['name'] }}</span>
-                @endif
-            </li>
-        @endforeach
-        </ul>
-    @endif
 
     @if ($relatedVideos->count() > 0)
         <div class="o-article__related o-article__related--videos">
