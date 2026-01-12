ff7775f83cac62337004d96e4e31996da8ec3f8d
Zach Garwood <iam@zachgarwood.com>
2026-01-12 10:56:25 -0600

    Add documentation for tabbed-details component [WEB-3214]

--- resources/views/components/organisms/_o-tabbed-details.blade.php
+++ resources/views/components/organisms/_o-tabbed-details.blade.php
@@ -1,3 +1,14 @@
+{{--
+Usage:
+    <x-tabbed-details :tab-count="2">
+        <x-slot name="tab1" title="Render Arbitrary Markup">
+            <span>Hello!</span>
+        </x-slot>
+        <x-slot name="tab2" title="Do Twill Stuff">
+            <div>{!! $item->renderBlocks() !!}</div>
+        </x-slot>
+    </x-tabbed-details>
+--}}
 <div class="o-tabbed-details" style="--tab-count: {{ $tabCount }}">
     @for ($tabIndex = 1; $tabIndex <= $tabCount; $tabIndex++)
         <details name="tabbed-details" style="--tab: {{ $tabIndex }}" @if ($tabIndex === 1) open @endif>
