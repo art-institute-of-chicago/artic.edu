638f72087d5f482924e89106a027e8fa3fb21ec6
nikhil trivedi <ntrivedi@artic.edu>
2026-01-29 15:27:59 -0600

    Build basic scaffolding for video archive [WEB-3275]

---
+++ resources/views/site/videos.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+@section('content')
+
+@php
+    if (request()->query('category')) {
+        if (in_array(request()->query('category'), ['videos', 'shorts', 'playlists'], true)) {
+            $currentCategory = Str::ucfirst(request()->query('category'));
+        }
+        else {
+            $currentCategory = \App\Models\VideoCategory::where('id', request()->query('category'))->pluck('name')->first();
+        }
+    }
+
+    $currentDuration = request()->query('duration') ? \App\Models\Video::$durations[request()->query('duration')] : null;
+@endphp
+
+<section class="o-articles">
+
+    @component('components.molecules._m-header-block')
+      Explore Videos
+    @endcomponent
+
+    @component('components.molecules._m-links-bar')
+        @slot('variation','m-links-bar--articles')
+        @slot('primaryHtml')
+            <li class="m-links-bar__item m-links-bar__item--primary">
+                @component('components.atoms._dropdown')
+                  @slot('prompt', isset($currentCategory) ? $currentCategory : 'All Categories')
+                  @slot('ariaTitle', 'Filter by')
+                  @slot('variation','dropdown--filter f-link')
+                  @slot('font', null)
+                  @slot('options', $categories)
+                @endcomponent
+            </li>
+            <li class="m-links-bar__item m-links-bar__item--primary">
+                @component('components.atoms._dropdown')
+                  @slot('prompt', isset($currentDuration) ? $currentDuration : 'Any Duration')
+                  @slot('ariaTitle', 'Filter by')
+                  @slot('variation','dropdown--filter f-link')
+                  @slot('font', null)
+                  @slot('options', $durations)
+                @endcomponent
+            </li>
+            @if (isset($currentCategory) || isset($currentDuration))
+              <li class="m-links-bar__item m-links-bar__item--primary">
+                  <a href="{{ route('videos.archive') }}" class="f-link">Clear all</a>
+              </li>
+            @endif
+          @endslot
+          @slot('secondaryHtml')
+              <p class="f-secondary">
+                {{ $videosCount }} items
+              </p>
+          @endslot
+    @endcomponent
+
+  @component('components.atoms._hr')
+  @endcomponent
+
+  @if ($videos->isEmpty())
+    <div class="m-no-results">
+      @component('components.atoms._title')
+          @slot('tag','h2')
+          @slot('font', 'f-list-3')
+          {!! $text ?? 'Sorry, we couldn’t find any results matching your criteria.' !!}
+      @endcomponent
+    </div>
+  @else
+    @component('components.organisms._o-grid-listing')
+        @slot('variation', 'o-grid-listing--gridlines-cols o-grid-listing--gridlines-top')
+        @slot('cols_small','2')
+        @slot('cols_medium','3')
+        @slot('cols_large','4')
+        @slot('cols_xlarge','4')
+        @foreach ($videos as $item)
+            @component('components.molecules._m-listing----media')
+                @slot('item', $item)
+                @slot('imageSettings', array(
+                'fit' => 'crop',
+                'ratio' => '16:9',
+                'srcset' => array(200,400,600,1000),
+                'sizes' => ImageHelpers::aic_gridListingImageSizes(array(
+                        'xsmall' => '1',
+                        'small' => '2',
+                        'medium' => '3',
+                        'large' => '4',
+                        'xlarge' => '4',
+                )),
+            ))
+            @endcomponent
+        @endforeach
+    @endcomponent
+  @endif
+
+  @php
+    $GLOBALS['paginationAjaxScrollTarget'] = 'listing';
+  @endphp
+  {!! $videos->links() !!}
+  @php
+    $GLOBALS['paginationAjaxScrollTarget'] = null;
+  @endphp
+
+</section>
+
+@endsection
