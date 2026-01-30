f437036f723c8ff8206eb6687d8a9cb56370d30e
Zach Garwood <iam@zachgarwood.com>
2026-01-29 19:39:00 -0600

    Fix missing behaviour [WEB-3262]

--- resources/views/site/shortsDetail.blade.php
+++ resources/views/site/shortsDetail.blade.php
@@ -6,7 +6,9 @@
             <div class="short-video previous-video">
                 @if ($previousItem)
                     @component('components.atoms._link')
-                        @slot('href', route('shorts.show', ['video' => $previousItem]) . '?player')
+                        @slot('href', route('shorts.show', ['video' => $previousItem]))
+                        @slot('behavior', 'getUrl')
+                        @slot('dataHref', route('shorts.show', ['video' => $previousItem]) . '?player')
                         @component('components.molecules._m-media')
                             @slot('variation', 'variation--short')
                             @slot('item', [
