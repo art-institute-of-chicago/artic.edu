a0ced81841a6338233b48d0d913bbf65438e8d00
Zach Garwood <iam@zachgarwood.com>
2026-02-05 13:36:51 -0600

    Fix typo [WEB-3213]

--- resources/views/components/molecules/_m-listing----playlist-grid-item.blade.php
+++ resources/views/components/molecules/_m-listing----playlist-grid-item.blade.php
@@ -8,7 +8,7 @@
         ->take(2)
         ->map(fn($video) => ImageHelpers::youtubeItemAsArray($video));
     $videoCount = $playlist->videos()->published()->count();
-    $label = isset($label) ? $label : $videoCount . ' ' . Str::plural('videos', $videosCount);
+    $label = isset($label) ? $label : $videoCount . ' ' . Str::plural('videos', $videoCount);
     $labelPosition = isset($labelPosition) ? $labelPosition : 'overlay';
     $title = isset($title) ? $title : $playlist->title;
     $tag = isset($tag) ? $tag : null;
