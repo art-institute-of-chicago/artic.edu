dd5adf65c9fe3d1027878c4e957ed02865375ebc
Zach Garwood <iam@zachgarwood.com>
2026-02-10 14:45:42 -0600

    Keep Transcript tab open when using timestamps [WEB-3280]
    Also, why did I make the tabbed details 1-indexed!? Gross!

--- resources/views/components/organisms/_o-tabbed-details.blade.php
+++ resources/views/components/organisms/_o-tabbed-details.blade.php
@@ -1,21 +1,24 @@
 {{--
 Usage:
-    <x-tabbed-details :tab-count="2">
-        <x-slot name="tab1" title="Render Arbitrary Markup">
+    <x-tabbed-details :tab-count="2" :open-tab-index="0">
+        <x-slot name="tab0" title="Render Arbitrary Markup">
             <span>Hello!</span>
         </x-slot>
-        <x-slot name="tab2" title="Do Twill Stuff">
+        <x-slot name="tab1" title="Do Twill Stuff">
             <div>{!! $item->renderBlocks() !!}</div>
         </x-slot>
     </x-tabbed-details>
 --}}
+@php
+    $openTabIndex = isset($openTabIndex) ? (int) $openTabIndex : 0;
+@endphp
 <div class="o-tabbed-details" style="--tab-count: {{ $tabCount }}">
-    @for ($tabIndex = 1; $tabIndex <= $tabCount; $tabIndex++)
+    @for ($tabIndex = 0; $tabIndex < $tabCount; $tabIndex++)
         <details
             name="tabbed-details"
             class="o-tabbed-details__tab"
             style="--tab: {{ $tabIndex }}"
-            @if ($tabIndex === 1) open @endif
+            @if ($tabIndex === $openTabIndex) open @endif
         >
             <summary class="o-tabbed-details__tab-title">
                 {{ ${'tab' . $tabIndex}->attributes->get('title') }}
