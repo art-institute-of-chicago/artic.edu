76ef903fa05eb374651d6efdb6ebc30d44804107
Zach Garwood <iam@zachgarwood.com>
2026-02-12 08:05:28 -0600

    Make DeepSource recommended changes [WEB-3280]

--- frontend/js/functions/core/youtubeEmbed.js
+++ frontend/js/functions/core/youtubeEmbed.js
@@ -1,30 +1,31 @@
 import { triggerCustomEvent } from '@area17/a17-helpers';
 
 const youtubeEmbed = function(iframe) {
-  let youtubePlayer;
-  let youtubePlayerTimer;
-  let playedChecks = {
+  const playedChecks = {
     25: false,
     50: false,
     75: false,
     100: false
   };
 
+  let youtubePlayer;
+  let youtubePlayerTimer;
+
   function _onYouTubePlayerPlaying() {
-    let playedPercent = Math.round(youtubePlayer.getCurrentTime()/youtubePlayer.getDuration() * 100);
-    for (let playedCheck in playedChecks) {
-      let percentComparison = (playedCheck === 100) ? 99 : playedCheck;
+    const playedPercent = Math.round(youtubePlayer.getCurrentTime()/youtubePlayer.getDuration() * 100);
+    for (const playedCheck in playedChecks) {
+      const percentComparison = (playedCheck === 100) ? 99 : playedCheck;
       if (playedPercent >= percentComparison && !playedChecks[playedCheck]) {
         playedChecks[playedCheck] = true;
         triggerCustomEvent(document, 'gtm:push', {
-          'event': playedCheck + '%',
+          'event': `${playedCheck}%`,
           'eventCategory': 'video-engagement',
         });
       }
     }
   }
 
-  function _onYouTubePlayerStateChange(event) {
+  function _onYouTubePlayerStateChange() {
     if (youtubePlayer.getPlayerState() === 1) {
       youtubePlayerTimer = setInterval(_onYouTubePlayerPlaying,250);
       triggerCustomEvent(document, 'gtm:push', {
@@ -40,7 +41,9 @@ const youtubeEmbed = function(iframe) {
       }
       try {
         clearInterval(youtubePlayerTimer);
-      } catch(err){}
+      } catch(err) {
+        // noop
+      }
     }
   }
 
@@ -61,15 +64,15 @@ const youtubeEmbed = function(iframe) {
   }
 
   if (!document.getElementById('youtubeapijs')) {
-    let youtubeScript = document.createElement('script');
+    const youtubeScript = document.createElement('script');
     youtubeScript.src = 'https://www.youtube.com/iframe_api';
     youtubeScript.id = 'youtubeapijs';
-    let firstScript = document.getElementsByTagName('script')[0];
+    const firstScript = document.getElementsByTagName('script')[0];
     firstScript.parentNode.insertBefore(youtubeScript, firstScript);
   }
 
   if (!iframe.id) {
-    iframe.id = 'youtube_' + Math.random().toString(36).substr(2, 9);
+    iframe.id = `youtube_${Math.random().toString(36).substring(2, 9)}`;
   }
 
   _initYoutubePlayer();
