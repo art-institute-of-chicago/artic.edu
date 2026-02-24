16dd4518ee91c17df1e81da6e17b1cead8c70622
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-02-24 11:46:50 -0600

    Revert bad merge [WEB-3303]

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -7,12 +7,40 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VideoFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = Video::class;
 
+    /**
+     * Define the model's default state.
+     */
     public function definition(): array
     {
         return [
-            'youtube_id' => fake()->bothify('???????????'),
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'published' => true,
+            'is_listed' => true,
+            'is_short' => false,
+            'youtube_id' => $this->faker->regexify('[a-zA-Z0-9_-]{11}'),
+            'privacy' => 'public',
         ];
+    }
+
+    public function withYoutubeId(string $youtubeId): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'youtube_id' => $youtubeId,
+        ]);
+    }
+
+    public function unpublished(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'published' => false,
+        ]);
     }
 }
