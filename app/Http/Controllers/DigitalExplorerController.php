9a3d58297692f4943b48a7fce35625d6910851e2
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-02-24 13:48:09 -0600

    Remove unused import [WEB-3303]

--- app/Http/Controllers/DigitalExplorerController.php
+++ app/Http/Controllers/DigitalExplorerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\DigitalExplorer;
 use App\Repositories\DigitalExplorerRepository;
-use Illuminate\Support\Facades\Response;
 
 class DigitalExplorerController extends FrontController
 {
