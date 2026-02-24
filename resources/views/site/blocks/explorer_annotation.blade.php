c9bb6f0f09ec2e268e9ad41f5476f72ae0de559a
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-02-24 13:29:12 -0600

    Add comment for clarification [WEB-3303]

--- resources/views/site/blocks/explorer_annotation.blade.php
+++ resources/views/site/blocks/explorer_annotation.blade.php
@@ -2,5 +2,5 @@
     /** @var \A17\Twill\Services\Blocks\RenderData $renderData */
 @endphp
 
-
+{{-- Annotations are the only things that have nested blocks so we call the blade to render the HTML --}}
 {!! $renderData->renderChildren('annotation_block_blocks') !!}
