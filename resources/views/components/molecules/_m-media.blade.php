73e86d50ad8a5171b0b93621889438c6b3b0e687
nikhil trivedi <ntrivedi@artic.edu>
2026-03-04 16:26:31 -0600

    Remove extraneaous close bracket [WEB-3282]

--- resources/views/components/molecules/_m-media.blade.php
+++ resources/views/components/molecules/_m-media.blade.php
@@ -178,7 +178,6 @@
         @if ($useContain && !($item['isArtwork'] ?? false) && ($size === 's' || $size === 'm' || $size === 'l'))
           <div class="m-media__contain--spacer" style="--aspect-ratio: {{ intval($item['media']['height'] ?? 10) / intval($item['media']['width'] ?? 16) * 100 }}%; padding-bottom: var(--aspect-ratio); width: 100%;"></div>
         @endif
-    >
         @if ($type == 'image')
             @if ($showUrlFullscreen)
                 <a href="{!! $item['urlTitle'] !!}"{!! (isset($item['gtmAttributes'])) ? ' '.$item['gtmAttributes'].'' : '' !!}>
