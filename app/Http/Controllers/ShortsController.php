6593f6fe0ef6746ab85b2afb39fb7a564ee83678
nikhil trivedi <ntrivedi@artic.edu>
2026-03-12 14:28:47 -0500

    Add embed URL to each short item as a data attribute [WEB-3301]

--- app/Http/Controllers/ShortsController.php
+++ app/Http/Controllers/ShortsController.php
@@ -57,6 +57,7 @@ class ShortsController extends FrontController
                 return [$short->id => $this->dataAttributes([
                     'embedId' => 'shorts-player-iframe',
                     'loadVideoById' => $short->youtube_id,
+                    'fullVideoUrl' => EmbedConverterFacade::sanitizeUrl($short->embedUrl, ['autoplay' => true])
                 ])];
             });
 
