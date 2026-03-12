83776de1518ca6960378032f84f501be6c021b57
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-03-12 11:31:43 -0500

    Create and use layout_block [IP-246]

---
+++ resources/views/site/blocks/layout_block.blade.php
@@ -0,0 +1,41 @@
+@php
+    /** @var \A17\Twill\Services\Blocks\RenderData $renderData */
+
+    $styles = [
+        'display' => $block->input('display'),
+        'gap' => $block->input('gap'),
+        'padding-top' => $block->input('padding_top'),
+        'padding-bottom' => $block->input('padding_bottom'),
+        'padding-left' => $block->input('padding_left'),
+        'padding-right' => $block->input('padding_right'),
+        'margin-top' => $block->input('margin_top'),
+        'margin-bottom' => $block->input('margin_bottom'),
+        'margin-left' => $block->input('margin_left'),
+        'margin-right' => $block->input('margin_right'),
+    ];
+
+    if ($block->input('display') === 'flex') {
+        $styles['flex-direction'] = $block->input('flex_direction');
+        $styles['justify-content'] = $block->input('justify_content');
+        $styles['align-items'] = $block->input('align_items');
+        $styles['flex-wrap'] = $block->input('flex_wrap') ? 'wrap' : 'nowrap';
+    }
+
+    if ($block->input('display') === 'grid' && $block->input('layout_type') !== 'default') {
+        $styles['grid-template-columns'] = $block->input('layout_type');
+    }
+
+    if ($block->input('width') && $block->input('layout_type') === 'default') {
+        $styles['width'] = $block->input('width');
+    }
+
+    $styleLine = collect($styles)->filter()->map(fn($v, $k) => "$k: $v")->implode('; ');
+@endphp
+
+<div
+    id="{{ $block->input('custom_id') }}"
+    class="{{ "o-blocks o-layout_block". $block->input('custom_class') }}"
+    style="{{ 'max-height: 100%; height: 100%; '. $styleLine }}"
+>
+    {!! $renderData->renderChildren('layout_block_blocks') !!}
+</div>
