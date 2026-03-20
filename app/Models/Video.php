4e2f480d505be426058c66131ff6c379289fe8e0
Zach Garwood <iam@zachgarwood.com>
2026-03-20 12:21:44 -0500

    Reimplement global available scope [WEB-3330]

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -238,9 +238,7 @@ class Video extends AbstractModel
     protected static function booted(): void
     {
         static::addGlobalScope('available', function (Builder $query) {
-            if (!App::environment('production')) {
-                $query->where('privacy', '<>', 'private');
-            }
+            $query->where('privacy', '<>', 'private');
         });
         // Sort videos by most recent `uploaded_at` date.
         static::addGlobalScope('byRecent', function (Builder $query) {
