554cc445226f8143dc75ba2796c32c035e230d36
Zach Garwood <iam@zachgarwood.com>
2026-03-20 22:28:41 -0500

    Remove unnecessary privacy choice [WEB-3330]

--- resources/views/twill/videos/create.blade.php
+++ resources/views/twill/videos/create.blade.php
@@ -7,19 +7,3 @@
     name='youtube_id'
     label='YouTube ID'
 />
-<x-twill::radios
-    name='privacy'
-    label='Privacy'
-    default='public'
-    :inline='true'
-    :options="[
-        [
-            'value' => 'public',
-            'label' => 'Public'
-        ],
-        [
-            'value' => 'unlisted',
-            'label' => 'Unlisted'
-        ]
-    ]"
-/>
