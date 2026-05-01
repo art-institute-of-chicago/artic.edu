f185e3ceb4c11490c3228299399f4318f3adce79
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-05-01 14:34:23 -0500

    Create rotate arrows overlay component [IP-249]

---
+++ resources/views/components/molecules/_m-rotate-arrows.blade.php
@@ -0,0 +1,9 @@
+<div class="m-rotate-arrows">
+    <div class="m-rotate-arrows__arrow m-rotate-arrows__arrow--left">
+        <svg aria-hidden="true" class="icon--arrow--24"><use xlink:href="#icon--arrow" /></svg>
+    </div>
+    <span class="m-rotate-arrows__text">Rotate</span>
+    <div class="m-rotate-arrows__arrow m-rotate-arrows__arrow--right">
+        <svg aria-hidden="true" class="icon--arrow--24"><use xlink:href="#icon--arrow" /></svg>
+    </div>
+</div>
