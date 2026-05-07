888041c09c4767129f6312f237830f6de41ded71
nikhil trivedi <ntrivedi@artic.edu>
2026-05-07 16:30:51 -0500

    Add meta descriptions to landing pages [WEB-3359]

--- resources/views/twill/landingPages/editorial/form.blade.php
+++ resources/views/twill/landingPages/editorial/form.blade.php
@@ -100,4 +100,22 @@
 
 </x-twill::formFieldset>
 
+<x-twill::formFieldset id="metadata" title="Overwrite default metadata (optional)">
+    <x-twill::input
+        name='meta_title'
+        label='Metadata Title'
+    />
+    <x-twill::input
+        name='meta_description'
+        label='Metadata Description'
+        type='textarea'
+    />
+    <x-twill::input
+        name='search_tags'
+        label='Internal Search Tags'
+        type='textarea'
+    />
+    <p>Comma-separated list of words or phrases. Don't worry about grammar or similar word variations. This field is intended to assist our internal search engine in finding your content. These tags will not be shown to website users and will have no effect on external search engines, e.g. Google.</p>
+</x-twill::formFieldset>
+
 @stop
