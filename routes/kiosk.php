0b1620ae75fec5a05e5430a7c3135725aabbb8f9
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-05-13 17:20:52 -0500

    Fix routes for DE and include preview in Kiosk domain [IP-250]

--- routes/kiosk.php
+++ routes/kiosk.php
@@ -1,15 +1,18 @@
 <?php
 
 use App\Http\Controllers\DigitalExplorerController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InteractiveFeatureExperiencesController;
+use App\Http\Controllers\PreviewController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('p/{hash}', [PreviewController::class, 'show'])->name('previewLink');
 
 Route::get(
     '/interactive-features/{slug}',
     [InteractiveFeatureExperiencesController::class, 'show']
 )->name('interactiveFeatures.show');
 
-Route::get('/digital-explorers/{id}', [DigitalExplorerController::class, 'show'])->name('digitalExplorer.show');
+Route::get('/digital-explorers/{id}/{slug?}', [DigitalExplorerController::class, 'show'])->name('digitalExplorer.show');
 
 // Only needed so that the kiosk doesn't fallback to the web routes.
 Route::fallback(fn () => abort(404));
