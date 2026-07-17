f566142127cb45b322734f3411acc4bcdfc3d676
nikhil trivedi <ntrivedi@artic.edu>
2026-07-17 12:09:43 -0500

    Replace <em> with <span> in _form-error-msg atom [WEB-3395]

--- resources/views/components/atoms/_form-error-msg.blade.php
+++ resources/views/components/atoms/_form-error-msg.blade.php
@@ -1,1 +1,1 @@
-<em class="error-msg {{ $font ?? 'f-secondary' }}{{ (isset($variation)) ? ' '.$variation : '' }}">{{ $slot ?? '' }}</em>
+<span class="error-msg {{ $font ?? 'f-secondary' }}{{ (isset($variation)) ? ' '.$variation : '' }}">{{ $slot ?? '' }}</span>
