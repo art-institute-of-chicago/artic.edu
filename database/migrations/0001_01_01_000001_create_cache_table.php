4db1df633383ab7969c9e7f653dcb10971068f42
nikhil trivedi <ntrivedi@artic.edu>
2026-07-27 12:05:53 -0500

    Lint code [WEB-3426]

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -14,14 +14,12 @@ return new class () extends Migration {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration')->index();
-            ;
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
             $table->integer('expiration')->index();
-            ;
         });
     }
 
