7e02c9cf9d154214ab26c456a7c004720758c587
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-09-01 16:11:47 -0500

    Fix Laravel upgrade bug [ART-100]

--- app/Http/Middleware/CheckFileExtension.php
+++ app/Http/Middleware/CheckFileExtension.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,7 +19,7 @@ class CheckFileExtension
         $fileExtensions = config('twill.file_library.allowed_extensions');
         $allExtensions = array_unique(array_merge($mediaExtensions, $fileExtensions));
 
-        $filename = array_last($request->segments());
+        $filename = Arr::last($request->segments());
         $extension = strtolower(File::extension($filename));
         if ($extension && !in_array($extension, $allExtensions)) {
             abort(404);
