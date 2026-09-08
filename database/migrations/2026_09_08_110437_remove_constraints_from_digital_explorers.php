2e7163e9b4c75d65def5cfc6dc42de3632306601
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-09-08 11:10:51 -0500

    Remove character constraint from info_description [IP-263]

---
+++ database/migrations/2026_09_08_110437_remove_constraints_from_digital_explorers.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('digital_explorers', function (Blueprint $table) {
+            $table->text('info_description')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('digital_explorers', function (Blueprint $table) {
+            $table->string('info_description')->change();
+        });
+    }
+};
