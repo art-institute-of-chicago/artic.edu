0e2c0e5a872d79a00fa86a83390142a0da7d9552
nikhil trivedi <ntrivedi@artic.edu>
2026-09-10 16:55:06 -0500

    Add docbocks to `ImageHelpers::SRCSET_WIDTHS` arrays [IMG-62]

--- app/Helpers/ImageHelpers.php
+++ app/Helpers/ImageHelpers.php
@@ -10,8 +10,19 @@ use Ramsey\Uuid\Uuid;
 
 class ImageHelpers
 {
+    /**
+     * Srcset widths for small image displays.
+     */
     public const SRCSET_WIDTHS_SMALL = [200,400,600,843];
+
+    /**
+     * Srcset widths for standard image displays.
+     */
     public const SRCSET_WIDTHS = [200,400,600,843,1200,1686];
+
+    /**
+     * Srcset widths for large image displays.
+     */
     public const SRCSET_WIDTHS_LARGE = [200,400,600,843,1200,1686,3000];
 
     /**
