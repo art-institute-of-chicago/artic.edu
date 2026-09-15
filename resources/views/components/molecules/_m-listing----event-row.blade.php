18cae479ee6f12f3beae0c070b81c0c4d98286ce
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-09-15 10:02:52 -0500

    Use occurrence url attr [WEB-3492]

--- resources/views/components/molecules/_m-listing----event-row.blade.php
+++ resources/views/components/molecules/_m-listing----event-row.blade.php
@@ -1,5 +1,5 @@
 <{{ $tag ?? 'li' }} class="m-listing m-listing--w-meta-bottom m-listing--hover-bar{{ (isset($variation)) ? ' '.$variation : '' }}{{ $item->closingSoon ? " m-listing--limited" : "" }}{{ $item->exclusive ? " m-listing--membership" : "" }}">
-    <a href="{{ route('events.show', $item) }}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
+    <a href="{{ $item->occurrence_url }}" class="m-listing__link"{!! (isset($gtmAttributes)) ? ' '.$gtmAttributes.'' : '' !!}>
         <span class="m-listing__img{{ (isset($imgVariation)) ? ' '.$imgVariation : '' }}{{ ($item->videoFront) ? ' m-listing__img--video' : '' }}">
             @if ($item->imageFront('hero'))
                 @component('components.atoms._img')
