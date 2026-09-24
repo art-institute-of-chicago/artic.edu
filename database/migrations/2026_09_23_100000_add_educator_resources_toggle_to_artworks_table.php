a48229474d42ab83598789f50867f682fdbedd10
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-09-24 12:46:37 -0500

    Create toggles for auto related content [ART-118]

---
+++ database/migrations/2026_09_23_100000_add_educator_resources_toggle_to_artworks_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->boolean('toggle_autoeducator_resources')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->dropColumn(['toggle_autoeducator_resources']);
+        });
+    }
+};
