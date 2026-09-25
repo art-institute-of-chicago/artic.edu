53f8eaa0f3433972bcbfa564dccde909ed666fea
nikhil trivedi <ntrivedi@artic.edu>
2026-09-25 10:29:36 -0500

    Fix educationResources route in department Twill form [WEB-3498]

--- resources/views/twill/departments/form.blade.php
+++ resources/views/twill/departments/form.blade.php
@@ -108,7 +108,7 @@
                 ],
                 [
                     'label' => 'Educational Resources',
-                    'name' => 'collection.researchResources.educatorResources'
+                    'name' => 'collection.educatorResources'
                 ],
                 [
                     'label' => 'Interactive Features',
